5d2da8781d37c50c9d42060b5ac0a9a9213927a9
Lars Ellenberg <lars.ellenberg@linbit.com>
2004-05-27 12:46:17 +0000

    r1215: bump cvs date after checkin of CTH

--- drbd/drbd/linux/drbd_config.h
+++ drbd/drbd/linux/drbd_config.h
@@ -20,7 +20,7 @@
 #ifndef DRBD_CONFIG_H
 #define DRBD_CONFIG_H
 
-#define REL_VERSION "0.7-pre7 cvs $Date: 2004/05/26 11:30:28 $"
+#define REL_VERSION "0.7-pre7 cvs $Date: 2004/05/27 12:46:17 $"
 #define API_VERSION 73
 #define PRO_VERSION 72
 
