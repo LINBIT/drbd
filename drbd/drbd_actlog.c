791de99269db323486f31f361421da952a0fccc3
Philipp Reisner <philipp.reisner@linbit.com>
2004-09-09 15:45:46 +0000

    r1534: drbd_rs_cancel_all() has to set resync_locked to zero, otherwise subsequent calls to rs_begin_io() will block forever.

--- drbd/drbd_actlog.c
+++ drbd/drbd_actlog.c
@@ -936,6 +936,7 @@ void drbd_rs_cancel_all(drbd_dev* mdev)
 		clear_bit(BME_NO_WRITES,&bm_ext->flags);
 		lc_del(mdev->resync,&bm_ext->lce);
 	}
+	atomic_set(&mdev->resync_locked,0);   
 	spin_unlock_irq(&mdev->al_lock);
 	wake_up(&mdev->al_wait);
 }
