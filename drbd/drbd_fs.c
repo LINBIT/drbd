ef88aa024325c07cbfe053fc9a85eb62570e8915
Lars Ellenberg <lars.ellenberg@linbit.com>
2005-07-18 10:19:46 +0000

    r1885: while resizing the bitmap, we have to write everything, if the meta data has moved. we have to shrink the al, if we are smaller now, and we have to write at least the bitmap, if we are bigger now.
    just do all this if (la_size_changed || md_moved)

--- drbd/drbd_fs.c
+++ drbd/drbd_fs.c
@@ -83,19 +83,16 @@ int drbd_determin_dev_size(struct Drbd_Conf* mdev)
 	rv = do_determin_dev_size(mdev);
 
 	la_size_changed = (la_size != mdev->la_size);
-	md_moved = (pmdss != drbd_md_ss(mdev) /* && mdev->md_index == -1 */);
+	md_moved = pmdss != drbd_md_ss(mdev) /* && mdev->md_index == -1 */;
 
 	if ( md_moved ) {
 		WARN("Moving meta-data.\n");
 		D_ASSERT(mdev->md_index == -1);
 	}
 
-	if( mdev->la_size < la_size || md_moved) {
+	if ( la_size_changed || md_moved ) {
 		drbd_al_shrink(mdev); // All extents inactive.
 		drbd_bm_write(mdev);  // write bitmap
-	}
-
-	if ( la_size_changed || md_moved ) {
 		// Write mdev->la_size to [possibly new position on] disk.
 		drbd_md_write(mdev);
 	}
