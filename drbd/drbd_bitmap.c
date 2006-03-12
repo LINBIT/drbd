dca4fd6202d4551c19e459e770eec7c48140a620
Philipp Reisner <philipp.reisner@linbit.com>
2006-03-12 09:40:26 +0000

    r2114: min/max ;)

--- drbd/drbd_bitmap.c
+++ drbd/drbd_bitmap.c
@@ -786,7 +786,7 @@ void drbd_bm_set_find(drbd_dev *mdev, unsigned long i)
 	spin_lock_irq(&b->bm_lock);
 	BM_PARANOIA_CHECK();
 
-	b->bm_fo = max_t(unsigned long, i, b->bm_bits);
+	b->bm_fo = min_t(unsigned long, i, b->bm_bits);
 
 	spin_unlock_irq(&b->bm_lock);
 }
