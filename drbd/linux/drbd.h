d43cf82288b7cbd703aa0b587c4ab32605fb97c5
Philipp Reisner <philipp.reisner@linbit.com>
2007-12-03 17:42:30 +0100

    Fixing arch incompat bitfild layouts. [ Maxim Uvarov ]

--- drbd/linux/drbd.h
+++ drbd/linux/drbd.h
@@ -27,8 +27,6 @@
 #define DRBD_H
 #include <linux/drbd_config.h>
 
-#include <asm/types.h>
-
 #ifdef __KERNEL__
 #include <linux/types.h>
 #else
@@ -36,6 +34,9 @@
 #include <sys/wait.h>
 #include <limits.h>
 #endif
+
+#include <asm/types.h>
+#include <asm/byteorder.h>
 
 enum io_error_handler {
 	PassOn, /* FIXME should the better be named "Ignore"? */
@@ -176,18 +177,41 @@ typedef enum {
 	disk_mask=15
 } drbd_disks_t;
 
+/* According to gcc's docs is the ...
+ * The order of allocation of bit-fields within a unit (C90 6.5.2.1, C99 6.7.2.1).
+ * Determined by ABI.
+ * pointed out by Maxim Uvarov q<muvarov@ru.mvista.com>
+ * even though we transmit as "cpu_to_be32(state)",
+ * the offsets of the bitfields still need to be swapped
+ * on different endianess.
+ */
 typedef union {
 	struct {
-		unsigned role : 2 ;   // 3/4      primary/secondary/unknown
-		unsigned peer : 2 ;   // 3/4      primary/secondary/unknown
-		unsigned conn : 5 ;   // 17/32    cstates
-		unsigned disk : 4 ;   // 8/16     from Diskless to UpToDate
-		unsigned pdsk : 4 ;   // 8/16     from Diskless to UpToDate
-		unsigned susp : 1 ;   // 2/2      IO suspended  no/yes
-		unsigned aftr_isp : 1 ; // isp .. imposed sync pause
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+		unsigned role : 2 ;   /* 3/4	 primary/secondary/unknown */
+		unsigned peer : 2 ;   /* 3/4	 primary/secondary/unknown */
+		unsigned conn : 5 ;   /* 17/32	 cstates */
+		unsigned disk : 4 ;   /* 8/16	 from Diskless to UpToDate */
+		unsigned pdsk : 4 ;   /* 8/16	 from Diskless to UpToDate */
+		unsigned susp : 1 ;   /* 2/2	 IO suspended  no/yes */
+		unsigned aftr_isp : 1 ; /* isp .. imposed sync pause */
 		unsigned peer_isp : 1 ;
 		unsigned user_isp : 1 ;
-		unsigned _pad : 11;   // 0        unused
+		unsigned _pad : 11;   /* 0	 unused */
+#elif defined(__BIG_ENDIAN_BITFIELD)
+		unsigned _pad : 11;   /* 0	 unused */
+		unsigned user_isp : 1 ;
+		unsigned peer_isp : 1 ;
+		unsigned aftr_isp : 1 ; /* isp .. imposed sync pause */
+		unsigned susp : 1 ;   /* 2/2	 IO suspended  no/yes */
+		unsigned pdsk : 4 ;   /* 8/16	 from Diskless to UpToDate */
+		unsigned disk : 4 ;   /* 8/16	 from Diskless to UpToDate */
+		unsigned conn : 5 ;   /* 17/32	 cstates */
+		unsigned peer : 2 ;   /* 3/4	 primary/secondary/unknown */
+		unsigned role : 2 ;   /* 3/4	 primary/secondary/unknown */
+#else
+# error "this endianess is not supported"
+#endif
 	};
 	unsigned int i;
 } drbd_state_t;
