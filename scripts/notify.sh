ff04449befc49d03cb9d6ed3b53aeb7c7ee58d53
Lars Ellenberg <lars.ellenberg@linbit.com>
2008-07-07 18:30:16 +0200

    notify.sh: pipe output through logger; wrap some long lines

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -4,6 +4,11 @@
 # This is meant to be invoked via a symlink in /usr/lib/drbd,
 # by drbdadm's userspace callouts.
 
+# try to get possible output on stdout/err to syslog
+PROG=${0##*/}
+exec > >(2>&- ; logger -t "$PROG[$$]" -p local5.info) 2>&1
+echo "invoked for $DRBD_RESOURCE"
+
 RECIPIENT=$1
 
 # check arguments specified on command line
@@ -20,19 +25,33 @@ for var in DRBD_RESOURCE DRBD_PEER; do
 	fi
 done
 
-case "$(basename $0)" in
+: ${DRBD_CONF:="usually /etc/drbd.conf"}
+
+case "$0" in
 	*split-brain.sh)
 		SUBJECT="DRBD split brain on resource $DRBD_RESOURCE"
-		BODY="DRBD has detected split brain on resource $DRBD_RESOURCE between $(hostname) and $DRBD_PEER. Please rectify this immediately.\nPlease see http://www.drbd.org/users-guide/s-resolve-split-brain.html for details on doing so."
+		BODY="
+DRBD has detected split brain on resource $DRBD_RESOURCE
+between $(hostname) and $DRBD_PEER.
+Please rectify this immediately.
+Please see http://www.drbd.org/users-guide/s-resolve-split-brain.html for details on doing so."
 		;;
 	*out-of-sync.sh)
 		SUBJECT="DRBD resource $DRBD_RESOURCE has out-of-sync blocks"
-		BODY="DRBD has detected out-of-sync blocks on resource $DRBD_RESOURCE between $(hostname) and $DRBD_PEER. Please see the system logs for details."
+		BODY="
+DRBD has detected out-of-sync blocks on resource $DRBD_RESOURCE
+between $(hostname) and $DRBD_PEER.
+Please see the system logs for details."
 		;;
 	*)
 		SUBJECT="Unspecified DRBD notification"
-		BODY="DRBD on $(hostname) was configured to launch a notification handler for resource $DRBD_RESOURCE, but no specific notification event was set.\nThis is most likely due to DRBD misconfiguration. Please check your configuration file (usually /etc/drbd.conf)."
+		BODY="
+DRBD on $(hostname) was configured to launch a notification handler
+for resource $DRBD_RESOURCE,
+but no specific notification event was set.
+This is most likely due to DRBD misconfiguration.
+Please check your configuration file ($DRBD_CONF)."
 		;;
 esac
 
-echo -e "$BODY" | mail -s "$SUBJECT" $RECIPIENT
+echo "$BODY" | mail -s "$SUBJECT" $RECIPIENT
