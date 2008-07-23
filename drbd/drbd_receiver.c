cb4ee83c7bf8b4becd4ed6ebcc13c1036a6e146c
Lars Ellenberg <lars.ellenberg@linbit.com>
2008-07-23 19:56:58 +0200

    don't be so noisy when connect fails with EHOSTUNREACH

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -651,6 +651,7 @@ out:
 		case EINTR: case ERESTARTSYS:
 			/* peer not (yet) available, network problem */
 		case ECONNREFUSED: case ENETUNREACH:
+		case EHOSTDOWN:    case EHOSTUNREACH:
 #if 0
 			DBG("%s failure ignored, err = %d\n",
 					what, err);
