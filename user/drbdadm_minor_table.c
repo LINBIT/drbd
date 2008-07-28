4af3feeaaa995046e62de13245e32371905971b9
Lars Ellenberg <lars.ellenberg@linbit.com>
2008-07-28 16:20:28 +0200

    drbdadm_minor_table: lookup_minor now does readlink

--- user/drbdadm_minor_table.c
+++ user/drbdadm_minor_table.c
@@ -42,8 +42,34 @@
 
 #define MINOR_TABLE_DIR "/var/lib/drbd/"
 #define MAX_MINOR 256
-#define MAX_LINELEN 2048
 
+#define MAX_REGISTER_PATH_LEN	1024
+
+/* buf has to be big enough to hold that path.
+ * it is assumed that sprintf cannot fail :-] */
+void linkname_from_minor(char *buf, int minor)
+{
+	sprintf(buf, "%s/drbd-minor-%d.conf", MINOR_TABLE_DIR, minor);
+}
+
+int unregister_minor(int minor)
+{
+	char buf[255];
+
+	if (minor >= MAX_MINOR || minor < 0) {
+		fprintf(stderr, "unregister_minor: minor too big (%d).\n", minor);
+		return -1;
+	}
+
+	linkname_from_minor(buf, minor);
+	if (unlink(buf) < 0) {
+		if (errno != ENOENT) {
+			perror("unlink");
+			return -1;
+		}
+	}
+	return 0;
+}
 
 int register_minor(int minor, const char *path)
 {
@@ -56,52 +82,64 @@ int register_minor(int minor, const char *path)
 		return -1;
 	}
 
-	sprintf(buf, "%s/drbd-minor-%d.conf", MINOR_TABLE_DIR, minor);
-	if (unlink(buf) < 0) {
-		if (errno != ENOENT) {
-			perror("unlink");
-			return -1;
-		}
-	}
+	linkname_from_minor(buf, minor);
 
-	if (!path || !path[0]) {
+	if (!path || !path[0])
 		fprintf(stderr, "Cannot register an empty path.\n");
-	} else if (path[0] != '/') {
+	else if (path[0] != '/')
 		fprintf(stderr, "Absolute path expected, "
 			"won't register relative path (%s).\n", path);
-	} else if (stat(path, &stat_buf) < 0) {
+	else if (strlen(path) >= MAX_REGISTER_PATH_LEN)
+		fprintf(stderr, "path (%s):\ntoo long to be registered, "
+				"max path len supported: %u\n",
+				path, MAX_REGISTER_PATH_LEN-1);
+	else if (stat(path, &stat_buf) < 0)
 		fprintf(stderr, "stat(%s): %m\n", path);
-	} else if (symlink(path, buf) < 0) {
+	else if (unlink(buf) < 0 && errno != ENOENT)
+		fprintf(stderr, "unlink(%s): %m\n", buf);
+	else if (symlink(path, buf) < 0)
 		fprintf(stderr, "symlink(%s, %s): %m\n", path, buf);
-	} else {
+	else
 		/* it did work out after all! */
 		err = 0;
-	}
+
 	return err;
 }
 
-
-/* CAUTION
- * returns static buffer! */
+/* This returns a static buffer containing the real
+ * configuration file known to be used last for this minor.
+ * If you need the return value longer, stuff it away with strdup. */
 char *lookup_minor(int minor)
 {
 	static char buf[255];
+	static char resolved_path[MAX_REGISTER_PATH_LEN+1];
 	struct stat stat_buf;
+	ssize_t len;
 
 	if (minor >= MAX_MINOR || minor < 0) {
 		fprintf(stderr, "register_minor: minor too big (%d).\n", minor);
 		return NULL;
 	}
 
-	sprintf(buf, "%s/drbd-minor-%d.conf", MINOR_TABLE_DIR, minor);
+	linkname_from_minor(buf, minor);
 
 	if (stat(buf, &stat_buf) < 0) {
-		if (errno != ENOENT) {
+		if (errno != ENOENT)
 			fprintf(stderr, "stat(%s): %m\n", buf);
-		}
 		return NULL;
 	}
-	return buf;
+
+	len = readlink(buf, resolved_path, sizeof(resolved_path)-1);
+	if (len < 0) {
+		perror("readlink");
+		return NULL;
+	}
+	if (len >= MAX_REGISTER_PATH_LEN)
+		fprintf(stderr, "readlink(%s): result has probably been truncated\n",
+				buf);
+
+	resolved_path[len] = '\0';
+	return resolved_path;
 }
 
 
@@ -111,7 +149,7 @@ int main(int argc, char ** argv)
 {
 	register_minor(1, "/etc/drbd-xy.conf");
 	register_minor(15, "/etc/drbd-82.conf");
-	register_minor(14, "../../../../../../etc/drbd-82.conf");
+	register_minor(14, "/../../../../../../etc/drbd-82.conf");
 	printf("Minor 1 is %s.\n", lookup_minor(1));
 	printf("Minor 2 is %s.\n", lookup_minor(2));
 	printf("Minor 14 is %s.\n", lookup_minor(14));
