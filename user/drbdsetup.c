27f2ffdced9c615bd3c422c5ee812c90472de4e1
Lars Ellenberg <lars.ellenberg@linbit.com>
2008-08-16 20:50:07 +0200

    drbdsetup: stat(/proc/drbd) instead of netlink ping-pong with timeout in ensure_drbd_driver_is_present

--- user/drbdsetup.c
+++ user/drbdsetup.c
@@ -1920,28 +1920,22 @@ void close_cn(int sk_nl)
 
 void ensure_drbd_driver_is_present(void)
 {
-	struct drbd_tag_list *tl;
-	char buffer[4096];
-	int sk_nl, rr;
+	struct stat sb;
+	int err;
 
-	sk_nl = open_cn();
-	/* Might print:
-	   Missing privileges? You should run this as root.
-	   Connector module not loaded? try 'modprobe cn'. */
-	if (sk_nl < 0) exit(20);
+	err = stat("/proc/drbd", &sb);
+	if (!err)
+		return;
 
-	tl = create_tag_list(2);
-	add_tag(tl, TT_END, NULL, 0); // close the tag list
+	if (err == ENOENT)
+		fprintf(stderr, "DRBD driver appears to be missing\n");
+	else
+		fprintf(stderr, "Could not stat(\"/proc/drbd\"): %m\n");
 
-	tl->drbd_p_header->packet_type = P_get_state;
-	tl->drbd_p_header->drbd_minor = 0;
-	tl->drbd_p_header->flags = 0;
+	fprintf(stderr, "do you need to load the module?\n"
+			"try: modprobe drbd\n");
 
-	rr = call_drbd(sk_nl, tl, (struct nlmsghdr*)buffer, 4096, 500);
-	/* Might print: (after 500ms)
-	   No response from the DRBD driver! Is the module loaded? */
-	close_cn(sk_nl);
-	if (rr == -2) exit(20);
+	exit(20);
 }
 
 int main(int argc, char** argv)
