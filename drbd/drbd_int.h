LINBIT/drbd

commit e02c596d4bc7b754f3d9bfdd903395ff95e228ce

drbd_get_max_capacity: fix clipping logic for external meta data
also consistently store max_capacity instead just capacity
into the mdev->bc->known_size.

drbd/drbd_int.h
@@ -1159,8 +1159,9 @@ struct bm_extent {
  * _storage_ sector is located in */
 #define BM_SECT_TO_EXT(x)   ((x)>>(BM_EXT_SIZE_B-9))
 
-/* who much _storage_ sectors we have per bitmap sector */
-#define BM_SECT_PER_EXT     (1ULL << (BM_EXT_SIZE_B-9))
+/* how much _storage_ sectors we have per bitmap sector */
+#define BM_EXT_TO_SECT(x)   ((sector_t)(x) << (BM_EXT_SIZE_B-9))
+#define BM_SECT_PER_EXT     BM_EXT_TO_SECT(1)
 
 /* in one sector of the bitmap, we have this many activity_log extents. */
 #define AL_EXT_PER_BM_SECT  (1 << (BM_EXT_SIZE_B - AL_EXTENT_SIZE_B))
@@ -1666,20 +1667,28 @@ static inline sector_t drbd_md_last_sector(struct drbd_backing_dev *bdev)
  * current implementation will oops sooner or later */
 static inline sector_t drbd_get_max_capacity(struct drbd_backing_dev *bdev)
 {
+	sector_t s;
 	switch (bdev->dc.meta_dev_idx) {
 	case DRBD_MD_INDEX_INTERNAL:
 	case DRBD_MD_INDEX_FLEX_INT:
-		return drbd_get_capacity(bdev->backing_bdev)
+		s = drbd_get_capacity(bdev->backing_bdev)
 			? min_t(sector_t, DRBD_MAX_SECTORS_FLEX,
 					drbd_md_first_sector(bdev))
 			: 0;
+		break;
 	case DRBD_MD_INDEX_FLEX_EXT:
-		return min_t(sector_t, DRBD_MAX_SECTORS_FLEX,
+		s = min_t(sector_t, DRBD_MAX_SECTORS_FLEX,
 				drbd_get_capacity(bdev->backing_bdev));
+		/* clip at maximum size the meta device can support */
+		s = min_t(sector_t, s,
+			BM_EXT_TO_SECT(bdev->md.md_size_sect
+				     - bdev->md.bm_offset));
+		break;
 	default:
-		return min_t(sector_t, DRBD_MAX_SECTORS,
+		s = min_t(sector_t, DRBD_MAX_SECTORS,
 				drbd_get_capacity(bdev->backing_bdev));
 	}
+	return s;
 }
 
 /* returns the sector number of our meta data 'super' block */

drbd/drbd_nl.c
@@ -943,7 +943,7 @@ STATIC int drbd_nl_disk_conf(struct drbd_conf *mdev, struct drbd_nl_cfg_req *nlp
 
 	/* Make sure the new disk is big enough
 	 * (we may currently be Primary with no local disk...) */
-	if (drbd_get_capacity(nbc->backing_bdev) <
+	if (drbd_get_max_capacity(nbc) <
 	    drbd_get_capacity(mdev->this_bdev)) {
 		retcode = LDDeviceTooSmall;
 		goto release_bdev2_fail;
@@ -1458,8 +1458,8 @@ STATIC int drbd_nl_resize(struct drbd_conf *mdev, struct drbd_nl_cfg_req *nlp,
 		goto fail;
 	}
 
-	if (mdev->bc->known_size != drbd_get_capacity(mdev->bc->backing_bdev)) {
-		mdev->bc->known_size = drbd_get_capacity(mdev->bc->backing_bdev);
+	if (mdev->bc->known_size != drbd_get_max_capacity(mdev->bc)) {
+		mdev->bc->known_size = drbd_get_max_capacity(mdev->bc);
 		ldsc = 1;
 	}
 

drbd/drbd_receiver.c
@@ -2365,8 +2365,8 @@ STATIC int receive_sizes(struct drbd_conf *mdev, struct Drbd_Header *h)
 	}
 
 	if (inc_local(mdev)) {
-		if (mdev->bc->known_size != drbd_get_capacity(mdev->bc->backing_bdev)) {
-			mdev->bc->known_size = drbd_get_capacity(mdev->bc->backing_bdev);
+		if (mdev->bc->known_size != drbd_get_max_capacity(mdev->bc)) {
+			mdev->bc->known_size = drbd_get_max_capacity(mdev->bc);
 			ldsc = 1;
 		}
 

commit 9f53d01be41fbdf52ac004f31b5b28c581d6e401

Fixed the online resize again
Was broken with commit e02c596d4bc7b754f3d9bfdd903395ff95e228ce
(Was only in 8.3.0rc0 release).

drbd/drbd_int.h
@@ -810,7 +810,7 @@ struct drbd_backing_dev {
 	struct file *md_file;
 	struct drbd_md md;
 	struct disk_conf dc; /* The user provided config... */
-	sector_t known_size;
+	sector_t known_size; /* last known size of that backing device */
 };
 
 struct drbd_md_io {

drbd/drbd_nl.c
@@ -952,7 +952,7 @@ STATIC int drbd_nl_disk_conf(struct drbd_conf *mdev, struct drbd_nl_cfg_req *nlp
 		goto release_bdev2_fail;
 	}
 
-	nbc->known_size = drbd_get_max_capacity(nbc);
+	nbc->known_size = drbd_get_capacity(nbc->backing_bdev);
 
 	retcode = _drbd_request_state(mdev, NS(disk, Attaching), ChgStateVerbose);
 	if (retcode < SS_Success)
@@ -1461,8 +1461,8 @@ STATIC int drbd_nl_resize(struct drbd_conf *mdev, struct drbd_nl_cfg_req *nlp,
 		goto fail;
 	}
 
-	if (mdev->bc->known_size != drbd_get_max_capacity(mdev->bc)) {
-		mdev->bc->known_size = drbd_get_max_capacity(mdev->bc);
+	if (mdev->bc->known_size != drbd_get_capacity(mdev->bc->backing_bdev)) {
+		mdev->bc->known_size = drbd_get_capacity(mdev->bc->backing_bdev);
 		ldsc = 1;
 	}
 

drbd/drbd_receiver.c
@@ -2365,8 +2365,8 @@ STATIC int receive_sizes(struct drbd_conf *mdev, struct Drbd_Header *h)
 	}
 
 	if (inc_local(mdev)) {
-		if (mdev->bc->known_size != drbd_get_max_capacity(mdev->bc)) {
-			mdev->bc->known_size = drbd_get_max_capacity(mdev->bc);
+		if (mdev->bc->known_size != drbd_get_capacity(mdev->bc->backing_bdev)) {
+			mdev->bc->known_size = drbd_get_capacity(mdev->bc->backing_bdev);
 			ldsc = 1;
 		}
 