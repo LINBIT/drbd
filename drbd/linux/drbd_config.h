aab32c03052bcd8f31071abcdfad9c566a108adc
Philipp Reisner <philipp.reisner@linbit.com>
2009-03-17 13:02:40 +0100

    Preparing 8.3.1rc1

--- drbd/linux/drbd_config.h
+++ drbd/linux/drbd_config.h
@@ -22,7 +22,7 @@
 
 extern const char *drbd_buildtag(void);
 
-#define REL_VERSION "8.3.0"
+#define REL_VERSION "8.3.1rc1"
 #define API_VERSION 88
 #define PRO_VERSION_MIN 86
 #define PRO_VERSION_MAX 89
