58bb3e73371d0db2975147108d0e5910606f9c84
Lars Ellenberg <lars.ellenberg@linbit.com>
2009-04-14 13:20:27 +0200

    drbdadm error reporting: assign c_resource_start, c_section_start and fline again at appropriate locations

--- user/drbdadm_parser.c
+++ user/drbdadm_parser.c
@@ -276,7 +276,9 @@ static void parse_global(void)
 	}
 	EXP('{');
 	while (1) {
-		switch (yylex()) {
+		int token = yylex();
+		fline = line;
+		switch (token) {
 		case TK_DISABLE_IP_VERIFICATION:
 			global_options.disable_ip_verification = 1;
 			break;
@@ -325,11 +327,13 @@ static struct d_option *parse_options_d(int token_switch, int token_option,
 	enum range_checks rc;
 
 	struct d_option *options = NULL, *ro = NULL;
+	c_section_start = line;
 	fline = line;
 
 	EXP('{');
 	while (1) {
 		token = yylex();
+		fline = line;
 		if (token == token_switch) {
 			options = APPEND(options, new_opt(yylval.txt, NULL));
 		} else if (token == token_option) {
@@ -370,7 +374,9 @@ static void parse_host_body(struct d_host_info *host,
 {
 	EXP('{');
 	while (1) {
-		switch (yylex()) {
+		int token = yylex();
+		fline = line;
+		switch (token) {
 		case TK_DISK:
 			check_uniq("disk statement", "%s:%s:disk", res->name,
 				   host->name);
@@ -555,16 +561,17 @@ void net_delegate(void *ctx)
 struct d_resource* parse_resource(char* res_name, enum pr_flags flags)
 {
 	struct d_resource* res;
-	int token;
 
-	fline = line;
+	c_resource_start = line;
 
 	res=calloc(1,sizeof(struct d_resource));
 	res->name = res_name;
 	res->next = NULL;
 
 	while(1) {
-		switch((token=yylex())) {
+		int token = yylex();
+		fline = line;
+		switch(token) {
 		case TK_PROTOCOL:
 			check_uniq("protocol statement","%s: protocol",res->name);
 			EXP(TK_STRING);
@@ -654,7 +661,9 @@ void my_parse(void)
 	config = NULL;
 
 	while (1) {
-		switch (yylex()) {
+		int token = yylex();
+		fline = line;
+		switch(token) {
 		case TK_GLOBAL:
 			parse_global();
 			break;
