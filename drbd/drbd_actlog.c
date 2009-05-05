fe4a906a195606a80b7b03f370df622ccf9c3e49
Philipp Reisner <philipp.reisner@linbit.com>
2009-05-05 16:02:21 +0200

    Improving a comment
    corresponding drbd 8.3 commit: a82614269a28eb9b0619feddcb6d213f17245374

--- drbd/drbd_actlog.c
+++ drbd/drbd_actlog.c
@@ -183,10 +183,10 @@ int drbd_md_sync_page_io(struct drbd_conf *mdev, struct drbd_backing_dev *bdev,
 	return ok;
 }
 
-/* I do not believe that all storage medias can guarantee atomic
- * 512 byte write operations. When the journal is read, only
- * transactions with correct xor_sums are considered.
- * sizeof() = 512 byte */
+/* We maintain a trivial check sum in our on disk activity log.
+ * With that we can ensure correct operation even when the storage
+ * device might do a partial (last) sector write while loosing power.
+ */
 struct __attribute__((packed)) al_transaction {
 	u32       magic;
 	u32       tr_number;
