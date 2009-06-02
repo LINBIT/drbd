0663c48070e477f21e16e24ce4a221f19c657ba9
Florian Haas <florian.haas@linbit.com>
2009-06-02 17:08:46 +0200

    Pacemaker resource level fencing script: renamed to crm-fence-peer.sh, inverted constraint logic.

--- scripts/crm-fence-peer.sh
+++ scripts/crm-fence-peer.sh
@@ -6,17 +6,17 @@ PROG=${0##*/}
 exec > >(2>&- ; logger -t "$PROG[$$]" -p local5.info) 2>&1
 echo "invoked for $DRBD_RESOURCE"
 
-# Default to sending email to root, unless otherwise specified
+# The CIB resource name. Must be passed in.
 CIB_RESOURCE=${1}
 
 # check arguments specified on command line
-if [ -z "$CRM_RESOURCE" ]; then
+if [ -z "$CIB_RESOURCE" ]; then
 	echo "You must specify a resource defined in the CIB when using this handler." >&2
 	exit 1
 fi
 
 # check envars normally passed in by drbdadm
-for var in DRBD_RESOURCE DRBD_PEER; do
+for var in DRBD_RESOURCE; do
 	if [ -z "${!var}" ]; then
 		echo "Environment variable \$$var not found (this is normally passed in by drbdadm)." >&2
 		exit 1
@@ -28,18 +28,26 @@ done
 DRBD_LOCAL_HOST=$(hostname)
 
 case `basename $0` in
-    *outdate-peer.sh)
-	crm configure <<EOF
-location drbd-outdate-${CIB_RESOURCE} ${CIB_RESOURCE} 
-  rule \$id=drbd-outdate-rule-${CIB_RESOURCE} \$role="Master" -inf: \#uname eq ${DRBD_PEER}
-commit
-EOF
+    crm-fence-peer.sh)
+	crm configure location \
+	    drbd-fence-${CIB_RESOURCE}${CIB_RESOURCE} \
+	    rule \$id=drbd-fence-rule-${CIB_RESOURCE} \
+	    \$role="Master" -inf: \#uname ne ${DRBD_LOCAL_HOST}
+	rc=$?
+	if [ $rc -eq 0 ]; then
+            # 4: successfully outdated (per the exit code convention
+	    # of the DRBD "fence-peer" handler.
+	    exit 4
+	fi
 	;;
-rc=$?
+    crm-unfence-peer.sh)
+	crm configure delete drbd-fence-${CIB_RESOURCE}
+	rc=$?
+	if [ $rc -eq 0 ]; then
+	    exit 0
+	fi
+	;;
+esac
 
-if [ $rc -eq 0 ]; then
-    # 4: successfully outdated
-    exit 4
-fi
 # 1: unexpected error
 exit 1
