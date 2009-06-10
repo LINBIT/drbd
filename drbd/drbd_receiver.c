d8f2b51b40d42912570f1118ddaf175d3568b82c
Philipp Reisner <philipp.reisner@linbit.com>
2009-06-10 14:11:02 +0200

    Fixed a deadlock when using automatic split brain recovery when both nodes are primary.
    To reproduct:
    
    config:
            net {
                    allow-two-primaries;
                    after-sb-0pri discard-least-changes;
                    after-sb-1pri call-pri-lost-after-sb;
                    after-sb-2pri call-pri-lost-after-sb;
            }
    
    Make both primary, interrupt the network, let them reconnect.

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -1883,6 +1883,10 @@ STATIC int drbd_asb_recover_1p(struct drbd_conf *mdev) __must_hold(local)
 		hg = drbd_asb_recover_0p(mdev);
 		if (hg == -1 && mdev->state.role == R_PRIMARY) {
 			self = drbd_set_role(mdev, R_SECONDARY, 0);
+			 /* drbd_change_state() does not sleep while in SS_IN_TRANSIENT_STATE,
+			  * we might be here in C_WF_REPORT_PARAMS which is transient.
+			  * we do not need to wait for the after state change work either. */
+			self = drbd_change_state(mdev, CS_VERBOSE, NS(role, R_SECONDARY));
 			if (self != SS_SUCCESS) {
 				drbd_khelper(mdev, "pri-lost-after-sb");
 			} else {
@@ -1921,7 +1925,10 @@ STATIC int drbd_asb_recover_2p(struct drbd_conf *mdev) __must_hold(local)
 	case ASB_CALL_HELPER:
 		hg = drbd_asb_recover_0p(mdev);
 		if (hg == -1) {
-			self = drbd_set_role(mdev, R_SECONDARY, 0);
+			 /* drbd_change_state() does not sleep while in SS_IN_TRANSIENT_STATE,
+			  * we might be here in C_WF_REPORT_PARAMS which is transient.
+			  * we do not need to wait for the after state change work either. */
+			self = drbd_change_state(mdev, CS_VERBOSE, NS(role, R_SECONDARY));
 			if (self != SS_SUCCESS) {
 				drbd_khelper(mdev, "pri-lost-after-sb");
 			} else {
