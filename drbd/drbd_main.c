LINBIT/drbd

commit fb51e2eb1fac83839231499333bf683629388484

Fixing an issue with online resize [Bugz 231]
  When the disk of the primary was smaller than the disk of the secondary
  and both disks get resized to something bigger and the "drbdadm resync"
  command is issued on the primary node, one got uggly log lines:
  cs:SyncSource rs_left=35779 > rs_total=25599 (rs_failed 0)

Fixed now.

  The issue was that the primary node evaluates the constraints
  locally before it sends the new sizes to the peer. It actually
  sees two resize events:
    1) A bigger size is possible since our disk got larger.
    2) An even bigger size gets possibe since the peer's disk
       got larger as well.

The solution is to postpone the start of the resync until we
get the size reply. We make sure the peer sends his sizes by
misusing the c_size member of p_sizes packet. By setting it
to 0, the peer has to send his sizes.

drbd/drbd_main.c
@@ -890,6 +890,9 @@ STATIC int is_valid_state_transition(struct drbd_conf *mdev,
 	    os.conn < C_CONNECTED)
 		rv = SS_NEED_CONNECTION;
 
+	if (ns.conn == C_SYNC_SOURCE && os.conn >= C_SYNC_SOURCE)
+		rv = SS_RESYNC_RUNNING;
+
 	return rv;
 }
 
@@ -1403,7 +1406,7 @@ STATIC void after_state_ch(struct drbd_conf *mdev, union drbd_state os,
 	    os.disk == D_ATTACHING && ns.disk == D_NEGOTIATING) {
 		kfree(mdev->p_uuid); /* We expect to receive up-to-date UUIDs soon. */
 		mdev->p_uuid = NULL; /* ...to not use the old ones in the mean time */
-		drbd_send_sizes(mdev);  /* to start sync... */
+		drbd_send_sizes(mdev, 0);  /* to start sync... */
 		drbd_send_uuids(mdev);
 		drbd_send_state(mdev);
 	}
@@ -1935,7 +1938,7 @@ int drbd_send_sync_uuid(struct drbd_conf *mdev, u64 val)
 			     (struct p_header *)&p, sizeof(p));
 }
 
-int drbd_send_sizes(struct drbd_conf *mdev)
+int drbd_send_sizes(struct drbd_conf *mdev, int trigger_reply)
 {
 	struct p_sizes p;
 	sector_t d_size, u_size;
@@ -1957,7 +1960,7 @@ int drbd_send_sizes(struct drbd_conf *mdev)
 
 	p.d_size = cpu_to_be64(d_size);
 	p.u_size = cpu_to_be64(u_size);
-	p.c_size = cpu_to_be64(drbd_get_capacity(mdev->this_bdev));
+	p.c_size = cpu_to_be64(trigger_reply ? 0 : drbd_get_capacity(mdev->this_bdev));
 	p.max_segment_size = cpu_to_be32(mdev->rq_queue->max_segment_size);
 	p.queue_order_type = cpu_to_be32(q_order_type);
 

drbd/drbd_int.h
@@ -926,6 +926,8 @@ enum {
 				 * but worker thread is still handling the cleanup.
 				 * reconfiguring (nl_disk_conf, nl_net_conf) is dissalowed,
 				 * while this is set. */
+	RESIZE_PENDING,		/* Size change detected locally, waiting for the response from
+				 * the peer, if it changed there as well. */
 };
 
 struct drbd_bitmap; /* opaque for drbd_conf */
@@ -1248,7 +1250,7 @@ extern int drbd_send_protocol(struct drbd_conf *mdev);
 extern int drbd_send_uuids(struct drbd_conf *mdev);
 extern int drbd_send_uuids_skip_initial_sync(struct drbd_conf *mdev);
 extern int drbd_send_sync_uuid(struct drbd_conf *mdev, u64 val);
-extern int drbd_send_sizes(struct drbd_conf *mdev);
+extern int drbd_send_sizes(struct drbd_conf *mdev, int trigger_reply);
 extern int _drbd_send_state(struct drbd_conf *mdev);
 extern int drbd_send_state(struct drbd_conf *mdev);
 extern int _drbd_send_cmd(struct drbd_conf *mdev, struct socket *sock,

drbd/drbd_nl.c
@@ -560,15 +560,12 @@ enum determine_dev_size drbd_determin_dev_size(struct drbd_conf *mdev) __must_ho
 	md_moved = prev_first_sect != drbd_md_first_sector(mdev->ldev)
 		|| prev_size	   != mdev->ldev->md.md_size_sect;
 
-	if (md_moved) {
-		dev_warn(DEV, "Moving meta-data.\n");
-		/* assert: (flexible) internal meta data */
-	}
-
 	if (la_size_changed || md_moved) {
 		drbd_al_shrink(mdev); /* All extents inactive. */
-		dev_info(DEV, "Writing the whole bitmap, size changed\n");
-		rv = drbd_bitmap_io(mdev, &drbd_bm_write, "size changed");
+		dev_info(DEV, "Writing the whole bitmap, %s\n",
+			 la_size_changed && md_moved ? "size changed and md moved" :
+			 la_size_changed ? "size changed" : "md moved");
+		rv = drbd_bitmap_io(mdev, &drbd_bm_write, "size changed"); /* does drbd_resume_io() ! */
 		drbd_md_mark_dirty(mdev);
 	}
 
@@ -1542,10 +1539,11 @@ STATIC int drbd_nl_resize(struct drbd_conf *mdev, struct drbd_nl_cfg_req *nlp,
 	}
 
 	if (mdev->state.conn == C_CONNECTED && (dd != unchanged || ldsc)) {
-		drbd_send_uuids(mdev);
-		drbd_send_sizes(mdev);
 		if (dd == grew)
-			resync_after_online_grow(mdev);
+			set_bit(RESIZE_PENDING, &mdev->flags);
+
+		drbd_send_uuids(mdev);
+		drbd_send_sizes(mdev, 1);
 	}
 
  fail:

drbd/drbd_receiver.c
@@ -947,7 +947,7 @@ retry:
 
 	drbd_send_protocol(mdev);
 	drbd_send_sync_param(mdev, &mdev->sync_conf);
-	drbd_send_sizes(mdev);
+	drbd_send_sizes(mdev, 0);
 	drbd_send_uuids(mdev);
 	drbd_send_state(mdev);
 	clear_bit(USE_DEGR_WFC_T, &mdev->flags);
@@ -2894,9 +2894,10 @@ STATIC int receive_sizes(struct drbd_conf *mdev, struct p_header *h)
 		    drbd_get_capacity(mdev->this_bdev) || ldsc) {
 			/* we have different sizes, probabely peer
 			 * needs to know my new size... */
-			drbd_send_sizes(mdev);
+			drbd_send_sizes(mdev, 0);
 		}
-		if (dd == grew && mdev->state.conn == C_CONNECTED) {
+		if (test_and_clear_bit(RESIZE_PENDING, &mdev->flags) ||
+		    (dd == grew && mdev->state.conn == C_CONNECTED)) {
 			if (mdev->state.pdsk >= D_INCONSISTENT &&
 			    mdev->state.disk >= D_INCONSISTENT)
 				resync_after_online_grow(mdev);

drbd/drbd_worker.c
@@ -1313,6 +1313,11 @@ void drbd_start_resync(struct drbd_conf *mdev, enum drbd_conns side)
 	union drbd_state ns;
 	int r;
 
+	if (mdev->state.conn >= C_SYNC_SOURCE) {
+		dev_err(DEV, "Resync already running!\n");
+		return;
+	}
+
 	trace_drbd_resync(mdev, TRACE_LVL_SUMMARY, "Resync starting: side=%s\n",
 			  side == C_SYNC_TARGET ? "SyncTarget" : "SyncSource");
 

commit 4025bd74dab9da5d95ea1daebe0a277ce06c9dd1

Fixed a regression introduced with fb51e2eb1fac83839231499333bf683629388484
That additional check was too restrictive.

drbd/drbd_main.c
@@ -890,9 +890,6 @@ STATIC int is_valid_state_transition(struct drbd_conf *mdev,
 	    os.conn < C_CONNECTED)
 		rv = SS_NEED_CONNECTION;
 
-	if (ns.conn == C_SYNC_SOURCE && os.conn >= C_SYNC_SOURCE)
-		rv = SS_RESYNC_RUNNING;
-
 	return rv;
 }
 