770ba7e8ec26873885ee49499f091b3e27187b21
Lars Ellenberg <lars.ellenberg@linbit.com>
2009-09-01 16:14:18 +0200

    use GFP_NOIO when broadcasting events and state changes

--- drbd/drbd_nl.c
+++ drbd/drbd_nl.c
@@ -2196,7 +2196,7 @@ void drbd_bcast_state(struct drbd_conf *mdev, union drbd_state state)
 	reply->ret_code = NO_ERROR;
 
 	trace_drbd_netlink(cn_reply, 0);
-	cn_netlink_send(cn_reply, CN_IDX_DRBD, GFP_KERNEL);
+	cn_netlink_send(cn_reply, CN_IDX_DRBD, GFP_NOIO);
 }
 
 void drbd_bcast_ev_helper(struct drbd_conf *mdev, char *helper_name)
@@ -2229,7 +2229,7 @@ void drbd_bcast_ev_helper(struct drbd_conf *mdev, char *helper_name)
 	reply->ret_code = NO_ERROR;
 
 	trace_drbd_netlink(cn_reply, 0);
-	cn_netlink_send(cn_reply, CN_IDX_DRBD, GFP_KERNEL);
+	cn_netlink_send(cn_reply, CN_IDX_DRBD, GFP_NOIO);
 }
 
 void drbd_bcast_ee(struct drbd_conf *mdev,
@@ -2301,7 +2301,7 @@ void drbd_bcast_ee(struct drbd_conf *mdev,
 	reply->ret_code = NO_ERROR;
 
 	trace_drbd_netlink(cn_reply, 0);
-	cn_netlink_send(cn_reply, CN_IDX_DRBD, GFP_KERNEL);
+	cn_netlink_send(cn_reply, CN_IDX_DRBD, GFP_NOIO);
 	kfree(cn_reply);
 }
 
@@ -2341,7 +2341,7 @@ void drbd_bcast_sync_progress(struct drbd_conf *mdev)
 	reply->ret_code = NO_ERROR;
 
 	trace_drbd_netlink(cn_reply, 0);
-	cn_netlink_send(cn_reply, CN_IDX_DRBD, GFP_KERNEL);
+	cn_netlink_send(cn_reply, CN_IDX_DRBD, GFP_NOIO);
 }
 
 #ifdef NETLINK_ROUTE6
@@ -2411,7 +2411,7 @@ void drbd_nl_send_reply(struct cn_msg *req, int ret_code)
 	reply->ret_code = ret_code;
 
 	trace_drbd_netlink(cn_reply, 0);
-	rr = cn_netlink_send(cn_reply, CN_IDX_DRBD, GFP_KERNEL);
+	rr = cn_netlink_send(cn_reply, CN_IDX_DRBD, GFP_NOIO);
 	if (rr && rr != -ESRCH)
 		printk(KERN_INFO "drbd: cn_netlink_send()=%d\n", rr);
 }
