0acb7c07a61225ba880fde2a32b8f5f8fa49c8cc
Philipp Reisner <philipp.reisner@linbit.com>
2009-09-14 16:44:50 +0200

    Credits where credits are due

--- drbd/drbd_main.c
+++ drbd/drbd_main.c
@@ -7,6 +7,9 @@
    Copyright (C) 1999-2008, Philipp Reisner <philipp.reisner@linbit.com>.
    Copyright (C) 2002-2008, Lars Ellenberg <lars.ellenberg@linbit.com>.
 
+   Thanks to Carter Burden, Bart Grantham and Gennadiy Nerubayev
+   from Logicworks, Inc. for making SDP replication support possible.
+
    drbd is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
