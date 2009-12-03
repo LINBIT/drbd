1ba61a490add9f335663aa55c86225f4312af786
Philipp Reisner <philipp.reisner@linbit.com>
2009-12-03 17:15:21 +0100

    drbd_req.c: use part_[inc|dec]_in_flight()

--- drbd/drbd_req.c
+++ drbd/drbd_req.c
@@ -60,8 +60,8 @@ static void _drbd_start_io_acct(struct drbd_conf *mdev, struct drbd_request *req
 	cpu = part_stat_lock();
 	part_stat_inc(cpu, &mdev->vdisk->part0, ios[rw]);
 	part_stat_add(cpu, &mdev->vdisk->part0, sectors[rw], bio_sectors(bio));
+	part_inc_in_flight(&mdev->vdisk->part0, rw);
 	part_stat_unlock();
-	mdev->vdisk->part0.in_flight++;
 #endif
 }
 
@@ -82,8 +82,8 @@ static void _drbd_end_io_acct(struct drbd_conf *mdev, struct drbd_request *req)
 	cpu = part_stat_lock();
 	part_stat_add(cpu, &mdev->vdisk->part0, ticks[rw], duration);
 	part_round_stats(cpu, &mdev->vdisk->part0);
+	part_dec_in_flight(&mdev->vdisk->part0, rw);
 	part_stat_unlock();
-	mdev->vdisk->part0.in_flight--;
 #endif
 }
 
