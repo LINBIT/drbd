5070565476d6c445b76c1f7ddf2966ce588eb7cb
Philipp Reisner <philipp.reisner@linbit.com>
2009-12-06 23:50:24 +0100

    drbd: Following the hmac change to SHASH (see linux commit 8bd1209cfff)
    Compatibility for older kernels will be added later

--- drbd/drbd_nl.c
+++ drbd/drbd_nl.c
@@ -1287,8 +1287,7 @@ STATIC int drbd_nl_net_conf(struct drbd_conf *mdev, struct drbd_nl_cfg_req *nlp,
 			goto fail;
 		}
 
-		if (crypto_tfm_alg_type(crypto_hash_tfm(tfm))
-						!= CRYPTO_ALG_TYPE_HASH) {
+		if (crypto_tfm_alg_type(crypto_hash_tfm(tfm)) != CRYPTO_ALG_TYPE_SHASH) {
 			retcode = ERR_AUTH_ALG_ND;
 			goto fail;
 		}
