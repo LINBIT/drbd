3eb7628c91d94b21d36723ada8bc5191d7201a16
Philipp Reisner <philipp.reisner@linbit.com>
2010-06-02 14:31:29 +0200

    drbd: Fixed a race between disk-attach and unexpected state changes
    This was a very hard to trigger race condition.
    
    If we got a state packet from the peer, after drbd_nl_disk() has
    already changed the disk state to D_NEGOTIATING but
    after_state_ch() was not yet run by the worker, then receive_state()
    might called drbd_sync_handshake(), which in turn crashed
    when accessing p_uuid.

--- drbd/drbd_nl.c
+++ drbd/drbd_nl.c
@@ -1129,6 +1129,12 @@ STATIC int drbd_nl_disk_conf(struct drbd_conf *mdev, struct drbd_nl_cfg_req *nlp
 		mdev->new_state_tmp.i = ns.i;
 		ns.i = os.i;
 		ns.disk = D_NEGOTIATING;
+
+		/* We expect to receive up-to-date UUIDs soon.
+		   To avoid a race in receive_state, free p_uuid while
+		   holding req_lock. I.e. atomic with the state change */
+		kfree(mdev->p_uuid);
+		mdev->p_uuid = NULL;
 	}
 
 	DRBD_STATE_DEBUG_INIT_VAL(ns);
