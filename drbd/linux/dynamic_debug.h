5447f025e97ce49a708e3caa18afe202bf833e6b
Lars Ellenberg <lars.ellenberg@linbit.com>
2010-10-13 18:18:14 +0200

    compat: improve compatibility with kernels that still have dynamic_printk.h, not yet dynamic_debug.h

--- drbd/linux/dynamic_debug.h
+++ drbd/linux/dynamic_debug.h
@@ -1,6 +1,8 @@
-#ifndef DYNAMIC_DEBUG_H
-#define DYNAMIC_DEBUG_H
+#ifndef _DYNAMIC_DEBUG_H
+#define _DYNAMIC_DEBUG_H
 
+#ifndef dynamic_dev_dbg
 #define dynamic_dev_dbg(dev, fmt, ...)
+#endif
 
 #endif
