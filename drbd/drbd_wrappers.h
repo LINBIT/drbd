7aced8dfae22558d8618f3bb1f66c55cd5c514b6
Lars Ellenberg <lars.ellenberg@linbit.com>
2010-12-03 10:06:40 +0100

    compat: regression fix: max bio size limitation enforcement on 2.6.31 to 2.6.33
    In current kernels, the accessor function blk_queue_max_hw_sectors
    does the right thing, and blk_queue_max_sectors is no longer there.
    
    In earlier kernels, blk_queue_max_hw_sectors is missing, and
    blk_queue_max_sectors it the function to use to set these limits.
    
    In 2.6.31 blk_queue_max_hw_sectors was introduced, but with different
    semantics. That function got removed again, and the "do-the-right-thing"
    function was renamed from blk_queue_max_sectors renamed to
    blk_queue_max_hw_sectors.
    
    We missed this subtlety before, triggereing only on the existence
    of the blk_queue_max_hw_sectors function, not on its temporary
    different semantics.
    
    Impact:
    If the 8.3.9 side runs on one of the mentioned kernel versions,
     * the upgrade path from <= 8.3.7 to 8.3.9 is broken
     * a mixed cluster with <= 8.3.7 will not work
     * the upgrade path from 8.3.8 to 8.3.9 does work, however
     * a mixed cluster with 8.3.8 and 8.3.9 will work only
       as long as the 8.3.9 side remains secondary,
       or the 8.3.9 side becomes primary, but sees only bio sizes <= 32k.

--- drbd/drbd_wrappers.h
+++ drbd/drbd_wrappers.h
@@ -723,6 +723,12 @@ static inline void blk_queue_max_hw_sectors(struct request_queue *q, unsigned in
 {
 	blk_queue_max_sectors(q, max);
 }
+#elif LINUX_VERSION_CODE < KERNEL_VERSION(2,6,34)
+	/* For kernel versions 2.6.31 to 2.6.33 inclusive, even though
+	 * blk_queue_max_hw_sectors is present, we actually need to use
+	 * blk_queue_max_sectors to set max_hw_sectors. :-(
+	 */
+#define blk_queue_max_hw_sectors(q, max)	blk_queue_max_sectors(q, max)
 #endif
 
 #ifdef NEED_BLK_QUEUE_MAX_SEGMENTS
