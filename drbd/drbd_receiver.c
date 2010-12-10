f37854d2f92ff0625ca11eef348dc214629cf252
Andreas Gruenbacher <agruen@linbit.com>
2010-12-10 17:09:10 +0100

    drbd: receive_bitmap: Fix the wrong return value

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -3737,7 +3737,7 @@ STATIC int receive_bitmap(struct drbd_conf *mdev, enum drbd_packets cmd, unsigne
 				goto out;
 			if (data_size <= (sizeof(*p) - sizeof(p->head))) {
 				dev_err(DEV, "ReportCBitmap packet too small (l:%u)\n", data_size);
-				return FAILED;
+				return false;
 			}
 			ret = decode_bitmap_c(mdev, p, &c);
 		} else {
