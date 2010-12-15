c6f3c74d0847f20cc43517ed281b50d4ed26efc2
Andreas Gruenbacher <agruen@linbit.com>
2010-12-15 17:04:36 +0100

    drbd: Add drbd_send_all(): Send an entire buffer

--- drbd/drbd_main.c
+++ drbd/drbd_main.c
@@ -1836,6 +1836,24 @@ int drbd_send(struct drbd_tconn *tconn, struct socket *sock,
 	return sent;
 }
 
+/**
+ * drbd_send_all  -  Send an entire buffer
+ *
+ * Returns 0 upon success and a negative error value otherwise.
+ */
+int drbd_send_all(struct drbd_tconn *tconn, struct socket *sock, void *buffer,
+		  size_t size, unsigned msg_flags)
+{
+	int err;
+
+	err = drbd_send(tconn, sock, buffer, size, msg_flags);
+	if (err < 0)
+		return err;
+	if (err != size)
+		return -EIO;
+	return 0;
+}
+
 #ifdef BD_OPS_USE_FMODE
 static int drbd_open(struct block_device *bdev, fmode_t mode)
 #else
