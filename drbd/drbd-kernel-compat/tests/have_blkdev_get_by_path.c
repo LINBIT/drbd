dbfc67f8041f5c95e4a384fc2d2ee4956d02afab
Andreas Gruenbacher <agruen@linbit.com>
2011-01-25 13:35:39 +0100

    drbd: compat: Add tests for blkdev_get_by_path() and open_bdev_exclusive()

---
+++ drbd/drbd-kernel-compat/tests/have_blkdev_get_by_path.c
@@ -0,0 +1,11 @@
+#include <linux/fs.h>
+
+/*
+ * In kernel version 2.6.38-rc1, open_bdev_exclusive() was replaced by
+ * blkdev_get_by_path(); see commits e525fd89 and d4d77629.
+ */
+void foo(void) {
+	struct block_device *blkdev;
+
+	blkdev = blkdev_get_by_path("", (fmode_t) 0, (void *) 0);
+}
