5897260a988313612d577c8e0db6c6848ef13f25
Philipp Reisner <philipp.reisner@linbit.com>
2011-02-08 11:09:18 +0100

    drbd: Use and idr data structure to map volume numbers to mdev pointers

--- drbd/drbd_int.h
+++ drbd/drbd_int.h
@@ -37,6 +37,7 @@
 #include <linux/tcp.h>
 #include <linux/mutex.h>
 #include <linux/genhd.h>
+#include <linux/idr.h>
 #include <net/tcp.h>
 
 #include "compat.h"
@@ -1039,8 +1040,9 @@ struct drbd_tconn {			/* is a resource from the config file */
 	char *name;			/* Resource name */
 	struct list_head all_tconn;	/* List of all drbd_tconn, prot by global_state_lock */
 	struct drbd_conf *volume0;	/* TODO: Remove me again */
-	unsigned long flags;
+	struct idr volumes;             /* <tconn, vnr> to mdev mapping */
 
+	unsigned long flags;
 	struct net_conf *net_conf;	/* protected by get_net_conf() and put_net_conf() */
 	atomic_t net_cnt;		/* Users of net_conf */
 	wait_queue_head_t net_cnt_wait;
