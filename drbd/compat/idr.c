d6a70266a3aac29cf30c19d0d7bf9a620c7e335f
Lars Ellenberg <lars.ellenberg@linbit.com>
2011-02-18 14:05:03 +0100

    compat: fix compilation on 2.6.32
    on 2.6.32, rcu_dereference_raw() is still just rcu_dereference().

--- drbd/compat/idr.c
+++ drbd/compat/idr.c
@@ -5,6 +5,10 @@
 /* The idr_get_next() function exists since 2009-04-02 Linux-2.6.29 (commit 38460b48)
    but is exported for use in modules since 2010-01-29 Linux-2.6.35 (commit 4d1ee80f)  */
 #ifndef IDR_GET_NEXT_EXPORTED
+#ifndef rcu_dereference_raw
+/* see c26d34a rcu: Add lockdep-enabled variants of rcu_dereference() */
+#define rcu_dereference_raw(p) rcu_dereference(p)
+#endif
 void *idr_get_next(struct idr *idp, int *nextidp)
 {
 	struct idr_layer *p, *pa[MAX_LEVEL];
