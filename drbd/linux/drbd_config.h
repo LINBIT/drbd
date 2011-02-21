f6033027c85fdcee7a73d18dfb883d6c9f89cfdf
Lars Ellenberg <lars.ellenberg@linbit.com>
2011-02-21 14:54:33 +0100

    drbd: bump version string to 8.4.0-nqty

--- drbd/linux/drbd_config.h
+++ drbd/linux/drbd_config.h
@@ -32,7 +32,7 @@ extern const char *drbd_buildtag(void);
 
 /* End of external module for 2.6.33 stuff */
 
-#define REL_VERSION "8.3.10rc3"
+#define REL_VERSION "8.4.0-nqty"	/* not quite there yet */
 #define API_VERSION 88
 #define PRO_VERSION_MIN 86
 #define PRO_VERSION_MAX 100
