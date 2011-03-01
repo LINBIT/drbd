5f9ca9af1cb35cac49b304274d3a7a12e73fb290
Philipp Reisner <philipp.reisner@linbit.com>
2011-03-01 17:58:30 +0100

    backport: compat: Backport of time_is_before_eq_jiffies()

--- drbd/drbd_wrappers.h
+++ drbd/drbd_wrappers.h
@@ -942,6 +942,14 @@ static inline signed long schedule_timeout_uninterruptible(signed long timeout)
 	__x == 0 ? __y : ((__y == 0) ? __x : min(__x, __y)); })
 #endif
 
+/* Introduced with 2.6.26. See include/linux/jiffies.h */
+#ifndef time_is_before_eq_jiffies
+#define time_is_before_jiffies(a) time_after(jiffies, a)
+#define time_is_after_jiffies(a) time_before(jiffies, a)
+#define time_is_before_eq_jiffies(a) time_after_eq(jiffies, a)
+#define time_is_after_eq_jiffies(a) time_before_eq(jiffies, a)
+#endif
+
 #ifdef COMPAT_BIO_SPLIT_HAS_BIO_SPLIT_POOL_PARAMETER
 #define bio_split(bi, first_sectors) bio_split(bi, bio_split_pool, first_sectors)
 #endif
