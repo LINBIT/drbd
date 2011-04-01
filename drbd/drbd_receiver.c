6105fd99eb39f186ab4728223ba77001ddf4c5a5
Andreas Gruenbacher <agruen@linbit.com>
2011-04-01 12:49:42 +0200

    drbd: drbd_send_ping(), drbd_send_ping(): Return 0 upon success and an error code otherwise

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -4765,7 +4765,7 @@ STATIC int got_RqSReply(struct drbd_tconn *tconn, struct packet_info *pi)
 
 STATIC int got_Ping(struct drbd_tconn *tconn, struct packet_info *pi)
 {
-	return drbd_send_ping_ack(tconn);
+	return !drbd_send_ping_ack(tconn);
 
 }
 
@@ -5117,7 +5117,7 @@ int drbd_asender(struct drbd_thread *thi)
 	while (get_t_state(thi) == RUNNING) {
 		drbd_thread_current_set_cpu(thi);
 		if (test_and_clear_bit(SEND_PING, &tconn->flags)) {
-			if (!drbd_send_ping(tconn)) {
+			if (drbd_send_ping(tconn)) {
 				conn_err(tconn, "drbd_send_ping has failed\n");
 				goto reconnect;
 			}
