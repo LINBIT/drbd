ec3dcc09129d0b2bad0c37d936a14fef6af4d851
Lars Ellenberg <lars.ellenberg@linbit.com>
2011-06-28 13:22:48 +0200

    drbd: we should write meta data updates with FLUSH FUA
    We used to write these with BIO_RW_BARRIER aka REQ_HARDBARRIER (unless
    disabled in the configuration). The correct semantic now would be to
    write with FLUSH/FUA.
    For example, with activity log transactions, FUA alone is not enough, we
    need the corresponding bitmap update (and all related application
    updates) on stable storage as well.

--- drbd/drbd_actlog.c
+++ drbd/drbd_actlog.c
@@ -85,7 +85,7 @@ STATIC int _drbd_md_sync_page_io(struct drbd_conf *mdev,
 	md_io.error = 0;
 
 	if ((rw & WRITE) && !test_bit(MD_NO_BARRIER, &mdev->flags))
-		rw |= DRBD_REQ_FUA;
+		rw |= DRBD_REQ_FUA | DRBD_REQ_FLUSH;
 	rw |= DRBD_REQ_UNPLUG | DRBD_REQ_SYNC;
 
 #ifndef REQ_FLUSH
