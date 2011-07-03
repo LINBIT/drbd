ae8a0711d581dba268d482c0f916fb9b516dd532
Andreas Gruenbacher <agruen@linbit.com>
2011-07-03 23:32:26 +0200

    drbd: Rename drbd_{create,delete}_minor -> drbd_{create,delete}_device
    Signed-off-by: Andreas Gruenbacher <agruen@linbit.com>
    Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>

--- drbd/drbd_nl.c
+++ drbd/drbd_nl.c
@@ -3363,7 +3363,7 @@ int drbd_adm_new_minor(struct sk_buff *skb, struct genl_info *info)
 	if (adm_ctx.device)
 		goto out;
 
-	retcode = drbd_create_minor(adm_ctx.connection, dh->minor, adm_ctx.volume);
+	retcode = drbd_create_device(adm_ctx.connection, dh->minor, adm_ctx.volume);
 out:
 	drbd_adm_finish(info, retcode);
 	return 0;
@@ -3378,7 +3378,7 @@ static enum drbd_ret_code adm_del_minor(struct drbd_device *device)
 	    device->state.role == R_SECONDARY) {
 		_drbd_request_state(device, NS(conn, C_WF_REPORT_PARAMS),
 				    CS_VERBOSE + CS_WAIT_COMPLETE);
-		drbd_delete_minor(device);
+		drbd_delete_device(device);
 		return NO_ERROR;
 	} else
 		return ERR_MINOR_CONFIGURED;
