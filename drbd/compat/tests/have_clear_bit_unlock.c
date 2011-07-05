a7c815178ca8dec61c0004b15751bae2fd0c8f62
Philipp Reisner <philipp.reisner@linbit.com>
2011-07-05 18:20:32 +0200

    build: Workaround for bad asm/bitops.h on s390

--- drbd/compat/tests/have_clear_bit_unlock.c
+++ drbd/compat/tests/have_clear_bit_unlock.c
@@ -1,3 +1,13 @@
+#include <asm/system.h>
+/* Including asm/system.h is necessary for s390.
+
+   They define smp_mb__before_clear_bit() in asm/system.h
+   From asm/bitops.h they include asm-generic/bitops/lock.h
+   The macro defining clear_bit_unlock() in
+   asm-generic/bitops/lock.h needs smp_mb__before_clear_bit().
+
+   They fail to include asm/system.h from asm/bitops.h
+*/
 #include <linux/bitops.h>
 
 void foo()
