9daa26d46d54e8e09fc1f6e3821b0198075c2e7c
Philipp Reisner <philipp.reisner@linbit.com>
2011-09-05 16:22:33 +0200

    drbd: Consider the discard-my-data flag for all volumes [bugz 359]
    ...not only for the first volume

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -868,6 +868,7 @@ STATIC int conn_connect(struct drbd_tconn *tconn)
 	struct drbd_conf *mdev;
 	struct net_conf *nc;
 	int vnr, timeout, try, h, ok;
+	bool discard_my_data;
 
 	if (conn_request_state(tconn, NS(conn, C_WF_CONNECTION), CS_VERBOSE) < SS_SUCCESS)
 		return -2;
@@ -986,6 +987,7 @@ retry:
 
 	msock->sk->sk_rcvtimeo = nc->ping_int*HZ;
 	timeout = nc->timeout * HZ / 10;
+	discard_my_data = nc->discard_my_data;
 	rcu_read_unlock();
 
 	msock->sk->sk_sndtimeo = timeout;
@@ -1023,6 +1025,12 @@ retry:
 	idr_for_each_entry(&tconn->volumes, mdev, vnr) {
 		kref_get(&mdev->kref);
 		rcu_read_unlock();
+
+		if (discard_my_data)
+			set_bit(DISCARD_MY_DATA, &mdev->flags);
+		else
+			clear_bit(DISCARD_MY_DATA, &mdev->flags);
+
 		drbd_connected(mdev);
 		kref_put(&mdev->kref, &drbd_minor_destroy);
 		rcu_read_lock();
@@ -1033,6 +1041,14 @@ retry:
 		return 0;
 
 	drbd_thread_start(&tconn->asender);
+
+	mutex_lock(&tconn->conf_update);
+	/* The discard_my_data flag is a single-shot modifier to the next
+	 * connection attempt, the handshake of which is now well underway.
+	 * No need for rcu style copying of the whole struct
+	 * just to clear a single value. */
+	tconn->net_conf->discard_my_data = 0;
+	mutex_unlock(&tconn->conf_update);
 
 	return h;
 
@@ -3107,9 +3123,9 @@ STATIC enum drbd_conns drbd_sync_handshake(struct drbd_conf *mdev, enum drbd_rol
 	}
 
 	if (hg == -100) {
-		if (nc->discard_my_data && !(mdev->p_uuid[UI_FLAGS]&1))
+		if (test_bit(DISCARD_MY_DATA, &mdev->flags) && !(mdev->p_uuid[UI_FLAGS]&1))
 			hg = -1;
-		if (!nc->discard_my_data && (mdev->p_uuid[UI_FLAGS]&1))
+		if (!test_bit(DISCARD_MY_DATA, &mdev->flags) && (mdev->p_uuid[UI_FLAGS]&1))
 			hg = 1;
 
 		if (abs(hg) < 100)
@@ -4064,9 +4080,7 @@ STATIC int receive_state(struct drbd_tconn *tconn, struct packet_info *pi)
 		}
 	}
 
-	mutex_lock(&mdev->tconn->conf_update);
-	mdev->tconn->net_conf->discard_my_data = 0; /* without copy; single bit op is atomic */
-	mutex_unlock(&mdev->tconn->conf_update);
+	clear_bit(DISCARD_MY_DATA, &mdev->flags);
 
 	drbd_md_sync(mdev); /* update connected indicator, la_size, ... */
 
