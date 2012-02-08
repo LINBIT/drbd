55a81dcc13bf199cfa1bb6695267deec5adc92ca
Lars Ellenberg <lars.ellenberg@linbit.com>
2012-02-08 15:37:31 +0100

    drbd: fix binary-incompatible build on some platforms
    The type of the make_request_fn changed from int to void in upstream
    kernel. To be compatible with both older and newer kernels, our compat.h
    auto-detect magic tries to figure this out.
    
    However, at least on Ubuntu Lucid, the compiler ignores the
    pragma -Werror we rely on there, leading to us using void,
    where the rest of the kernel expects an int return code.
    
    This caused interesting BUGs like (short version):
     BUG: unable to handle kernel NULL pointer dereference at 0000000000000008
     IP: [<ffffffff81439544>] clone_endio+0x34/0xe0
     Pid: 3517, comm: kdmflush Not tainted 2.6.32-38-server #83-Ubuntu X8DTN
     Call Trace:
      [<ffffffff81173d2d>] bio_endio+0x1d/0x40
      [<ffffffffa03a87fb>] drbd_make_request+0x34b/0x350 [drbd]
      [<ffffffff812a22a1>] generic_make_request+0x1b1/0x4f0
      [<ffffffff81438fcd>] __map_bio+0xad/0x130
      [<ffffffff814394fd>] __clone_and_map+0x4ad/0x4c0
      [<ffffffff8143a5d8>] __split_and_process_bio+0x108/0x190
      [<ffffffff8143a6b6>] dm_flush+0x56/0x70
    
    Fix: in compat/tests/have_void_make_request.c, don't rely on -Werror, but
     BUILD_BUG_ON(!(__same_type(&drbd_make_request, make_request_fn)));

--- drbd/compat/tests/have_void_make_request.c
+++ drbd/compat/tests/have_void_make_request.c
@@ -1,6 +1,9 @@
 #include <linux/blkdev.h>
 
+/* hm. sometimes this pragma is ignored :(
+ * use BUILD_BUG_ON instead.
 #pragma GCC diagnostic warning "-Werror"
+ */
 
 /* in Commit 5a7bbad27a410350e64a2d7f5ec18fc73836c14f (between Linux-3.1 and 3.2)
    make_request() becomes type void. Before it had type int.
@@ -12,7 +15,5 @@ void drbd_make_request(struct request_queue *q, struct bio *bio)
 
 void foo(void)
 {
-	struct request_queue *q = NULL;
-
-	blk_queue_make_request(q, drbd_make_request);
+	BUILD_BUG_ON(!(__same_type(&drbd_make_request, make_request_fn)));
 }
