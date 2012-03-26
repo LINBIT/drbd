c46fc4b845d06f9b612614fe57ad49e70a3733c6
Lars Ellenberg <lars.ellenberg@linbit.com>
2012-03-26 16:55:46 +0200

    drbd: conflicting writes: make wake_up of waiting peer_requests explicit

--- drbd/drbd_req.c
+++ drbd/drbd_req.c
@@ -244,6 +244,16 @@ static void drbd_remove_request_interval(struct rb_root *root,
 		wake_up(&mdev->misc_wait);
 }
 
+static void maybe_wakeup_conflicting_requests(struct drbd_request *req)
+{
+	const unsigned long s = req->rq_state;
+	if (s & RQ_LOCAL_PENDING && !(s & RQ_LOCAL_ABORTED))
+		return;
+	if (req->i.waiting)
+		/* Retry all conflicting peer requests.  */
+		wake_up(&req->w.mdev->misc_wait);
+}
+
 /* Helper for __req_mod().
  * Set m->bio to the master bio, if it is fit to be completed,
  * or leave it alone (it is initialized to NULL in __req_mod),
@@ -267,10 +277,6 @@ void _req_may_be_done(struct drbd_request *req, struct bio_and_error *m)
 	 */
 	if (s & RQ_LOCAL_PENDING && !(s & RQ_LOCAL_ABORTED))
 		return;
-	if (req->i.waiting) {
-		/* Retry all conflicting peer requests.  */
-		wake_up(&mdev->misc_wait);
-	}
 	if (s & RQ_NET_QUEUED)
 		return;
 	if (s & RQ_NET_PENDING)
@@ -420,6 +426,7 @@ int __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		req->rq_state |= (RQ_LOCAL_COMPLETED|RQ_LOCAL_OK);
 		req->rq_state &= ~RQ_LOCAL_PENDING;
 
+		maybe_wakeup_conflicting_requests(req);
 		_req_may_be_done_not_susp(req, m);
 		put_ldev(mdev);
 		break;
@@ -437,6 +444,7 @@ int __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		req->rq_state &= ~RQ_LOCAL_PENDING;
 
 		__drbd_chk_io_error(mdev, false);
+		maybe_wakeup_conflicting_requests(req);
 		_req_may_be_done_not_susp(req, m);
 		put_ldev(mdev);
 		break;
@@ -647,6 +655,7 @@ int __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		dec_ap_pending(mdev);
 		atomic_sub(req->i.size >> 9, &mdev->ap_in_flight);
 		req->rq_state &= ~RQ_NET_PENDING;
+		maybe_wakeup_conflicting_requests(req);
 		_req_may_be_done_not_susp(req, m);
 		break;
 
@@ -658,6 +667,7 @@ int __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		 */
 		D_ASSERT(req->rq_state & RQ_NET_PENDING);
 		req->rq_state |= RQ_POSTPONED;
+		maybe_wakeup_conflicting_requests(req);
 		_req_may_be_done_not_susp(req, m);
 		break;
 
@@ -675,6 +685,7 @@ int __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		if (!(req->rq_state & RQ_WRITE))
 			goto goto_read_retry_local;
 
+		maybe_wakeup_conflicting_requests(req);
 		_req_may_be_done_not_susp(req, m);
 		/* else: done by HANDED_OVER_TO_NETWORK */
 		break;
