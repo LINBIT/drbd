d3d0639bb74329b5e56cc62182b3c7c83377c979
Philipp Reisner <philipp.reisner@linbit.com>
2013-03-20 12:04:49 +0100

    compat: idr_alloc() for kernels older than v3.9

--- drbd/drbd_wrappers.h
+++ drbd/drbd_wrappers.h
@@ -1229,4 +1229,24 @@ static inline void list_splice_tail_init(struct list_head *list,
 }
 #endif
 
+#ifndef COMPAT_HAVE_IDR_ALLOC
+static inline int idr_alloc(struct idr *idr, void *ptr, int start, int end, gfp_t gfp_mask)
+{
+	int rv, got;
+
+	if (!idr_pre_get(idr, gfp_mask))
+		return -ENOMEM;
+	rv = idr_get_new_above(idr, ptr, start, &got);
+	if (rv < 0)
+		return rv;
+
+	if (got >= end) {
+		idr_remove(idr, got);
+		return -ENOSPC;
+	}
+
+	return got;
+}
+#endif
+
 #endif
