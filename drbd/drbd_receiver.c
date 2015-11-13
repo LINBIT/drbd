ba18acd7416e8798c82cdaa76726c41cf00705bf
Lars Ellenberg <lars.ellenberg@linbit.com>
2015-11-13 14:18:56 +0100

    fixup: drbd: allow parallel flushes for multi-volume resources
    During merge of various attempts of the above patch, I managed
    to wait for an uninitialized, otherwise unused, on-stack completion
    in drbd_flush_after_epoch(), instead of the intended &ctx.done.
    
    This apparently went unnoticed because most of the time, the timing
    would cause wait_on_completion() to be skipped.  Also most setups
    disable these flushes anyways, and the whole function would be skipped.
    Or, the uninitialized on-stack value would happen to contain ->done == 1
    already.  This all could lead to deadlocks, or crashes due to access of
    uninizialized pointers.

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -1419,7 +1419,6 @@ static enum finish_epoch drbd_flush_after_epoch(struct drbd_connection *connecti
 {
 	if (connection->resource->write_ordering >= WO_BDEV_FLUSH) {
 		struct drbd_peer_device *peer_device;
-		struct completion done;
 		struct issue_flush_context ctx;
 		int vnr;
 
@@ -1445,7 +1444,7 @@ static enum finish_epoch drbd_flush_after_epoch(struct drbd_connection *connecti
 		/* Do we want to add a timeout,
 		 * if disk-timeout is set? */
 		if (!atomic_dec_and_test(&ctx.pending))
-			wait_for_completion(&done);
+			wait_for_completion(&ctx.done);
 
 		if (ctx.error) {
 			/* would rather check on EOPNOTSUPP, but that is not reliable.
