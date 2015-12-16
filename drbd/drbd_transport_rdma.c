d3a4ce2b915f6bbe197763f2c6e98d375217db02
Philipp Reisner <philipp.reisner@linbit.com>
2015-12-16 17:45:47 +0100

    bump version number to 2.0.0

--- drbd/drbd_transport_rdma.c
+++ drbd/drbd_transport_rdma.c
@@ -75,7 +75,7 @@ MODULE_AUTHOR("Philipp Reisner <philipp.reisner@linbit.com>");
 MODULE_AUTHOR("Lars Ellenberg <lars.ellenberg@linbit.com>");
 MODULE_DESCRIPTION("RDMA transport layer for DRBD");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("1.0.0");
+MODULE_VERSION("2.0.0");
 
 int allocation_size;
 /* module_param(allocation_size, int, 0664);
