605dd5cfa53dab854fa36e654346e54387668c08
Philipp Reisner <philipp.reisner@linbit.com>
2016-03-31 11:31:36 +0200

    drbd: Correctly name a macro parameter
    in the parameter pointer to devices are passed, not to resources.
    Name it correctly.

--- drbd/drbd_int.h
+++ drbd/drbd_int.h
@@ -1436,10 +1436,10 @@ static inline unsigned drbd_req_state_by_peer_device(struct drbd_request *req,
 #define for_each_peer_device_safe(peer_device, tmp, device) \
 	list_for_each_entry_safe(peer_device, tmp, &device->peer_devices, peer_devices)
 
-#define for_each_peer_device_ref(peer_device, m, resource)		\
-	for (peer_device = __drbd_next_peer_device_ref(&m, NULL, resource); \
+#define for_each_peer_device_ref(peer_device, m, device)		\
+	for (peer_device = __drbd_next_peer_device_ref(&m, NULL, device); \
 	     peer_device;						\
-	     peer_device = __drbd_next_peer_device_ref(&m, peer_device, resource))
+	     peer_device = __drbd_next_peer_device_ref(&m, peer_device, device))
 
 static inline unsigned int device_to_minor(struct drbd_device *device)
 {
