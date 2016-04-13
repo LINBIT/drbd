1b601d6fb7263fa301d5541b018ac96a8ff6258d
Philipp Reisner <philipp.reisner@linbit.com>
2016-04-13 09:01:24 +0200

    compat: Compat infrastructure for ahash & shash crypto

---
+++ drbd/compat/tests/have_AHASH_REQUEST_ON_STACK.c
@@ -0,0 +1,6 @@
+#include <crypto/hash.h>
+
+void foo(void) {
+	struct crypto_ahash tfm;
+	AHASH_REQUEST_ON_STACK(desc, &tfm);
+}
---
+++ drbd/compat/tests/have_SHASH_DESC_ON_STACK.c
@@ -0,0 +1,6 @@
+#include <crypto/hash.h>
+
+void foo(void) {
+	struct crypto_shash tfm;
+	SHASH_DESC_ON_STACK(desc, &tfm);
+}
