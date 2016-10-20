7b2a7c1ea898ec70908fdeb0630b78d63d32dcbf
Philipp Reisner <philipp.reisner@linbit.com>
2016-10-20 10:10:14 +0200

    drbd: compat for 4.8: rename bi_rw to bi_opf, add backward compat
    For older kernel, define bi_opf to bi_rw.
    4.8 kernel should use bi_opf already.

--- drbd/drbd-kernel-compat/drbd_wrappers.h
+++ drbd/drbd-kernel-compat/drbd_wrappers.h
@@ -715,8 +715,8 @@ static inline void blk_queue_write_cache(struct request_queue *q, bool enabled, 
  * 7b6d91daee5cac6402186ff224c3af39d79f4a0e and
  * 7cc015811ef8992dfcce314d0ed9642bc18143d1
  * We communicate between different systems,
- * so we have to somehow semantically map the bi_rw flags
- * bi_rw (some kernel version) -> data packet flags -> bi_rw (other kernel version)
+ * so we have to somehow semantically map the bi_opf flags
+ * bi_opf (some kernel version) -> data packet flags -> bi_opf (other kernel version)
  */
 
 #if defined(bio_set_op_attrs)
@@ -886,6 +886,8 @@ static inline void blk_queue_write_cache(struct request_queue *q, bool enabled, 
 
 
 #ifndef bio_set_op_attrs /* compat for Linux before 4.8 {{{2 */
+
+#define bi_opf bi_rw
 
 #ifndef REQ_WRITE
 /* before 2.6.36 */
