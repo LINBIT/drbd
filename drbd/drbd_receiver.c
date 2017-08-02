LINBIT/drbd

commit 504e46823c3bf03d3850fbe73d13e42b4d614803

drbd: Fix the no-resync case when a diskfull secondary returns to a diskless primary
A corner case that was not covered by the test cases before.

drbd/drbd_receiver.c
@@ -582,7 +582,7 @@ int drbd_connected(struct drbd_peer_device *peer_device)
 	err = drbd_send_sync_param(peer_device);
 	if (!err)
 		err = drbd_send_sizes(peer_device, 0, 0);
-	if (!err && device->disk_state[NOW] > D_DISKLESS)
+	if (!err)
 		err = drbd_send_uuids(peer_device, 0, 0);
 	if (!err) {
 		set_bit(INITIAL_STATE_SENT, &peer_device->flags);
@@ -6148,7 +6148,7 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 	struct drbd_device *device = NULL;
 	struct p_state *p = pi->data;
 	union drbd_state old_peer_state, peer_state;
-	enum drbd_disk_state peer_disk_state;
+	enum drbd_disk_state peer_disk_state, new_disk_state = D_MASK;
 	enum drbd_repl_state new_repl_state;
 	bool peer_was_resync_target, try_to_get_resync = false;
 	int rv;
@@ -6340,9 +6340,14 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 	} else if (peer_state.role == R_PRIMARY &&
 		   peer_device->disk_state[NOW] == D_UNKNOWN && peer_state.disk == D_DISKLESS &&
 		   device->disk_state[NOW] >= D_NEGOTIATING && device->disk_state[NOW] < D_UP_TO_DATE) {
-		/* I got connected to a diskless primary. Try to get a resync from
-		   some other node that is D_UP_TO_DATE. */
-		try_to_get_resync = true;
+		/* I got connected to a diskless primary */
+		if (peer_device->current_uuid == drbd_current_uuid(device)) {
+			drbd_info(peer_device, "Upgrading local disk to D_UP_TO_DATE since current UUID matches.\n");
+			new_disk_state = D_UP_TO_DATE;
+		} else {
+			/* Try to get a resync from some other node that is D_UP_TO_DATE. */
+			try_to_get_resync = true;
+		}
 	}
 
 	spin_lock_irq(&resource->req_lock);
@@ -6354,6 +6359,8 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 		goto retry;
 	}
 	clear_bit(CONSIDER_RESYNC, &peer_device->flags);
+	if (new_disk_state != D_MASK)
+		__change_disk_state(device, new_disk_state);
 	if (device->disk_state[NOW] != D_NEGOTIATING)
 		__change_repl_state(peer_device, new_repl_state);
 	if (connection->peer_role[NOW] == R_UNKNOWN || peer_state.role == R_SECONDARY)
@@ -6917,7 +6924,8 @@ out:
 	return 0;
 }
 
-/* Accept a new current UUID generated on a diskless node, that just became primary */
+/* Accept a new current UUID generated on a diskless node, that just became primary
+   (or during handshake) */
 static int receive_current_uuid(struct drbd_connection *connection, struct packet_info *pi)
 {
 	struct drbd_resource *resource = connection->resource;
@@ -6933,9 +6941,13 @@ static int receive_current_uuid(struct drbd_connection *connection, struct packe
 
 	current_uuid = be64_to_cpu(p->uuid);
 	weak_nodes = be64_to_cpu(p->weak_nodes);
+	peer_device->current_uuid = current_uuid;
+
+	if (connection->peer_role[NOW] == R_UNKNOWN)
+		return 0;
+
 	if (current_uuid == drbd_current_uuid(device))
 		return 0;
-	peer_device->current_uuid = current_uuid;
 
 	if (get_ldev_if_state(device, D_UP_TO_DATE)) {
 		if (connection->peer_role[NOW] == R_PRIMARY) {

drbd/drbd_int.h
@@ -1440,7 +1440,7 @@ extern void drbd_send_twopc_reply(struct drbd_connection *connection,
 				  enum drbd_packet, struct twopc_reply *);
 extern void drbd_send_peers_in_sync(struct drbd_peer_device *, u64, sector_t, int);
 extern int drbd_send_peer_dagtag(struct drbd_connection *connection, struct drbd_connection *lost_peer);
-extern void drbd_send_current_uuid(struct drbd_peer_device *peer_device, u64 current_uuid, u64 weak_nodes);
+extern int drbd_send_current_uuid(struct drbd_peer_device *peer_device, u64 current_uuid, u64 weak_nodes);
 extern void drbd_backing_dev_free(struct drbd_device *device, struct drbd_backing_dev *ldev);
 extern void drbd_cleanup_device(struct drbd_device *device);
 extern void drbd_print_uuids(struct drbd_peer_device *peer_device, const char *text);

drbd/drbd_main.c
@@ -1353,7 +1353,8 @@ static int _drbd_send_uuids110(struct drbd_peer_device *peer_device, u64 uuid_fl
 	int p_size = sizeof(*p);
 
 	if (!get_ldev_if_state(device, D_NEGOTIATING))
-		return 0;
+		return drbd_send_current_uuid(peer_device, device->exposed_data_uuid,
+					      drbd_weak_nodes_device(device));
 
 	peer_md = device->ldev->md.peers;
 
@@ -1436,16 +1437,17 @@ void drbd_print_uuids(struct drbd_peer_device *peer_device, const char *text)
 	}
 }
 
-void drbd_send_current_uuid(struct drbd_peer_device *peer_device, u64 current_uuid, u64 weak_nodes)
+int drbd_send_current_uuid(struct drbd_peer_device *peer_device, u64 current_uuid, u64 weak_nodes)
 {
 	struct p_current_uuid *p;
 
 	p = drbd_prepare_command(peer_device, sizeof(*p), DATA_STREAM);
-	if (p) {
-		p->uuid = cpu_to_be64(current_uuid);
-		p->weak_nodes = cpu_to_be64(weak_nodes);
-		drbd_send_command(peer_device, P_CURRENT_UUID, DATA_STREAM);
-	}
+	if (!p)
+		return -EIO;
+
+	p->uuid = cpu_to_be64(current_uuid);
+	p->weak_nodes = cpu_to_be64(weak_nodes);
+	return drbd_send_command(peer_device, P_CURRENT_UUID, DATA_STREAM);
 }
 
 void drbd_gen_and_send_sync_uuid(struct drbd_peer_device *peer_device)

commit 11acd9525597cbcbd153308073b36ab7552f891e

drbd: Fix a issue with UUID handling that can lead to a forgotten resync
Do not a accept an update to the current-UUID from a diskless primary before
the initial state packet was received.

The bug was introduced with 504e46823c3b, since it enabled that a diskless
node sends its own current-UUID before the initial state packet. Reverting
the change in drbd_connected() would be the wrong approach, that would break
the "no resync" case again.

Ignoring it receive-side is the correct solution.

Here an example of events in the logfile when the bug strikes:

zfs3 goes away:

2017-07-28T15:05:21.244539 compute11 kernel: drbd 755d27d1-be78-4e3d-a6a6-7ca9fb3f2316 zfs3: PingAck did not arrive in time.
2017-07-28T15:05:21.244555 compute11 kernel: drbd 755d27d1-be78-4e3d-a6a6-7ca9fb3f2316 zfs3: conn( Connected -> NetworkFailure ) peer( Secondary -> Unk
2017-07-28T15:05:21.324535 compute11 kernel: drbd 755d27d1-be78-4e3d-a6a6-7ca9fb3f2316/0 drbd3004: sending new current UUID: CDFD12A8BDF99F0C

zfs1 sees the new current UUID.

2017-07-28T15:05:21.321512 zfs1 kernel: drbd 755d27d1-be78-4e3d-a6a6-7ca9fb3f2316/0 drbd3165 compute11: received new current UUID: CDFD12A8BDF99F0C weak_nodes=FFFFFFFFFFFFFFF4
2017-07-28T15:06:20.559472 zfs1 kernel: drbd 755d27d1-be78-4e3d-a6a6-7ca9fb3f2316 zfs3: PingAck did not arrive in time.

zfs2 gets it as well.

2017-07-28T15:05:21.324167 zfs2 kernel: drbd 755d27d1-be78-4e3d-a6a6-7ca9fb3f2316/0 drbd3165 compute11: received new current UUID: CDFD12A8BDF99F0C weak_nodes=FFFFFFFFFFFFFFF4
2017-07-28T15:06:14.557907 zfs2 kernel: drbd 755d27d1-be78-4e3d-a6a6-7ca9fb3f2316 zfs3: PingAck did not arrive in time.

so far all good.

zfs3 comes back, and connects to compute11 first. Note, it gets the network config for all
peers concurrently. This bug only triggers if it connects to the diskless primary first.
Before you look at the log lines about establishing the connection, note that the local

drbd/drbd_receiver.c
@@ -6954,7 +6954,7 @@ static int receive_current_uuid(struct drbd_connection *connection, struct packe
 	if (current_uuid == drbd_current_uuid(device))
 		return 0;
 
-	if (get_ldev_if_state(device, D_UP_TO_DATE)) {
+	if (get_ldev_if_state(device, D_UP_TO_DATE) && test_bit(INITIAL_STATE_RECEIVED, &peer_device->flags)) {
 		if (connection->peer_role[NOW] == R_PRIMARY) {
 			drbd_warn(peer_device, "received new current UUID: %016llX "
 				  "weak_nodes=%016llX\n", current_uuid, weak_nodes);