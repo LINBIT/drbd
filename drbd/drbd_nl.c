LINBIT/drbd

commit 753f249ba0e98b82af120c588c571c37341a8a4a

drbd: switch to call_rcu() in del_connection()
it turns out that the synchronize_rcu() takes considerable amount of
time when deleting many connections.

The idea is to drop the last kref out of the call_rcu() callback.

That means that on the read-sides, the assumpltion that everything
that can be found in an RCU'd list or idr still has a kref ref
of at least one is still true.

drbd/drbd_nl.c
@@ -3479,7 +3479,7 @@ fail_free_connection:
 		drbd_unregister_connection(connection);
 		synchronize_rcu();
 	}
-	drbd_put_connection(connection);
+	drbd_reclaim_connection(&connection->rcu);
 fail_put_transport:
 	drbd_put_transport_class(tr_class);
 fail:
@@ -3891,8 +3891,7 @@ static void del_connection(struct drbd_connection *connection)
 					 NOTIFY_DESTROY | NOTIFY_CONTINUES);
 	notify_connection_state(NULL, 0, connection, NULL, NOTIFY_DESTROY);
 	mutex_unlock(&notification_mutex);
-	synchronize_rcu();
-	drbd_put_connection(connection);
+	call_rcu(&connection->rcu, drbd_reclaim_connection);
 }
 
 static int adm_disconnect(struct sk_buff *skb, struct genl_info *info, bool destroy)

drbd/drbd_int.h
@@ -1049,6 +1049,8 @@ struct drbd_connection {
 
 	unsigned int peer_node_id;
 	struct list_head twopc_parent_list;
+	struct rcu_head rcu;
+
 	struct drbd_transport transport; /* The transport needs to be the last member. The acutal
 					    implementation might have more members than the
 					    abstract one. */
@@ -1771,7 +1773,7 @@ extern enum drbd_ret_code drbd_create_device(struct drbd_config_context *adm_ctx
 extern void drbd_unregister_device(struct drbd_device *);
 extern void drbd_reclaim_device(struct rcu_head *);
 extern void drbd_unregister_connection(struct drbd_connection *);
-extern void drbd_put_connection(struct drbd_connection *);
+extern void drbd_reclaim_connection(struct rcu_head *);
 void del_connect_timer(struct drbd_connection *connection);
 
 extern struct drbd_resource *drbd_create_resource(const char *, struct res_opts *);

drbd/drbd_main.c
@@ -3422,8 +3422,6 @@ void drbd_destroy_connection(struct kref *kref)
 	idr_destroy(&connection->peer_devices);
 
 	kfree(connection->transport.net_conf);
-	drbd_put_send_buffers(connection);
-	conn_free_crypto(connection);
 	kref_debug_destroy(&connection->kref_debug);
 	kfree(connection);
 	kref_debug_put(&resource->kref_debug, 3);
@@ -3783,14 +3781,14 @@ void drbd_reclaim_device(struct rcu_head *rp)
  * drbd_unregister_connection()  -  make a connection "invisible"
  *
  * Remove the connection from the drbd object model.  Keep reference counts on
- * connection->kref; they are dropped in drbd_put_connection().
+ * connection->kref; they are dropped in drbd_reclaim_connection().
  */
 void drbd_unregister_connection(struct drbd_connection *connection)
 {
 	struct drbd_resource *resource = connection->resource;
 	struct drbd_peer_device *peer_device;
 	LIST_HEAD(work_list);
-	int vnr;
+	int vnr, rr;
 
 	spin_lock_irq(&resource->req_lock);
 	set_bit(C_UNREGISTERED, &connection->flags);
@@ -3805,20 +3803,6 @@ void drbd_unregister_connection(struct drbd_connection *connection)
 	list_for_each_entry(peer_device, &work_list, peer_devices)
 		drbd_debugfs_peer_device_cleanup(peer_device);
 	drbd_debugfs_connection_cleanup(connection);
-}
-
-void del_connect_timer(struct drbd_connection *connection)
-{
-	if (del_timer_sync(&connection->connect_timer)) {
-		kref_debug_put(&connection->kref_debug, 11);
-		kref_put(&connection->kref, drbd_destroy_connection);
-	}
-}
-
-void drbd_put_connection(struct drbd_connection *connection)
-{
-	struct drbd_peer_device *peer_device;
-	int vnr, rr;
 
 	del_connect_timer(connection);
 
@@ -3831,6 +3815,24 @@ void drbd_put_connection(struct drbd_connection *connection)
 		drbd_err(connection, "%d EEs in net list found!\n", rr);
 
 	drbd_transport_shutdown(connection, DESTROY_TRANSPORT);
+	drbd_put_send_buffers(connection);
+	conn_free_crypto(connection);
+}
+
+void del_connect_timer(struct drbd_connection *connection)
+{
+	if (del_timer_sync(&connection->connect_timer)) {
+		kref_debug_put(&connection->kref_debug, 11);
+		kref_put(&connection->kref, drbd_destroy_connection);
+	}
+}
+
+void drbd_reclaim_connection(struct rcu_head *rp)
+{
+	struct drbd_connection *connection =
+		container_of(rp, struct drbd_connection, rcu);
+	struct drbd_peer_device *peer_device;
+	int vnr;
 
 	idr_for_each_entry(&connection->peer_devices, peer_device, vnr) {
 		kref_debug_put(&connection->kref_debug, 3);

commit fd663f5e607b3cfe9c533bcf08e4bdb846f289b8

drbd: regression fix: transport refcount leak in error path
Fixes: 753f249ba0e9 drbd: switch to call_rcu() in del_connection()

which moved
        drbd_put_send_buffers(connection);
        conn_free_crypto(connection);
from drbd_destroy_connection(), and
        drbd_transport_shutdown(connection, DESTROY_TRANSPORT);
from drbd_put_connection()
into drbd_unregister_connection(),

which means we need to now unconditionally call drbd_unregister_connection(),
even in the error path of adm_new_connection().

drbd/drbd_nl.c
@@ -3475,10 +3475,8 @@ static int adm_new_connection(struct drbd_connection **ret_conn,
 unlock_fail_free_connection:
 	mutex_unlock(&adm_ctx->resource->conf_update);
 fail_free_connection:
-	if (!list_empty(&connection->connections)) {
-		drbd_unregister_connection(connection);
-		synchronize_rcu();
-	}
+	drbd_unregister_connection(connection);
+	synchronize_rcu();
 	drbd_reclaim_connection(&connection->rcu);
 fail_put_transport:
 	drbd_put_transport_class(tr_class);