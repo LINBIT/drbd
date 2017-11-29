66cc089e5c3e5f59c4b14258a8215f9e106fca32
Roland Kammerer <roland.kammerer@linbit.com>
2017-11-29 14:26:04 +0100

    compat: RHEL >= 6.8 has atomic_dec_if_positive
    This function is provided in include/linux/atomic.h

--- drbd/drbd-kernel-compat/tests/have_atomic_dec_if_positive.c
+++ drbd/drbd-kernel-compat/tests/have_atomic_dec_if_positive.c
@@ -1,5 +1,5 @@
 #include <linux/version.h>
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,37)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,37) || (defined(RHEL_RELEASE_CODE) && (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(6,8)))
 #include <linux/atomic.h>
 #else
 #include <asm/atomic.h>
