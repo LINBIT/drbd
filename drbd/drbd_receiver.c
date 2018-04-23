LINBIT/drbd

commit d987c27a74a44d1b3b28da457c5d98ec494d64dd

drbd: Automatically resolve split-brain in a specific case with quorum
Consider three nodes A, B and C, with quorum enabled. A service
runs on node B at the moment:

   A
  / \
 C---B*

First action A gets isolated from the cluster

   A

 C---B*

A lost quorum, but that does not affect anyone. Now, the application is IO
idle at the moment, so no new current-UUID is generated at B at this
point. The next action is that B gets isolted

   A

 C   B*

Immediately after becoming isolated, the application still running on B
issues a write request. B creates a new UUID, sends that to C, and sends
the write. Since no ack comes back from C B realizes that it lost quorum.
The write request will be either completed with an error or forzen
(depending on the configured quorum mode). The application terminates after
seeing the IO error.
In the next step A gets released from isolation

   A*
  /
 C   B

C and A connect. No resync as they are still on the same datase. The
two nodes have quorum, the application gets started on A. It isseus as
write request, a new current UUID is created on A. With the next
step B gets connects to A first and then to B.

   A*
  / \
 C---B

B created a new current data UUID while it was isolated but DRBD never
completed an IO within that data epoch. B also crated a new current
data UUID and potentially completed IO requests, it has quorum.

This is a split-brain between B and A.

This commit introduces automatically resolving of such split-brain
situation. The condition is that one of the two partitions has quorum
and the other partition does not have quorum.

drbd/drbd_receiver.c
@@ -3863,14 +3863,15 @@ static enum drbd_repl_state drbd_attach_handshake(struct drbd_peer_device *peer_
  * on failure.
  */
 static enum drbd_repl_state drbd_sync_handshake(struct drbd_peer_device *peer_device,
-						enum drbd_role peer_role,
-						enum drbd_disk_state peer_disk_state) __must_hold(local)
+						union drbd_state peer_state) __must_hold(local)
 {
 	struct drbd_device *device = peer_device->device;
 	struct drbd_connection *connection = peer_device->connection;
 	enum drbd_disk_state disk_state;
 	struct net_conf *nc;
 	int hg, rule_nr, rr_conflict, always_asbp, peer_node_id = 0, r;
+	enum drbd_role peer_role = peer_state.role;
+	enum drbd_disk_state peer_disk_state = peer_state.disk;
 
 	hg = drbd_handshake(peer_device, &rule_nr, &peer_node_id, true);
 
@@ -3908,16 +3909,25 @@ static enum drbd_repl_state drbd_sync_handshake(struct drbd_peer_device *peer_de
 			   + (peer_role == R_PRIMARY);
 		int forced = (hg == -100);
 
-		switch (pcount) {
-		case 0:
-			hg = drbd_asb_recover_0p(peer_device);
-			break;
-		case 1:
-			hg = drbd_asb_recover_1p(peer_device);
-			break;
-		case 2:
-			hg = drbd_asb_recover_2p(peer_device);
-			break;
+		if (device->resource->res_opts.quorum != QOU_OFF &&
+		    connection->agreed_pro_version >= 113) {
+			if (device->have_quorum[NOW] && !peer_state.quorum)
+				hg = 2;
+			else if (!device->have_quorum[NOW] && peer_state.quorum)
+				hg = -2;
+		}
+		if (hg > 2 && hg < -2) {
+			switch (pcount) {
+			case 0:
+				hg = drbd_asb_recover_0p(peer_device);
+				break;
+			case 1:
+				hg = drbd_asb_recover_1p(peer_device);
+				break;
+			case 2:
+				hg = drbd_asb_recover_2p(peer_device);
+				break;
+			}
 		}
 		if (abs(hg) < 100) {
 			drbd_warn(device, "Split-Brain detected, %d primaries, "
@@ -6446,7 +6456,7 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 				     peer_state.conn == L_STARTING_SYNC_T));
 
 		if (consider_resync) {
-			new_repl_state = drbd_sync_handshake(peer_device, peer_state.role, peer_disk_state);
+			new_repl_state = drbd_sync_handshake(peer_device, peer_state);
 		} else if (old_peer_state.conn == L_ESTABLISHED &&
 			   (peer_state.disk == D_NEGOTIATING ||
 			    old_peer_state.disk == D_NEGOTIATING)) {

drbd/drbd_state.c
@@ -830,6 +830,7 @@ union drbd_state drbd_get_device_state(struct drbd_device *device, enum which_st
 	union drbd_state rv = drbd_get_resource_state(device->resource, which);
 
 	rv.disk = device->disk_state[which];
+	rv.quorum = device->have_quorum[which];
 
 	return rv;
 }
@@ -2532,6 +2533,7 @@ static union drbd_state state_change_word(struct drbd_state_change *state_change
 	state.susp = resource_state_change->susp[which] || state_change_is_susp_quorum(state_change, which);
 	state.susp_nod = resource_state_change->susp_nod[which];
 	state.susp_fen = state_change_is_susp_fen(state_change, which);
+	state.quorum = device_state_change->have_quorum[which];
 	state.disk = device_state_change->disk_state[which];
 	if (n_connection != -1) {
 		struct drbd_connection_state_change *connection_state_change =

drbd/linux/drbd_config.h
@@ -34,7 +34,7 @@ extern const char *drbd_buildtag(void);
 
 #define REL_VERSION "9.0.12-1"
 #define PRO_VERSION_MIN 86
-#define PRO_VERSION_MAX 112
+#define PRO_VERSION_MAX 113
 
 #ifndef __CHECKER__   /* for a sparse run, we need all STATICs */
 #define DBG_ALL_SYMBOLS /* no static functs, improves quality of OOPS traces */

commit 767b582f71b0a2547b79b17e1d5c81fee84089a6

drbd: Fix recent regression, call after-split-brain-recovery handlers
Commit d987c27 introduced a regression. When a split brain could not
be automatically resovled, the after-split-brain-recovery handlers
gets no longer called.
A consequence of this is that if at lease one side is secondary while
they connect, DRBD will not do the default actions, which is to
disconnect.

drbd/drbd_receiver.c
@@ -3963,7 +3963,7 @@ static enum drbd_repl_state drbd_sync_handshake(struct drbd_peer_device *peer_de
 			else if (!device->have_quorum[NOW] && peer_state.quorum)
 				hg = -2;
 		}
-		if (hg > 2 && hg < -2) {
+		if (abs(hg) > 2) {
 			switch (pcount) {
 			case 0:
 				hg = drbd_asb_recover_0p(peer_device);