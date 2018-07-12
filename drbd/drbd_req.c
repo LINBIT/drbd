61ee37bd49e3e96057817156678fc4e8c1f67703
Philipp Reisner <philipp.reisner@linbit.com>
2018-07-12 10:09:16 +0200

    Revert "drbd: fix "Logic BUG" after P_NEG_DREPLY"
    This reverts commit 536f9b58c6f22e84ea3c7f45c084883b750adeb1.
    
    When a secondary gets an IO error from its backend it sends back a P_NEG_ACK
    and counts the op in the epoch. I.e. we need to keep it until we get the
    P_BARRIER_ACK covering it.  Before this commit it aborts the connection with
    "BAD! BarrierAck #2 received, but no epoch in tl!?"
    
    BTW, this brings the code in sync with drbd-8.4 again.  The commit message
    of the reverted commit gives no info of the nature of the bug that was
    inteded to get fixed.  So, I assume that the commint I am reverting was
    simply wrong.

--- drbd/drbd_req.c
+++ drbd/drbd_req.c
@@ -1054,7 +1054,7 @@ int __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		break;
 
 	case NEG_ACKED:
-		mod_rq_state(req, m, peer_device, RQ_NET_OK|RQ_NET_PENDING, RQ_NET_DONE);
+		mod_rq_state(req, m, peer_device, RQ_NET_OK|RQ_NET_PENDING, 0);
 		break;
 
 	case COMPLETION_RESUMED:
