d9721c833c265e6d1ca17b230833eb800bd5f60a
Joel Colledge <joel.colledge@linbit.com>
2019-02-25 10:47:25 +0100

    drbd: Add journal source file

---
+++ drbd/drbd_journal.c
@@ -0,0 +1,26 @@
+/*
+   drbd_journal.c
+
+   This file is part of DRBD by Philipp Reisner and Lars Ellenberg.
+
+   Copyright (C) 2004-2019, LINBIT Information Technologies GmbH.
+   Copyright (C) 2004-2019, Philipp Reisner <philipp.reisner@linbit.com>.
+   Copyright (C) 2004-2019, Lars Ellenberg <lars.ellenberg@linbit.com>.
+   Copyright (C) 2019, Joel Colledge <joel.colledge@linbit.com>.
+
+   drbd is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   drbd is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with drbd; see the file COPYING.  If not, write to
+   the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "drbd_int.h"
