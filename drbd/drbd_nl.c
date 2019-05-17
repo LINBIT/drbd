d5d1ee9a659b29fd0e25b1b8fb3514b4b3b35d4b
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2019-05-17 14:11:24 +0200

    drbd: remove NETLINK_CB_PORTID compat macro
    The macro was used to have backwards compatibility with the .portid (formerly
    .pid) struct member, which was renamed in the upstream kernel. This is now
    handled by a coccinelle patch, so don't use the macro anymore.

--- drbd/drbd_nl.c
+++ drbd/drbd_nl.c
@@ -5021,7 +5021,7 @@ found_resource:
 	goto out;
 
 put_result:
-	dh = genlmsg_put(skb, NETLINK_CB_PORTID(cb->skb),
+	dh = genlmsg_put(skb, NETLINK_CB(cb->skb).portid,
 			cb->nlh->nlmsg_seq, &drbd_genl_family,
 			NLM_F_MULTI, DRBD_ADM_GET_RESOURCES);
 	err = -ENOMEM;
@@ -5150,7 +5150,7 @@ int drbd_adm_dump_devices(struct sk_buff *skb, struct netlink_callback *cb)
 	goto out;  /* no more devices */
 
 put_result:
-	dh = genlmsg_put(skb, NETLINK_CB_PORTID(cb->skb),
+	dh = genlmsg_put(skb, NETLINK_CB(cb->skb).portid,
 			cb->nlh->nlmsg_seq, &drbd_genl_family,
 			NLM_F_MULTI, DRBD_ADM_GET_DEVICES);
 	err = -ENOMEM;
@@ -5311,7 +5311,7 @@ found_resource:
 	goto out;  /* no more resources */
 
 put_result:
-	dh = genlmsg_put(skb, NETLINK_CB_PORTID(cb->skb),
+	dh = genlmsg_put(skb, NETLINK_CB(cb->skb).portid,
 			cb->nlh->nlmsg_seq, &drbd_genl_family,
 			NLM_F_MULTI, DRBD_ADM_GET_CONNECTIONS);
 	err = -ENOMEM;
@@ -5484,7 +5484,7 @@ found_peer_device:
 	goto next_device;
 
 put_result:
-	dh = genlmsg_put(skb, NETLINK_CB_PORTID(cb->skb),
+	dh = genlmsg_put(skb, NETLINK_CB(cb->skb).portid,
 			cb->nlh->nlmsg_seq, &drbd_genl_family,
 			NLM_F_MULTI, DRBD_ADM_GET_PEER_DEVICES);
 	err = -ENOMEM;
