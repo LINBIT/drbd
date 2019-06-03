8f069130197c6a81e188a839c03d400e3c75d791
Roland Kammerer <roland.kammerer@linbit.com>
2019-06-03 13:34:18 +0200

    genpatchnames for coccinelle/spatch based compat infrastructure
    genpatchnames includes a compat.h and outputs the necessary
    semantic patches (.cocci files) to apply.

---
+++ drbd/drbd-kernel-compat/gen_patch_names.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <linux/limits.h>
+#include "compat.h"
+
+#define SEP "_"
+
+#define ARRAY_LEN(a) (sizeof(a)/sizeof((a))[0])
+
+#define __append(s) do {						\
+	if (ARRAY_LEN(buf) - strlen(buf) < strlen(s) + 1) {		\
+		fprintf(stderr, "BUFSIZE too small, aborting\n");	\
+		exit(1);						\
+	}								\
+	strcat(buf, s);							\
+} while(0)
+
+/* this is the tricky part:
+ * if a define is defined but empty (e.g., #define X), it is stringified: ""
+ * if a define is not defined, we get the stringified name of the macro: "X"
+ * This would produce false output if something is actually defined to a value (e.g., #define X foo)
+ * our compat.h fulfils these properties, but use that machinery with care!
+ * This is as good as it gets with cpp, m4 FTW ;-) */
+#define _append(d,n) do {		\
+	if (strlen(#d) == 0) {		\
+		__append("yes" SEP);	\
+		all_no = false;		\
+	} else {			\
+		__append("no" SEP);	\
+		all_yes = false;	\
+	}				\
+	__append(n);			\
+} while(0)
+
+#define append1(d1,n1)			do {_append(d1,n1);} while(0)
+#define append2(d1,n1,d2,n2)		do {_append(d1,n1); __append(SEP SEP); _append(d2,n2);} while(0)
+#define append3(d1,n1,d2,n2,d3,n3)	do {_append(d1,n1); __append(SEP SEP); _append(d2,n2); __append(SEP SEP); _append(d3,n3);} while(0)
+
+/* iay == IgnoreAllYes, do not generate output if all defines are defined
+ * ian == IgnoreAllNo, do not generate output if none of the defines is defined */
+#define patch(n,name,iay,ian,...) do {			\
+	bool all_yes = true, all_no = true;             \
+	buf[0] = '\0';                                  \
+	__append(name SEP SEP);				\
+	append##n(__VA_ARGS__);				\
+	if (! ((iay && all_yes) || (ian && all_no)) ) {	\
+		for (int i = 0; i < strlen(buf); i++)   \
+			buf[i] = tolower(buf[i]);       \
+		printf("%s\n", buf);                    \
+	}						\
+} while(0)
+
+int main(int argc, char **argv)
+{
+	/* shared buffer */
+	char buf[PATH_MAX] = {0};
+
+	/* ONLY TRUE */
+	/* patch(1, "footrue", false, true, FOO, "foo"); */
+
+	/* ONLY FALSE */
+	/* patch(1, "foofalse", true, false, FOO, "foo"); */
+
+	/* BOTH CASES */
+	/* patch(1, "fooboth", false, false, FOO, "foo"); */
+
+	/* we have nothing, need all of them */
+	/* patch(2, "none", false, false, */
+	/* 		COMPAT_BLKDEV_ISSUE_ZEROOUT_DISCARD, "discard", */
+	/* 		COMPAT_BLKDEV_ISSUE_ZEROOUT_BLKDEV_IFL_WAIT, "ifl_wait"); */
+	/* #<{(| we have all of this, need none them |)}># */
+	/* patch(2, "all", false, false, */
+	/* 		COMPAT_DRBD_RELEASE_RETURNS_VOID, "returns_void", */
+	/* 		COMPAT_HAVE_AHASH_REQUEST_ON_STACK, "req_on_stack"); */
+	/* patch(2, "2nd_feature", false, false, */
+	/* 		COMPAT_HAVE_ATOMIC_IN_FLIGHT, "atomic_in_flight", */
+	/* 		COMPAT_HAVE_BD_CLAIM_BY_DISK, "bd_claim_by_disk"); */
+
+	/* patch(1, "block_device_operations_release", true, false,
+	   COMPAT_DRBD_RELEASE_RETURNS_VOID, "is_void"); */
+
+/* #if !defined(COMPAT_HAVE_BD_UNLINK_DISK_HOLDER) || defined(COMPAT_HAVE_BD_CLAIM_BY)
+	patch(2, "claim_disk", true, false,
+	      COMPAT_HAVE_BD_UNLINK_DISK_HOLDER, "link",
+	      COMPAT_HAVE_BD_CLAIM_BY_DISK, "claim");
+#endif */
+
+/* #define BLKDEV_ISSUE_ZEROOUT_EXPORTED */
+/* #define BLKDEV_ZERO_NOUNMAP */
+
+// #ifndef BLKDEV_ISSUE_ZEROOUT_EXPORTED
+/* Was introduced with 2.6.34 */
+//	patch(1, "zeroout", false, false,
+//			BLKDEV_ISSUE_ZEROOUT_EXPORTED, "exported");
+//#else
+/* synopsis changed a few times, though */
+//#if  defined(BLKDEV_ZERO_NOUNMAP)
+/* >= v4.12 */
+/* use blkdev_issue_zeroout() as written out in the actual source code.
+ * right now, we only use it with flags = BLKDEV_ZERO_NOUNMAP */
+
+/* no op */
+//#else
+//	patch(2, "zeroout", false, false,
+//			COMPAT_BLKDEV_ISSUE_ZEROOUT_BLKDEV_IFL_WAIT, "ifl_wait",
+//		  			COMPAT_BLKDEV_ISSUE_ZEROOUT_DISCARD, "discard");
+//#endif
+//#endif
+
+	return 0;
+}
