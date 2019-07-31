4ff94aff559ba0a404dd38dfdce5c1dc4876c72a
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2019-07-31 09:42:22 +0200

    compat: remove compat code for blk_queue_merge_bvec
    The whole concept of blk_queue_merge_bvec was removed in upstream at
    some point, so now our codebase is cluttered with "mostly dead" code
    and ifdefs to go with it.
    
    To handle this more nicely, a compat patch is introduced that adds the
    code in question when it is needed.

--- drbd/drbd_main.c
+++ drbd/drbd_main.c
@@ -3744,9 +3744,6 @@ enum drbd_ret_code drbd_create_device(struct drbd_config_context *adm_ctx, unsig
 
 	blk_queue_make_request(q, drbd_make_request);
 	blk_queue_write_cache(q, true, true);
-#ifdef COMPAT_HAVE_BLK_QUEUE_MERGE_BVEC
-	blk_queue_merge_bvec(q, drbd_merge_bvec);
-#endif
 #ifdef blk_queue_plugged
 	q->queue_lock = &resource->req_lock; /* needed since we use */
 	/* plugging on a queue, that actually has no requests! */
--- drbd/drbd_req.c
+++ drbd/drbd_req.c
@@ -2308,43 +2308,6 @@ blk_qc_t drbd_make_request(struct request_queue *q, struct bio *bio)
 	return BLK_QC_T_NONE;
 }
 
-/* This is called by bio_add_page().
- *
- * q->max_hw_sectors and other global limits are already enforced there.
- *
- * We need to call down to our lower level device,
- * in case it has special restrictions.
- *
- * As long as the BIO is empty we have to allow at least one bvec,
- * regardless of size and offset, so no need to ask lower levels.
- */
-#ifdef COMPAT_HAVE_BLK_QUEUE_MERGE_BVEC
-int drbd_merge_bvec(struct request_queue *q,
-		struct bvec_merge_data *bvm,
-		struct bio_vec *bvec)
-{
-	struct drbd_device *device = (struct drbd_device *) q->queuedata;
-	unsigned int bio_size = bvm->bi_size;
-	int limit = DRBD_MAX_BIO_SIZE;
-	int backing_limit;
-
-	if (bio_size && get_ldev(device)) {
-		unsigned int max_hw_sectors = queue_max_hw_sectors(q);
-		struct request_queue * const b =
-			device->ldev->backing_bdev->bd_disk->queue;
-		if (b->merge_bvec_fn) {
-			bvm->bi_bdev = device->ldev->backing_bdev;
-			backing_limit = b->merge_bvec_fn(b, bvm, bvec);
-			limit = min(limit, backing_limit);
-		}
-		put_ldev(device);
-		if ((limit >> 9) > max_hw_sectors)
-			limit = max_hw_sectors << 9;
-	}
-	return limit;
-}
-#endif
-
 static unsigned long time_min_in_future(unsigned long now,
 		unsigned long t1, unsigned long t2)
 {
