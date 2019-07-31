39bfa343dcd20564d73602e42eea3586c67e7517
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2019-07-31 09:39:42 +0200

    compat: drop compatibility code for __acquires
    __acquires has been around since at least v2.6.12, and the commit
    introducing our compat code for it was made in 2008. Thus, drop
    compatibility for these ancient kernels.

--- drbd/drbd_int.h
+++ drbd/drbd_int.h
@@ -69,21 +69,6 @@
 # define __must_hold(x)
 #endif
 
-/* Compatibility for older kernels */
-#ifndef __acquires
-# ifdef __CHECKER__
-#  define __acquires(x)	__attribute__((context(x,0,1)))
-#  define __releases(x)	__attribute__((context(x,1,0)))
-#  define __acquire(x)	__context__(x,1)
-#  define __release(x)	__context__(x,-1)
-# else
-#  define __acquires(x)
-#  define __releases(x)
-#  define __acquire(x)	(void)0
-#  define __release(x)	(void)0
-# endif
-#endif
-
 /* module parameter, defined in drbd_main.c */
 extern unsigned int drbd_minor_count;
 extern unsigned int drbd_protocol_version_min;
