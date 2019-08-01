73fc3bef59f089a90b1d89ac882d07b26348e08d
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2019-08-01 10:47:11 +0200

    new test: have_req_op_write_same
    Also test for an intermediate state, where the flag was called
    REQ_WRITE_SAME instead of REQ_OP_WRITE_SAME.

---
+++ drbd/drbd-kernel-compat/tests/have_req_op_write_same.c
@@ -0,0 +1,4 @@
+/* {"version": "4.10", "commit": "ef295ecf090d3e86e5b742fc6ab34f1122a43773", "comment": "REQ_WRITE_SAME got renamed to REQ_OP_WRITE_SAME", "author": "Christoph Hellwig <hch@lst.de>", "date": "Fri Oct 28 08:48:16 2016 -0600"} */
+#include <linux/blk_types.h>
+
+enum req_opf dummy = REQ_OP_WRITE_SAME;
