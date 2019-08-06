ed8c87518a17a1a06540eef2668c568e514a42fb
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2019-08-06 11:30:06 +0200

    compat test and coccinelle patch for REQ_PRIO

--- drbd/drbd-kernel-compat/gen_patch_names.c
+++ drbd/drbd-kernel-compat/gen_patch_names.c
@@ -245,6 +245,9 @@ int main(int argc, char **argv)
 	patch(1, "struct_kernel_param_ops", true, false,
 	      COMPAT_HAVE_STRUCT_KERNEL_PARAM_OPS, "present");
 
+	patch(1, "req_prio", true, false,
+	      COMPAT_HAVE_REQ_PRIO, "present");
+
 /* #define BLKDEV_ISSUE_ZEROOUT_EXPORTED */
 /* #define BLKDEV_ZERO_NOUNMAP */
 
