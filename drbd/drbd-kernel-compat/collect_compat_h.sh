71e57ae63ca1c1fdb6f9f20cd88f47bf4e96f9d6
Philipp Reisner <philipp.reisner@linbit.com>
2019-08-06 11:00:06 +0200

    Improve collect_compat_h.sh to not touch hashed dirs that do not need to change
    Since spatch runs are quite expensive in terms of runtime, collect_compat_h.sh
    should be more carefull to not generate unnecessary work.
    
    Leave hashed dirs in place when the compat.h did not change. With that it also
    preserve the possibly existing compat.patch.

--- drbd/drbd-kernel-compat/collect_compat_h.sh
+++ drbd/drbd-kernel-compat/collect_compat_h.sh
@@ -6,6 +6,7 @@
 #
 N_CONFIGS=0
 N_UNIQUE=0
+N_PRESERVED=0
 
 if [ "$(uname -n)" = "thank" ]; then
 	FILES=$((cd /home/lbbuild/lbbuild/localpkgs/drbd-9-compat-latest; find . -name "compat.h*" \
@@ -22,14 +23,29 @@ else
 fi
 
 
-rm -rf cocci_cache/*
+mkdir -p cocci_cache.previous
+for F in cocci_cache/*; do
+    test -d $F && mv $F cocci_cache.previous/
+done
 
 for F in $FILES; do
+    PREV=false
+    N_CONFIGS=$((N_CONFIGS + 1))
     MD5SUM_OUTPUT=$(md5sum $F)
     MD5SUM=${MD5SUM_OUTPUT%% *}
+
+    if test -e cocci_cache.previous/$MD5SUM; then
+	mv cocci_cache.previous/$MD5SUM cocci_cache/
+	PREV=true
+	# Maybe we can preserves a compat.patch
+	N_PRESERVED=$((N_PRESERVED + 1))
+	N_UNIQUE=$((N_UNIQUE + 1))
+    fi
+
     if test ! -e cocci_cache/$MD5SUM; then
 	mkdir cocci_cache/$MD5SUM
 	mv $F cocci_cache/$MD5SUM/compat.h
+	NEW_MD5SUMS="$NEW_MD5SUMS $MD5SUM"
 	N_UNIQUE=$((N_UNIQUE + 1))
     else
 	rm $F
@@ -42,6 +58,11 @@ for F in $FILES; do
 	D="${D%/*}"
     done
 
+    if $PREV; then
+	# Leave kernrelease.txt untouched
+	continue
+    fi
+
     KERNELRELEASE=${F#*compat.h.}
     if test -e cocci_cache/$MD5SUM/kernelrelease.txt; then
 	SEP=" "
@@ -51,13 +72,19 @@ for F in $FILES; do
     echo -n "$SEP$KERNELRELEASE" >> cocci_cache/$MD5SUM/kernelrelease.txt
 
     # Progress
-    N_CONFIGS=$((N_CONFIGS + 1))
     printf "%3d %3d %-60s\r" $N_CONFIGS $N_UNIQUE $F
 done
 
 # Trailing newline for kernelrelease.txt
-for F in cocci_cache/*/kernelrelease.txt; do
+for MD5SUM in $NEW_MD5SUMS; do
+    F=cocci_cache/$MD5SUM/kernelrelease.txt
     echo "" >> $F
 done
 
-printf "%3d config.h processed, %d unique found                   \n" $N_CONFIGS $N_UNIQUE
+shopt -s nullglob
+REMOVED_A=(cocci_cache.previous/*)
+N_REMOVED=${#REMOVED_A[@]}
+rm -rf cocci_cache.previous
+
+printf "%3d config.h processed, %d unique found, (%d preserved, %d removed)      \n" \
+       $N_CONFIGS $N_UNIQUE $N_PRESERVED $N_REMOVED
