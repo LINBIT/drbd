8d3a731a180f0c34f17dab23994567df8f479eae
Philipp Reisner <philipp.reisner@linbit.com>
2019-09-04 09:10:19 +0200

    drbd: mark peers with a dedicated flag we have a bitmap for
    The new flag is called MDF_HAVE_BITMAP. Nearly all of that patch
    was generated by spatch/coccinelle. Only changes in drbd_md_encode(),
    drbd_md_decode(), free_bitmap_index() and allocate_bitmap_index()
    are human made.
    
    @@
    struct drbd_peer_md *peer_md;
    int i;
    @@
    -peer_md[i].bitmap_index != -1
    +peer_md[i].flags & MDF_HAVE_BITMAP
    
    @@
    struct drbd_peer_md *peer_md;
    int i;
    @@
    -peer_md[i].bitmap_index == -1
    +!(peer_md[i].flags & MDF_HAVE_BITMAP)
    
    @@
    struct drbd_peer_md *peer_md;
    @@
    -peer_md->bitmap_index == -1
    +!(peer_md->flags & MDF_HAVE_BITMAP)
    
    @@
    struct drbd_peer_md *peer_md;
    @@
    -peer_md->bitmap_index != -1
    +peer_md->flags & MDF_HAVE_BITMAP
    
    @@
    struct drbd_md *md;
    int i;
    @@
    -md->peers[i].bitmap_index == -1
    +!(md->peers[i].flags & MDF_HAVE_BITMAP)
    
    @@
    struct drbd_md *md;
    int i;
    @@
    -md->peers[i].bitmap_index != -1
    +md->peers[i].flags & MDF_HAVE_BITMAP

--- drbd/drbd_state.c
+++ drbd/drbd_state.c
@@ -140,7 +140,7 @@ static bool may_be_up_to_date(struct drbd_device *device, enum which_state which
 		if (node_id == device->ldev->md.node_id)
 			continue;
 
-		if (peer_md->bitmap_index == -1 && !(peer_md->flags & MDF_NODE_EXISTS))
+		if (!(peer_md->flags & MDF_HAVE_BITMAP) && !(peer_md->flags & MDF_NODE_EXISTS))
 			continue;
 
 		if (!(peer_md->flags & MDF_PEER_FENCING))
@@ -1317,7 +1317,7 @@ static void __calc_quorum_with_disk(struct drbd_device *device, struct quorum_de
 			}
 		}
 
-		if (peer_md->bitmap_index == -1 && !(peer_md->flags & MDF_NODE_EXISTS) &&
+		if (!(peer_md->flags & MDF_HAVE_BITMAP) && !(peer_md->flags & MDF_NODE_EXISTS) &&
 		    !is_intentional_diskless) {
 			continue;
 		}
