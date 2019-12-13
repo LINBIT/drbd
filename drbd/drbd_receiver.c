464c8ccf8829e39d03160e4f4c7f613ffaba9f77
Philipp Reisner <philipp.reisner@linbit.com>
2019-12-13 12:59:10 +0100

    drbd: Fix order/priority in uuid_compare()
    This again somehting that was correct in the drbd-8.4 time, but is no longer
    a fit in the drbd-9 (more than two nodes) world.
    
    Becoming Sync-source (with bitmap) has to have precendence over sync-target
    (set all bits).

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -3919,14 +3919,6 @@ static enum sync_strategy drbd_uuid_compare(struct drbd_peer_device *peer_device
 			return rv;
 	}
 
-	*rule_nr = 60;
-	self = drbd_current_uuid(device) & ~UUID_PRIMARY;
-	for (i = 0; i < ARRAY_SIZE(peer_device->history_uuids); i++) {
-		peer = peer_device->history_uuids[i] & ~UUID_PRIMARY;
-		if (self == peer)
-			return SYNC_TARGET_SET_BITMAP;
-	}
-
 	*rule_nr = 70;
 	self = drbd_bitmap_uuid(peer_device) & ~UUID_PRIMARY;
 	peer = peer_device->current_uuid & ~UUID_PRIMARY;
@@ -3947,6 +3939,14 @@ static enum sync_strategy drbd_uuid_compare(struct drbd_peer_device *peer_device
 			*peer_node_id = i;
 			return SYNC_SOURCE_COPY_BITMAP;
 		}
+	}
+
+	*rule_nr = 60;
+	self = drbd_current_uuid(device) & ~UUID_PRIMARY;
+	for (i = 0; i < ARRAY_SIZE(peer_device->history_uuids); i++) {
+		peer = peer_device->history_uuids[i] & ~UUID_PRIMARY;
+		if (self == peer)
+			return SYNC_TARGET_SET_BITMAP;
 	}
 
 	if (connection->agreed_pro_version < 110) {
