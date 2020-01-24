2e90f3b4f249afcdde4efa88704c9c299e12be88
Stephen Kitt <steve@sk2.org>
2020-01-24 21:03:07 +0100

    drbd: fifo_alloc() should use struct_size
    Switching to struct_size for the allocation in fifo_alloc avoids
    hard-coding the type of fifo_buffer.values in fifo_alloc. It also
    provides overflow protection; to avoid pessimistic code being
    generated by the compiler as a result, this patch also switches
    fifo_size to unsigned, propagating the change as appropriate.
    
    Reviewed-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Signed-off-by: Stephen Kitt <steve@sk2.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -4562,7 +4562,7 @@ static int receive_SyncParam(struct drbd_connection *connection, struct packet_i
 	const int apv = connection->agreed_pro_version;
 	struct fifo_buffer *old_plan = NULL, *new_plan = NULL;
 	struct drbd_resource *resource = connection->resource;
-	int fifo_size = 0;
+	unsigned int fifo_size = 0;
 	int err;
 
 	peer_device = conn_peer_device(connection, pi->vnr);
