LINBIT/drbd

commit 91e95a7cb5c82176b0c8b5f484a675051b033705

drbd: Avoid false split brains by adding peer's bitmaps slots to own history
It is possible to produce cases where the only place a UUID is
is in the bitmap-uuids of the peers.

An example to trigger the issue:
   3 nodes: A, B, C. (A = Primary, Quorum enabled)
   Down C (gracefully)
   Down B (gracefully, allows A to keep quorum due to "Last man standing"
   Let C resync to A
   Connect B to C and see a split brain.

Fix that problem by adding such UUIDs to the history as soon as we receive
them.

drbd/drbd_main.c
@@ -4496,11 +4496,22 @@ void drbd_md_mark_dirty(struct drbd_device *device)
 void _drbd_uuid_push_history(struct drbd_device *device, u64 val) __must_hold(local)
 {
 	struct drbd_md *md = &device->ldev->md;
-	int i;
+	int node_id, i;
 
 	if (val == UUID_JUST_CREATED)
 		return;
-	val &= ~1;  /* The lowest bit only indicates that the node was primary */
+
+	val &= ~UUID_PRIMARY;
+
+	if (val == (md->current_uuid & ~UUID_PRIMARY))
+		return;
+
+	for (node_id = 0; node_id < DRBD_NODE_ID_MAX; node_id++) {
+		if (node_id == md->node_id)
+			continue;
+		if (val == (md->peers[node_id].bitmap_uuid & ~UUID_PRIMARY))
+			return;
+	}
 
 	for (i = 0; i < ARRAY_SIZE(md->history_uuids); i++) {
 		if (md->history_uuids[i] == val)

drbd/drbd_receiver.c
@@ -5168,6 +5168,8 @@ static void drbd_resync(struct drbd_peer_device *peer_device,
 
 static void update_bitmap_slot_of_peer(struct drbd_peer_device *peer_device, int node_id, u64 bitmap_uuid)
 {
+	struct drbd_device *device = peer_device->device;
+
 	if (peer_device->bitmap_uuids[node_id] && bitmap_uuid == 0) {
 		/* If we learn from a neighbor that it no longer has a bitmap
 		   against a third node, we need to deduce from that knowledge
@@ -5182,6 +5184,11 @@ static void update_bitmap_slot_of_peer(struct drbd_peer_device *peer_device, int
 			peer_device2->bitmap_uuids[node_id2] = 0;
 		}
 		rcu_read_unlock();
+	}
+
+	if (bitmap_uuid != -1 && get_ldev(device)) {
+		_drbd_uuid_push_history(device, bitmap_uuid);
+		put_ldev(device);
 	}
 	peer_device->bitmap_uuids[node_id] = bitmap_uuid;
 }

commit b4b9bf6f62ced77e090e7f443a7bdae65e16bd35

drbd: Fix pushing bitmap UUID into history when changed
91e95a7cb5c82176b0c8b5f484a675051b033705 introduced a regression that
caused an unexpected split-brain disconnection. The issue was that the
bitmap UUID was updated after pushing the old one into the history.
Since the mentioned commit, UUIDs are not added to the history if they
are in a bitmap UUID slot. As a result, the old bitmap UUID was not
added to the history.

To reproduce the issue with 3 nodes, first arrange for data generations:
* Newest data on A
* Older data on B
* Oldest data on C (but not the initial data generation)
Then:
* Connect A to B
* Connect B to C

C should successfully sync from B. The issue causes a split-brain
disconnection.

drbd/drbd_main.c
@@ -4939,11 +4939,12 @@ static u64 __set_bitmap_slots(struct drbd_device *device, u64 bitmap_uuid, u64 d
 		if (!(peer_md[node_id].flags & MDF_HAVE_BITMAP))
 			continue;
 		if (peer_md[node_id].bitmap_uuid != bitmap_uuid) {
-			_drbd_uuid_push_history(device, peer_md[node_id].bitmap_uuid);
+			u64 previous_bitmap_uuid = peer_md[node_id].bitmap_uuid;
 			/* drbd_info(device, "XXX bitmap[node_id=%d] = %llX\n", node_id, bitmap_uuid); */
 			peer_md[node_id].bitmap_uuid = bitmap_uuid;
 			peer_md[node_id].bitmap_dagtag =
 				bitmap_uuid ? device->resource->dagtag_sector : 0;
+			_drbd_uuid_push_history(device, previous_bitmap_uuid);
 			drbd_md_mark_dirty(device);
 			modified |= NODE_MASK(node_id);
 		}