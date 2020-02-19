LINBIT/drbd

commit d184d3b32675693653d6a04f7a59b3067c725159

drbd: Improve logging of drbd_khelper()
Make it visible in the logs if the helper was called on an device, connection
or for an peer_device.

drbd/drbd_nl.c
@@ -485,10 +485,20 @@ static char **make_envp(struct env *env)
 	return NULL;
 }
 
+/* Macro refers to local variables peer_device, device and connection! */
+#define magic_printk(level, fmt, args...)				\
+	if (peer_device)						\
+		__drbd_printk_peer_device(level, peer_device, fmt, args); \
+	else if (device)						\
+		__drbd_printk_device(level, device, fmt, args);		\
+	else								\
+		__drbd_printk_connection(level, connection, fmt, args);
+
 int drbd_khelper(struct drbd_device *device, struct drbd_connection *connection, char *cmd)
 {
 	struct drbd_resource *resource = device ? device->resource : connection->resource;
 	char *argv[] = {usermode_helper, cmd, resource->name, NULL };
+	struct drbd_peer_device *peer_device = NULL;
 	struct env env = { .size = PAGE_SIZE };
 	char **envp;
 	int ret;
@@ -561,17 +571,16 @@ int drbd_khelper(struct drbd_device *device, struct drbd_connection *connection,
 	else if (connection)
 		conn_md_sync(connection);
 
-	drbd_info(resource, "helper command: %s %s\n", usermode_helper, cmd);
+	if (connection && device)
+		peer_device = conn_peer_device(connection, device->vnr);
+
+	magic_printk(KERN_INFO, "helper command: %s %s\n", usermode_helper, cmd);
 	notify_helper(NOTIFY_CALL, device, connection, cmd, 0);
 	ret = call_usermodehelper(usermode_helper, argv, envp, UMH_WAIT_PROC);
-	if (ret)
-		drbd_warn(resource, "helper command: %s %s exit code %u (0x%x)\n",
-				usermode_helper, cmd,
-				(ret >> 8) & 0xff, ret);
-	else
-		drbd_info(resource, "helper command: %s %s exit code %u (0x%x)\n",
-				usermode_helper, cmd,
-				(ret >> 8) & 0xff, ret);
+	magic_printk(ret ? KERN_WARNING : KERN_INFO,
+		     "helper command: %s %s exit code %u (0x%x)\n",
+		     usermode_helper, cmd,
+		     (ret >> 8) & 0xff, ret);
 	notify_helper(NOTIFY_RESPONSE, device, connection, cmd, ret);
 
 	if (current == resource->worker.task)
@@ -588,6 +597,8 @@ int drbd_khelper(struct drbd_device *device, struct drbd_connection *connection,
 		 "out of memory\n", cmd, ret);
 	return 0;
 }
+
+#undef magic_printk
 
 static bool initial_states_pending(struct drbd_connection *connection)
 {

commit da0c245168e9fa2291dfc13c6bb20ab73b2aa53c

drbd: Fix logging when usermode helper fails
An unfortunate combination of the ternary operator and macro expansion
caused the log message to be lost whenever the usermode helper returned
a non-zero code. Basically, the macros concatentate the level with the
format string. This means that, when the level includes the ternary
operator and the conditional evaluates to the first argument, only the
level is used as the actual format string passed to printk. Only when
the conditional evaluates to the second argument, is the specified
format string actually passed through.

Fixes a regression in d184d3b32675693653d6a04f7a59b3067c725159.

drbd/drbd_nl.c
@@ -572,12 +572,14 @@ static char **make_envp(struct env *env)
 
 /* Macro refers to local variables peer_device, device and connection! */
 #define magic_printk(level, fmt, args...)				\
-	if (peer_device)						\
-		drbd_printk(level, peer_device, fmt, args);		\
-	else if (device)						\
-		drbd_printk(level, device, fmt, args);			\
-	else								\
-		drbd_printk(level, connection, fmt, args);
+	do {								\
+		if (peer_device)					\
+			drbd_printk(level, peer_device, fmt, args);	\
+		else if (device)					\
+			drbd_printk(level, device, fmt, args);		\
+		else							\
+			drbd_printk(level, connection, fmt, args);	\
+	} while (0)
 
 static int drbd_khelper(struct drbd_device *device, struct drbd_connection *connection, char *cmd)
 {
@@ -688,10 +690,15 @@ static int drbd_khelper(struct drbd_device *device, struct drbd_connection *conn
 	magic_printk(KERN_INFO, "helper command: %s %s\n", drbd_usermode_helper, cmd);
 	notify_helper(NOTIFY_CALL, device, connection, cmd, 0);
 	ret = call_usermodehelper(drbd_usermode_helper, argv, envp, UMH_WAIT_PROC);
-	magic_printk(ret ? KERN_WARNING : KERN_INFO,
-		     "helper command: %s %s exit code %u (0x%x)\n",
-		     drbd_usermode_helper, cmd,
-		     (ret >> 8) & 0xff, ret);
+	if (ret)
+		magic_printk(KERN_WARNING,
+			     "helper command: %s %s exit code %u (0x%x)\n",
+			     drbd_usermode_helper, cmd,
+			     (ret >> 8) & 0xff, ret);
+	else
+		magic_printk(KERN_INFO,
+			     "helper command: %s %s exit code 0\n",
+			     drbd_usermode_helper, cmd);
 	notify_helper(NOTIFY_RESPONSE, device, connection, cmd, ret);
 
 	if (current == resource->worker.task)