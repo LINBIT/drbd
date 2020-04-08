LINBIT/drbd

commit 9f19be58a3349e900adc3890ec654c2ea8b82cb6

drbd: Lock UUIDs between sending and comparing to the peer's version
There is at least one fundamental problem with retry connect upon
UUIDs/flags change between send and receive. A node might alter its
state based on such outdated data, before the fact that the data
was outdated is detected. Worse, only one of the nodes detects the fact.

Right now it only gets locked between the initial UUID packet and when
that gets received.

With that patch, the comm_current_uuid and comm_uuid_flags fields and
associated checks become redundant. I leave them in for now to have
it double checked. Might be removed later.

drbd/drbd_receiver.c
@@ -675,8 +675,12 @@ int drbd_connected(struct drbd_peer_device *peer_device)
 	err = drbd_send_sync_param(peer_device);
 	if (!err)
 		err = drbd_send_sizes(peer_device, 0, 0);
-	if (!err)
+	if (!err) {
+		down_read(&device->uuid_sem);
+		set_bit(HOLDING_UUID_READ_LOCK, &peer_device->flags);
+
 		err = drbd_send_uuids(peer_device, 0, weak_nodes);
+	}
 	if (!err) {
 		set_bit(INITIAL_STATE_SENT, &peer_device->flags);
 		err = drbd_send_current_state(peer_device);
@@ -6917,6 +6921,11 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 		peer_disk_state = D_CONSISTENT;
 	}
 
+	/* This is after the point where we did UUID comparison and joined with the
+	   diskless case again. Releasing uuid_sem here */
+	if (test_and_clear_bit(HOLDING_UUID_READ_LOCK, &peer_device->flags))
+		up_read(&device->uuid_sem);
+
 	spin_lock_irq(&resource->req_lock);
 	begin_state_change_locked(resource, begin_state_chg_flags);
 	if (old_peer_state.i != drbd_get_peer_device_state(peer_device, NOW).i) {
@@ -7805,6 +7814,9 @@ static void drain_resync_activity(struct drbd_connection *connection)
 static void peer_device_disconnected(struct drbd_peer_device *peer_device)
 {
 	struct drbd_device *device = peer_device->device;
+
+	if (test_and_clear_bit(HOLDING_UUID_READ_LOCK, &peer_device->flags))
+		up_read(&device->uuid_sem);
 
 	/* need to do it again, drbd_finish_peer_reqs() may have populated it
 	 * again via drbd_try_clear_on_disk_bm(). */

drbd/drbd_int.h
@@ -561,6 +561,7 @@ enum peer_device_flag {
 				   D_UP_TO_DATE before becoming secondary! */
 	AHEAD_TO_SYNC_SOURCE,   /* Ahead -> SyncSource queued */
 	SYNC_TARGET_TO_BEHIND,  /* SyncTarget, wait for Behind */
+	HOLDING_UUID_READ_LOCK, /* did a down_read(&device->uuid_sem) */
 };
 
 /* We could make these currently hardcoded constants configurable
@@ -1312,6 +1313,7 @@ struct drbd_device {
 	wait_queue_head_t seq_wait;
 	u64 exposed_data_uuid; /* UUID of the exposed data */
 	u64 next_exposed_data_uuid;
+	struct rw_semaphore uuid_sem;
 	atomic_t rs_sect_ev; /* for submitted resync data rate, both */
 	struct pending_bitmap_work_s {
 		atomic_t n;		/* inc when queued here, */

drbd/drbd_main.c
@@ -3653,7 +3653,6 @@ enum drbd_ret_code drbd_create_device(struct drbd_config_context *adm_ctx, unsig
 	device->minor = minor;
 	device->vnr = vnr;
 	device->device_conf = *device_conf;
-
 #ifdef PARANOIA
 	SET_MDEV_MAGIC(device);
 #endif
@@ -3690,6 +3689,8 @@ enum drbd_ret_code drbd_create_device(struct drbd_config_context *adm_ctx, unsig
 	init_waitqueue_head(&device->misc_wait);
 	init_waitqueue_head(&device->al_wait);
 	init_waitqueue_head(&device->seq_wait);
+
+	init_rwsem(&device->uuid_sem);
 
 	q = blk_alloc_queue(GFP_KERNEL);
 	if (!q)
@@ -4716,6 +4717,7 @@ static void __drbd_uuid_new_current(struct drbd_device *device, bool forced, boo
 	u64 got_new_bitmap_uuid, weak_nodes, val, old_current_uuid;
 	int err;
 
+	down_write(&device->uuid_sem);
 	spin_lock_irq(&device->ldev->md.uuid_lock);
 	got_new_bitmap_uuid = rotate_current_into_bitmap(device,
 					forced ? initial_resync_nodes(device) : 0,
@@ -4723,6 +4725,7 @@ static void __drbd_uuid_new_current(struct drbd_device *device, bool forced, boo
 
 	if (!got_new_bitmap_uuid) {
 		spin_unlock_irq(&device->ldev->md.uuid_lock);
+		up_write(&device->uuid_sem);
 		return;
 	}
 
@@ -4733,22 +4736,29 @@ static void __drbd_uuid_new_current(struct drbd_device *device, bool forced, boo
 
 	/* get it to stable storage _now_ */
 	err = drbd_md_sync(device);
-	if (err) {
-		__drbd_uuid_set_current(device, old_current_uuid);
-		return;
-	}
+	if (err)
+		_drbd_uuid_set_current(device, old_current_uuid);
+
+	downgrade_write(&device->uuid_sem);
+
+	if (err)
+		goto out;
 
 	weak_nodes = drbd_weak_nodes_device(device);
 	drbd_info(device, "new current UUID: %016llX weak: %016llX\n",
 		  device->ldev->md.current_uuid, weak_nodes);
 
 	if (!send)
-		return;
+		goto out;
 
 	for_each_peer_device(peer_device, device) {
 		if (peer_device->repl_state[NOW] >= L_ESTABLISHED)
 			drbd_send_uuids(peer_device, forced ? 0 : UUID_FLAG_NEW_DATAGEN, weak_nodes);
 	}
+
+out:
+	up_read(&device->uuid_sem);
+
 }
 
 static bool peer_can_fill_a_bitmap_slot(struct drbd_peer_device *peer_device)
@@ -4889,6 +4899,7 @@ void drbd_uuid_received_new_current(struct drbd_peer_device *from_pd, u64 val, u
 	u64 receipients = 0;
 	bool set_current = true;
 
+	down_write(&device->uuid_sem);
 	spin_lock_irq(&device->ldev->md.uuid_lock);
 
 	for_each_peer_device(peer_device, device) {
@@ -4924,8 +4935,10 @@ void drbd_uuid_received_new_current(struct drbd_peer_device *from_pd, u64 val, u
 	}
 
 	spin_unlock_irq(&device->ldev->md.uuid_lock);
+	downgrade_write(&device->uuid_sem);
 	if (set_current)
 		drbd_propagate_uuids(device, receipients);
+	up_read(&device->uuid_sem);
 }
 
 static u64 __set_bitmap_slots(struct drbd_device *device, u64 bitmap_uuid, u64 do_nodes) __must_hold(local)

drbd/drbd_sender.c
@@ -1116,6 +1116,7 @@ int drbd_resync_finished(struct drbd_peer_device *peer_device,
 
 	drbd_ping_peer(connection);
 
+	down_write(&device->uuid_sem);
 	spin_lock_irq(&device->resource->req_lock);
 	begin_state_change_locked(device->resource, CS_VERBOSE);
 	old_repl_state = repl_state[NOW];
@@ -1243,6 +1244,7 @@ out_unlock:
 
 	resync_again(device, source_m, target_m);
 	spin_unlock_irq(&device->resource->req_lock);
+	up_write(&device->uuid_sem);
 	if (connection->after_reconciliation.lost_node_id != -1)
 		after_reconciliation_resync(connection);
 

commit 63d37a6a52d9134d8afecfeb4a9430023dbb968e

drbd: Fix deadlock upon adding a minor while a connection is established
Commit 9f19be58 introduced a regression. The change missed the
fact that drbd_connected() is also called from drbd_create_device().
When it gets call from there, the read_lock on uuid_sem might not
be released before it tries to do a write_lock on uuid_sem.

Fix that by moving the read_lock.

drbd/drbd_receiver.c
@@ -675,12 +675,8 @@ int drbd_connected(struct drbd_peer_device *peer_device)
 	err = drbd_send_sync_param(peer_device);
 	if (!err)
 		err = drbd_send_sizes(peer_device, 0, 0);
-	if (!err) {
-		down_read_non_owner(&device->uuid_sem);
-		set_bit(HOLDING_UUID_READ_LOCK, &peer_device->flags);
-
+	if (!err)
 		err = drbd_send_uuids(peer_device, 0, weak_nodes);
-	}
 	if (!err) {
 		set_bit(INITIAL_STATE_SENT, &peer_device->flags);
 		err = drbd_send_current_state(peer_device);
@@ -717,7 +713,13 @@ static void conn_connect2(struct drbd_connection *connection)
 		kref_get(&device->kref);
 		/* connection cannot go away: caller holds a reference. */
 		rcu_read_unlock();
+
+		down_read_non_owner(&device->uuid_sem);
+		set_bit(HOLDING_UUID_READ_LOCK, &peer_device->flags);
+		/* since drbd_connected() is also called from drbd_create_device()
+		   aquire lock here before calling drbd_connected(). */
 		drbd_connected(peer_device);
+
 		rcu_read_lock();
 		kref_put(&device->kref, drbd_destroy_device);
 	}