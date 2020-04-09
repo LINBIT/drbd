7a65e630ff81dd9384b46127551ebaa99fed8866
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2020-04-09 11:19:16 +0200

    compat: fix compat test for struct_size
    Fixup for recent commit a4eb4424f24d ("compat: test and patch for
    struct_size"). Turns out this had a false negative on recent kernels
    because I forgot to include a header in the test.

--- drbd/drbd-kernel-compat/tests/have_struct_size.c
+++ drbd/drbd-kernel-compat/tests/have_struct_size.c
@@ -1,5 +1,6 @@
 /* {"version":"4.17", "commit": "610b15c50e86eb1e4b77274fabcaea29ac72d6a8", "comment": "Since Linux 4.17, struct_size should be used to get the size of a struct with a trailing array", "author": "Kees Cook <keescook@chromium.org>", "date": "Mon May 7 16:47:02 2018 -0700" } */
 #include <linux/module.h>
+#include <linux/overflow.h>
 
 struct x {
 	int some;
