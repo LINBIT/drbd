f550237a9c14d01ac0cc5de8298e8a196a72a5a7
Philipp Reisner <philipp.reisner@linbit.com>
2020-06-05 12:19:57 +0200

    drbd: Fixup to the conditions when a diskless primary accepts a current UUID
    For background information see commit ef38fd55a6
    'drbd: Propagate the current-UUID to diskless/secondary nodes'
    then read commit 06ef96edab
    'drbd: Minor fix to UUID handling for dual primary/diskless and early writes'
    
    Do not addopt random UUID updates sent our way. Only do so if
    this node is secondary or if it is a diskless primary waiting
    for the creation of a new current UUID.

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -5228,6 +5228,7 @@ static int __receive_uuids(struct drbd_peer_device *peer_device, u64 node_mask)
 {
 	enum drbd_repl_state repl_state = peer_device->repl_state[NOW];
 	struct drbd_device *device = peer_device->device;
+	struct drbd_resource *resource = device->resource;
 	int updated_uuids = 0, err = 0;
 	bool bad_server;
 
@@ -5299,8 +5300,9 @@ static int __receive_uuids(struct drbd_peer_device *peer_device, u64 node_mask)
 	} else if (device->disk_state[NOW] < D_INCONSISTENT &&
 		   repl_state >= L_ESTABLISHED &&
 		   peer_device->disk_state[NOW] == D_UP_TO_DATE &&
-		   peer_device->current_uuid != device->exposed_data_uuid) {
-		struct drbd_resource *resource = device->resource;
+		   peer_device->current_uuid != device->exposed_data_uuid &&
+		   (resource->role[NOW] == R_SECONDARY ||
+		    test_and_clear_bit(NEW_CUR_UUID, &device->flags))) {
 
 		spin_lock_irq(&resource->req_lock);
 		if (resource->remote_state_change) {
