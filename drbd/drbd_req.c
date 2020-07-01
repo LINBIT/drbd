42f0bbbacf60c44dbfc2988079b54417f32147b9
Christoph Hellwig <hch@lst.de>
2020-07-01 10:59:30 +0200

    drbd: stop using ->queuedata
    Instead of setting up the queuedata as well just use one private data
    field.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drbd/drbd_req.c
+++ drbd/drbd_req.c
@@ -2194,8 +2194,7 @@ static bool drbd_fail_request_early(struct drbd_device *device, struct bio *bio)
 
 blk_qc_t drbd_submit_bio(struct bio *bio)
 {
-	struct request_queue *q = bio->bi_disk->queue;
-	struct drbd_device *device = (struct drbd_device *) q->queuedata;
+	struct drbd_device *device = bio->bi_disk->private_data;
 #ifdef CONFIG_DRBD_TIMING_STATS
 	ktime_t start_kt;
 #endif
