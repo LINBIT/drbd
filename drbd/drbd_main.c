cf884a2b8bdc88e70f5e7f042d38a079b4ec5089
Christoph Hellwig <hch@lst.de>
2020-07-01 11:06:22 +0200

    writeback: remove bdi->congested_fn
    Except for pktdvd, the only places setting congested bits are file
    systems that allocate their own backing_dev_info structures.  And
    pktdvd is a deprecated driver that isn't useful in stack setup
    either.  So remove the dead congested_fn stacking infrastructure.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Song Liu <song@kernel.org>
    Acked-by: David Sterba <dsterba@suse.com>
    [axboe: fixup unused variables in bcache/request.c]
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drbd/drbd_main.c
+++ drbd/drbd_main.c
@@ -3101,63 +3101,6 @@ static void drbd_cleanup(void)
 	pr_info("module cleanup done.\n");
 }
 
-/**
- * drbd_congested() - Callback for the flusher thread
- * @congested_data:	User data
- * @bdi_bits:		Bits the BDI flusher thread is currently interested in
- *
- * Returns 1<<WB_async_congested and/or 1<<WB_sync_congested if we are congested.
- */
-static int drbd_congested(void *congested_data, int bdi_bits)
-{
-	struct drbd_device *device = congested_data;
-	struct request_queue *q;
-	int r = 0;
-
-	if (!may_inc_ap_bio(device)) {
-		/* DRBD has frozen IO */
-		r = bdi_bits;
-		goto out;
-	}
-
-	if (test_bit(CALLBACK_PENDING, &device->resource->flags)) {
-		r |= (1 << WB_async_congested);
-		/* Without good local data, we would need to read from remote,
-		 * and that would need the worker thread as well, which is
-		 * currently blocked waiting for that usermode helper to
-		 * finish.
-		 */
-		if (!get_ldev_if_state(device, D_UP_TO_DATE))
-			r |= (1 << WB_sync_congested);
-		else
-			put_ldev(device);
-		r &= bdi_bits;
-		goto out;
-	}
-
-	if (get_ldev(device)) {
-		q = bdev_get_queue(device->ldev->backing_bdev);
-		r = bdi_congested(q->backing_dev_info, bdi_bits);
-		put_ldev(device);
-	}
-
-	if (bdi_bits & (1 << WB_async_congested)) {
-		struct drbd_peer_device *peer_device;
-
-		rcu_read_lock();
-		for_each_peer_device_rcu(peer_device, device) {
-			if (test_bit(NET_CONGESTED, &peer_device->connection->transport.flags)) {
-				r |= (1 << WB_async_congested);
-				break;
-			}
-		}
-		rcu_read_unlock();
-	}
-
-out:
-	return r;
-}
-
 static void drbd_init_workqueue(struct drbd_work_queue* wq)
 {
 	spin_lock_init(&wq->q_lock);
@@ -3728,9 +3671,6 @@ enum drbd_ret_code drbd_create_device(struct drbd_config_context *adm_ctx, unsig
 	device->this_bdev = bdget(MKDEV(DRBD_MAJOR, minor));
 	/* we have no partitions. we contain only ourselves. */
 	device->this_bdev->bd_contains = device->this_bdev;
-
-	q->backing_dev_info->congested_fn = drbd_congested;
-	q->backing_dev_info->congested_data = device;
 
 	blk_queue_write_cache(q, true, true);
 
