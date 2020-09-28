fdfae4ba0d082beb2862bf1f098f755ced51f614
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2020-09-28 11:29:49 +0200

    ci: specify release when building dummy release
    For now, this is still always -1, but it opens up the possibility to set
    the release to something different in the future.

--- .gitlab/gitlab-utils.sh
+++ .gitlab/gitlab-utils.sh
@@ -30,19 +30,20 @@ drbd_fetch_lbbuildctl() {
 # Replace the drbd version number in all necessary places
 drbd_dummy_release() {
 	local version="$1"
+	local release="$2"
 
-	dummy-release.sh drbd "$version" drbd-kernel.spec
+	dummy-release.sh drbd "$version" "$release" drbd-kernel.spec
 
-	sed -re "s/(#define REL_VERSION) .*/\1 \"${version}-1\"/g" drbd/linux/drbd_config.h > drbd/linux/drbd_config.h.tmp
+	sed -re "s/(#define REL_VERSION) .*/\1 \"${version}-${release}\"/g" drbd/linux/drbd_config.h > drbd/linux/drbd_config.h.tmp
 	mv drbd/linux/drbd_config.h{.tmp,}
 
 	for i in 7 8; do
-		sed -re "s/(ENV DRBD_VERSION) .*/\1 ${version}-1/g" docker/Dockerfile.centos${i} > docker/Dockerfile.centos${i}.tmp
+		sed -re "s/(ENV DRBD_VERSION) .*/\1 ${version}-${release}/g" docker/Dockerfile.centos${i} > docker/Dockerfile.centos${i}.tmp
 		mv docker/Dockerfile.centos${i}{.tmp,}
 	done
 
 	cat > ChangeLog << EOF
-${version}-1 (api:genl2/proto:86-117/transport:14)
+${version}-${release} (api:genl2/proto:86-117/transport:14)
 --------
 
   * Dummy release
