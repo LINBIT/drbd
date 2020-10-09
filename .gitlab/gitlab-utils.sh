029f5c3151c1d0906ecf18389de30831943c8c85
Joel Colledge <joel.colledge@linbit.com>
2020-10-09 13:58:49 +0200

    ci: adjust CI build to be similar to DRBD 10
    DRBD 10 excludes the release part from the "REL_VERSION", which is the
    version used in the name of the source tarball. In order to support
    this, "REL_VERSION" is passed as a parameter. Integrate this change in
    order to reduce merging effort.

--- .gitlab/gitlab-utils.sh
+++ .gitlab/gitlab-utils.sh
@@ -4,19 +4,20 @@
 drbd_dummy_release() {
 	local version="$1"
 	local release="$2"
+	local rel_version="$3"
 
 	dummy-release.sh drbd "$version" "$release" drbd-kernel.spec
 
-	sed -re "s/(#define REL_VERSION) .*/\1 \"${version}-${release}\"/g" drbd/linux/drbd_config.h > drbd/linux/drbd_config.h.tmp
+	sed -re "s/(#define REL_VERSION) .*/\1 \"$rel_version\"/g" drbd/linux/drbd_config.h > drbd/linux/drbd_config.h.tmp
 	mv drbd/linux/drbd_config.h{.tmp,}
 
 	for i in 7 8; do
-		sed -re "s/(ENV DRBD_VERSION) .*/\1 ${version}-${release}/g" docker/Dockerfile.centos${i} > docker/Dockerfile.centos${i}.tmp
+		sed -re "s/(ENV DRBD_VERSION) .*/\1 $rel_version/g" docker/Dockerfile.centos${i} > docker/Dockerfile.centos${i}.tmp
 		mv docker/Dockerfile.centos${i}{.tmp,}
 	done
 
 	cat > ChangeLog << EOF
-${version}-${release} (api:genl2/proto:86-117/transport:14)
+$rel_version (api:genl2/proto:86-117/transport:14)
 --------
 
   * Dummy release
