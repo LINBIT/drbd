ee025dc84a89064026de20236eff592037642c8a
Lukas Wunner <lukas@wunner.de>
2020-10-11 18:07:36 +0200

    compat: __vmalloc() pgprot argument was removed in v5.8
    Commit torvalds/linux@88dca4ca5a93 removed the pgprot argument from
    __vmalloc().  There's a single invocation in drbd_bitmap.c.  Drop the
    argument to stay as close as possible to upstream and apply a cocci
    patch for older kernels which reinstates the additional argument.
    
    Reported-by: Michael Hierweck <michael.hierweck@hostsharing.net>
    Signed-off-by: Lukas Wunner <lukas@wunner.de>

--- drbd/drbd_bitmap.c
+++ drbd/drbd_bitmap.c
@@ -366,8 +366,7 @@ static struct page **bm_realloc_pages(struct drbd_bitmap *b, unsigned long want)
 	new_pages = kzalloc(bytes, GFP_NOIO | __GFP_NOWARN);
 	if (!new_pages) {
 		new_pages = __vmalloc(bytes,
-				GFP_NOIO | __GFP_HIGHMEM | __GFP_ZERO,
-				PAGE_KERNEL);
+				GFP_NOIO | __GFP_HIGHMEM | __GFP_ZERO);
 		if (!new_pages)
 			return NULL;
 	}
