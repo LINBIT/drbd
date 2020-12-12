3867629fa33e5372848c8f31504f889dc7f6de57
Michael D Labriola <michael.d.labriola@gmail.com>
2020-12-12 17:05:33 -0500

    compat: fix include for submit_bio test
    Looks like this test is including the wrong header to get at struct
    block_device_operations.
    
    Signed-off-by: Michael D Labriola <michael.d.labriola@gmail.com>

--- drbd/drbd-kernel-compat/tests/have_submit_bio.c
+++ drbd/drbd-kernel-compat/tests/have_submit_bio.c
@@ -1,6 +1,6 @@
 /* { "version": "v5.8", "commit": "c62b37d96b6eb3ec5ae4cbe00db107bf15aebc93", "comment": "Since 5.8 make_request_fn has been replaced by a block_device_operations method called submit_bio", "author": "Christoph Hellwig <hch@lst.de>", "date": "Wed Jul 1 10:59:43 2020 +0200" } */
 
-#include <linux/blk_types.h>
+#include <linux/blkdev.h>
 
 void foo(struct block_device_operations *ops)
 {
