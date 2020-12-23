LINBIT/drbd

commit cd4288e8316090bd1f7de4a7f6a6fb090da2d438

drbd: another fine-grained lock pending_completion_lock

drbd/drbd_req.c
@@ -404,6 +404,7 @@ void drbd_req_complete(struct drbd_request *req, struct bio_and_error *m)
 	const unsigned s = req->local_rq_state;
 	struct drbd_device *device = req->device;
 	struct drbd_peer_device *peer_device;
+	unsigned long flags;
 	int error, ok = 0;
 
 	/*
@@ -521,7 +522,9 @@ void drbd_req_complete(struct drbd_request *req, struct bio_and_error *m)
 	 * or we will restart this request.
 	 * In either case, the request object will be destroyed soon,
 	 * so better remove it from all lists. */
+	spin_lock_irqsave(&device->pending_completion_lock, flags);
 	list_del_init(&req->req_pending_master_completion);
+	spin_unlock_irqrestore(&device->pending_completion_lock, flags);
 }
 
 /* still holds resource->req_lock */
@@ -715,11 +718,15 @@ static void mod_rq_state(struct drbd_request *req, struct bio_and_error *m,
 	}
 
 	if ((old_local & RQ_LOCAL_PENDING) && (clear_local & RQ_LOCAL_PENDING)) {
+		struct drbd_device *device = req->device;
+
 		if (req->local_rq_state & RQ_LOCAL_ABORTED)
 			kref_put(&req->kref, drbd_req_destroy);
 		else
 			++c_put;
+		spin_lock(&device->pending_completion_lock); /* local irq already disabled */
 		list_del_init(&req->req_pending_local);
+		spin_unlock(&device->pending_completion_lock);
 	}
 
 	if ((old_net & RQ_NET_PENDING) && (clear & RQ_NET_PENDING)) {
@@ -1550,8 +1557,10 @@ static void drbd_queue_write(struct drbd_device *device, struct drbd_request *re
 	spin_lock(&device->submit.lock);
 	list_add_tail(&req->tl_requests, &device->submit.writes);
 	spin_unlock(&device->submit.lock);
+	spin_lock_irq(&device->pending_completion_lock);
 	list_add_tail(&req->req_pending_master_completion,
 			&device->pending_master_completion[1 /* WRITE */]);
+	spin_unlock_irq(&device->pending_completion_lock);
 	queue_work(device->submit.wq, &device->submit.worker);
 	/* do_submit() may sleep internally on al_wait, too */
 	wake_up(&device->al_wait);
@@ -1834,6 +1843,7 @@ static void drbd_send_and_submit(struct drbd_device *device, struct drbd_request
 
 	/* If it took the fast path in drbd_request_prepare, add it here.
 	 * The slow path has added it already. */
+	spin_lock(&device->pending_completion_lock); /* local irq already disabled */
 	if (list_empty(&req->req_pending_master_completion))
 		list_add_tail(&req->req_pending_master_completion,
 			&device->pending_master_completion[rw == WRITE]);
@@ -1847,13 +1857,17 @@ static void drbd_send_and_submit(struct drbd_device *device, struct drbd_request
 		/* needs to be marked within the same spinlock
 		 * but we need to give up the spinlock to submit */
 		submit_private_bio = true;
-	} else if (no_remote) {
+		spin_unlock(&device->pending_completion_lock);
+	} else {
+		spin_unlock(&device->pending_completion_lock);
+		if (no_remote) {
 nodata:
-		if (drbd_ratelimit())
-			drbd_err(req->device, "IO ERROR: neither local nor remote data, sector %llu+%u\n",
-					(unsigned long long)req->i.sector, req->i.size >> 9);
-		/* A write may have been queued for send_oos, however.
-		 * So we can not simply free it, we must go through drbd_req_put_completion_ref() */
+			if (drbd_ratelimit())
+				drbd_err(req->device, "IO ERROR: neither local nor remote data, sector %llu+%u\n",
+					 (unsigned long long)req->i.sector, req->i.size >> 9);
+			/* A write may have been queued for send_oos, however.
+			 * So we can not simply free it, we must go through drbd_req_put_completion_ref() */
+		}
 	}
 
 out:
@@ -2458,9 +2472,12 @@ void request_timer_fn(struct timer_list *t)
 
 	spin_lock_irq(&device->resource->req_lock);
 	if (dt) {
-		unsigned long write_pre_submit_jif = now, read_pre_submit_jif = now;
+		unsigned long write_pre_submit_jif, read_pre_submit_jif;
+
+		spin_lock(&device->pending_completion_lock); /* local irq already disabled */
 		req_read = list_first_entry_or_null(&device->pending_completion[0], struct drbd_request, req_pending_local);
 		req_write = list_first_entry_or_null(&device->pending_completion[1], struct drbd_request, req_pending_local);
+		spin_unlock(&device->pending_completion_lock);
 
 		if (req_write)
 			write_pre_submit_jif = req_write->pre_submit_jif;

drbd/drbd_debugfs.c
@@ -203,14 +203,14 @@ static void seq_print_waiting_for_AL(struct seq_file *m, struct drbd_resource *r
 		struct drbd_request *req;
 		int n = atomic_read(&device->ap_actlog_cnt);
 		if (n) {
-			spin_lock_irq(&device->resource->req_lock);
+			spin_lock_irq(&device->pending_completion_lock);
 			req = list_first_entry_or_null(&device->pending_master_completion[1],
 				struct drbd_request, req_pending_master_completion);
 			/* if the oldest request does not wait for the activity log
 			 * it is not interesting for us here */
 			if (req && (req->local_rq_state & RQ_IN_ACT_LOG))
 				req = NULL;
-			spin_unlock_irq(&device->resource->req_lock);
+			spin_unlock_irq(&device->pending_completion_lock);
 		}
 		if (n) {
 			seq_printf(m, "%u\t%u\t", device->minor, device->vnr);
@@ -1019,7 +1019,6 @@ static int device_act_log_extents_show(struct seq_file *m, void *ignored)
 static int device_oldest_requests_show(struct seq_file *m, void *ignored)
 {
 	struct drbd_device *device = m->private;
-	struct drbd_resource *resource = device->resource;
 	ktime_t now = ktime_get();
 	unsigned long jif = jiffies;
 	struct drbd_request *r1, *r2;
@@ -1029,7 +1028,7 @@ static int device_oldest_requests_show(struct seq_file *m, void *ignored)
 	seq_printf(m, "v: %u\n\n", 0);
 
 	seq_puts(m, RQ_HDR);
-	spin_lock_irq(&resource->req_lock);
+	spin_lock_irq(&device->pending_completion_lock);
 	/* WRITE, then READ */
 	for (i = 1; i >= 0; --i) {
 		r1 = list_first_entry_or_null(&device->pending_master_completion[i],
@@ -1041,7 +1040,7 @@ static int device_oldest_requests_show(struct seq_file *m, void *ignored)
 		if (r2 && r2 != r1)
 			seq_print_one_request(m, r2, now, jif);
 	}
-	spin_unlock_irq(&resource->req_lock);
+	spin_unlock_irq(&device->pending_completion_lock);
 	return 0;
 }
 

drbd/drbd_int.h
@@ -1322,6 +1322,7 @@ struct drbd_device {
 
 	/* for statistics and timeouts */
 	/* [0] read, [1] write */
+	spinlock_t pending_completion_lock;
 	struct list_head pending_master_completion[2];
 	struct list_head pending_completion[2];
 

drbd/drbd_main.c
@@ -3732,6 +3732,7 @@ enum drbd_ret_code drbd_create_device(struct drbd_config_context *adm_ctx, unsig
 #endif
 	spin_lock_init(&device->al_lock);
 
+	spin_lock_init(&device->pending_completion_lock);
 	INIT_LIST_HEAD(&device->pending_master_completion[0]);
 	INIT_LIST_HEAD(&device->pending_master_completion[1]);
 	INIT_LIST_HEAD(&device->pending_completion[0]);

commit ec43f70c23674d14c2df91d7329df0abaf099609

drbd: fix corruption of pending_master_completion list
This fixes corruption of the device->pending_master_completion[WRITE]
list, whose entries are req->req_pending_master_completion. The issue
caused the following warning in kernels with list debugging enabled:
[11696.882946] WARNING: CPU: 0 PID: 4613 at lib/list_debug.c:59 __list_del_entry+0xa1/0xd0
[11696.882947] list_del corruption. prev->next should be ffff9898feefec78, but was ffff98983ce0d358
...
[11696.883013] Call Trace:
[11696.883017]  [<ffffffffb3d7ff85>] dump_stack+0x19/0x1b
[11696.883020]  [<ffffffffb369bd18>] __warn+0xd8/0x100
[11696.883024]  [<ffffffffb369bd9f>] warn_slowpath_fmt+0x5f/0x80
[11696.883027]  [<ffffffffb39a4ca1>] __list_del_entry+0xa1/0xd0
[11696.883037]  [<ffffffffc0b2ada0>] drbd_req_put_completion_ref+0x220/0x3f0 [drbd]
[11696.883047]  [<ffffffffc0b2b3ff>] mod_rq_state+0x48f/0x830 [drbd]
[11696.883057]  [<ffffffffc0b2ba39>] __req_mod+0x159/0x9d0 [drbd]
[11696.883066]  [<ffffffffc0b17e1b>] validate_req_change_req_state+0xbb/0x130 [drbd]
[11696.883075]  [<ffffffffc0b181dd>] got_BlockAck+0xcd/0x200 [drbd]
[11696.883085]  [<ffffffffc0b291c9>] drbd_ack_receiver+0x239/0x600 [drbd]

The cause of the issue was that the request was queued on
device->submit.writes before being added to
device->pending_master_completion. Hence, with unfortunate timing, it
would be added to device->pending_master_completion in
drbd_send_and_submit and then again in drbd_queue_write. This corrupted
the list.

The problem was introduced in
cd4288e83160 drbd: another fine-grained lock pending_completion_lock
and related commits removing req_lock.

Fix it by correcting the order we add the request to each of the lists.

drbd/drbd_req.c
@@ -1573,13 +1573,13 @@ static void drbd_queue_write(struct drbd_device *device, struct drbd_request *re
 {
 	if (req->private_bio)
 		atomic_inc(&device->ap_actlog_cnt);
-	spin_lock(&device->submit.lock);
-	list_add_tail(&req->list, &device->submit.writes);
-	spin_unlock(&device->submit.lock);
 	spin_lock_irq(&device->pending_completion_lock);
 	list_add_tail(&req->req_pending_master_completion,
 			&device->pending_master_completion[1 /* WRITE */]);
 	spin_unlock_irq(&device->pending_completion_lock);
+	spin_lock(&device->submit.lock);
+	list_add_tail(&req->list, &device->submit.writes);
+	spin_unlock(&device->submit.lock);
 	queue_work(device->submit.wq, &device->submit.worker);
 	/* do_submit() may sleep internally on al_wait, too */
 	wake_up(&device->al_wait);