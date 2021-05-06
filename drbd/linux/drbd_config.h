a60cffa380085d75c5f62b6bcb500c5b43ca801e
Philipp Reisner <philipp.reisner@linbit.com>
2021-05-06 09:59:12 +0200

    Prepare 9.1.2

--- drbd/linux/drbd_config.h
+++ drbd/linux/drbd_config.h
@@ -20,7 +20,7 @@ extern const char *drbd_buildtag(void);
 
 /* End of external module for 2.6.33 stuff */
 
-#define REL_VERSION "9.1.2-rc.1"
+#define REL_VERSION "9.1.2"
 #define PRO_VERSION_MIN 110
 #define PRO_VERSION_MAX 120
 
