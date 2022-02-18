de32acd7ad1fcfe2f2dbc1551cc669e1d5e2d524
Philipp Reisner <philipp.reisner@linbit.com>
2022-02-18 13:42:31 +0100

    drbd: Only allow local promotion if fencing of all peers was successful
    In case fencing is configured, require that fencing of all unreachable
    peers were successful.
    
    This looks like was wrong since multiple peers got introduced. Probably
    nobody realized, since when there are three nodes or more, people
    prefer quorum over fencing.

--- drbd/drbd_nl.c
+++ drbd/drbd_nl.c
@@ -1027,6 +1027,7 @@ drbd_set_role(struct drbd_resource *resource, enum drbd_role role, bool force, s
 	const char *err_str = NULL;
 	enum chg_state_flags flags = CS_ALREADY_SERIALIZED | CS_DONT_RETRY | CS_WAIT_COMPLETE;
 	struct block_device *bdev = NULL;
+	bool fenced_peers = false;
 
 retry:
 
@@ -1093,7 +1094,7 @@ retry:
 		}
 		/* in case we first succeeded to outdate,
 		 * but now suddenly could establish a connection */
-		if (rv == SS_CW_FAILED_BY_PEER) {
+		if (rv == SS_CW_FAILED_BY_PEER && fenced_peers) {
 			flags &= ~CS_FP_LOCAL_UP_TO_DATE;
 			continue;
 		}
@@ -1120,6 +1121,7 @@ retry:
 			struct drbd_connection *connection;
 			u64 im;
 
+			fenced_peers = true;
 			up(&resource->state_sem); /* Allow connect while fencing */
 			for_each_connection_ref(connection, im, resource) {
 				struct drbd_peer_device *peer_device;
@@ -1134,13 +1136,15 @@ retry:
 					if (device->disk_state[NOW] != D_CONSISTENT)
 						continue;
 
-					if (conn_try_outdate_peer(connection))
-						flags |= CS_FP_LOCAL_UP_TO_DATE;
+					if (!conn_try_outdate_peer(connection))
+						fenced_peers = false;
 				}
 			}
 			down(&resource->state_sem);
-			if (flags & CS_FP_LOCAL_UP_TO_DATE)
+			if (fenced_peers) {
+				flags |= CS_FP_LOCAL_UP_TO_DATE;
 				continue;
+			}
 		}
 
 		if (rv == SS_NO_QUORUM && force && !(flags & CS_FP_OUTDATE_PEERS)) {
