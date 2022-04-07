a1dc9ecdbb9d652a9d4e3183fb543584645c9aba
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2022-04-07 12:15:18 +0200

    drbd: change drbd_submit_bio return type to void
    See upstream Linux kernel commit 3e08773c3841 ("block: switch polling to
    be bio based").

--- drbd/drbd_req.c
+++ drbd/drbd_req.c
@@ -2193,7 +2193,7 @@ static bool drbd_fail_request_early(struct drbd_device *device, struct bio *bio)
 	return false;
 }
 
-blk_qc_t drbd_submit_bio(struct bio *bio)
+void drbd_submit_bio(struct bio *bio)
 {
 	struct drbd_device *device = bio->bi_bdev->bd_disk->private_data;
 #ifdef CONFIG_DRBD_TIMING_STATS
@@ -2204,7 +2204,7 @@ blk_qc_t drbd_submit_bio(struct bio *bio)
 	if (drbd_fail_request_early(device, bio)) {
 		bio->bi_status = BLK_STS_IOERR;
 		bio_endio(bio);
-		return BLK_QC_T_NONE;
+		return;
 	}
 
 	blk_queue_split(&bio);
@@ -2212,7 +2212,7 @@ blk_qc_t drbd_submit_bio(struct bio *bio)
 	if (device->cached_err_io) {
 		bio->bi_status = BLK_STS_IOERR;
 		bio_endio(bio);
-		return BLK_QC_T_NONE;
+		return;
 	}
 
 	/* This is both an optimization: READ of size 0, nothing to do
@@ -2224,15 +2224,13 @@ blk_qc_t drbd_submit_bio(struct bio *bio)
 	if (bio_op(bio) == REQ_OP_READ && bio->bi_iter.bi_size == 0) {
 		WARN_ONCE(1, "size zero read from upper layers");
 		bio_endio(bio);
-		return BLK_QC_T_NONE;
+		return;
 	}
 
 	ktime_get_accounting(start_kt);
 	start_jif = jiffies;
 
 	__drbd_make_request(device, bio, start_kt, start_jif);
-
-	return BLK_QC_T_NONE;
 }
 
 static unsigned long time_min_in_future(unsigned long now,
