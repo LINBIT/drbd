LINBIT/drbd

commit 9a11ebeb56e20b2e053b1dfb8d2c2a903bc94b59

drbd: peer_ack (6) free pages of peer_requests after the ack was sent

drbd/drbd_receiver.c
@@ -462,7 +462,10 @@ static int drbd_finish_peer_reqs(struct drbd_device *device)
 		err2 = peer_req->w.cb(&peer_req->w, !!err);
 		if (!err)
 			err = err2;
-		if (list_empty(&peer_req->recv_order))
+		if (!list_empty(&peer_req->recv_order)) {
+			drbd_free_pages(device, peer_req->pages, 0);
+			peer_req->pages = NULL;
+		} else
 			drbd_free_peer_req(device, peer_req);
 	}
 	wake_up(&device->ee_wait);

commit 254f91494b5fa2e30b0aa816ea573edb3babac99

drbd: fix a race freeing peer request pages
Commits e061feb8 and 9a11ebeb5 introduced a bug involving a race calling
drbd_free_page_chain().

drbd_free_page_chain() may be called concurrently from
drbd_finish_peer_reqs() and got_peer_ack(). The sequence is as follows:

[ack_sender]                          [receiver]
drbd_finish_peer_reqs
e_end_block
drbd_may_finish_epoch
(Barrier ack sent to peer)
                                      got_peer_ack
                                      drbd_free_peer_req
drbd_free_page_chain                  drbd_free_page_chain
spin_lock
page_chain_add
spin_unlock                           spin_lock
                                      page_chain_add
                                      spin_unlock

We can see that page_chain_add() will be called twice which will lead to
a crash.

Swap drbd_may_finish_epoch() and drbd_free_page_chain() in e_end_block()
to ensure that got_peer_ack() will not be called while still processing
the request in drbd_finish_peer_reqs().

Signed-off-by: Rui Xu <rui.xu@easystack.cn>
Signed-off-by: Joel Colledge <joel.colledge@linbit.com>

drbd/drbd_receiver.c
@@ -2573,9 +2573,13 @@ static int e_end_block(struct drbd_work *w, int cancel)
 	} else
 		D_ASSERT(device, drbd_interval_empty(&peer_req->i));
 
-	drbd_may_finish_epoch(peer_device->connection, peer_req->epoch, EV_PUT + (cancel ? EV_CLEANUP : 0));
-
 	drbd_free_page_chain(&peer_device->connection->transport, &peer_req->page_chain, 0);
+
+	drbd_may_finish_epoch(peer_device->connection, peer_req->epoch, EV_PUT + (cancel ? EV_CLEANUP : 0));
+	/* Do not use peer_req after this point. We may have sent the
+	 * corresponding barrier and received the corresponding peer ack. As a
+	 * result, peer_req may have been freed. */
+
 	return err;
 }
 