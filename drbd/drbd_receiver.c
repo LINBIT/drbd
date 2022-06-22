ce9a99268de8717a8c183027855a35ec9878a5ff
Philipp Reisner <philipp.reisner@linbit.com>
2022-06-22 11:06:23 +0200

    drbd: we received the packet, not matter if we can write it

--- drbd/drbd_receiver.c
+++ drbd/drbd_receiver.c
@@ -8593,9 +8593,10 @@ static int receive_rs_deallocated(struct drbd_connection *connection, struct pac
 	if (!peer_req)
 		return -EIO;
 
-	if (get_ldev(device)) {
-		dec_rs_pending(peer_device);
+	dec_rs_pending(peer_device);
+	atomic_add(size >> 9, &device->rs_sect_ev);
 
+	if (get_ldev(device)) {
 		inc_unacked(peer_device);
 
 		peer_req->block_id = ID_SYNCER;
@@ -8607,7 +8608,6 @@ static int receive_rs_deallocated(struct drbd_connection *connection, struct pac
 		list_move_tail(&peer_req->w.list, &connection->sync_ee);
 		spin_unlock_irq(&connection->peer_reqs_lock);
 
-		atomic_add(size >> 9, &device->rs_sect_ev);
 		drbd_conflict_submit_resync_request(peer_req);
 
 		/* No put_ldev() here. Gets called in drbd_endio_write_sec_final(). */
