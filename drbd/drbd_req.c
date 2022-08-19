LINBIT/drbd

commit d6a86c27605e9f550d95725f10b17d67b7a94a24

drbd: skip done requests in tl_walk for connection
Since we perform tl_walk from finish_state_change(), we want to avoid
extremely long transfer log walks.

This situation could occur when we also have a lagging protocol A peer.

This is compensation for the fact that we no longer use
TL_NEXT_REQUEST_RESEND to avoid long transfer log walks under a lock.

In order to implement this, we reinterpret req_not_net_done slightly. It
can now point to requests which have not yet been sent.

drbd/drbd_req.c
@@ -634,6 +634,8 @@ static void advance_cache_ptr(struct drbd_connection *connection,
 	}
 	list_for_each_entry_continue_rcu(req, &connection->resource->transfer_log, tl_requests) {
 		const unsigned s = READ_ONCE(req->net_rq_state[connection->peer_node_id]);
+		if (!(s & RQ_NET_MASK))
+			continue;
 		if (((s & is_set) == is_set) && !(s & is_clear))
 			break;
 	}
@@ -718,18 +720,18 @@ static void mod_rq_state(struct drbd_request *req, struct bio_and_error *m,
 		atomic_inc(&req->completion_ref);
 	}
 
-	if (!(old_net & RQ_NET_QUEUED) && (set & RQ_NET_QUEUED))
+	if (!(old_net & RQ_NET_QUEUED) && (set & RQ_NET_QUEUED)) {
+		set_cache_ptr_if_null(&connection->req_not_net_done, req);
 		atomic_inc(&req->completion_ref);
+	}
 
 	if (!(old_net & RQ_EXP_BARR_ACK) && (set & RQ_EXP_BARR_ACK))
 		kref_get(&req->kref); /* wait for the DONE */
 
 	if (!(old_net & RQ_NET_SENT) && (set & RQ_NET_SENT)) {
 		/* potentially already completed in the ack_receiver thread */
-		if (!(old_net & RQ_NET_DONE)) {
+		if (!(old_net & RQ_NET_DONE))
 			atomic_add(req_payload_sectors(req), &peer_device->connection->ap_in_flight);
-			set_cache_ptr_if_null(&connection->req_not_net_done, req);
-		}
 		if (req->net_rq_state[idx] & RQ_NET_PENDING)
 			set_cache_ptr_if_null(&connection->req_ack_pending, req);
 	}
@@ -807,7 +809,7 @@ static void mod_rq_state(struct drbd_request *req, struct bio_and_error *m,
 		advance_cache_ptr(connection, &connection->req_ack_pending,
 				  req, RQ_NET_SENT | RQ_NET_PENDING, 0);
 		advance_cache_ptr(connection, &connection->req_not_net_done,
-				  req, RQ_NET_SENT, RQ_NET_DONE);
+				  req, 0, RQ_NET_DONE);
 	}
 
 	/* potentially complete and destroy */
@@ -2399,6 +2401,15 @@ static bool net_timeout_reached(struct drbd_request *net_req,
 
 	if (time_in_range(now, connection->last_reconnect_jif, connection->last_reconnect_jif + ent))
 		return false;
+
+	/* We should not blame the peer for being unresponsive, if we did not
+	 * send the request yet. */
+	if (!(net_req->net_rq_state[peer_node_id] & RQ_NET_SENT)) {
+		drbd_warn(peer_device,
+			"We did not send a request for %ums > ko-count (%u) * timeout (%u * 0.1s)\n",
+			jiffies_to_msecs(now - pre_send_jif), ko_count, timeout);
+		return false;
+	}
 
 	if (net_req->net_rq_state[peer_node_id] & RQ_NET_PENDING) {
 		drbd_warn(peer_device, "Remote failed to finish a request within %ums > ko-count (%u) * timeout (%u * 0.1s)\n",

drbd/drbd_req.h
@@ -285,9 +285,10 @@ extern void __req_mod(struct drbd_request *req, enum drbd_req_event what,
 extern void complete_master_bio(struct drbd_device *device,
 		struct bio_and_error *m);
 extern void request_timer_fn(struct timer_list *t);
-extern void tl_walk(struct drbd_connection *connection, enum drbd_req_event what);
+extern void tl_walk(struct drbd_connection *connection, struct drbd_request **from_req, enum drbd_req_event what);
 extern void __tl_walk(struct drbd_resource *const resource,
 		struct drbd_connection *const connection,
+		struct drbd_request **from_req,
 		const enum drbd_req_event what);
 extern void drbd_destroy_peer_ack_if_done(struct drbd_peer_ack *peer_ack);
 extern int w_queue_peer_ack(struct drbd_work *w, int cancel);

drbd/drbd-kernel-compat/drbd_wrappers.h
@@ -77,6 +77,13 @@
 	     ++id, entry = (typeof(entry))idr_get_next((idp), &(id)))
 #endif
 
+/* introduced in v4.17-rc7-25-gead9ad7253f4 */
+#ifndef list_for_each_entry_from_rcu
+#define list_for_each_entry_from_rcu(pos, head, member)			\
+	for (; &(pos)->member != (head);					\
+		pos = list_entry_rcu(pos->member.next, typeof(*(pos)), member))
+#endif
+
 /* introduced in v3.13-rc2-4-g462225ae47d7 */
 #ifndef RCU_INITIALIZER
 #define RCU_INITIALIZER(v) (typeof(*(v)) *)(v)

drbd/drbd_int.h
@@ -1113,6 +1113,8 @@ struct drbd_connection {
 	 * so we can look up the oldest pending requests more quickly.
 	 * TODO: RCU */
 	struct drbd_request *req_ack_pending;
+	/* The oldest request that is or was queued for this peer, but is not
+	 * done towards it. */
 	struct drbd_request *req_not_net_done;
 
 	unsigned int s_cb_nr; /* keeps counting up */

drbd/drbd_main.c
@@ -469,7 +469,8 @@ bail:
 
 /**
  * _tl_walk() - Walks the transfer log, and applies an action to all requests
- * @connection:	DRBD connection to operate on.
+ * @connection: DRBD connection to operate on
+ * @from_req    If set, the walk starts from the request that this points to
  * @what:       The action/event to perform with all request objects
  *
  * @what might be one of CONNECTION_LOST, CONNECTION_LOST_WHILE_SUSPENDED,
@@ -477,13 +478,18 @@ bail:
  */
 void __tl_walk(struct drbd_resource *const resource,
 		struct drbd_connection *const connection,
+		struct drbd_request **from_req,
 		const enum drbd_req_event what)
 {
 	struct drbd_peer_device *peer_device;
-	struct drbd_request *req;
+	struct drbd_request *req = NULL;
 
 	rcu_read_lock();
-	list_for_each_entry_rcu(req, &resource->transfer_log, tl_requests) {
+	if (from_req)
+		req = READ_ONCE(*from_req);
+	if (!req)
+		req = list_entry_rcu(resource->transfer_log.next, struct drbd_request, tl_requests);
+	list_for_each_entry_from_rcu(req, &resource->transfer_log, tl_requests) {
 		/* Skip if the request has already been destroyed. */
 		if (!kref_get_unless_zero(&req->kref))
 			continue;
@@ -496,12 +502,12 @@ void __tl_walk(struct drbd_resource *const resource,
 	rcu_read_unlock();
 }
 
-void tl_walk(struct drbd_connection *connection, enum drbd_req_event what)
+void tl_walk(struct drbd_connection *connection, struct drbd_request **from_req, enum drbd_req_event what)
 {
 	struct drbd_resource *resource = connection->resource;
 
 	read_lock_irq(&resource->state_rwlock);
-	__tl_walk(connection->resource, connection, what);
+	__tl_walk(connection->resource, connection, from_req, what);
 	read_unlock_irq(&resource->state_rwlock);
 }
 

drbd/drbd_receiver.c
@@ -7808,8 +7808,8 @@ static void conn_disconnect(struct drbd_connection *connection)
 	/* Apply these changes after peer_device_disconnected() because that
 	 * may cause the loss of other connections to be detected, which can
 	 * change the suspended state. */
-	tl_walk(connection, resource->cached_susp ?
-			CONNECTION_LOST_WHILE_SUSPENDED : CONNECTION_LOST);
+	tl_walk(connection, &connection->req_not_net_done,
+			resource->cached_susp ? CONNECTION_LOST_WHILE_SUSPENDED : CONNECTION_LOST);
 
 	i = drbd_free_peer_reqs(connection, &connection->read_ee, true);
 	if (i)

drbd/drbd_state.c
@@ -2714,7 +2714,7 @@ static void finish_state_change(struct drbd_resource *resource, struct completio
 				walk_event = has_starting_resyncs(connection) ? CANCEL_SUSPENDED_IO : RESEND;
 
 			if (walk_event != -1)
-				__tl_walk(resource, connection, walk_event);
+				__tl_walk(resource, connection, &connection->req_not_net_done, walk_event);
 
 			/* Since we are in finish_state_change(), and the state
 			 * was previously not C_CONNECTED, the sender cannot
@@ -2759,7 +2759,7 @@ static void finish_state_change(struct drbd_resource *resource, struct completio
 		resource->peers_at_quorum_loss = connected_peers_mask(resource, OLD);
 
 	if (resource_suspended[OLD] && !resource_suspended[NEW])
-		__tl_walk(resource, NULL, COMPLETION_RESUMED);
+		__tl_walk(resource, NULL, NULL, COMPLETION_RESUMED);
 
 	if (susp_quorum[OLD] && !susp_quorum[NEW])
 		check_new_uuid_after_quorum_suspend(resource);

commit c6c1b4d9faea237a19a9770546685b303dd84ef1

drbd: update req_not_net_done safely
It was possible for the caching pointer "req_not_net_done" to skip a
request. If the connection was lost at that point, the remaining
requests would be marked as RQ_NET_DONE, but this one would be stuck
forever. This occurred when the caching pointer was advanced between:
drbd_process_write_request(req);
and
list_add_tail_rcu(&req->tl_requests, &resource->transfer_log);

This bug was introduced by
d6a86c27605e drbd: skip done requests in tl_walk for connection
In particular, the change to the condition defining req_not_net_done.

In more detail:
* Request r0 has been sent, but not yet barrier-acked,
  req_not_net_done == r0.
* r1 is submitted.
* drbd_send_and_submit(r1) reaches drbd_process_write_request(); this
  calls down to set_cache_ptr_if_null(req_not_net_done, r1), which has
  no effect because req_not_net_done is not NULL.
* Another CPU handles the barrier ack for r0;
  advance_cache_ptr(req_not_net_done, r0) finds no further requests in
  the transfer log and sets req_not_net_done to NULL.
* drbd_send_and_submit(r1) adds r1 to the transfer log.
* Now there is a non-NET_DONE request r1 in the transfer log, while
  req_not_net_done is NULL.

This then leads to problems, for instance:
* r2 is submitted.
* drbd_send_and_submit(r2) reaches drbd_process_write_request();
  this calls down to set_cache_ptr_if_null(req_not_net_done, r2),
  which sets req_not_net_done = r2.
* Now there is a non-NET_DONE request r1 in the transfer log that is
  behind req_not_net_done.
* The connection is lost, tl_walk() marks r2 with NET_DONE, but r1
  remains forever without NET_DONE.

Avoid this by adding the request to the transfer log and then setting
RQ_NET_QUEUED. As a result, set_cache_ptr_if_null() will take care of
setting the caching pointer if required.

In order to implement this, we need to set some net flag for peers to
which out-of-sync should be sent. Use RQ_NET_PENDING for this purpose.

drbd/drbd_req.c
@@ -606,11 +606,43 @@ static void advance_conn_req_next(struct drbd_connection *connection, struct drb
 			found_req = req;
 			break;
 		}
+		/* Found a request which is for this peer but not yet queued.
+		 * Do not skip past it. */
+		if (s & RQ_NET_PENDING && !(s & RQ_NET_SENT))
+			break;
 	}
 	rcu_read_unlock();
 	connection->todo.req_next = found_req;
 }
 
+/**
+ * set_cache_ptr_if_null() - Set caching pointer to given request if not currently set.
+ * @cache_ptr: Pointer to set.
+ * @req: Request to potentially set the pointer to.
+ *
+ * The caching pointer system is designed to track the oldest request in the
+ * transfer log fulfilling some condition. In particular, a combination of
+ * flags towards a given peer. This condition must guarantee that the request
+ * will not be destroyed.
+ *
+ * This system is implemented by set_cache_ptr_if_null() and
+ * advance_cache_ptr(). A request must be in the transfer log and fulfil the
+ * condition before set_cache_ptr_if_null() is called. If
+ * set_cache_ptr_if_null() is called before this request is in the transfer log
+ * or before it fulfils the condition, the pointer may be advanced past this
+ * request, or unset, which also has the effect of skipping the request.
+ *
+ * Once the condition is no longer fulfilled for a request, advance_cache_ptr()
+ * must be called. If the caching pointer currently points to this request,
+ * this will advance it to the next request fulfilling the condition.
+ *
+ * set_cache_ptr_if_null() may be called concurrently with itself and with
+ * advance_cache_ptr(). However, advance_cache_ptr() must not be called
+ * concurrently for a given caching pointer. If it were, the call for the older
+ * request may advance the pointer to the newer request, although the newer
+ * request has concurrently been modified such that it no longer fulfils the
+ * condition.
+ */
 static void set_cache_ptr_if_null(struct drbd_request **cache_ptr, struct drbd_request *req)
 {
 	struct drbd_request *prev_req, *old_req = NULL;
@@ -626,6 +658,7 @@ static void set_cache_ptr_if_null(struct drbd_request **cache_ptr, struct drbd_r
 	rcu_read_unlock();
 }
 
+/* See set_cache_ptr_if_null(). */
 static void advance_cache_ptr(struct drbd_connection *connection,
 			      struct drbd_request __rcu **cache_ptr, struct drbd_request *req,
 			      unsigned int is_set, unsigned int is_clear)
@@ -949,7 +982,7 @@ void __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		mod_rq_state(req, m, peer_device, RQ_LOCAL_PENDING, RQ_LOCAL_COMPLETED);
 		break;
 
-	case QUEUE_FOR_NET_READ:
+	case NEW_NET_READ:
 		/* READ, and
 		 * no local disk,
 		 * or target area marked as invalid,
@@ -967,10 +1000,10 @@ void __req_mod(struct drbd_request *req, enum drbd_req_event what,
 
 		D_ASSERT(device, !(req->net_rq_state[idx] & RQ_NET_MASK));
 		D_ASSERT(device, !(req->local_rq_state & RQ_LOCAL_MASK));
-		mod_rq_state(req, m, peer_device, 0, RQ_NET_PENDING|RQ_NET_QUEUED);
+		mod_rq_state(req, m, peer_device, 0, RQ_NET_PENDING);
 		break;
 
-	case QUEUE_FOR_NET_WRITE:
+	case NEW_NET_WRITE:
 		/* assert something? */
 		/* from __drbd_make_request only */
 
@@ -991,7 +1024,7 @@ void __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		D_ASSERT(device, !(req->net_rq_state[idx] & RQ_NET_MASK));
 
 		/* queue work item to send data */
-		mod_rq_state(req, m, peer_device, 0, RQ_NET_PENDING|RQ_NET_QUEUED|RQ_EXP_BARR_ACK|
+		mod_rq_state(req, m, peer_device, 0, RQ_NET_PENDING|RQ_EXP_BARR_ACK|
 				drbd_protocol_state_bits(peer_device->connection));
 
 		/* Close the epoch, in case it outgrew the limit.
@@ -1016,7 +1049,11 @@ void __req_mod(struct drbd_request *req, enum drbd_req_event what,
 			start_new_tl_epoch(device->resource);
 		break;
 
-	case QUEUE_FOR_SEND_OOS:
+	case NEW_NET_OOS:
+		mod_rq_state(req, m, peer_device, 0, RQ_NET_PENDING);
+		break;
+
+	case ADDED_TO_TRANSFER_LOG:
 		mod_rq_state(req, m, peer_device, 0, RQ_NET_QUEUED);
 		break;
 
@@ -1043,9 +1080,9 @@ void __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		break;
 
 	case OOS_HANDED_TO_NETWORK:
-		/* Was not set PENDING, no longer QUEUED, so is now DONE
+		/* No longer PENDING or QUEUED, so is now DONE
 		 * as far as this connection is concerned. */
-		mod_rq_state(req, m, peer_device, RQ_NET_QUEUED, RQ_NET_DONE);
+		mod_rq_state(req, m, peer_device, RQ_NET_PENDING|RQ_NET_QUEUED, RQ_NET_DONE);
 		break;
 
 	case CONNECTION_LOST:
@@ -1553,12 +1590,23 @@ static int drbd_process_write_request(struct drbd_request *req)
 
 		if (remote) {
 			++count;
-			_req_mod(req, QUEUE_FOR_NET_WRITE, peer_device);
+			_req_mod(req, NEW_NET_WRITE, peer_device);
 		} else
-			_req_mod(req, QUEUE_FOR_SEND_OOS, peer_device);
+			_req_mod(req, NEW_NET_OOS, peer_device);
 	}
 
 	return count;
+}
+
+static void drbd_queue_request(struct drbd_request *req)
+{
+	struct drbd_device *device = req->device;
+	struct drbd_peer_device *peer_device;
+
+	for_each_peer_device(peer_device, device) {
+		if (req->net_rq_state[peer_device->node_id] & RQ_NET_PENDING)
+			_req_mod(req, ADDED_TO_TRANSFER_LOG, peer_device);
+	}
 }
 
 static void drbd_process_discard_or_zeroes_req(struct drbd_request *req, int flags)
@@ -1894,7 +1942,7 @@ static void drbd_send_and_submit(struct drbd_device *device, struct drbd_request
 				no_remote = true;
 		} else {
 			if (peer_device)
-				_req_mod(req, QUEUE_FOR_NET_READ, peer_device);
+				_req_mod(req, NEW_NET_READ, peer_device);
 			else
 				no_remote = true;
 		}
@@ -1902,6 +1950,11 @@ static void drbd_send_and_submit(struct drbd_device *device, struct drbd_request
 		/* req may now be accessed by other threads - do not modify
 		 * "immutable" fields after this point */
 		list_add_tail_rcu(&req->tl_requests, &resource->transfer_log);
+
+		/* Do this after adding to the transfer log so that the
+		 * caching pointer req_not_net_done is set if
+		 * necessary. */
+		drbd_queue_request(req);
 	}
 	spin_unlock(&resource->tl_update_lock);
 

drbd/drbd_req.h
@@ -66,12 +66,10 @@
 enum drbd_req_event {
 	TO_BE_SUBMITTED,
 
-	/* XXX yes, now I am inconsistent...
-	 * these are not "events" but "actions"
-	 * oh, well... */
-	QUEUE_FOR_NET_WRITE,
-	QUEUE_FOR_NET_READ,
-	QUEUE_FOR_SEND_OOS,
+	NEW_NET_READ,
+	NEW_NET_WRITE,
+	NEW_NET_OOS,
+	ADDED_TO_TRANSFER_LOG,
 
 	/* For an empty flush, mark that a corresponding barrier has been sent
 	 * to this peer. This causes it to complete "successfully", even if the
@@ -120,13 +118,11 @@ enum drbd_req_state_bits {
 	 * <none>:
 	 *   No network required, or not yet processed.
 	 * pending:
-	 *   Intended for this peer, but connection lost. If IO is suspended,
-	 *   it will stay in this state until the connection is restored or IO
-	 *   is resumed.
+	 *   Intended for this peer, but not yet queued, or connection lost. If
+	 *   IO is suspended, it will stay in this state until the connection
+	 *   is restored or IO is resumed.
 	 * pending,queued:
 	 *   To be sent, on transfer log to be processed by sender.
-	 * queued:
-	 *   Queued for sending P_OUT_OF_SYNC.
 	 * pending,sent:
 	 *   Sent, expecting P_RECV_ACK (B) or P_WRITE_ACK (C).
 	 * sent,ok:

drbd/drbd_sender.c
@@ -2617,9 +2617,12 @@ static struct drbd_request *__next_request_for_connection(
 	list_for_each_entry_rcu(req, &connection->resource->transfer_log, tl_requests) {
 		unsigned s = req->net_rq_state[connection->peer_node_id];
 		connection->send.seen_dagtag_sector = req->dagtag_sector;
-		if (!(s & RQ_NET_QUEUED))
-			continue;
-		return req;
+		if (s & RQ_NET_QUEUED)
+			return req;
+		/* Found a request which is for this peer but not yet queued.
+		 * Do not skip past it. */
+		if (s & RQ_NET_PENDING && !(s & RQ_NET_SENT))
+			return NULL;
 	}
 	return NULL;
 }