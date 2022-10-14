7b09ce0ff6034e6e41d15f04107f8f930a391815
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2022-10-14 14:35:16 +0200

    compat: test and patch for strscpy

---
+++ drbd/drbd-kernel-compat/tests/have_strscpy.c
@@ -0,0 +1,6 @@
+/* { "version": "v4.2-rc2", "commit": "30035e45753b708e7d47a98398500ca005e02b86", "comment": "strscpy was introduced as a replacement for strlcpy", "author": "Chris Metcalf <cmetcalf@ezchip.com>", "date": "Wed Apr 29 12:52:04 2015 -0400" } */
+
+ssize_t foo(char *dest, const char *src, size_t count)
+{
+	return strscpy(dest, src, count);
+}
