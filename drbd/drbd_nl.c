1ee94e099dd259bcd7fe02d17c027f59bf1186a5
Uladzislau Rezki (Sony) <urezki@gmail.com>
2023-02-01 16:08:08 +0100

    drbd: Rename kvfree_rcu() to kvfree_rcu_mightsleep()
    The kvfree_rcu() macro's single-argument form is deprecated.  Therefore
    switch to the new kvfree_rcu_mightsleep() variant. The goal is to
    avoid accidental use of the single-argument forms, which can introduce
    functionality bugs in atomic contexts and latency bugs in non-atomic
    contexts.
    
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Philipp Reisner <philipp.reisner@linbit.com>
    Cc: Lars Ellenberg <lars.ellenberg@linbit.com>
    Begrudgingly-acked-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>

--- drbd/drbd_nl.c
+++ drbd/drbd_nl.c
@@ -2297,7 +2297,7 @@ int drbd_adm_disk_opts(struct sk_buff *skb, struct genl_info *info)
 			drbd_send_sync_param(peer_device);
 	}
 
-	kvfree_rcu(old_disk_conf);
+	kvfree_rcu_mightsleep(old_disk_conf);
 	mod_timer(&device->request_timer, jiffies + HZ);
 	goto success;
 
@@ -3729,7 +3729,7 @@ int drbd_adm_net_opts(struct sk_buff *skb, struct genl_info *info)
 
 	mutex_unlock(&connection->mutex[DATA_STREAM]);
 	mutex_unlock(&connection->resource->conf_update);
-	kvfree_rcu(old_net_conf);
+	kvfree_rcu_mightsleep(old_net_conf);
 
 	if (connection->cstate[NOW] >= C_CONNECTED) {
 		struct drbd_peer_device *peer_device;
@@ -3861,7 +3861,7 @@ int drbd_adm_peer_device_opts(struct sk_buff *skb, struct genl_info *info)
 
 	rcu_assign_pointer(peer_device->conf, new_peer_device_conf);
 
-	kvfree_rcu(old_peer_device_conf);
+	kvfree_rcu_mightsleep(old_peer_device_conf);
 	kfree(old_plan);
 
 	if (0) {
@@ -4832,7 +4832,7 @@ int drbd_adm_resize(struct sk_buff *skb, struct genl_info *info)
 		new_disk_conf->disk_size = (sector_t)rs.resize_size;
 		rcu_assign_pointer(device->ldev->disk_conf, new_disk_conf);
 		mutex_unlock(&device->resource->conf_update);
-		kvfree_rcu(old_disk_conf);
+		kvfree_rcu_mightsleep(old_disk_conf);
 		new_disk_conf = NULL;
 	}
 
@@ -7143,7 +7143,7 @@ int drbd_adm_rename_resource(struct sk_buff *skb, struct genl_info *info)
 	}
 	old_res_name = resource->name;
 	resource->name = new_res_name;
-	kvfree_rcu(old_res_name);
+	kvfree_rcu_mightsleep(old_res_name);
 
 	drbd_debugfs_resource_rename(resource, new_res_name);
 
