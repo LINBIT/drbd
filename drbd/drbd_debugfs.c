LINBIT/drbd

commit decdceee8d505dbf259cd3cb81c935098d9141df

drbd: debugfs: iterate over connections in resource//in_flight_summary
In resource//in_flight_summary,
we showed transport stats for the "first_connection" only.

Iterate over all connections
and use a table format to present the information.

drbd/drbd_debugfs.c
@@ -430,7 +430,6 @@ static void seq_print_resource_transfer_log_summary(struct seq_file *m,
 	seq_printf(m, "%u total\n", count);
 }
 
-/* TODO: transfer_log and friends should be moved to resource */
 static int resource_in_flight_summary_show(struct seq_file *m, void *pos)
 {
 	struct drbd_resource *resource = m->private;
@@ -440,15 +439,8 @@ static int resource_in_flight_summary_show(struct seq_file *m, void *pos)
 	ktime_t now = ktime_get();
 	unsigned long jif = jiffies;
 
-	connection = first_connection(resource);
-	transport = &connection->transport;
-	/* This does not happen, actually.
-	 * But be robust and prepare for future code changes. */
-	if (!connection || !kref_get_unless_zero(&connection->kref))
-		return -ESTALE;
-
 	/* BUMP me if you change the file format/content/presentation */
-	seq_printf(m, "v: %u\n\n", 0);
+	seq_printf(m, "v: %u\n\n", 1);
 
 	seq_puts(m, "oldest bitmap IO\n");
 	seq_print_resource_pending_bitmap_io(m, resource, jif);
@@ -459,14 +451,23 @@ static int resource_in_flight_summary_show(struct seq_file *m, void *pos)
 	seq_putc(m, '\n');
 
 	seq_puts(m, "transport buffer stats\n");
-	/* for each connection ... once we have more than one */
+	seq_puts(m, "peer\ttransport class\tunread receive buffer\tunacked send buffer\n");
 	rcu_read_lock();
-	if (transport->ops->stream_ok(transport, DATA_STREAM)) {
-		transport->ops->stats(transport, &transport_stats);
-		seq_printf(m, "unread receive buffer: %u Byte\n",
-				transport_stats.unread_received);
-		seq_printf(m, "unacked send buffer: %u Byte\n",
+	for_each_connection_rcu(connection, resource) {
+		char *name;
+
+		transport = &connection->transport;
+		name = rcu_dereference(transport->net_conf)->name;
+		seq_printf(m, "%s\t%s\t", name, transport->class->name);
+
+		if (transport->ops->stream_ok(transport, DATA_STREAM)) {
+			transport->ops->stats(transport, &transport_stats);
+			seq_printf(m, "%u\t%u\n",
+				transport_stats.unread_received,
 				transport_stats.unacked_send);
+		} else {
+			seq_printf(m, "-\t-\n");
+		}
 	}
 	rcu_read_unlock();
 	seq_putc(m, '\n');

commit 83138dd3f92800e6338910ae87107375394b18a8

drbd: debugfs: remove kref_put() on undefined connection
Reading "in_flight_summary" in debugfs caused an Oops:

[   23.410052] BUG: unable to handle kernel paging request at 0000000100000010
...
[   23.416967] RIP: 0010:drbd_destroy_connection+0x52/0xf0 [drbd]
...
[   23.441445] Call Trace:
[   23.442160]  resource_in_flight_summary_show+0x73d/0x820 [drbd]

Fix this by not using connection outside of the loop where it is
defined.

Fixes: decdceee8d50 ("drbd: debugfs: iterate over connections in resource//in_flight_summary")

drbd/drbd_debugfs.c
@@ -356,7 +356,6 @@ static void seq_print_resource_pending_peer_requests(struct seq_file *m,
 
 static void seq_print_resource_transfer_log_summary(struct seq_file *m,
 	struct drbd_resource *resource,
-	struct drbd_connection *connection,
 	ktime_t now, unsigned long jif)
 {
 	struct drbd_request *req;
@@ -479,13 +478,12 @@ static int resource_in_flight_summary_show(struct seq_file *m, void *pos)
 	seq_putc(m, '\n');
 
 	seq_puts(m, "oldest application requests\n");
-	seq_print_resource_transfer_log_summary(m, resource, connection, now, jif);
+	seq_print_resource_transfer_log_summary(m, resource, now, jif);
 	seq_putc(m, '\n');
 
 	jif = jiffies - jif;
 	if (jif)
 		seq_printf(m, "generated in %d ms\n", jiffies_to_msecs(jif));
-	kref_put(&connection->kref, drbd_destroy_connection);
 	return 0;
 }
 