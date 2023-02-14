LINBIT/drbd

commit cf32d08baf6e4ced3659a5aa34d5af03b2001a36

drbd: Update the peer role with the reachability packets
Before the peer_role was updated with the P_STATE packtes.
When a peer becomes secondary can also be derived from the
P_PRI_REACHABLE packtes. Since we have the latter one we
need to use them in order to make sure we act consistent
with handling the weakness, and the peer's state.

drbd/drbd_receiver.c
@@ -5066,17 +5066,6 @@ STATIC int receive_state(struct drbd_connection *connection, struct packet_info 
 
 	peer_state.i = be32_to_cpu(p->state);
 
-	if (!peer_device) {
-		unsigned long irq_flags;
-
-		begin_state_change(resource, &irq_flags, CS_VERBOSE);
-		__change_peer_role(connection, peer_state.role);
-		rv = end_state_change(resource, &irq_flags);
-		if (rv < SS_SUCCESS)
-			return -EIO;
-		return 0;
-	}
-
 	peer_disk_state = peer_state.disk;
 	if (peer_state.disk == D_NEGOTIATING) {
 		peer_disk_state = peer_device->uuid_flags & UUID_FLAG_INCONSISTENT ?
@@ -5213,7 +5202,8 @@ STATIC int receive_state(struct drbd_connection *connection, struct packet_info 
 	}
 	clear_bit(CONSIDER_RESYNC, &peer_device->flags);
 	__change_repl_state(peer_device, new_repl_state);
-	__change_peer_role(connection, peer_state.role);
+	if (connection->peer_role[NOW] == R_UNKNOWN)
+		__change_peer_role(connection, peer_state.role);
 	__change_peer_weak(connection, peer_state.weak);
 	__change_peer_disk_state(peer_device, peer_disk_state);
 	__change_resync_susp_peer(peer_device, peer_state.aftr_isp | peer_state.user_isp);
@@ -5762,12 +5752,16 @@ static int receive_reachability(struct drbd_connection *connection, struct packe
 {
 	struct drbd_resource *resource = connection->resource;
 	const int my_node_id = resource->res_opts.node_id;
+	const int peer_node_id = connection->net_conf->peer_node_id;
 	struct p_pri_reachable *p = pi->data;
 	unsigned long irq_flags;
 
 	begin_state_change(resource, &irq_flags, CS_VERBOSE);
 	connection->primary_mask = be64_to_cpu(p->primary_mask) & ~(1ULL << my_node_id);
 	__change_weak(resource, drbd_calc_weak(resource));
+	if (!(connection->primary_mask & NODE_MASK(peer_node_id)) &&
+	    connection->peer_role[NOW] != R_SECONDARY)
+		__change_peer_role(connection, R_SECONDARY);
 	end_state_change(resource, &irq_flags);
 
 	return 0;

drbd/drbd_int.h
@@ -1181,9 +1181,7 @@ extern void drbd_gen_and_send_sync_uuid(struct drbd_peer_device *);
 extern int drbd_attach_peer_device(struct drbd_peer_device *);
 extern int drbd_send_sizes(struct drbd_peer_device *, int trigger_reply, enum dds_flags flags);
 extern int drbd_send_state(struct drbd_peer_device *, union drbd_state);
-extern int conn_send_state(struct drbd_connection *, union drbd_state);
 extern int drbd_send_current_state(struct drbd_peer_device *);
-extern int conn_send_current_state(struct drbd_connection *, bool);
 extern int drbd_send_sync_param(struct drbd_peer_device *);
 extern void drbd_send_b_ack(struct drbd_connection *connection, u32 barrier_nr, u32 set_size);
 extern int drbd_send_ack(struct drbd_peer_device *, enum drbd_packet,

drbd/drbd_main.c
@@ -1155,37 +1155,6 @@ int drbd_send_current_state(struct drbd_peer_device *peer_device)
 	return drbd_send_state(peer_device, drbd_get_peer_device_state(peer_device, NOW));
 }
 
-int conn_send_current_state(struct drbd_connection *connection, bool conf_update_locked)
-{
-	if (connection->agreed_pro_version >= 100 &&
-	    connection->agreed_pro_version < 110) {
-		struct drbd_resource *resource = connection->resource;
-		struct drbd_peer_device *peer_device;
-		int vnr;
-
-		/* We are talking to drbd 8.4, which does not understand this
-		 * packet type.  Send one old-style packet per peer device
-		 * instead.  (Protocols before that don't support volumes, and
-		 * they will be happy with the one packet that drbd 9 uses.
-		 */
-
-		if (!conf_update_locked)
-			mutex_lock(&resource->conf_update);
-		idr_for_each_entry(&connection->peer_devices, peer_device, vnr) {
-			int rv;
-
-			rv = drbd_send_current_state(peer_device);
-			if (rv)
-				break;
-		}
-		if (!conf_update_locked)
-			mutex_unlock(&resource->conf_update);
-		return 0;
-	}
-
-	return conn_send_state(connection, drbd_get_connection_state(connection, NOW));
-}
-
 /**
  * drbd_send_state() - Sends the drbd state to the peer
  * @device:	DRBD device.
@@ -1206,19 +1175,6 @@ int drbd_send_state(struct drbd_peer_device *peer_device, union drbd_state state
 
 	p->state = cpu_to_be32(state.i); /* Within the send mutex */
 	return drbd_send_command(peer_device, sock, P_STATE, sizeof(*p), NULL, 0);
-}
-
-int conn_send_state(struct drbd_connection *connection, union drbd_state state)
-{
-	struct drbd_socket *sock;
-	struct p_state *p;
-
-	sock = &connection->data;
-	p = conn_prepare_command(connection, sock);
-	if (!p)
-		return -EIO;
-	p->state = cpu_to_be32(state.i); /* Within the send mutex */
-	return send_command(connection, -1, sock, P_STATE, sizeof(*p), NULL, 0);
 }
 
 int conn_send_state_req(struct drbd_connection *connection, int vnr, enum drbd_packet cmd,

drbd/drbd_nl.c
@@ -879,20 +879,6 @@ drbd_set_role(struct drbd_resource *resource, enum drbd_role role, bool force)
 		}
 	}
 
-	if (role == R_SECONDARY) {
-		struct drbd_connection *connection;
-
-		/* When changing our role from primary to secondary, we inform
-		 * peers about the state change with a per-connection packet:
-		 * otherwise, if no volumes were defined, the peer wouldn't be
-		 * notified.
-		 */
-		for_each_connection(connection, resource) {
-			if (connection->cstate[NOW] == C_CONNECTED)
-				conn_send_current_state(connection, true);
-		}
-	}
-
 	idr_for_each_entry(&resource->devices, device, minor) {
 		 struct drbd_peer_device *peer_device;
 

commit c3a6b2422276256cb15b06270ad48f2a3ef12470

drbd: allow peer to promote with the P_STATE packet
Normally, peer promotion occurs when the P_TWOPC_COMMIT packet is
received at the end of the two-phase commit. However, there are race
conditions that can cause P_STATE to be sent with the updated state
before P_TWOPC_COMMIT. Prior to this commit, such a race this could
cause the initial resync to be skipped.

Specifically this problem occurred because receive_state() only applied
the change of peer disk state to D_UP_TO_DATE, not the peer role change
to R_PRIMARY. Then when the P_TWOPC_COMMIT was received, only the peer
role was changed. As a result, neither state change fulfilled the
condition in finish_state_change() to set CONSIDER_RESYNC.

Such a race condition can occur when "resync-after" is used. The other
resource starting a resync causes our "resync-susp" status flags to be
changed. This change can occur while we are executing a two-phase
commit.

Fix this by allowing the peer role to be changed by receive_state(). We
trust our peer. If it claims to be Primary, then it must be in the
process of completing the promotion two-phase commit.

The exception removed by this commit was introduced by
cf32d08baf6e drbd: Update the peer role with the reachability packets

This commit was actually concerned with demotion rather than promotion.

As it happens, peer demotion with the P_STATE packet was then enabled in
1582135b67cc drbd: Use the P_STATE packet to inform peers about demotion

But promotion remained excluded. There never was a reason to disallow
peer promotion with P_STATE, so it is OK to enable it.

drbd/drbd_receiver.c
@@ -7475,8 +7475,7 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 		__change_disk_state(device, new_disk_state);
 	if (device->disk_state[NOW] != D_NEGOTIATING)
 		__change_repl_state(peer_device, new_repl_state);
-	if (connection->peer_role[NOW] == R_UNKNOWN || peer_state.role == R_SECONDARY)
-		__change_peer_role(connection, peer_state.role);
+	__change_peer_role(connection, peer_state.role);
 	if (peer_state.disk != D_NEGOTIATING)
 		__change_peer_disk_state(peer_device, peer_disk_state);
 	__change_resync_susp_peer(peer_device, peer_state.aftr_isp | peer_state.user_isp);