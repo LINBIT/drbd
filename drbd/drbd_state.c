LINBIT/drbd

commit 3e7fefb34a86d73e206ea1400a1de412f4548b8b

drbd: delay promotion until try_become_up_to_date() has run
This fixes a bug which caused an unexpected connection loss after a
Primary is isolated and one of the remaining nodes promotes quickly
afterwards. The characteristic portion of the log looks like:

19:24:49 n25 drbd res: role( Secondary -> Primary )
19:24:49 n25 drbd res: State change failed: Need access to UpToDate data
19:24:49 n25 drbd res/0 drbd100 n21: Failed: pdsk( UpToDate -> Consistent )
19:24:49 n25 drbd res n21: conn( Connected -> Disconnecting ) peer( Secondary -> Unknown )
19:24:49 n25 drbd res/0 drbd100: quorum( yes -> no )
19:24:49 n25 drbd res/0 drbd100 n21: pdsk( UpToDate -> DUnknown ) repl( Established -> Off )
19:24:49 n25 drbd res n21: error receiving P_STATE, e: -5 l: 0!

The steps to trigger this bug are as follows:
* Nodes A, B and C, all with disk and connected.
* Node B is Primary.
* Node B is isolated from network.
* Nodes A and C each notice that B is no longer reachable and change
  their disk state to Consistent until they can determine whether they
  are UpToDate or Outdated.
* Node C is promoted. This succeeds because C has not yet received the
  P_STATE packet from A which would indicate that A is no longer
  UpToDate. That is, C still has a peer-disk state of UpToDate for A.
* The P_STATE packet from A arrives at C. Since C is Primary and its
  disk is still Consistent, this state change is rejected. C drops its
  connection to A.

A natural fix for this would be to ensure that node A sends its P_STATE
packet before acknowledging the two-phase commit for C's promotion.
However this does not help because P_STATE is sent on the data socket,
while P_TWOPC_YES is sent on the control socket. So they can arrive in
either order.

Instead we fix it by delaying promotion until try_become_up_to_date()
has run. That way we at least know the new state of our own disk, which
is sufficient in this case.

drbd/drbd_state.c
@@ -1551,6 +1551,22 @@ static enum drbd_state_rv __is_valid_soft_transition(struct drbd_resource *resou
 	/* See drbd_state_sw_errors in drbd_strings.c */
 
 	if (role[OLD] != R_PRIMARY && role[NEW] == R_PRIMARY) {
+		/* SS_NO_UP_TO_DATE_DISK is not quite accurate here. We may
+		 * have an up-to-date peer. However, we cannot trust that
+		 * information. TRY_BECOME_UP_TO_DATE_PENDING is set when we
+		 * have lost our connection to a primary peer. It is possible
+		 * that the "up-to-date peer" has also lost its connection to
+		 * the primary peer, but we have not yet received the
+		 * corresponding P_STATE packet.
+		 *
+		 * So return SS_NO_UP_TO_DATE_DISK conservatively. This causes
+		 * drbd_set_role() to release state_sem and wait, which gives
+		 * try_become_up_to_date() a chance to run. That will set our
+		 * disk state appropriately depending on whether our partition
+		 * is isolated from the original primary peer. */
+		if (test_bit(TRY_BECOME_UP_TO_DATE_PENDING, &resource->flags))
+			return SS_NO_UP_TO_DATE_DISK;
+
 		for_each_connection_rcu(connection, resource) {
 			struct net_conf *nc;
 
@@ -2413,6 +2429,13 @@ static bool has_starting_resyncs(struct drbd_connection *connection)
 	return false;
 }
 
+static bool should_try_become_up_to_date(struct drbd_device *device, enum drbd_disk_state *disk_state,
+		enum which_state which)
+{
+	return disk_state[OLD] == D_UP_TO_DATE && disk_state[NEW] == D_CONSISTENT &&
+			may_return_to_up_to_date(device, which);
+}
+
 /**
  * finish_state_change  -  carry out actions triggered by a state change
  */
@@ -2791,6 +2814,11 @@ static void finish_state_change(struct drbd_resource *resource)
 
 		if (role[OLD] == R_PRIMARY && role[NEW] == R_SECONDARY)
 			clear_bit(NEW_CUR_UUID, &device->flags);
+
+		/* The NEW state version here is the same as the NOW version in
+		 * the context of w_after_state_change(). */
+		if (should_try_become_up_to_date(device, disk_state, NEW))
+			set_bit(TRY_BECOME_UP_TO_DATE_PENDING, &resource->flags);
 	}
 
 	for_each_connection(connection, resource) {
@@ -3956,8 +3984,10 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 		if (disk_state[OLD] == D_UP_TO_DATE && disk_state[NEW] == D_INCONSISTENT)
 			send_new_state_to_all_peer_devices(state_change, n_device);
 
-		if (disk_state[OLD] == D_UP_TO_DATE && disk_state[NEW] == D_CONSISTENT &&
-		    may_return_to_up_to_date(device, NOW))
+		/* We cannot test the flag TRY_BECOME_UP_TO_DATE_PENDING here
+		 * because that would cause us to queue the work more often
+		 * than necessary. */
+		if (should_try_become_up_to_date(device, disk_state, NOW))
 			try_become_up_to_date = true;
 
 		if (test_bit(TRY_TO_GET_RESYNC, &device->flags)) {

drbd/drbd_int.h
@@ -794,6 +794,9 @@ enum resource_flag {
 	TWOPC_ABORT_LOCAL,
 	TWOPC_EXECUTED,         /* Commited or aborted */
 	TWOPC_STATE_CHANGE_PENDING, /* set between sending commit and changing local state */
+	TRY_BECOME_UP_TO_DATE_PENDING,  /* set when we change our disk state to
+		* D_CONSISTENT until we have determined whether we can return to
+		* being D_UP_TO_DATE */
 	DEVICE_WORK_PENDING,	/* tell worker that some device has pending work */
 	PEER_DEVICE_WORK_PENDING,/* tell worker that some peer_device has pending work */
 	RESOURCE_WORK_PENDING,  /* tell worker that some peer_device has pending work */

drbd/drbd_nl.c
@@ -961,6 +961,9 @@ static bool after_primary_lost_events_settled(struct drbd_resource *resource)
 	struct drbd_device *device;
 	int vnr;
 
+	if (test_bit(TRY_BECOME_UP_TO_DATE_PENDING, &resource->flags))
+		return false;
+
 	rcu_read_lock();
 	idr_for_each_entry(&resource->devices, device, vnr) {
 		enum drbd_disk_state disk_state = device->disk_state[NOW];

drbd/drbd_sender.c
@@ -2393,6 +2393,8 @@ static int try_become_up_to_date(struct drbd_resource *resource)
 		up(&resource->state_sem);
 		if (rv == SS_TIMEOUT || rv == SS_CONCURRENT_ST_CHG)
 			goto repost;
+		clear_bit(TRY_BECOME_UP_TO_DATE_PENDING, &resource->flags);
+		wake_up_all(&resource->state_wait);
 		drbd_notify_peers_lost_primary(resource);
 	} else {
 	repost:

commit f2ca948f2af036923d91a2a7d5943a0a93e133b0

drbd: allow state change to continue when twopc pending after lost peer
The commit
9e4e702b580f drbd: initiate an empty two-phase-commit after a secondary peer crashed
caused the flag TWOPC_AFTER_LOST_PEER_PENDING to be set when a peer is
lost or disconnects.

The commit
3e7fefb34a86 drbd: delay promotion until try_become_up_to_date() has run
caused SS_NO_UP_TO_DATE_DISK to be returned from the state change
attempt when TWOPC_AFTER_LOST_PEER_PENDING was set.

The commit
de32acd7ad1f drbd: Only allow local promotion if fencing of all peers was successful
caused CS_FP_LOCAL_UP_TO_DATE to be set when the state change return
value is SS_NO_UP_TO_DATE_DISK and we have no peers to fence.

This combination of commits made the following sequence with nodes A, B
and C possible:
* A is primary
* An attempt is started to promote C. This waits in the "rv ==
  SS_TWO_PRIMARIES && !retried_ss_two_primaries" case of
  drbd_set_role().
* A is demoted and gracefully disconnected from C and B
* C sets TWOPC_AFTER_LOST_PEER_PENDING due to the disconnect of A, but
  does not run twopc_after_lost_peer() because state_sem is held by the
  promoting thread.
* C continues in drbd_set_role() and retries the promotion state change.
* This state change fails with SS_NO_UP_TO_DATE_DISK due to the
  TWOPC_AFTER_LOST_PEER_PENDING flag.
* The fencing case triggers, even though there is no fencing occurring
  here, causing CS_FP_LOCAL_UP_TO_DATE to be set.
* C promotes successfully, and generates a new UUID to due
  CS_FP_LOCAL_UP_TO_DATE, which is sent to B without
  UUID_FLAG_NEW_DATAGEN.
* B receives this new UUID, but due to the missing flag does not update
  its own current UUID.
* B and C are now both UpToDate and have different current UUIDs, but
  they do not determine any reason to trigger a resync.
* This causes problems later, such as spurious "split-brain" between A
  and B.

This code was added as an additional precaution. It is not needed to fix
the normal case for which commit 3e7fefb34a86 was introduced. The check
in after_primary_lost_events_settled() ensures that
try_become_up_to_date() runs before the attempted state change for the
promotion. Remove the code to prevent the sequence described above.

Fixes: 3e7fefb34a86

drbd/drbd_state.c
@@ -1579,22 +1579,6 @@ static enum drbd_state_rv __is_valid_soft_transition(struct drbd_resource *resou
 	/* See drbd_state_sw_errors in drbd_strings.c */
 
 	if (role[OLD] != R_PRIMARY && role[NEW] == R_PRIMARY) {
-		/* SS_NO_UP_TO_DATE_DISK is not quite accurate here. We may
-		 * have an up-to-date peer. However, we cannot trust that
-		 * information. TWOPC_AFTER_LOST_PEER_PENDING is set when we
-		 * have lost our connection to a primary peer. It is possible
-		 * that the "up-to-date peer" has also lost its connection to
-		 * the primary peer, but we have not yet received the
-		 * corresponding P_STATE packet.
-		 *
-		 * So return SS_NO_UP_TO_DATE_DISK conservatively. This causes
-		 * drbd_set_role() to release state_sem and wait, which gives
-		 * try_become_up_to_date() a chance to run. That will set our
-		 * disk state appropriately depending on whether our partition
-		 * is isolated from the original primary peer. */
-		if (test_bit(TWOPC_AFTER_LOST_PEER_PENDING, &resource->flags))
-			return SS_NO_UP_TO_DATE_DISK;
-
 		for_each_connection_rcu(connection, resource) {
 			struct net_conf *nc;
 