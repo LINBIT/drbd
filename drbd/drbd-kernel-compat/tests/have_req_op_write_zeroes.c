00b63d9dfb200f557d6f0d391f77fe37fb951761
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2023-04-11 15:12:20 +0200

    compat: fix req_op_write_zeroes test
    Some kernels have enum req_op, while others have enum req_opf. We only
    care about the constant here, so just use an int.

--- drbd/drbd-kernel-compat/tests/have_req_op_write_zeroes.c
+++ drbd/drbd-kernel-compat/tests/have_req_op_write_zeroes.c
@@ -1,3 +1,5 @@
+/* { "version": "v4.10-rc1", "commit": "a6f0788ec2881ac14e97ff7fa6a78a807f87b5ba", "comment": "add support for REQ_OP_WRITE_ZEROES", "author": "Chaitanya Kulkarni <chaitanya.kulkarni@hgst.com>", "date": "Wed Nov 30 12:28:59 2016 -0800" } */
+
 #include <linux/blk_types.h>
 
-enum req_opf dummy = REQ_OP_WRITE_ZEROES;
+int dummy = REQ_OP_WRITE_ZEROES;
