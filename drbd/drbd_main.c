LINBIT/drbd

commit b2e87c5b2eebcd4620b714f79c155c5bef9a933b

drbd: Do not call debugfs_remove() while holding a spin_lock

drbd/drbd_main.c
@@ -3367,15 +3367,19 @@ void drbd_unregister_connection(struct drbd_connection *connection)
 {
 	struct drbd_resource *resource = connection->resource;
 	struct drbd_peer_device *peer_device;
+	LIST_HEAD(work_list);
 	int vnr;
 
 	spin_lock_irq(&resource->req_lock);
 	idr_for_each_entry(&connection->peer_devices, peer_device, vnr) {
-		drbd_debugfs_peer_device_cleanup(peer_device);
 		list_del_rcu(&peer_device->peer_devices);
+		list_add(&peer_device->peer_devices, &work_list);
 	}
 	list_del_rcu(&connection->connections);
 	spin_unlock_irq(&resource->req_lock);
+
+	list_for_each_entry(peer_device, &work_list, peer_devices)
+		drbd_debugfs_peer_device_cleanup(peer_device);
 	drbd_debugfs_connection_cleanup(connection);
 }
 

commit b1c150b2f839e74eae9027e88355d58ce37f13df

drbd: do not re-use peer_device->peer_devices after list_del_rcu()
Another thread may be traversing the list. This thread needs the
list_head to continue its traversal. If we re-use the list_head for a
list on our stack, then the other thread will access our stack as if the
next peer device were there.

This caused crashes. Specifically when drbd_unregister_connection() was
called while drbd_ldev_destroy() was running, panics such as the
following were observed:

BUG: unable to handle kernel NULL pointer dereference at 000000000000005a
IP: [<ffffffffc08e9464>] lc_destroy+0x14/0x80 [drbd]
...
Oops: 0000 [#1] SMP
...
Workqueue: events drbd_ldev_destroy [drbd]
...
Call Trace:
[<ffffffffc08eb334>] drbd_ldev_destroy+0x54/0x100 [drbd]
[<ffffffffb5ebdfdf>] process_one_work+0x17f/0x440
[<ffffffffb5ebf0f6>] worker_thread+0x126/0x3c0

Fixes: b2e87c5b2eeb ("drbd: Do not call debugfs_remove() while holding a spin_lock")

drbd/drbd_main.c
@@ -4030,21 +4030,19 @@ void drbd_unregister_connection(struct drbd_connection *connection)
 {
 	struct drbd_resource *resource = connection->resource;
 	struct drbd_peer_device *peer_device;
-	LIST_HEAD(work_list);
 	int vnr, rr;
+
+	idr_for_each_entry(&connection->peer_devices, peer_device, vnr)
+		drbd_debugfs_peer_device_cleanup(peer_device);
 
 	write_lock_irq(&resource->state_rwlock);
 	set_bit(C_UNREGISTERED, &connection->flags);
 	smp_wmb();
-	idr_for_each_entry(&connection->peer_devices, peer_device, vnr) {
+	idr_for_each_entry(&connection->peer_devices, peer_device, vnr)
 		list_del_rcu(&peer_device->peer_devices);
-		list_add(&peer_device->peer_devices, &work_list);
-	}
 	list_del_rcu(&connection->connections);
 	write_unlock_irq(&resource->state_rwlock);
 
-	list_for_each_entry(peer_device, &work_list, peer_devices)
-		drbd_debugfs_peer_device_cleanup(peer_device);
 	drbd_debugfs_connection_cleanup(connection);
 
 	del_connect_timer(connection);