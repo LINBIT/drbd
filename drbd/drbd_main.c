LINBIT/drbd

commit 0aeea3432534ceb0cdb57572bb310029abdf2802

drbd: Convert del_timer*() to timer_shutdown*()
Based on upstream linux kernel commit
292a089d78d3 ("treewide: Convert del_timer*() to timer_shutdown*()").

Excerpt from the original message:

> Due to several bugs caused by timers being re-armed after they are
> shutdown and just before they are freed, a new state of timers was added
> called "shutdown".  After a timer is set to this state, then it can no
> longer be re-armed.

Unlike the original patch, this converts all appropriate callers of
del_timer_sync within DRBD.

drbd/drbd_main.c
@@ -4000,7 +4000,7 @@ void drbd_unregister_device(struct drbd_device *device)
 
 	destroy_workqueue(device->submit.wq);
 	device->submit.wq = NULL;
-	del_timer_sync(&device->request_timer);
+	timer_shutdown_sync(&device->request_timer);
 }
 
 void drbd_reclaim_device(struct rcu_head *rp)
@@ -4064,7 +4064,7 @@ void drbd_unregister_connection(struct drbd_connection *connection)
 
 void del_connect_timer(struct drbd_connection *connection)
 {
-	if (del_timer_sync(&connection->connect_timer)) {
+	if (timer_shutdown_sync(&connection->connect_timer)) {
 		kref_debug_put(&connection->kref_debug, 11);
 		kref_put(&connection->kref, drbd_destroy_connection);
 	}

drbd/drbd_nl.c
@@ -6375,9 +6375,9 @@ static int adm_del_resource(struct drbd_resource *resource)
 	drbd_debugfs_resource_cleanup(resource);
 	mutex_unlock(&resources_mutex);
 
-	del_timer_sync(&resource->twopc_timer);
-	del_timer_sync(&resource->peer_ack_timer);
-	del_timer_sync(&resource->repost_up_to_date_timer);
+	timer_shutdown_sync(&resource->twopc_timer);
+	timer_shutdown_sync(&resource->peer_ack_timer);
+	timer_shutdown_sync(&resource->repost_up_to_date_timer);
 	call_rcu(&resource->rcu, drbd_reclaim_resource);
 
 	mutex_lock(&notification_mutex);

commit 778cd89d143a29e6cee3f143523a5fcb235e2e33

drbd: use del_timer for connect_timer, not timer_shutdown
The newly invented "shutdown" timer state indicates that the timer
should no longer be able to be re-armed because it will be freed soon.

Upon first glance, this usage of del_timer seemed like a candidate for
conversion to timer_shutdown_sync, but it turns out I missed another
usage in drbd_receiver.c, in conn_disconnect().

The relatively subtle but disastrous effect of this is that, once a
connection is disconnected, it can no longer be connected because the
connect_timer cannot be re-armed.

So, we need to use both variants. In drbd_unregister_connection(), the
timer actually goes away "for good", so use timer_shutdown_sync there.
In conn_disconnect(), keep using del_timer.

Fixes: 0aeea3432534 ("drbd: Convert del_timer*() to timer_shutdown*()")

drbd/drbd_main.c
@@ -4018,6 +4018,22 @@ void drbd_reclaim_device(struct rcu_head *rp)
 	}
 }
 
+static void shutdown_connect_timer(struct drbd_connection *connection)
+{
+	if (timer_shutdown_sync(&connection->connect_timer)) {
+		kref_debug_put(&connection->kref_debug, 11);
+		kref_put(&connection->kref, drbd_destroy_connection);
+	}
+}
+
+void del_connect_timer(struct drbd_connection *connection)
+{
+	if (del_timer_sync(&connection->connect_timer)) {
+		kref_debug_put(&connection->kref_debug, 11);
+		kref_put(&connection->kref, drbd_destroy_connection);
+	}
+}
+
 /**
  * drbd_unregister_connection()  -  make a connection "invisible"
  *
@@ -4043,7 +4059,7 @@ void drbd_unregister_connection(struct drbd_connection *connection)
 
 	drbd_debugfs_connection_cleanup(connection);
 
-	del_connect_timer(connection);
+	shutdown_connect_timer(connection);
 
 	rr = drbd_free_peer_reqs(connection, &connection->done_ee, false);
 	if (rr)
@@ -4056,14 +4072,6 @@ void drbd_unregister_connection(struct drbd_connection *connection)
 	drbd_transport_shutdown(connection, DESTROY_TRANSPORT);
 	drbd_put_send_buffers(connection);
 	conn_free_crypto(connection);
-}
-
-void del_connect_timer(struct drbd_connection *connection)
-{
-	if (timer_shutdown_sync(&connection->connect_timer)) {
-		kref_debug_put(&connection->kref_debug, 11);
-		kref_put(&connection->kref, drbd_destroy_connection);
-	}
 }
 
 void drbd_reclaim_connection(struct rcu_head *rp)