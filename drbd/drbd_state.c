LINBIT/drbd

commit 501071ca86ede44d125a3688e0b11e2d6abf3539

drbd: move bitmap clearing from strategy_to_repl_state() to w_after_state_change()
Remove the side effects of strategy_to_repl_state() that require to down()
the uuid_sem. This is a preparation step for holding uuid_sem longer.

drbd/drbd_state.c
@@ -3506,6 +3506,27 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 			if (disk_state[OLD] == D_INCONSISTENT && disk_state[NEW] == D_UP_TO_DATE &&
 			    peer_disk_state[OLD] == D_INCONSISTENT && peer_disk_state[NEW] == D_UP_TO_DATE)
 				send_state_others = peer_device;
+
+			/* connect without resync or remote attach without resync */
+			if (disk_state[NOW] >= D_OUTDATED && repl_state[NEW] == L_ESTABLISHED &&
+			    ((repl_state[OLD] == L_OFF &&
+			      (peer_disk_state[NEW] >= D_OUTDATED ||
+			       (peer_disk_state[NEW] == D_DISKLESS && !want_bitmap(peer_device)))) ||
+			     (peer_disk_state[OLD] == D_DISKLESS && peer_disk_state[NEW] >= D_OUTDATED))) {
+				u64 peer_current_uuid = peer_device->current_uuid & ~UUID_PRIMARY;
+				u64 my_current_uuid = drbd_current_uuid(device) & ~UUID_PRIMARY;
+
+				if (peer_current_uuid == my_current_uuid && get_ldev(device)) {
+					down_write(&device->uuid_sem);
+					drbd_uuid_set_bitmap(peer_device, 0);
+					up_write(&device->uuid_sem);
+					drbd_print_uuids(peer_device, "cleared bm UUID and bitmap");
+					drbd_bitmap_io_from_worker(device, &drbd_bmio_clear_one_peer,
+								   "clearing bm one peer", BM_LOCK_CLEAR | BM_LOCK_BULK,
+								   peer_device);
+					put_ldev(device);
+				}
+			}
 		}
 
 		for (n_connection = 0; n_connection < state_change->n_connections; n_connection++) {
@@ -3525,16 +3546,6 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 			union drbd_state new_state =
 				state_change_word(state_change, n_device, n_connection, NEW);
 			bool send_uuids, send_state = false;
-
-			if (repl_state[OLD] == L_OFF &&
-			    (peer_disk_state[NOW] == D_UP_TO_DATE || !want_bitmap(peer_device)) &&
-			    drbd_bitmap_uuid(peer_device) && get_ldev(device)) {
-				down_write(&device->uuid_sem);
-				drbd_uuid_set_bitmap(peer_device, 0);
-				up_write(&device->uuid_sem);
-				drbd_print_uuids(peer_device, "cleared bm UUID");
-				put_ldev(device);
-			}
 
 			/* In case we finished a resync as resync-target update all neighbors
 			   about having a bitmap_uuid of 0 towards the previous sync-source.

drbd/drbd_int.h
@@ -1632,6 +1632,7 @@ extern int drbd_bmio_set_n_write(struct drbd_device *device, struct drbd_peer_de
 extern int drbd_bmio_clear_all_n_write(struct drbd_device *device, struct drbd_peer_device *) __must_hold(local);
 extern int drbd_bmio_set_all_n_write(struct drbd_device *device, struct drbd_peer_device *) __must_hold(local);
 extern int drbd_bmio_set_allocated_n_write(struct drbd_device *,struct drbd_peer_device *) __must_hold(local);
+extern int drbd_bmio_clear_one_peer(struct drbd_device *, struct drbd_peer_device *) __must_hold(local);
 extern bool drbd_device_stable(struct drbd_device *device, u64 *authoritative);
 extern void drbd_flush_peer_acks(struct drbd_resource *resource);
 extern void drbd_cork(struct drbd_connection *connection, enum drbd_stream stream);

drbd/drbd_main.c
@@ -5197,6 +5197,13 @@ int drbd_bmio_clear_all_n_write(struct drbd_device *device,
 	return drbd_bm_write(device, NULL);
 }
 
+int drbd_bmio_clear_one_peer(struct drbd_device *device,
+			     struct drbd_peer_device *peer_device) __must_hold(local)
+{
+	drbd_bm_clear_many_bits(peer_device, 0, -1UL);
+	return drbd_bm_write(device, NULL);
+}
+
 static int w_bitmap_io(struct drbd_work *w, int unused)
 {
 	struct bm_io_work *work =

drbd/drbd_receiver.c
@@ -4348,7 +4348,6 @@ static enum drbd_repl_state strategy_to_repl_state(struct drbd_peer_device *peer
 						   enum drbd_role peer_role,
 						   enum sync_strategy strategy)
 {
-	struct drbd_device *device = peer_device->device;
 	enum drbd_role role = peer_device->device->resource->role[NOW];
 	enum drbd_repl_state rv;
 
@@ -4369,27 +4368,7 @@ static enum drbd_repl_state strategy_to_repl_state(struct drbd_peer_device *peer
 	} else if (strategy_descriptor(strategy).is_sync_target) {
 		rv = L_WF_BITMAP_T;
 	} else {
-		u64 peer_current_uuid = peer_device->current_uuid & ~UUID_PRIMARY;
-		u64 my_current_uuid = drbd_current_uuid(device) & ~UUID_PRIMARY;
-
 		rv = L_ESTABLISHED;
-		if (peer_current_uuid == my_current_uuid &&
-				!(peer_device->uuid_flags & UUID_FLAG_SYNC_TARGET) &&
-				device->disk_state[NOW] >= D_OUTDATED &&
-				peer_device->disk_state[NOW] >= D_OUTDATED) {
-			if (drbd_bitmap_uuid(peer_device)) {
-				drbd_info(peer_device, "clearing bitmap UUID and bitmap content (%lu bits)\n",
-					  drbd_bm_total_weight(peer_device));
-				down_write(&device->uuid_sem);
-				drbd_uuid_set_bitmap(peer_device, 0);
-				up_write(&device->uuid_sem);
-
-			} else if (drbd_bm_total_weight(peer_device)) {
-				drbd_info(peer_device, "bitmap content (%lu bits)\n",
-					  drbd_bm_total_weight(peer_device));
-			}
-			drbd_bm_clear_many_bits(peer_device, 0, -1UL);
-		}
 	}
 
 	return rv;

commit 309a2ac12e5e99954a81433f115bed783c5af561

drbd: Fix condition for clearing bitmap
When two nodes connect (or one side attaches a disk) and no resync
starts, we should remove the bitmap (and bitmap-uuid) between the two.

The bug was that this also cleared the bitmap if the peer is a
permanently diskless node. That was wrong.

It looks like an attempt to keep the bitmap-UUIDs to intentional
diskless peers all zero. It was wrong since we should keep that
bitmap-UUID as long as the local node has a bitmap towards this peer.

Fixes: 501071ca86ed "drbd: move bitmap clearing from strategy_to_repl_state() to w_after_state_change()"

drbd/drbd_state.c
@@ -3696,9 +3696,7 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 
 			/* connect without resync or remote attach without resync */
 			if (disk_state[NOW] >= D_OUTDATED && repl_state[NEW] == L_ESTABLISHED &&
-			    ((repl_state[OLD] == L_OFF &&
-			      (peer_disk_state[NEW] >= D_OUTDATED ||
-			       (peer_disk_state[NEW] == D_DISKLESS && !want_bitmap(peer_device)))) ||
+			    ((repl_state[OLD] == L_OFF && peer_disk_state[NEW] >= D_OUTDATED) ||
 			     (peer_disk_state[OLD] == D_DISKLESS && peer_disk_state[NEW] >= D_OUTDATED))) {
 				u64 peer_current_uuid = peer_device->current_uuid & ~UUID_PRIMARY;
 				u64 my_current_uuid = drbd_current_uuid(device) & ~UUID_PRIMARY;