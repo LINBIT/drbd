LINBIT/drbd

commit 61f1c6dd132884c81135b166a24adcd78877937f

drbd: serialize syncs from multiple sources
This fixes stuck sync states that occurred as follows:
* Node C is syncing from node B
* C connects to A and switches to sync from A instead
* Some leftover sync responses are received from B

Now when C receives the sync response from B, recv_resync_read sets the
bitmap bits for all peers, including against A. However, resync_next_bit
for A might already be past this sector. As a result, the sync A->C
stalls once resync_next_bit reaches the end of the device. The bits that
were marked out-of-sync by recv_resync_read are never synced, and so C
never finishes the sync from A.

Fix this by ensuring that resync requests are only active to one peer at
a time. In order to achieve this, an additional flag "resync_active" is
added to the peer device state indicating whether there may be resync
activity currently. This flag is set from the state machinery when it
determines that resync activity on the other peers has terminated. It is
unset whenever sync activity finishes.

drbd/drbd_state.c
@@ -360,6 +360,9 @@ struct drbd_state_change *remember_state_change(struct drbd_resource *resource, 
 			memcpy(peer_device_state_change->resync_susp_other_c,
 			       peer_device->resync_susp_other_c,
 			       sizeof(peer_device->resync_susp_other_c));
+			memcpy(peer_device_state_change->resync_active,
+			       peer_device->resync_active,
+			       sizeof(peer_device->resync_active));
 			peer_device_state_change++;
 		}
 		device_state_change++;
@@ -441,6 +444,7 @@ void copy_old_to_new_state_change(struct drbd_state_change *state_change)
 		OLD_TO_NEW(p->resync_susp_peer);
 		OLD_TO_NEW(p->resync_susp_dependency);
 		OLD_TO_NEW(p->resync_susp_other_c);
+		OLD_TO_NEW(p->resync_active);
 	}
 
 #undef OLD_TO_NEW
@@ -523,6 +527,8 @@ static bool state_has_changed(struct drbd_resource *resource)
 				peer_device->resync_susp_dependency[NEW] ||
 			    peer_device->resync_susp_other_c[OLD] !=
 				peer_device->resync_susp_other_c[NEW] ||
+			    peer_device->resync_active[OLD] !=
+				peer_device->resync_active[NEW] ||
 			    peer_device->uuid_flags & UUID_FLAG_GOT_STABLE)
 				return true;
 		}
@@ -564,6 +570,8 @@ static void ___begin_state_change(struct drbd_resource *resource)
 				peer_device->resync_susp_dependency[NOW];
 			peer_device->resync_susp_other_c[NEW] =
 				peer_device->resync_susp_other_c[NOW];
+			peer_device->resync_active[NEW] =
+				peer_device->resync_active[NOW];
 		}
 	}
 }
@@ -772,6 +780,8 @@ static enum drbd_state_rv ___end_state_change(struct drbd_resource *resource, st
 				peer_device->resync_susp_dependency[NEW];
 			peer_device->resync_susp_other_c[NOW] =
 				peer_device->resync_susp_other_c[NEW];
+			peer_device->resync_active[NOW] =
+				peer_device->resync_active[NEW];
 		}
 		device->cached_state_unstable = !state_is_stable(device);
 		device->cached_err_io =
@@ -1026,61 +1036,6 @@ static bool resync_suspended(struct drbd_peer_device *peer_device, enum which_st
 	return peer_device->resync_susp_user[which] ||
 	       peer_device->resync_susp_peer[which] ||
 	       resync_susp_comb_dep(peer_device, which);
-}
-
-static void set_resync_susp_other_c(struct drbd_peer_device *peer_device, bool val, bool start)
-{
-	struct drbd_device *device = peer_device->device;
-	struct drbd_peer_device *p;
-	enum drbd_repl_state r;
-
-	/* When the resync_susp_other_connection flag gets cleared, make sure it gets
-	   cleared first on all connections where we are L_PAUSED_SYNC_T. Clear it on
-	   one L_PAUSED_SYNC_T at a time. Only if we have no connection that is
-	   L_PAUSED_SYNC_T clear it on all L_PAUSED_SYNC_S connections at once. */
-
-	if (val) {
-		for_each_peer_device(p, device) {
-			if (p == peer_device)
-				continue;
-
-			r = p->repl_state[NEW];
-			p->resync_susp_other_c[NEW] = true;
-
-			if (start && p->disk_state[NEW] >= D_INCONSISTENT && r == L_ESTABLISHED)
-				p->repl_state[NEW] = L_PAUSED_SYNC_T;
-
-			if (r == L_SYNC_SOURCE)
-				p->repl_state[NEW] = L_PAUSED_SYNC_S;
-			else if (r == L_SYNC_TARGET)
-				p->repl_state[NEW] = L_PAUSED_SYNC_T;
-		}
-	} else {
-		for_each_peer_device(p, device) {
-			if (p == peer_device)
-				continue;
-
-			r = p->repl_state[NEW];
-			if (r == L_PAUSED_SYNC_S)
-				continue;
-
-			p->resync_susp_other_c[NEW] = false;
-			if (r == L_PAUSED_SYNC_T && !resync_suspended(p, NEW)) {
-				p->repl_state[NEW] = L_SYNC_TARGET;
-				return;
-			}
-		}
-
-		for_each_peer_device(p, device) {
-			if (p == peer_device)
-				continue;
-
-			p->resync_susp_other_c[NEW] = false;
-
-			if (p->repl_state[NEW] == L_PAUSED_SYNC_S && !resync_suspended(p, NEW))
-				p->repl_state[NEW] = L_SYNC_SOURCE;
-		}
-	}
 }
 
 static int scnprintf_resync_suspend_flags(char *buffer, size_t size,
@@ -1824,31 +1779,88 @@ static bool is_sync_target_other_c(struct drbd_peer_device *ign_peer_device)
 	return false;
 }
 
-static void select_best_resync_source(struct drbd_peer_device *candidate_pd)
+static void drbd_start_other_targets_paused(struct drbd_peer_device *peer_device)
 {
-	struct drbd_device *device = candidate_pd->device;
-	struct drbd_peer_device *current_pd;
-	long diff_w, candidate_w, current_w;
+	struct drbd_device *device = peer_device->device;
+	struct drbd_peer_device *p;
 
-	for_each_peer_device_rcu(current_pd, device) {
-		if (current_pd == candidate_pd)
+	for_each_peer_device(p, device) {
+		if (p == peer_device)
 			continue;
-		if (current_pd->repl_state[NEW] == L_SYNC_TARGET)
-			goto found_pd;
-	}
-	return;
 
-found_pd:
-	candidate_w = drbd_bm_total_weight(candidate_pd);
-	current_w = drbd_bm_total_weight(current_pd);
-	diff_w = candidate_w - current_w;
-	if (diff_w < -256L) {
-		/* Only switch resync source if it is at least 1MByte storage
-		   (256 bits) less to resync than from the previous sync source */
-		candidate_pd->repl_state[NEW] = L_SYNC_TARGET;
-		candidate_pd->resync_susp_other_c[NEW] = false;
-		current_pd->repl_state[NEW] = L_PAUSED_SYNC_T;
-		current_pd->resync_susp_other_c[NEW] = true;
+		if (p->disk_state[NEW] >= D_INCONSISTENT && p->repl_state[NEW] == L_ESTABLISHED)
+			p->repl_state[NEW] = L_PAUSED_SYNC_T;
+	}
+}
+
+static bool drbd_is_sync_target_candidate(struct drbd_peer_device *peer_device)
+{
+	if (!repl_is_sync_target(peer_device->repl_state[NEW]))
+		return false;
+
+	if (peer_device->resync_susp_dependency[NEW] ||
+			peer_device->resync_susp_peer[NEW] ||
+			peer_device->resync_susp_user[NEW])
+		return false;
+
+	if (peer_device->disk_state[NEW] < D_OUTDATED)
+		return false;
+
+	return true;
+
+}
+
+static void drbd_select_sync_target(struct drbd_device *device)
+{
+	struct drbd_peer_device *peer_device;
+	struct drbd_peer_device *target_current = NULL;
+	struct drbd_peer_device *target_active = NULL;
+	struct drbd_peer_device *target_desired = NULL;
+
+	/* Find current and active resync peers. */
+	for_each_peer_device_rcu(peer_device, device) {
+		if (peer_device->repl_state[OLD] == L_SYNC_TARGET && drbd_is_sync_target_candidate(peer_device))
+			target_current = peer_device;
+
+		if (peer_device->resync_active[NEW])
+			target_active = peer_device;
+	}
+
+	/* Choose desired resync peer. */
+	for_each_peer_device_rcu(peer_device, device) {
+		if (!drbd_is_sync_target_candidate(peer_device))
+			continue;
+
+		if (target_desired && drbd_bm_total_weight(peer_device) > drbd_bm_total_weight(target_desired))
+			continue;
+
+		target_desired = peer_device;
+	}
+
+	/* Keep current resync target if the alternative has less than 1MiB
+	 * storage (256 bits) less to resync. */
+	if (target_current && target_desired &&
+			drbd_bm_total_weight(target_current) < drbd_bm_total_weight(target_desired) + 256UL)
+		target_desired = target_current;
+
+	/* Do not activate/unpause a resync if some other is still active. */
+	if (target_desired && target_active && target_desired != target_active)
+		target_desired = NULL;
+
+	/* Activate resync (if not already active). */
+	if (target_desired)
+		target_desired->resync_active[NEW] = true;
+
+	/* Make sure that the targets are correctly paused/unpaused. */
+	for_each_peer_device_rcu(peer_device, device) {
+		enum drbd_repl_state *repl_state = peer_device->repl_state;
+
+		peer_device->resync_susp_other_c[NEW] = target_desired && peer_device != target_desired;
+
+		if (!repl_is_sync_target(repl_state[NEW]))
+			continue;
+
+		peer_device->repl_state[NEW] = peer_device == target_desired ? L_SYNC_TARGET : L_PAUSED_SYNC_T;
 	}
 }
 
@@ -1956,10 +1968,6 @@ static void sanitize_state(struct drbd_resource *resource)
 			enum drbd_disk_state *peer_disk_state = peer_device->disk_state;
 			struct drbd_connection *connection = peer_device->connection;
 			enum drbd_conn_state *cstate = connection->cstate;
-			enum drbd_disk_state min_disk_state, max_disk_state;
-			enum drbd_disk_state min_peer_disk_state, max_peer_disk_state;
-			enum drbd_role *peer_role = connection->peer_role;
-			bool uuids_match;
 
 			if (repl_state[NEW] < L_ESTABLISHED) {
 				peer_device->resync_susp_peer[NEW] = false;
@@ -1967,8 +1975,10 @@ static void sanitize_state(struct drbd_resource *resource)
 				    peer_disk_state[NEW] < D_INCONSISTENT)
 					peer_disk_state[NEW] = D_UNKNOWN;
 			}
-			if (repl_state[OLD] >= L_ESTABLISHED && repl_state[NEW] < L_ESTABLISHED)
+			if (repl_state[OLD] >= L_ESTABLISHED && repl_state[NEW] < L_ESTABLISHED) {
 				lost_connection = true;
+				peer_device->resync_active[NEW] = false;
+			}
 
 			/* Clear the aftr_isp when becoming unconfigured */
 			if (cstate[NEW] == C_STANDALONE &&
@@ -1982,6 +1992,7 @@ static void sanitize_state(struct drbd_resource *resource)
 			     peer_disk_state[NEW] <= D_FAILED)) {
 				repl_state[NEW] = L_ESTABLISHED;
 				clear_bit(RECONCILIATION_RESYNC, &peer_device->flags);
+				peer_device->resync_active[NEW] = false;
 			}
 
 			/* D_CONSISTENT vanish when we get connected (pre 9.0) */
@@ -2001,38 +2012,35 @@ static void sanitize_state(struct drbd_resource *resource)
 			    (role[OLD] != R_PRIMARY ||
 			     peer_disk_state[OLD] != D_UNKNOWN))
 				connection->susp_fen[NEW] = true;
+		}
+
+		drbd_select_sync_target(device);
+
+		for_each_peer_device_rcu(peer_device, device) {
+			enum drbd_repl_state *repl_state = peer_device->repl_state;
+			enum drbd_disk_state *peer_disk_state = peer_device->disk_state;
+			struct drbd_connection *connection = peer_device->connection;
+			enum drbd_disk_state min_disk_state, max_disk_state;
+			enum drbd_disk_state min_peer_disk_state, max_peer_disk_state;
+			enum drbd_role *peer_role = connection->peer_role;
+			bool uuids_match;
 
 			/* Pause a SyncSource until it finishes resync as target on other connections */
 			if (repl_state[OLD] != L_SYNC_SOURCE && repl_state[NEW] == L_SYNC_SOURCE &&
 			    is_sync_target_other_c(peer_device))
 				peer_device->resync_susp_other_c[NEW] = true;
 
-			if (peer_device->resync_susp_other_c[NEW] &&
-			    repl_state[NEW] == L_SYNC_TARGET)
-				select_best_resync_source(peer_device);
-
 			if (resync_suspended(peer_device, NEW)) {
 				if (repl_state[NEW] == L_SYNC_SOURCE)
 					repl_state[NEW] = L_PAUSED_SYNC_S;
-				if (repl_state[NEW] == L_SYNC_TARGET)
-					repl_state[NEW] = L_PAUSED_SYNC_T;
 			} else {
 				if (repl_state[NEW] == L_PAUSED_SYNC_S)
 					repl_state[NEW] = L_SYNC_SOURCE;
-				if (repl_state[NEW] == L_PAUSED_SYNC_T)
-					repl_state[NEW] = L_SYNC_TARGET;
 			}
-
-			/* This needs to be after the previous block, since we should not set
-			   the bit if we are paused ourselves */
-			if (repl_state[OLD] != L_SYNC_TARGET && repl_state[NEW] == L_SYNC_TARGET)
-				set_resync_susp_other_c(peer_device, true, false);
-			if (repl_state[OLD] == L_SYNC_TARGET && repl_state[NEW] != L_SYNC_TARGET)
-				set_resync_susp_other_c(peer_device, false, false);
 
 			/* Implication of the repl state on other peer's repl state */
 			if (repl_state[OLD] != L_STARTING_SYNC_T && repl_state[NEW] == L_STARTING_SYNC_T)
-				set_resync_susp_other_c(peer_device, true, true);
+				drbd_start_other_targets_paused(peer_device);
 
 						/* Implications of the repl state on the disk states */
 			min_disk_state = D_DISKLESS;
@@ -2485,8 +2493,6 @@ static void finish_state_change(struct drbd_resource *resource, struct completio
 						  -(long)peer_device->rs_mark_time[peer_device->rs_last_mark];
 				initialize_resync_progress_marks(peer_device);
 				peer_device->resync_next_bit = 0;
-				if (repl_state[NEW] == L_SYNC_TARGET)
-					mod_timer(&peer_device->resync_timer, jiffies);
 			}
 
 			if ((repl_state[OLD] == L_SYNC_TARGET  || repl_state[OLD] == L_SYNC_SOURCE) &&
@@ -3304,6 +3310,87 @@ static void check_may_resume_io_after_fencing(struct drbd_state_change *state_ch
 	}
 }
 
+static bool use_checksum_based_resync(struct drbd_connection *connection, struct drbd_device *device)
+{
+	bool csums_after_crash_only;
+	rcu_read_lock();
+	csums_after_crash_only = rcu_dereference(connection->transport.net_conf)->csums_after_crash_only;
+	rcu_read_unlock();
+	return connection->agreed_pro_version >= 89 &&		/* supported? */
+		connection->csums_tfm &&			/* configured? */
+		(csums_after_crash_only == false		/* use for each resync? */
+		 || test_bit(CRASHED_PRIMARY, &device->flags));	/* or only after Primary crash? */
+}
+
+static void drbd_run_resync(struct drbd_peer_device *peer_device, enum drbd_repl_state repl_state)
+{
+	struct drbd_device *device = peer_device->device;
+	struct drbd_connection *connection = peer_device->connection;
+	enum drbd_repl_state side = repl_is_sync_target(repl_state) ? L_SYNC_TARGET : L_SYNC_SOURCE;
+
+	drbd_info(peer_device, "Began resync as %s (will sync %lu KB [%lu bits set]).\n",
+			drbd_repl_str(repl_state),
+			(unsigned long) peer_device->rs_total << (BM_BLOCK_SHIFT-10),
+			(unsigned long) peer_device->rs_total);
+
+	if (side == L_SYNC_TARGET)
+		drbd_set_exposed_data_uuid(device, peer_device->current_uuid);
+
+	/* Forget potentially stale cached per resync extent bit-counts. */
+	drbd_rs_cancel_all(peer_device);
+
+	peer_device->use_csums = side == L_SYNC_TARGET ?
+		use_checksum_based_resync(connection, device) : false;
+
+	if (side == L_SYNC_TARGET &&
+			!(peer_device->uuid_flags & UUID_FLAG_STABLE) &&
+			!drbd_stable_sync_source_present(peer_device, NOW))
+		set_bit(UNSTABLE_RESYNC, &peer_device->flags);
+
+	/* Since protocol 96, we must serialize drbd_gen_and_send_sync_uuid
+	 * with w_send_oos, or the sync target will get confused as to
+	 * how much bits to resync.  We cannot do that always, because for an
+	 * empty resync and protocol < 95, we need to do it here, as we call
+	 * drbd_resync_finished from here in that case.
+	 * We drbd_gen_and_send_sync_uuid here for protocol < 96,
+	 * and from after_state_ch otherwise. */
+	if (side == L_SYNC_SOURCE && connection->agreed_pro_version < 96)
+		drbd_gen_and_send_sync_uuid(peer_device);
+
+	if (connection->agreed_pro_version < 95 && peer_device->rs_total == 0) {
+		/* This still has a race (about when exactly the peers
+		 * detect connection loss) that can lead to a full sync
+		 * on next handshake. In 8.3.9 we fixed this with explicit
+		 * resync-finished notifications, but the fix
+		 * introduces a protocol change.  Sleeping for some
+		 * time longer than the ping interval + timeout on the
+		 * SyncSource, to give the SyncTarget the chance to
+		 * detect connection loss, then waiting for a ping
+		 * response (implicit in drbd_resync_finished) reduces
+		 * the race considerably, but does not solve it. */
+		if (side == L_SYNC_SOURCE) {
+			struct net_conf *nc;
+			int timeo;
+
+			rcu_read_lock();
+			nc = rcu_dereference(connection->transport.net_conf);
+			timeo = nc->ping_int * HZ + nc->ping_timeo * HZ / 9;
+			rcu_read_unlock();
+			schedule_timeout_interruptible(timeo);
+		}
+		drbd_resync_finished(peer_device, D_MASK);
+	}
+
+	/* ns.conn may already be != peer_device->repl_state[NOW],
+	 * we may have been paused in between, or become paused until
+	 * the timer triggers.
+	 * No matter, that is handled in resync_timer_fn() */
+	if (repl_state == L_SYNC_TARGET)
+		drbd_uuid_resync_starting(peer_device);
+
+	drbd_md_sync_if_dirty(device);
+}
+
 
 /*
  * Perform after state change actions that may sleep.
@@ -3635,12 +3722,21 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 				drbd_uuid_new_current(device, false);
 			}
 
-			if (repl_state[OLD] != L_VERIFY_S && repl_state[NEW] == L_VERIFY_S && get_ldev(device)) {
+			if (repl_state[OLD] != L_VERIFY_S && repl_state[NEW] == L_VERIFY_S) {
 				drbd_info(peer_device, "Starting Online Verify from sector %llu\n",
 						(unsigned long long)peer_device->ov_position);
-				mod_timer(&peer_device->resync_timer, jiffies);
-				put_ldev(device);
+				drbd_queue_work_if_unqueued(
+						&peer_device->connection->sender_work,
+						&peer_device->resync_work);
 			}
+
+			if (!repl_is_sync(repl_state[OLD]) && repl_is_sync(repl_state[NEW]))
+				drbd_run_resync(peer_device, repl_state[NEW]);
+
+			if (!peer_device_state_change->resync_active[OLD] && peer_device_state_change->resync_active[NEW])
+				drbd_queue_work_if_unqueued(
+						&peer_device->connection->sender_work,
+						&peer_device->resync_work);
 		}
 
 		if (((role[OLD] == R_PRIMARY && role[NEW] == R_SECONDARY) || some_peer_demoted) &&

drbd/drbd_int.h
@@ -1131,6 +1131,7 @@ struct drbd_peer_device {
 	bool resync_susp_peer[2];
 	bool resync_susp_dependency[2];
 	bool resync_susp_other_c[2];
+	bool resync_active[2];
 	enum drbd_repl_state negotiation_result; /* To find disk state after attach */
 	unsigned int send_cnt;
 	unsigned int recv_cnt;
@@ -2500,27 +2501,38 @@ static inline int __sub_unacked(struct drbd_peer_device *peer_device, int n)
 	return atomic_sub_return(n, &peer_device->unacked_cnt);
 }
 
+static inline bool repl_is_sync_target(enum drbd_repl_state repl_state)
+{
+	return repl_state == L_SYNC_TARGET || repl_state == L_PAUSED_SYNC_T;
+}
+
+static inline bool repl_is_sync_source(enum drbd_repl_state repl_state)
+{
+	return repl_state == L_SYNC_SOURCE || repl_state == L_PAUSED_SYNC_S;
+}
+
+static inline bool repl_is_sync(enum drbd_repl_state repl_state)
+{
+	return repl_is_sync_source(repl_state) ||
+		repl_is_sync_target(repl_state);
+}
+
 static inline bool is_sync_target_state(struct drbd_peer_device *peer_device,
 					enum which_state which)
 {
-	enum drbd_repl_state repl_state = peer_device->repl_state[which];
-
-	return repl_state == L_SYNC_TARGET || repl_state == L_PAUSED_SYNC_T;
+	return repl_is_sync_target(peer_device->repl_state[which]);
 }
 
 static inline bool is_sync_source_state(struct drbd_peer_device *peer_device,
 					enum which_state which)
 {
-	enum drbd_repl_state repl_state = peer_device->repl_state[which];
-
-	return repl_state == L_SYNC_SOURCE || repl_state == L_PAUSED_SYNC_S;
+	return repl_is_sync_source(peer_device->repl_state[which]);
 }
 
 static inline bool is_sync_state(struct drbd_peer_device *peer_device,
 				 enum which_state which)
 {
-	return is_sync_source_state(peer_device, which) ||
-		is_sync_target_state(peer_device, which);
+	return repl_is_sync(peer_device->repl_state[which]);
 }
 
 static inline bool is_verify_state(struct drbd_peer_device *peer_device,

drbd/drbd_sender.c
@@ -452,21 +452,27 @@ int w_resync_timer(struct drbd_work *w, int cancel)
 	struct drbd_peer_device *peer_device =
 		container_of(w, struct drbd_peer_device, resync_work);
 
-	if (test_bit(SYNC_TARGET_TO_BEHIND, &peer_device->flags))
-		return 0;
-
-	mutex_lock(&peer_device->resync_next_bit_mutex);
 	switch (peer_device->repl_state[NOW]) {
 	case L_VERIFY_S:
+		mutex_lock(&peer_device->resync_next_bit_mutex);
 		make_ov_request(peer_device, cancel);
+		mutex_unlock(&peer_device->resync_next_bit_mutex);
 		break;
 	case L_SYNC_TARGET:
+		mutex_lock(&peer_device->resync_next_bit_mutex);
 		make_resync_request(peer_device, cancel);
+		mutex_unlock(&peer_device->resync_next_bit_mutex);
 		break;
 	default:
+		if (atomic_read(&peer_device->rs_sect_in) >= peer_device->rs_in_flight) {
+			struct drbd_resource *resource = peer_device->device->resource;
+			unsigned long irq_flags;
+			begin_state_change(resource, &irq_flags, 0);
+			peer_device->resync_active[NEW] = false;
+			end_state_change(resource, &irq_flags);
+		}
 		break;
 	}
-	mutex_unlock(&peer_device->resync_next_bit_mutex);
 
 	return 0;
 }
@@ -488,9 +494,6 @@ int w_send_uuids(struct drbd_work *w, int cancel)
 void resync_timer_fn(struct timer_list *t)
 {
 	struct drbd_peer_device *peer_device = from_timer(peer_device, t, resync_timer);
-
-	if (test_bit(SYNC_TARGET_TO_BEHIND, &peer_device->flags))
-		return;
 
 	drbd_queue_work_if_unqueued(
 		&peer_device->connection->sender_work,
@@ -1208,6 +1211,7 @@ int drbd_resync_finished(struct drbd_peer_device *peer_device,
 	   of application IO), and against connectivity loss just before we arrive here. */
 	if (peer_device->repl_state[NOW] <= L_ESTABLISHED)
 		goto out_unlock;
+	peer_device->resync_active[NEW] = false;
 	__change_repl_state(peer_device, L_ESTABLISHED);
 
 	aborted = device->disk_state[NOW] == D_OUTDATED && new_peer_disk_state == D_INCONSISTENT;
@@ -2047,18 +2051,6 @@ static void do_start_resync(struct drbd_peer_device *peer_device)
 	clear_bit(AHEAD_TO_SYNC_SOURCE, &peer_device->flags);
 }
 
-static bool use_checksum_based_resync(struct drbd_connection *connection, struct drbd_device *device)
-{
-	bool csums_after_crash_only;
-	rcu_read_lock();
-	csums_after_crash_only = rcu_dereference(connection->transport.net_conf)->csums_after_crash_only;
-	rcu_read_unlock();
-	return connection->agreed_pro_version >= 89 &&		/* supported? */
-		connection->csums_tfm &&			/* configured? */
-		(csums_after_crash_only == false		/* use for each resync? */
-		 || test_bit(CRASHED_PRIMARY, &device->flags));	/* or only after Primary crash? */
-}
-
 /**
  * drbd_start_resync() - Start the resync process
  * @side:	Either L_SYNC_SOURCE or L_SYNC_TARGET
@@ -2168,81 +2160,10 @@ skip_helper:
 	if (repl_state < L_ESTABLISHED)
 		r = SS_UNKNOWN_ERROR;
 
-	if (r == SS_SUCCESS) {
-		if (side == L_SYNC_TARGET)
-			drbd_set_exposed_data_uuid(device, peer_device->current_uuid);
-
+	if (r == SS_SUCCESS)
 		drbd_pause_after(device);
-		/* Forget potentially stale cached per resync extent bit-counts.
-		 * Open coded drbd_rs_cancel_all(device), we already have IRQs
-		 * disabled, and know the disk state is ok. */
-		spin_lock(&device->al_lock);
-		lc_reset(peer_device->resync_lru);
-		peer_device->resync_locked = 0;
-		peer_device->resync_wenr = LC_FREE;
-		spin_unlock(&device->al_lock);
-	}
 
 	unlock_all_resources();
-
-	if (r == SS_SUCCESS) {
-		drbd_info(peer_device, "Began resync as %s (will sync %lu KB [%lu bits set]).\n",
-		     drbd_repl_str(repl_state),
-		     (unsigned long) peer_device->rs_total << (BM_BLOCK_SHIFT-10),
-		     (unsigned long) peer_device->rs_total);
-		peer_device->use_csums = side == L_SYNC_TARGET ?
-			use_checksum_based_resync(connection, device) : false;
-
-		if ((side == L_SYNC_TARGET || side == L_PAUSED_SYNC_T) &&
-		    !(peer_device->uuid_flags & UUID_FLAG_STABLE) &&
-		    !drbd_stable_sync_source_present(peer_device, NOW))
-			set_bit(UNSTABLE_RESYNC, &peer_device->flags);
-
-		/* Since protocol 96, we must serialize drbd_gen_and_send_sync_uuid
-		 * with w_send_oos, or the sync target will get confused as to
-		 * how much bits to resync.  We cannot do that always, because for an
-		 * empty resync and protocol < 95, we need to do it here, as we call
-		 * drbd_resync_finished from here in that case.
-		 * We drbd_gen_and_send_sync_uuid here for protocol < 96,
-		 * and from after_state_ch otherwise. */
-		if (side == L_SYNC_SOURCE && connection->agreed_pro_version < 96)
-			drbd_gen_and_send_sync_uuid(peer_device);
-
-		if (connection->agreed_pro_version < 95 && peer_device->rs_total == 0) {
-			/* This still has a race (about when exactly the peers
-			 * detect connection loss) that can lead to a full sync
-			 * on next handshake. In 8.3.9 we fixed this with explicit
-			 * resync-finished notifications, but the fix
-			 * introduces a protocol change.  Sleeping for some
-			 * time longer than the ping interval + timeout on the
-			 * SyncSource, to give the SyncTarget the chance to
-			 * detect connection loss, then waiting for a ping
-			 * response (implicit in drbd_resync_finished) reduces
-			 * the race considerably, but does not solve it. */
-			if (side == L_SYNC_SOURCE) {
-				struct net_conf *nc;
-				int timeo;
-
-				rcu_read_lock();
-				nc = rcu_dereference(connection->transport.net_conf);
-				timeo = nc->ping_int * HZ + nc->ping_timeo * HZ / 9;
-				rcu_read_unlock();
-				schedule_timeout_interruptible(timeo);
-			}
-			drbd_resync_finished(peer_device, D_MASK);
-		}
-
-		/* ns.conn may already be != peer_device->repl_state[NOW],
-		 * we may have been paused in between, or become paused until
-		 * the timer triggers.
-		 * No matter, that is handled in resync_timer_fn() */
-		if (repl_state == L_SYNC_TARGET) {
-			drbd_uuid_resync_starting(peer_device);
-			mod_timer(&peer_device->resync_timer, jiffies);
-		}
-
-		drbd_md_sync_if_dirty(device);
-	}
 	put_ldev(device);
     out:
 	up(&device->resource->state_sem);

drbd/drbd_state_change.h
@@ -39,6 +39,7 @@ struct drbd_peer_device_state_change {
 	bool resync_susp_peer[2];
 	bool resync_susp_dependency[2];
 	bool resync_susp_other_c[2];
+	bool resync_active[2];
 };
 
 struct drbd_state_change_object_count {

commit 0a58e61fcd09a64e71c9eb85195894776717e527

drbd: ensure that resync work runs when states flap
It is possible for sync states to flap as below. Notice that the resync
towards d-150 starts, but is then paused because d-151 appears to be the
desired peer. But the resync to d-151 never starts. Instead d-150 is
chosen as the desired peer in the next state change and that connection
becomes SyncTarget again.

[   30.299134] drbd res/0 drbd1 d-150: repl( WFBitMapT -> SyncTarget )
[   30.301258] drbd res/0 drbd1 d-150: Began resync as SyncTarget (will sync 184320 KB [46080 bits set]).
...
[   30.737588] drbd res/0 drbd1 d-151: repl( WFBitMapT -> PausedSyncT ) resync-susp( connection dependency -> no )
[   30.740073] drbd res/0 drbd1 d-150: repl( SyncTarget -> PausedSyncT )
[   30.741976] drbd res/0 drbd1 d-150: Resync suspended
[   30.743946] drbd res/0 drbd1 d-151: Began resync as PausedSyncT (will sync 184320 KB [46080 bits set]).
[   30.796187] drbd res/0 drbd1 d-151: resync-susp( no -> connection dependency )
[   30.798806] drbd res/0 drbd1 d-150: repl( PausedSyncT -> SyncTarget )
[   30.801174] drbd res/0 drbd1 d-150: Syncer continues.

This can cause resync work to stop being scheduled. Using the names from
above:
* We are running our resync from d-150.
* Our replication state to d-150 changes to PausedSyncT.
* w_resync_timer() runs and does nothing because there are still
  outstanding resync requests. In particular, it does not re-arm the
  resync_timer.
* drbd_rs_all_in_flight_came_back() runs and schedules resync_work.
* w_resync_timer() runs and performs a state change to set resync_active
  to false. However, the state logic chooses d-150 as the desired peer
  again, so resync_active remains true.
* As a result, w_after_state_change() does not schedule resync_work.

Fix this by scheduling resync_work based on the change of replication
state rather than the change to resync_active.

Note that this always triggers when the previous code would have
triggered. A change of resync_active from false to true, means that the
peer was not the desired resync peer and now is, so it must also change
from a state other than SyncTarget, to SyncTarget.

Fixes: 61f1c6dd1328

drbd/drbd_state.c
@@ -3983,7 +3983,8 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 			if (!repl_is_sync(repl_state[OLD]) && repl_is_sync(repl_state[NEW]))
 				drbd_run_resync(peer_device, repl_state[NEW]);
 
-			if (!peer_device_state_change->resync_active[OLD] && peer_device_state_change->resync_active[NEW])
+			if (peer_device_state_change->repl_state[OLD] != L_SYNC_TARGET &&
+					peer_device_state_change->repl_state[NEW] == L_SYNC_TARGET)
 				drbd_queue_work_if_unqueued(
 						&peer_device->connection->sender_work,
 						&peer_device->resync_work);