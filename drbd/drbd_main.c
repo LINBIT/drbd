15cf257c277020e1ba76eff7aa99ed08d44f7d3c
Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>
2023-09-08 11:45:48 +0200

    drbd: stop defining __KERNEL_SYSCALLS__
    See also upstream Linux kernel commit
    d519df00938e ("drbd: stop defining __KERNEL_SYSCALLS__")
    
    Original message:
    
    __KERNEL_SYSCALLS__ hasn't been needed since Linux 2.6.19 so stop
    defining it.

--- drbd/drbd_main.c
+++ drbd/drbd_main.c
@@ -36,7 +36,6 @@
 #include <linux/notifier.h>
 #include <linux/workqueue.h>
 #include <linux/kthread.h>
-#define __KERNEL_SYSCALLS__
 #include <linux/unistd.h>
 #include <linux/vmalloc.h>
 #include <linux/device.h>
