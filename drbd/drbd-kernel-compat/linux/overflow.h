0ef6f4d39f03f315e9e127139f082799abf0139a
Philipp Reisner <philipp.reisner@linbit.com>
2023-10-19 17:29:16 +0200

    compat: Fix compatibility for check_add_overflow()
    It missed the case of a distro with gcc-5 but a kernel without
    overflow.h. That is, Ubuntu-xenial.

--- drbd/drbd-kernel-compat/linux/overflow.h
+++ drbd/drbd-kernel-compat/linux/overflow.h
@@ -4,6 +4,10 @@
 /* RHEL7 has a gcc-4.x which lacks __builtin_add_overflow() */
 #if __GNUC__ < 5
 #define check_add_overflow(a, b, d)  ({ *(d) = (a) + (b); *(d) < (a); })
+#else
+#ifndef check_add_overflow
+#define check_add_overflow(a, b, d) __builtin_add_overflow(a, b, d)
+#endif
 #endif
 
 #endif /* OVERFLOW_H */
