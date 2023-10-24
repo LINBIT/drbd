LINBIT/drbd

commit fd4d7b337a51a251e939633ac513802cc58fc59b

drbd: listeners need to hold a reference on the transport module
A listener object can live longer than a path or connection
objects. I.e. when it has a work item scheduled to carry out an
accept() call. We must ensure the module is not unloaded before that
work item is terminated. We can do that by holding references onto the
module from each listener.

drbd/drbd_transport.c
@@ -195,6 +195,7 @@ int drbd_get_listener(struct drbd_transport *transport, struct drbd_path *path,
 		init_completion(&listener->ready);
 		listener->listen_addr = *(struct sockaddr_storage *)addr;
 		listener->destroy = generic_listener_destroy;
+		listener->transport_class = NULL;
 
 		list_add(&listener->list, &resource->listeners);
 		needs_init = true;
@@ -206,7 +207,12 @@ int drbd_get_listener(struct drbd_transport *transport, struct drbd_path *path,
 	spin_unlock_bh(&resource->listeners_lock);
 
 	if (needs_init) {
-		err = init_listener(transport, addr, path->net, listener);
+		if (try_module_get(transport->class->module)) {
+			listener->transport_class = transport->class;
+			err = init_listener(transport, addr, path->net, listener);
+		} else {
+			err = -ENODEV;
+		}
 		listener->err = err;
 		complete_all(&listener->ready);
 		if (err)
@@ -233,6 +239,8 @@ void drbd_listener_destroy(struct kref *kref)
 	spin_unlock_bh(&resource->listeners_lock);
 
 	listener->destroy(listener);
+	if (listener->transport_class)
+		module_put(listener->transport_class->module);
 }
 
 void drbd_put_listener(struct drbd_path *path)

commit fde847df209925e80a9a966f4629afa0d95af591

drbd: do not access listener after freeing
This fixes the following fault:

drbd res lbtest-vm-71: conn( Connecting -> Disconnecting ) [disconnect]
general protection fault, probably for non-canonical address 0xaaaaaaaaaaaaaac2: 0000 [#1] SMP NOPTI
CPU: 0 PID: 1107 Comm: drbd_r_resync-s Tainted: P           OE     5.15.0-87-generic #97-Ubuntu
Hardware name: QEMU Standard PC (Q35 + ICH9, 2009), BIOS 1.15.0-1 04/01/2014
RIP: 0010:drbd_listener_destroy+0x67/0x80 [drbd]
Code: de 48 89 43 18 48 83 c0 22 48 89 43 20 e8 f1 5e fc c0 48 8b 83 e8 00 00 00 48 89 df e8 e2 da 1f c1 48 8b 43 10 48 85 c0 74 09 <48> 8b 78 18 e8 60 09 39 c0 5b 41 5c 5d e9 07 de 1f c1 0f 1f 80 00
RSP: 0018:ffffc90000b53c68 EFLAGS: 00010286
RAX: aaaaaaaaaaaaaaaa RBX: ffff888006a30c00 RCX: 0000000000000000
RDX: 0000000000001b0d RSI: ffffea000025ca08 RDI: ffffea00001a8c00
RBP: ffffc90000b53c78 R08: ffffea00001a8c80 R09: 0000000000000002
R10: 0000000000037680 R11: 0000000000000006 R12: ffff888005705cd0
R13: ffff888006a30c38 R14: dead000000000100 R15: ffff888006a34a00
FS:  0000000000000000(0000) GS:ffff88803ec00000(0000) knlGS:0000000000000000
CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
CR2: 000055a510939ed8 CR3: 0000000009b68000 CR4: 0000000000350ef0
Call Trace:
 <TASK>
 drbd_put_listener+0x8d/0xb0 [drbd]
 dtt_put_listeners+0x71/0xf0 [drbd_transport_tcp]
 dtt_connect+0x877/0xeb7 [drbd_transport_tcp]
 drbd_receiver+0x98/0x880 [drbd]

Prior to this change, "drbd_listener.destroy" was responsible for
freeing the listener. Hence accessing its members afterwards in order to
call module_put() was a use-after-free bug. Free the listener in
drbd_listener_destroy() instead.

Fixes: fd4d7b337a51 ("drbd: listeners need to hold a reference on the transport module")
Co-developed-by: Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>

drbd/drbd_transport.c
@@ -246,6 +246,7 @@ void drbd_listener_destroy(struct kref *kref)
 	listener->destroy(listener);
 	if (listener->transport_class)
 		module_put(listener->transport_class->module);
+	kfree(listener);
 }
 
 void drbd_put_listener(struct drbd_path *path)

drbd/drbd_transport_lb-tcp.c
@@ -1048,7 +1048,6 @@ static void dtl_destroy_listener(struct drbd_listener *generic_listener)
 
 	unregister_state_change(listener->s_listen->sk, listener);
 	sock_release(listener->s_listen);
-	kfree(listener);
 }
 
 static int dtl_init_listener(struct drbd_transport *transport,

drbd/drbd_transport_rdma.c
@@ -2817,7 +2817,6 @@ static void dtr_destroy_listener(struct drbd_listener *generic_listener)
 		container_of(generic_listener, struct dtr_listener, listener);
 
 	rdma_destroy_id(listener->cm.id);
-	kfree(listener);
 }
 
 static int dtr_init_listener(struct drbd_transport *transport, const struct sockaddr *addr, struct net *net, struct drbd_listener *drbd_listener)

drbd/drbd_transport_tcp.c
@@ -992,7 +992,6 @@ static void dtt_destroy_listener(struct drbd_listener *generic_listener)
 
 	unregister_state_change(listener->s_listen->sk, listener);
 	sock_release(listener->s_listen);
-	kfree(listener);
 }
 
 static int dtt_init_listener(struct drbd_transport *transport,