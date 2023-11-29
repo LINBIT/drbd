LINBIT/drbd

commit c95048652848f0e8ce74a3abcb120e5d6d737fb6

rdma: fix spurious false positive tx timeouts
The completion event might happen right after ib_post_send() before
arming the timer. Better arm the timer and do the kref_get() before
posting the send work request.

I need to revert everything in case posting the send work request
fails.

drbd/drbd_transport_rdma.c
@@ -2181,9 +2181,13 @@ static int __dtr_post_tx_desc(struct dtr_cm *cm, struct dtr_tx_desc *tx_desc)
 {
 	struct dtr_transport *rdma_transport =
 		container_of(cm->path->path.transport, struct dtr_transport, transport);
+	struct drbd_transport *transport = &rdma_transport->transport;
 	struct ib_send_wr send_wr;
 	const struct ib_send_wr *send_wr_failed;
 	struct ib_device *device = cm->id->device;
+	bool was_active, extra_ref = false;
+	unsigned long timeout;
+	struct net_conf *nc;
 	int i, err = -EIO;
 
 	send_wr.next = NULL;
@@ -2194,28 +2198,36 @@ static int __dtr_post_tx_desc(struct dtr_cm *cm, struct dtr_tx_desc *tx_desc)
 	send_wr.opcode = IB_WR_SEND_WITH_IMM;
 	send_wr.send_flags = IB_SEND_SIGNALED;
 
+	rcu_read_lock();
+	nc = rcu_dereference(transport->net_conf);
+	timeout = nc->ping_timeo;
+	rcu_read_unlock();
+
 	for (i = 0; i < tx_desc->nr_sges; i++)
 		ib_dma_sync_single_for_device(device, tx_desc->sge[i].addr,
 					      tx_desc->sge[i].length, DMA_TO_DEVICE);
+
+	if (atomic_inc_return(&cm->tx_descs_posted) == 1) {
+		extra_ref = true;
+		kref_get(&cm->kref); /* keep one extra ref as long as one tx is posted */
+	}
+
+	kref_get(&cm->kref);
+	was_active = mod_timer(&cm->tx_timeout, jiffies + timeout * HZ / 20);
+	if (was_active)
+		kref_put(&cm->kref, dtr_destroy_cm);
+
 	err = ib_post_send(cm->id->qp, &send_wr, &send_wr_failed);
-	if (!err) {
-		struct drbd_transport *transport = &rdma_transport->transport;
-		unsigned long timeout;
-		struct net_conf *nc;
-		bool was_active;
-
-		if (atomic_inc_return(&cm->tx_descs_posted) == 1)
-			kref_get(&cm->kref); /* keep one extra ref as long as one tx is posted */
-
-		rcu_read_lock();
-		nc = rcu_dereference(transport->net_conf);
-		timeout = nc->ping_timeo;
-		rcu_read_unlock();
-		was_active = mod_timer(&cm->tx_timeout, jiffies + timeout * HZ / 20);
-		if (!was_active)
-			kref_get(&cm->kref);
-	} else {
+	if (err) {
 		tr_err(&rdma_transport->transport, "ib_post_send() failed %d\n", err);
+		was_active = del_timer(&cm->tx_timeout);
+		if (!was_active)
+			was_active = cancel_work_sync(&cm->tx_timeout_work);
+		if (was_active)
+			kref_put(&cm->kref, dtr_destroy_cm);
+		if (extra_ref)
+			kref_put(&cm->kref, dtr_destroy_cm);
+		atomic_dec(&cm->tx_descs_posted);
 	}
 
 	return err;

commit 46dc2a5cd5bbed2400278f8e3834f639a20dda66

rdma: fix recently introduced ref-counting bug in an error code path
Only drop the extra reference when I decrement cm->tx_descs_posted
down to zero myself. Someone else might have increased
cm->tx_descs_posted in the meantime.

Fixes: c95048652 ("rdma: fix spurious false positive tx timeouts")

drbd/drbd_transport_rdma.c
@@ -2210,10 +2210,10 @@ static int __dtr_post_tx_desc(struct dtr_cm *cm, struct dtr_tx_desc *tx_desc)
 	struct ib_send_wr send_wr;
 	const struct ib_send_wr *send_wr_failed;
 	struct ib_device *device = cm->id->device;
-	bool was_active, extra_ref = false;
 	unsigned long timeout;
 	struct net_conf *nc;
 	int i, err = -EIO;
+	bool was_active;
 
 	send_wr.next = NULL;
 	send_wr.wr_id = (unsigned long)tx_desc;
@@ -2232,10 +2232,8 @@ static int __dtr_post_tx_desc(struct dtr_cm *cm, struct dtr_tx_desc *tx_desc)
 		ib_dma_sync_single_for_device(device, tx_desc->sge[i].addr,
 					      tx_desc->sge[i].length, DMA_TO_DEVICE);
 
-	if (atomic_inc_return(&cm->tx_descs_posted) == 1) {
-		extra_ref = true;
+	if (atomic_inc_return(&cm->tx_descs_posted) == 1)
 		kref_get(&cm->kref); /* keep one extra ref as long as one tx is posted */
-	}
 
 	kref_get(&cm->kref);
 	was_active = mod_timer(&cm->tx_timeout, jiffies + timeout * HZ / 20);
@@ -2250,9 +2248,8 @@ static int __dtr_post_tx_desc(struct dtr_cm *cm, struct dtr_tx_desc *tx_desc)
 			was_active = cancel_work_sync(&cm->tx_timeout_work);
 		if (was_active)
 			kref_put(&cm->kref, dtr_destroy_cm);
-		if (extra_ref)
+		if (atomic_dec_and_test(&cm->tx_descs_posted))
 			kref_put(&cm->kref, dtr_destroy_cm);
-		atomic_dec(&cm->tx_descs_posted);
 	}
 
 	return err;