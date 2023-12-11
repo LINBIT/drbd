LINBIT/drbd

commit e31d63966fac73fc000b584ccb2580016eccda17

drbd: fix susp_uuid clearing
I introduced susp_uuid for keeping a resource longer suspended for
writing a new current-uuid.
But in the following scenario, it failed to clear the susp_uuid bit:

 1. The network connection between a primary and secondary node
    fails. The primary has the setting quorum=2. IO freezes on the
    primary node.

 2. The user changes the quorum setting from 2 to one. The primary
    node generates a new current-uuid and clears the susp_uuid bit in
    a second state change.

 3. If, during the second state change, another condition for creating
    a new current UUID evaluates to true, the code fails to clear the
    suspen_uuid bit.

Fixing this in three places:
 1. in sanitize_state(): Only set susp_uuid[NEW] if susp_uuid[OLD]
    was not set. That avoids it happening a second time after
    one iteration.

 2. in w_after_state_chage(): Always end the cycle by setting clearing
    susp_uuid[NEW] if it is set. (Independently of susp_uuid[OLD].

 3. in finish_sate_change(): Do not set NEW_CUR_UUID again if we are
    in this operation.

Strictly speaking, (2) is unnecessary after (1), but I also do it
since it shortens the code. Also, (3) alone would fix the issue.

Fixes: d47f7456ab ("drbd: create new UUID before resuming IO upon
regaining quorum")

Co-developed-by: zhengbing.huang <zhengbing.huang@easystack.cn>
Co-developed-by: Lars Ellenberg <lars.ellenberg@linbit.com>

drbd/drbd_state.c
@@ -2353,7 +2353,8 @@ static void sanitize_state(struct drbd_resource *resource)
 	resource->susp_quorum[NEW] =
 		resource->res_opts.on_no_quorum == ONQ_SUSPEND_IO ? !resource_has_quorum : false;
 
-	if (resource_is_suspended(resource, OLD) && !resource_is_suspended(resource, NEW)) {
+	if (!resource->susp_uuid[OLD] &&
+	    resource_is_suspended(resource, OLD) && !resource_is_suspended(resource, NEW)) {
 		idr_for_each_entry(&resource->devices, device, vnr) {
 			if (test_bit(NEW_CUR_UUID, &device->flags)) {
 				resource->susp_uuid[NEW] = true;
@@ -2553,6 +2554,7 @@ static bool should_try_become_up_to_date(struct drbd_device *device, enum drbd_d
 static void finish_state_change(struct drbd_resource *resource, const char *tag)
 {
 	enum drbd_role *role = resource->role;
+	bool *susp_uuid = resource->susp_uuid;
 	struct drbd_device *device;
 	struct drbd_connection *connection;
 	bool starting_resync = false;
@@ -2828,7 +2830,10 @@ static void finish_state_change(struct drbd_resource *resource, const char *tag)
 		if (role[OLD] == R_SECONDARY && role[NEW] == R_PRIMARY)
 			create_new_uuid = true;
 
-		if (create_new_uuid)
+		/* When susp_uuid goes from true to false, we just created a new
+		 * current-uuid, it is pointless to do this one more time
+		 */
+		if (create_new_uuid && !(susp_uuid[OLD] && !susp_uuid[NEW]))
 			set_bit(__NEW_CUR_UUID, &device->flags);
 
 		if (disk_state[NEW] != D_NEGOTIATING && get_ldev_if_state(device, D_DETACHING)) {
@@ -4189,7 +4194,7 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 			still_connected = true;
 	}
 
-	if (!susp_uuid[OLD] && susp_uuid[NEW]) {
+	if (susp_uuid[NEW]) {
 		unsigned long irq_flags;
 
 		begin_state_change(resource, &irq_flags, CS_VERBOSE);

commit 92e245ba5fb07f84948d39d58c4e95093a0ed270

drbd: fix condition that allows creation of new current uuid
Ok, here is the whole explanation of how we keep a resource
suspended longer, to generate a new current UUID.

1 sanitize_state()
  susp_uuid = { false, true }
  DRBD keeps the resource suspended longer.

2 finish_state_change()
  it sets __NEW_CUR_UUID and that moves to NEW_CUR_UUID later.

3 w_after_state_change() when susp_uuid goes from false -> true then drbd_uuid_new_current()
4 w_after_state_change() Trigger another state change that sets susp_uuid[NEW] back to false.

5 sanitize_sate()    susp_uuid = { true, false }
6 finish_state_change() nothing relevant happens
7 w_after_state_change() susp_uuid goes from true -> false no NEW_CUR_UUID bit nothing happens
8 w_after_state_change() susp_uuid[NEW] == false -> nothing happens

Now let us assume between 3 and 4, another CPU does an unrelated
state change that usually triggers a new current uuid:

CPU0:
1 sanitize_state()
  susp_uuid = { false, true }
  DRBD keeps the resource suspended longer.

2 finish_state_change()
  it sets __NEW_CUR_UUID and that moves to NEW_CUR_UUID later.

3 w_after_state_change() when susp_uuid goes from false -> true then drbd_uuid_new_current()

    CPU1:
    Here, another thread starts another unrelated state change
    that again wants to set NEW_CUR_UUID. But let us go into the details.

    1 sanitize_state() susp_uuid = { true, true }
    2 finish_state_chanage()
      Prior to this change, __NEW_CUR_UUID would have been set.
      With this change, __NEW_CUR_UUID is not set, so NEW_CUR_UUID is not set again.
    3 w_after_state_change() nothing, since no NEW_CUR_UUID
    4 w_after_state_change() creates a state change to set susp_uuid[NEW] to false

CPU0 continues:
4 w_after_state_change() Trigger another state change that sets susp_uuid[NEW] back to false.
  This is an "empty state change, " which does nothing.

Fixes: e31d63966 ("drbd: fix susp_uuid clearing")

drbd/drbd_state.c
@@ -2830,10 +2830,8 @@ static void finish_state_change(struct drbd_resource *resource, const char *tag)
 		if (role[OLD] == R_SECONDARY && role[NEW] == R_PRIMARY)
 			create_new_uuid = true;
 
-		/* When susp_uuid goes from true to false, we just created a new
-		 * current-uuid, it is pointless to do this one more time
-		 */
-		if (create_new_uuid && !(susp_uuid[OLD] && !susp_uuid[NEW]))
+		/* Only a single new current uuid when susp_uuid becomes true */
+		if (create_new_uuid && !susp_uuid[OLD])
 			set_bit(__NEW_CUR_UUID, &device->flags);
 
 		if (disk_state[NEW] != D_NEGOTIATING && get_ldev_if_state(device, D_DETACHING)) {