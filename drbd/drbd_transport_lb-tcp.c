LINBIT/drbd

commit ba0c4191b6c4a5d860fd72fefaf4af35fd7e34b8

drbd_transport: disable sk_state_change during free
during kernel socket shutdown we might see state changes that lead to
NetworkFailure states. This seems to be a rare race, that is more likely
to happen on TLS-enabled sockets, perhaps because they have a longer
"shutdown sequence".

To fix, we reset the sk_state_change pointer for the control stream
when freeing the sockets.

drbd/drbd_transport_lb-tcp.c
@@ -1417,6 +1417,13 @@ static int dtl_set_active(struct drbd_transport *transport, bool active)
 		enum drbd_stream i;
 		int err;
 
+		if (path->flow[CONTROL_STREAM].socket) {
+			write_lock_bh(&path->flow[CONTROL_STREAM].socket->sk->sk_callback_lock);
+			path->flow[CONTROL_STREAM].socket->sk->sk_state_change =
+				path->flow[CONTROL_STREAM].original_sk_state_change;
+			write_unlock_bh(&path->flow[CONTROL_STREAM].socket->sk->sk_callback_lock);
+		}
+
 		for (i = DATA_STREAM; i <= CONTROL_STREAM; i++)
 			dtl_socket_free(transport, &path->flow[i].socket);
 

drbd/drbd_transport_tcp.c
@@ -220,6 +220,13 @@ static void dtt_free(struct drbd_transport *transport, enum drbd_tr_free_op free
 		tcp_transport->control_data_ready_work.func = NULL;
 	}
 
+	if (tcp_transport->stream[CONTROL_STREAM]) {
+		write_lock_bh(&tcp_transport->stream[CONTROL_STREAM]->sk->sk_callback_lock);
+		tcp_transport->stream[CONTROL_STREAM]->sk->sk_state_change =
+			tcp_transport->original_control_sk_state_change;
+		write_unlock_bh(&tcp_transport->stream[CONTROL_STREAM]->sk->sk_callback_lock);
+	}
+
 	synchronize_rcu();
 	for (i = DATA_STREAM; i <= CONTROL_STREAM; i++) {
 		dtt_socket_free(&tcp_transport->stream[i]);

commit 9368d9bd1cc1a75f9bf5d3b7b9249be302e13036

drbd_transport: avoid resetting sk_state_change with NULL
During disconnect, we reset the state change handler to avoid spurious
NetworkFailure states. However, the original implementation did not consider
that the changed state change handler may not have been set at all. This lead
to a NULL deref during the socket shutdown.

Fixes: ba0c4191b6c4 ("drbd_transport: disable sk_state_change during free")

drbd/drbd_transport_lb-tcp.c
@@ -1417,15 +1417,16 @@ static int dtl_set_active(struct drbd_transport *transport, bool active)
 		enum drbd_stream i;
 		int err;
 
-		if (path->flow[CONTROL_STREAM].socket) {
-			write_lock_bh(&path->flow[CONTROL_STREAM].socket->sk->sk_callback_lock);
-			path->flow[CONTROL_STREAM].socket->sk->sk_state_change =
-				path->flow[CONTROL_STREAM].original_sk_state_change;
-			write_unlock_bh(&path->flow[CONTROL_STREAM].socket->sk->sk_callback_lock);
-		}
+		for (i = DATA_STREAM; i <= CONTROL_STREAM; i++) {
+			if (path->flow[i].socket && path->flow[i].original_sk_state_change) {
+				write_lock_bh(&path->flow[i].socket->sk->sk_callback_lock);
+				path->flow[i].socket->sk->sk_state_change =
+					path->flow[i].original_sk_state_change;
+				write_unlock_bh(&path->flow[i].socket->sk->sk_callback_lock);
+			}
 
-		for (i = DATA_STREAM; i <= CONTROL_STREAM; i++)
 			dtl_socket_free(transport, &path->flow[i].socket);
+		}
 
 		err = dtl_path_adjust_listener(path, active);
 

drbd/drbd_transport_tcp.c
@@ -220,7 +220,8 @@ static void dtt_free(struct drbd_transport *transport, enum drbd_tr_free_op free
 		tcp_transport->control_data_ready_work.func = NULL;
 	}
 
-	if (tcp_transport->stream[CONTROL_STREAM]) {
+	if (tcp_transport->stream[CONTROL_STREAM] &&
+	    tcp_transport->original_control_sk_state_change) {
 		write_lock_bh(&tcp_transport->stream[CONTROL_STREAM]->sk->sk_callback_lock);
 		tcp_transport->stream[CONTROL_STREAM]->sk->sk_state_change =
 			tcp_transport->original_control_sk_state_change;