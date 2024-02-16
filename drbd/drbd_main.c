LINBIT/drbd

commit ac5bf0997080a9a67fa305c1f0deea266973b493

drbd: Improve the try_to_promote retry loop
Consider that the device might be held open read-only by udev
on one of the other nodes.

drbd/drbd_main.c
@@ -2352,15 +2352,24 @@ static int try_to_promote(struct drbd_resource *resource, struct drbd_device *de
 	int retry = 3, rv;
 	do {
 		rv = drbd_set_role(resource, R_PRIMARY, false);
-		if (rv >= SS_SUCCESS || rv != SS_TWO_PRIMARIES)
+		if (rv >= SS_SUCCESS) {
 			return rv;
-
-		timeout = wait_event_interruptible_timeout(resource->state_wait,
+		} else if (rv == SS_CW_FAILED_BY_PEER) {
+			/* Probably udev has it open read-only on one of the peers */
+			long t = schedule_timeout_interruptible(HZ / 5);
+			if (t < 0)
+				break;
+		} else if (rv == SS_TWO_PRIMARIES) {
+			/* Wait till the peer demoted itself */
+			timeout = wait_event_interruptible_timeout(resource->state_wait,
 				resource->role[NOW] == R_PRIMARY ||
 				(!primary_peer_present(resource) && any_disk_is_uptodate(device)),
 				timeout);
-		if (timeout <= 0)
-			break;
+			if (timeout <= 0)
+				break;
+		} else {
+			return rv;
+		}
 	} while (--retry);
 	return rv;
 }

commit 1d2276c379cf2873b9736a4e9aced595e0495754

drbd: make timeout during promotion attempt interruptible
When schedule_timeout_interruptible() ends early due to a signal, it
returns the remaining time, which is positive. So we should check for a
non-zero return value to detect this situation, rather than checking
whether the return value is negative.

schedule_timeout_interruptible() is a thin wrapper around
schedule_timeout(). From its docs:
> In all cases the return value is guaranteed to be non-negative.

Fixes: ac5bf0997080 ("drbd: Improve the try_to_promote retry loop")

drbd/drbd_main.c
@@ -2540,7 +2540,7 @@ static int try_to_promote(struct drbd_device *device, long timeout, bool ndelay)
 			   retry only if the timeout permits */
 			if (jiffies - start < HZ / 10) {
 				t = schedule_timeout_interruptible(HZ / 10);
-				if (t < 0)
+				if (t)
 					break;
 				timeout -= HZ / 10;
 			}