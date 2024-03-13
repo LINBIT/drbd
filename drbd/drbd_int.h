273009d147313c4b806187e09885b69f0ee65e5e
Johannes Thoma <johannes@johannesthoma.com>
2024-03-13 18:31:53 +0000

    WinDRBD: 1UL -> 1ULL in drbd_int.h
    Since sizeof(unsigned long) is 4 (instead of 8) even on
    64-bit systems, the UL number literals must be ULL. We
    could cocci it but maybe it is not worth the extra work.

--- drbd/drbd_int.h
+++ drbd/drbd_int.h
@@ -2035,7 +2035,7 @@ static inline sector_t device_bit_to_kb(struct drbd_device *device, unsigned lon
 #else
 /* We allow up to 1 PiB on 64 bit architectures as long as our meta data
  * is large enough. */
-#define DRBD_MAX_SECTORS (1UL << (50 - SECTOR_SHIFT))
+#define DRBD_MAX_SECTORS (1ULL << (50 - SECTOR_SHIFT))
 #endif
 
 #define DRBD_MAX_SIZE_H80_PACKET (1U << 15) /* Header 80 only allows packets up to 32KiB data */
