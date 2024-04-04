LINBIT/drbd

commit f647d1f7fff645d2c4818aafb6fcacf1bd66cbe6

drbd: set bitmap UUID when setting bitmap slot
Otherwise, after disconnecting and reconnecting, we run through the same
logic and set the bitmap again. This makes it difficult to finish a
large resync in an environment with an unstable network.

drbd/drbd_receiver.c
@@ -4312,6 +4312,18 @@ static int bitmap_mod_after_handshake(struct drbd_peer_device *peer_device, enum
 				     BM_LOCK_CLEAR | BM_LOCK_BULK, peer_device);
 		if (err)
 			return err;
+
+		if (drbd_current_uuid(device) != UUID_JUST_CREATED &&
+				strategy == SYNC_SOURCE_SET_BITMAP) {
+			/*
+			 * We have just written the bitmap slot. Update the
+			 * bitmap UUID so that the resync does not start from
+			 * the beginning again if we disconnect and reconnect.
+			 */
+			drbd_uuid_set_bitmap(peer_device, peer_device->current_uuid);
+			drbd_print_uuids(peer_device, "updated bitmap UUID");
+			drbd_md_sync(device);
+		}
 	}
 	return 0;
 }

commit be9a404134acc3d167e8a7e60adce4f1910a4893

drbd: fix continuation of an initial resync
Commit f647d1f7ff introduces the idea of setting the
sync-source side bitmap-slot to the current-uuid of the target.

This causes a problem when the peer's curreht-uuid is still at the
initial value of UUID_JUST_CREATED. At reconnect time, the sync source
finds that initial UUID_JUST_CREATED in unused bitmap-peer slots.

Fixes: f647d1f7ff ("drbd: set bitmap UUID when setting bitmap slot")

drbd/drbd_receiver.c
@@ -4316,11 +4316,16 @@ static int bitmap_mod_after_handshake(struct drbd_peer_device *peer_device, enum
 			return err;
 
 		if (drbd_current_uuid(device) != UUID_JUST_CREATED &&
-				strategy == SYNC_SOURCE_SET_BITMAP) {
+		    peer_device->current_uuid != UUID_JUST_CREATED &&
+		    strategy == SYNC_SOURCE_SET_BITMAP) {
 			/*
 			 * We have just written the bitmap slot. Update the
 			 * bitmap UUID so that the resync does not start from
 			 * the beginning again if we disconnect and reconnect.
+			 *
+			 * Initial resync continuation is handled in
+			 * drbd_start_resync() at comment:
+			 * prepare to continue an interrupted initial resync later
 			 */
 			drbd_uuid_set_bitmap(peer_device, peer_device->current_uuid);
 			drbd_print_uuids(peer_device, "updated bitmap UUID");