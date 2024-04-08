LINBIT/drbd

commit 1471184d79ecc595cd84a36a345375e538c39d57

drbd: fix path notifications for NOTIFY_EXISTS and NOTIFY_DESTROY
get_initial_state() should send NOTIFY_EXISTS for paths as well.

path NOTIFY_DESTROY was only sent from explicit adm_del_path(),
but not from implicit destruction from "down" or "del-peer".

extend drbd_path_events() with `bool destroyed`, and set that flag
when used from drbd_transport_shutdown(, DESTROY_TRANSPORT).

drbd/drbd_nl.c
@@ -6750,51 +6750,65 @@ void drbd_broadcast_peer_device_state(struct drbd_peer_device *peer_device)
 	mutex_unlock(&notification_mutex);
 }
 
-void notify_path(struct drbd_connection *connection, struct drbd_path *path,
-		 enum drbd_notification_type type)
+void notify_path_state(struct sk_buff *skb,
+		       unsigned int seq,
+		       /* until we have a backpointer in drbd_path, we need an explicit connection: */
+		       struct drbd_connection *connection,
+		       struct drbd_path *path,
+		       struct drbd_path_info *path_info,
+		       enum drbd_notification_type type)
 {
 	struct drbd_resource *resource = connection->resource;
-	struct drbd_path_info path_info;
-	unsigned int seq = atomic_inc_return(&drbd_genl_seq);
-	struct sk_buff *skb = NULL;
 	struct drbd_genlmsghdr *dh;
+	bool multicast = false;
 	int err;
 
-	path_info.path_established = path->established;
-
-	skb = genlmsg_new(NLMSG_GOODSIZE, GFP_NOIO);
-	err = -ENOMEM;
-	if (!skb)
-		goto fail;
+	if (!skb) {
+		seq = atomic_inc_return(&drbd_genl_seq);
+		skb = genlmsg_new(NLMSG_GOODSIZE, GFP_NOIO);
+		err = -ENOMEM;
+		if (!skb)
+			goto failed;
+		multicast = true;
+	}
 
 	err = -EMSGSIZE;
 	dh = genlmsg_put(skb, 0, seq, &drbd_genl_family, 0, DRBD_PATH_STATE);
 	if (!dh)
-		goto fail;
+		goto nla_put_failure;
 
 	dh->minor = -1U;
 	dh->ret_code = NO_ERROR;
-	mutex_lock(&notification_mutex);
 	if (nla_put_drbd_cfg_context(skb, resource, connection, NULL, path) ||
 	    nla_put_notification_header(skb, type) ||
-	    drbd_path_info_to_skb(skb, &path_info, true))
-		goto unlock_fail;
-
+	    drbd_path_info_to_skb(skb, path_info, true))
+		goto nla_put_failure;
 	genlmsg_end(skb, dh);
-	err = drbd_genl_multicast_events(skb, GFP_NOWAIT);
-	skb = NULL;
-	/* skb has been consumed or freed in netlink_broadcast() */
-	if (err && err != -ESRCH)
-		goto unlock_fail;
-	mutex_unlock(&notification_mutex);
+	if (multicast) {
+		err = drbd_genl_multicast_events(skb, GFP_NOWAIT);
+		/* skb has been consumed or freed in netlink_broadcast() */
+		if (err && err != -ESRCH)
+			goto failed;
+	}
 	return;
 
-unlock_fail:
-	mutex_unlock(&notification_mutex);
-fail:
+nla_put_failure:
 	nlmsg_free(skb);
-	drbd_err(resource, "Error %d while broadcasting event. Event seq:%u\n",
+failed:
+	/* FIXME add path specifics to our drbd_polymorph_printk.h */
+	drbd_err(connection, "path: Error %d while broadcasting event. Event seq:%u\n",
 		 err, seq);
+}
+
+void notify_path(struct drbd_connection *connection, struct drbd_path *path, enum drbd_notification_type type)
+{
+	struct drbd_path_info path_info;
+
+	path_info.path_established = path->established;
+	mutex_lock(&notification_mutex);
+	notify_path_state(NULL, 0, connection, path, &path_info, type);
+	mutex_unlock(&notification_mutex);
+
 }
 
 void notify_helper(enum drbd_notification_type type,
@@ -6881,7 +6895,8 @@ static unsigned int notifications_for_state_change(struct drbd_state_change *sta
 	return 1 +
 	       state_change->n_connections +
 	       state_change->n_devices +
-	       state_change->n_devices * state_change->n_connections;
+	       state_change->n_devices * state_change->n_connections +
+	       state_change->n_paths;
 }
 
 static int get_initial_state(struct sk_buff *skb, struct netlink_callback *cb)
@@ -6916,6 +6931,18 @@ static int get_initial_state(struct sk_buff *skb, struct netlink_callback *cb)
 		goto next;
 	}
 	n -= state_change->n_connections;
+	if (n < state_change->n_paths) {
+		struct drbd_path_state *path_state = &state_change->paths[n];
+		struct drbd_path_info path_info;
+
+		path_info.path_established = path_state->path_established;
+		notify_path_state(skb, seq,
+				path_state->connection,
+				path_state->path,
+				&path_info, NOTIFY_EXISTS | flags);
+		goto next;
+	}
+	n -= state_change->n_paths;
 	if (n < state_change->n_devices) {
 		notify_device_state_change(skb, seq, &state_change->devices[n],
 					   NOTIFY_EXISTS | flags);
@@ -6927,6 +6954,7 @@ static int get_initial_state(struct sk_buff *skb, struct netlink_callback *cb)
 						NOTIFY_EXISTS | flags);
 		goto next;
 	}
+	n -= state_change->n_devices * state_change->n_connections;
 
 next:
 	if (cb->args[4] == cb->args[3]) {

drbd/drbd_transport_tcp.c
@@ -205,8 +205,10 @@ static void dtt_free(struct drbd_transport *transport, enum drbd_tr_free_op free
 	for_each_path_ref(drbd_path, transport) {
 		bool was_established = drbd_path->established;
 		drbd_path->established = false;
-		if (was_established)
-			drbd_path_event(transport, drbd_path);
+		if (free_op == DESTROY_TRANSPORT)
+			drbd_path_event(transport, drbd_path, true);
+		else if (was_established)
+			drbd_path_event(transport, drbd_path, false);
 	}
 
 	if (free_op == DESTROY_TRANSPORT) {
@@ -1040,7 +1042,7 @@ randomize:
 
 	TR_ASSERT(transport, first_path == connect_to_path);
 	connect_to_path->path.established = true;
-	drbd_path_event(transport, &connect_to_path->path);
+	drbd_path_event(transport, &connect_to_path->path, false);
 	dtt_put_listeners(transport);
 
 	dsocket->sk->sk_reuse = SK_CAN_REUSE; /* SO_REUSEADDR */

drbd/drbd_state.c
@@ -272,45 +272,48 @@ bool resource_is_suspended(struct drbd_resource *resource, enum which_state whic
 }
 
 static void count_objects(struct drbd_resource *resource,
-			  unsigned int *n_devices,
-			  unsigned int *n_connections)
+			  struct drbd_state_change_object_count *ocnt)
 {
 	/* Caller holds req_lock */
+	struct drbd_path *path;
 	struct drbd_device *device;
 	struct drbd_connection *connection;
 	int vnr;
 
-	*n_devices = 0;
-	*n_connections = 0;
+	ocnt->n_devices = 0;
+	ocnt->n_connections = 0;
+	ocnt->n_paths = 0;
 
 	idr_for_each_entry(&resource->devices, device, vnr)
-		(*n_devices)++;
-	for_each_connection(connection, resource)
-		(*n_connections)++;
+		ocnt->n_devices++;
+	for_each_connection(connection, resource) {
+		ocnt->n_connections++;
+		list_for_each_entry(path, &connection->transport.paths, list) {
+			ocnt->n_paths++;
+		}
+	}
 }
 
-static struct drbd_state_change *alloc_state_change(unsigned int n_devices, unsigned int n_connections, gfp_t flags)
+static struct drbd_state_change *alloc_state_change(struct drbd_state_change_object_count *ocnt, gfp_t flags)
 {
 	struct drbd_state_change *state_change;
-	unsigned int size, n;
+	unsigned int size;
 
 	size = sizeof(struct drbd_state_change) +
-	       n_devices * sizeof(struct drbd_device_state_change) +
-	       n_connections * sizeof(struct drbd_connection_state_change) +
-	       n_devices * n_connections * sizeof(struct drbd_peer_device_state_change);
-	state_change = kmalloc(size, flags);
+	       ocnt->n_devices * sizeof(struct drbd_device_state_change) +
+	       ocnt->n_connections * sizeof(struct drbd_connection_state_change) +
+	       ocnt->n_devices * ocnt->n_connections * sizeof(struct drbd_peer_device_state_change) +
+	       ocnt->n_paths * sizeof(struct drbd_path_state);
+	state_change = kzalloc(size, flags);
 	if (!state_change)
 		return NULL;
-	state_change->n_devices = n_devices;
-	state_change->n_connections = n_connections;
+	state_change->n_connections = ocnt->n_connections;
+	state_change->n_devices = ocnt->n_devices;
+	state_change->n_paths = ocnt->n_paths;
 	state_change->devices = (void *)(state_change + 1);
-	state_change->connections = (void *)&state_change->devices[n_devices];
-	state_change->peer_devices = (void *)&state_change->connections[n_connections];
-	state_change->resource->resource = NULL;
-	for (n = 0; n < n_devices; n++)
-		state_change->devices[n].device = NULL;
-	for (n = 0; n < n_connections; n++)
-		state_change->connections[n].connection = NULL;
+	state_change->connections = (void *)&state_change->devices[ocnt->n_devices];
+	state_change->peer_devices = (void *)&state_change->connections[ocnt->n_connections];
+	state_change->paths = (void*)&state_change->peer_devices[ocnt->n_devices*ocnt->n_connections];
 	return state_change;
 }
 
@@ -319,17 +322,17 @@ struct drbd_state_change *remember_state_change(struct drbd_resource *resource, 
 	/* Caller holds req_lock */
 	struct drbd_state_change *state_change;
 	struct drbd_device *device;
-	unsigned int n_devices;
 	struct drbd_connection *connection;
-	unsigned int n_connections;
+	struct drbd_state_change_object_count ocnt;
 	int vnr;
 
 	struct drbd_device_state_change *device_state_change;
 	struct drbd_peer_device_state_change *peer_device_state_change;
 	struct drbd_connection_state_change *connection_state_change;
+	struct drbd_path_state *path_state; /* yes, not a _change :-( */
 
-	count_objects(resource, &n_devices, &n_connections);
-	state_change = alloc_state_change(n_devices, n_connections, gfp);
+	count_objects(resource, &ocnt);
+	state_change = alloc_state_change(&ocnt, gfp);
 	if (!state_change)
 		return NULL;
 
@@ -384,7 +387,10 @@ struct drbd_state_change *remember_state_change(struct drbd_resource *resource, 
 	}
 
 	connection_state_change = state_change->connections;
+	path_state = state_change->paths;
 	for_each_connection(connection, resource) {
+		struct drbd_path *path;
+
 		kref_get(&connection->kref);
 		kref_debug_get(&connection->kref_debug, 7);
 		connection_state_change->connection = connection;
@@ -394,6 +400,21 @@ struct drbd_state_change *remember_state_change(struct drbd_resource *resource, 
 		       connection->peer_role, sizeof(connection->peer_role));
 		memcpy(connection_state_change->susp_fen,
 		       connection->susp_fen, sizeof(connection->susp_fen));
+
+		list_for_each_entry(path, &connection->transport.paths, list) {
+			/* Share the connection kref with above.
+			 * Could also share the pointer, but would then need to
+			 * remember an additional n_paths per connection
+			 * count/offset (connection_state_change->n_paths++)
+			 * to be able to associate the paths with its connection.
+			 * So why not directly store the pointer here again. */
+			path_state->connection = connection;
+			kref_get(&path->kref);
+			path_state->path = path;
+			path_state->path_established = path->established;
+
+			path_state++;
+		}
 
 		connection_state_change++;
 	}
@@ -472,6 +493,12 @@ void forget_state_change(struct drbd_state_change *state_change)
 		if (connection) {
 			kref_debug_put(&connection->kref_debug, 7);
 			kref_put(&connection->kref, drbd_destroy_connection);
+		}
+	}
+	for (n = 0; n < state_change->n_paths; n++) {
+		struct drbd_path *path = state_change->paths[n].path;
+		if (path) {
+			kref_put(&path->kref, drbd_destroy_path);
 		}
 	}
 	kfree(state_change);

commit 20cded57d7affbcc5b1f1146320ad6ce244fff4d

drbd: move paths list manipulation out of transport.
The motivation for this change is to prevent overflow of the array
state_change->paths. The functions remember_state_change() and
dtt_add_path() may run concurrently. This caused more paths to be stored
in state_change->paths than the number for which memory had been
allocated. This overflow caused corruption of other data.

Fix this by protecting the paths list with conf_update and state_rwlock.
This prevents the list from changing while remember_state_change() runs.
This change requires the paths list to be managed by the DRBD module
instead of the transport itself.

In order to achieve this, split the transport connect() op into
prepare_connect(), connect() and finish_connect(). Also make the
transport free() op exclusive with paths list manipulation.

This change also fixes some potential crashes that could have been
triggered by deleting a path at an inconvenient time.

The memory corruption mentioned above was observed as follows:

list_add corruption. next->prev should be prev (ffff9e9b0d86c9c0), but was 0000000000000000. (next=ffff9e9b6af13a00).
------------[ cut here ]------------
kernel BUG at lib/list_debug.c:27!
RIP: 0010:__list_add_valid.cold+0x4b/0x5b
Call Trace:
lc_put+0xb0/0xe0 [lru_cache]
put_actlog+0x71/0xc0 [drbd]
got_peer_ack+0x1ad/0x2e0 [drbd]
drbd_ack_receiver+0x18d/0x590 [drbd]

When there is 1 device and 1 connection, but not yet any paths,
alloc_state_change() computes a size of 184 bytes for remembering the
state change. As a result, state_change is allocated in the kmalloc-192
slab. When a path is added between such a call to alloc_state_change()
and the iteration over the paths in remember_state_change(), then the
new path is stored to state_change->paths, overflowing the allocated
memory. Coincidentally, struct lru_cache is also allocated in the same
slab. The overflow can cause the start of drbd_device.act_log to be
overwritten with data from the struct drbd_path_state. When the
lru_cache.lru list is accessed, the corruption is observed.

Fixes: 1471184d79ec

drbd/drbd_int.h
@@ -901,8 +901,11 @@ struct drbd_resource {
 	struct list_head resources;     /* list entry in global resources list */
 	struct res_opts res_opts;
 	int max_node_id;
-	struct mutex conf_update;	/* for read-copy-update of net_conf and disk_conf
-					   and devices, connection and peer_devices lists */
+	/*
+	 * For read-copy-update of net_conf and disk_conf and devices,
+	 * connection, peer_devices and paths lists.
+	 */
+	struct mutex conf_update;
 	struct mutex adm_mutex;		/* mutex to serialize administrative requests */
 	struct mutex open_release;	/* serialize open/release */
 	struct {

drbd/drbd_main.c
@@ -2939,6 +2939,31 @@ static void drbd_release(struct gendisk *gd)
 	kref_put(&device->kref, drbd_destroy_device);  /* might destroy the resource as well */
 }
 
+static void drbd_remove_all_paths(struct drbd_connection *connection)
+{
+	struct drbd_resource *resource = connection->resource;
+	struct drbd_transport *transport = &connection->transport;
+	struct drbd_path *path, *tmp;
+
+	lockdep_assert_held(&resource->conf_update);
+
+	list_for_each_entry(path, &transport->paths, list)
+		set_bit(TR_UNREGISTERED, &path->flags);
+
+	/* Ensure flag visible before list manipulation. */
+	smp_wmb();
+
+	list_for_each_entry_safe(path, tmp, &transport->paths, list) {
+		/* Exclusive with reading state, in particular remember_state_change() */
+		write_lock_irq(&resource->state_rwlock);
+		list_del_rcu(&path->list);
+		write_unlock_irq(&resource->state_rwlock);
+
+		notify_path(connection, path, NOTIFY_DESTROY);
+		call_rcu(&path->rcu, drbd_reclaim_path);
+	}
+}
+
 void drbd_queue_unplug(struct drbd_device *device)
 {
 	struct drbd_resource *resource = device->resource;
@@ -3661,18 +3686,32 @@ fail:
 	return NULL;
 }
 
-/* free the transport specific members (e.g., sockets) of a connection */
+/**
+ * drbd_transport_shutdown() - Free the transport specific members (e.g., sockets) of a connection
+ *
+ * Must be called with conf_update held.
+ */
 void drbd_transport_shutdown(struct drbd_connection *connection, enum drbd_tr_free_op op)
 {
+	struct drbd_transport *transport = &connection->transport;
+
+	lockdep_assert_held(&connection->resource->conf_update);
+
 	mutex_lock(&connection->mutex[DATA_STREAM]);
 	mutex_lock(&connection->mutex[CONTROL_STREAM]);
 
 	flush_send_buffer(connection, DATA_STREAM);
 	flush_send_buffer(connection, CONTROL_STREAM);
 
-	connection->transport.class->ops.free(&connection->transport, op);
-	if (op == DESTROY_TRANSPORT)
-		drbd_put_transport_class(connection->transport.class);
+	/* Holding conf_update ensures that paths list is not modified concurrently. */
+	transport->class->ops.free(transport, op);
+	if (op == DESTROY_TRANSPORT) {
+		drbd_remove_all_paths(connection);
+
+		/* Wait for the delayed drbd_reclaim_path() calls. */
+		rcu_barrier();
+		drbd_put_transport_class(transport->class);
+	}
 
 	mutex_unlock(&connection->mutex[CONTROL_STREAM]);
 	mutex_unlock(&connection->mutex[DATA_STREAM]);
@@ -3683,6 +3722,8 @@ void drbd_destroy_path(struct kref *kref)
 	struct drbd_path *path = container_of(kref, struct drbd_path, kref);
 	struct drbd_connection *connection =
 		container_of(path->transport, struct drbd_connection, transport);
+
+	connection->transport.class->ops.remove_path(path);
 
 	kref_put(&connection->kref, drbd_destroy_connection);
 	kfree(path);

drbd/drbd_nl.c
@@ -4489,6 +4489,8 @@ static enum drbd_ret_code
 adm_add_path(struct drbd_config_context *adm_ctx,  struct genl_info *info)
 {
 	struct drbd_transport *transport = &adm_ctx->connection->transport;
+	struct drbd_resource *resource = adm_ctx->resource;
+	struct drbd_connection *connection = adm_ctx->connection;
 	struct nlattr **nested_attr_tb;
 	struct nlattr *my_addr, *peer_addr;
 	struct drbd_path *path;
@@ -4526,13 +4528,26 @@ adm_add_path(struct drbd_config_context *adm_ctx,  struct genl_info *info)
 
 	kref_init(&path->kref);
 
+	/* Exclusive with transport op "prepare_connect()" */
+	mutex_lock(&resource->conf_update);
+
 	err = transport->class->ops.add_path(path);
+
 	if (err) {
 		kref_put(&path->kref, drbd_destroy_path);
-		drbd_err(adm_ctx->connection, "add_path() failed with %d\n", err);
+		drbd_err(connection, "add_path() failed with %d\n", err);
 		drbd_msg_put_info(adm_ctx->reply_skb, "add_path on transport failed");
+		mutex_unlock(&resource->conf_update);
 		return ERR_INVALID_REQUEST;
 	}
+
+	/* Exclusive with reading state, in particular remember_state_change() */
+	write_lock_irq(&resource->state_rwlock);
+	list_add_tail_rcu(&path->list, &transport->paths);
+	write_unlock_irq(&resource->state_rwlock);
+
+	mutex_unlock(&resource->conf_update);
+
 	notify_path(adm_ctx->connection, path, NOTIFY_CREATE);
 	return NO_ERROR;
 }
@@ -4666,6 +4681,7 @@ static int drbd_adm_new_path(struct sk_buff *skb, struct genl_info *info)
 static enum drbd_ret_code
 adm_del_path(struct drbd_config_context *adm_ctx,  struct genl_info *info)
 {
+	struct drbd_resource *resource = adm_ctx->resource;
 	struct drbd_connection *connection = adm_ctx->connection;
 	struct drbd_transport *transport = &connection->transport;
 	struct nlattr **nested_attr_tb;
@@ -4701,9 +4717,25 @@ adm_del_path(struct drbd_config_context *adm_ctx,  struct genl_info *info)
 		if (!addr_eq_nla(&path->peer_addr, path->peer_addr_len, peer_addr))
 			continue;
 
-		err = transport->class->ops.remove_path(path);
-		if (err)
+		/* Exclusive with transport op "prepare_connect()" */
+		mutex_lock(&resource->conf_update);
+
+		if (!transport->class->ops.may_remove_path(path)) {
+			err = -EBUSY;
+			mutex_unlock(&resource->conf_update);
 			break;
+		}
+
+		set_bit(TR_UNREGISTERED, &path->flags);
+		/* Ensure flag visible before list manipulation. */
+		smp_wmb();
+
+		/* Exclusive with reading state, in particular remember_state_change() */
+		write_lock_irq(&resource->state_rwlock);
+		list_del_rcu(&path->list);
+		write_unlock_irq(&resource->state_rwlock);
+
+		mutex_unlock(&resource->conf_update);
 
 		notify_path(connection, path, NOTIFY_DESTROY);
 		/* Transport modules might use RCU on the path list. */

drbd/drbd_receiver.c
@@ -994,6 +994,26 @@ static int connect_work(struct drbd_work *work, int cancel)
 	return 0;
 }
 
+static int drbd_transport_connect(struct drbd_connection *connection)
+{
+	struct drbd_transport *transport = &connection->transport;
+	struct drbd_resource *resource = connection->resource;
+	int err = 0;
+
+	mutex_lock(&resource->conf_update);
+	err = transport->class->ops.prepare_connect(transport);
+	mutex_unlock(&resource->conf_update);
+
+	if (!err)
+		err = transport->class->ops.connect(transport);
+
+	mutex_lock(&resource->conf_update);
+	transport->class->ops.finish_connect(transport);
+	mutex_unlock(&resource->conf_update);
+
+	return err;
+}
+
 /*
  * Returns true if we have a valid connection.
  */
@@ -1022,7 +1042,7 @@ start:
 	 * protocol version; until we know better. */
 	connection->agreed_pro_version = drbd_protocol_version_min;
 
-	err = transport->class->ops.connect(transport);
+	err = drbd_transport_connect(connection);
 	if (err == -EAGAIN) {
 		enum drbd_conn_state cstate;
 		read_lock_irq(&resource->state_rwlock); /* See commit message */
@@ -8637,7 +8657,9 @@ static void conn_disconnect(struct drbd_connection *connection)
 	drbd_thread_stop(&connection->sender);
 	drbd_thread_start(&connection->sender);
 
+	mutex_lock(&resource->conf_update);
 	drbd_transport_shutdown(connection, CLOSE_CONNECTION);
+	mutex_unlock(&resource->conf_update);
 
 	cleanup_remote_state_change(connection);
 

drbd/drbd_transport_tcp.c
@@ -80,7 +80,9 @@ static void dtt_free(struct drbd_transport *transport, enum drbd_tr_free_op free
 static int dtt_init_listener(struct drbd_transport *transport, const struct sockaddr *addr,
 			     struct net *net, struct drbd_listener *drbd_listener);
 static void dtt_destroy_listener(struct drbd_listener *generic_listener);
+static int dtt_prepare_connect(struct drbd_transport *transport);
 static int dtt_connect(struct drbd_transport *transport);
+static void dtt_finish_connect(struct drbd_transport *transport);
 static int dtt_recv(struct drbd_transport *transport, enum drbd_stream stream, void **buf, size_t size, int flags);
 static int dtt_recv_pages(struct drbd_transport *transport, struct drbd_page_chain_head *chain, size_t size);
 static void dtt_stats(struct drbd_transport *transport, struct drbd_transport_stats *stats);
@@ -95,7 +97,8 @@ static bool dtt_hint(struct drbd_transport *transport, enum drbd_stream stream, 
 static void dtt_debugfs_show(struct drbd_transport *transport, struct seq_file *m);
 static void dtt_update_congested(struct drbd_tcp_transport *tcp_transport);
 static int dtt_add_path(struct drbd_path *path);
-static int dtt_remove_path(struct drbd_path *);
+static bool dtt_may_remove_path(struct drbd_path *);
+static void dtt_remove_path(struct drbd_path *);
 
 static struct drbd_transport_class tcp_transport_class = {
 	.name = "tcp",
@@ -107,7 +110,9 @@ static struct drbd_transport_class tcp_transport_class = {
 		.free = dtt_free,
 		.init_listener = dtt_init_listener,
 		.release_listener = dtt_destroy_listener,
+		.prepare_connect = dtt_prepare_connect,
 		.connect = dtt_connect,
+		.finish_connect = dtt_finish_connect,
 		.recv = dtt_recv,
 		.recv_pages = dtt_recv_pages,
 		.stats = dtt_stats,
@@ -120,6 +125,7 @@ static struct drbd_transport_class tcp_transport_class = {
 		.hint = dtt_hint,
 		.debugfs_show = dtt_debugfs_show,
 		.add_path = dtt_add_path,
+		.may_remove_path = dtt_may_remove_path,
 		.remove_path = dtt_remove_path,
 	},
 	.module = THIS_MODULE,
@@ -173,28 +179,18 @@ static void dtt_free(struct drbd_transport *transport, enum drbd_tr_free_op free
 		}
 	}
 
-	for_each_path_ref(drbd_path, transport) {
+	list_for_each_entry(drbd_path, &transport->paths, list) {
 		bool was_established = test_and_clear_bit(TR_ESTABLISHED, &drbd_path->flags);
 
-		if (free_op == DESTROY_TRANSPORT)
-			drbd_path_event(transport, drbd_path, true);
-		else if (was_established)
+		if (free_op == CLOSE_CONNECTION && was_established)
 			drbd_path_event(transport, drbd_path, false);
 	}
 
 	if (free_op == DESTROY_TRANSPORT) {
-		struct drbd_path *tmp;
-
 		for (i = DATA_STREAM; i <= CONTROL_STREAM; i++) {
 			free_page((unsigned long)tcp_transport->rbuf[i].base);
 			tcp_transport->rbuf[i].base = NULL;
 		}
-		spin_lock(&tcp_transport->paths_lock);
-		list_for_each_entry_safe(drbd_path, tmp, &transport->paths, list) {
-			list_del_rcu(&drbd_path->list);
-			kref_put(&drbd_path->kref, drbd_destroy_path);
-		}
-		spin_unlock(&tcp_transport->paths_lock);
 	}
 }
 
@@ -837,20 +833,18 @@ static void dtt_cleanup_accepted_sockets(struct dtt_path *path)
 	}
 }
 
-static void dtt_put_listeners(struct drbd_transport *transport)
+static void dtt_finish_connect(struct drbd_transport *transport)
 {
 	struct drbd_tcp_transport *tcp_transport =
 		container_of(transport, struct drbd_tcp_transport, transport);
-	struct drbd_path *drbd_path;
+	struct dtt_path *path;
 
 	spin_lock(&tcp_transport->paths_lock);
 	clear_bit(DTT_CONNECTING, &tcp_transport->flags);
 	spin_unlock(&tcp_transport->paths_lock);
 
-	for_each_path_ref(drbd_path, transport) {
-		struct dtt_path *path = container_of(drbd_path, struct dtt_path, path);
-
-		drbd_put_listener(drbd_path);
+	list_for_each_entry(path, &transport->paths, path.list) {
+		drbd_put_listener(&path->path);
 		dtt_cleanup_accepted_sockets(path);
 	}
 }
@@ -872,6 +866,45 @@ static struct dtt_path *dtt_next_path(struct dtt_path *path)
 	return container_of(drbd_path, struct dtt_path, path);
 }
 
+static int dtt_prepare_connect(struct drbd_transport *transport)
+{
+	struct drbd_tcp_transport *tcp_transport =
+		container_of(transport, struct drbd_tcp_transport, transport);
+	struct drbd_path *drbd_path;
+
+	for_each_path_ref(drbd_path, transport) {
+		struct dtt_path *path = container_of(drbd_path, struct dtt_path, path);
+
+		dtt_cleanup_accepted_sockets(path);
+	}
+
+	spin_lock(&tcp_transport->paths_lock);
+	set_bit(DTT_CONNECTING, &tcp_transport->flags);
+
+	list_for_each_entry(drbd_path, &transport->paths, list) {
+		if (!drbd_path->listener) {
+			int err;
+
+			kref_get(&drbd_path->kref);
+			spin_unlock(&tcp_transport->paths_lock);
+			err = drbd_get_listener(drbd_path);
+			kref_put(&drbd_path->kref, drbd_destroy_path);
+			if (err)
+				return err;
+			spin_lock(&tcp_transport->paths_lock);
+			drbd_path = list_first_entry_or_null(&transport->paths, struct drbd_path,
+					list);
+			if (drbd_path)
+				continue;
+			else
+				break;
+		}
+	}
+	spin_unlock(&tcp_transport->paths_lock);
+
+	return 0;
+}
+
 static int dtt_connect(struct drbd_transport *transport)
 {
 	struct drbd_tcp_transport *tcp_transport =
@@ -886,40 +919,14 @@ static int dtt_connect(struct drbd_transport *transport)
 	dsocket = NULL;
 	csocket = NULL;
 
-
-	for_each_path_ref(drbd_path, transport) {
-		struct dtt_path *path = container_of(drbd_path, struct dtt_path, path);
-
-		dtt_cleanup_accepted_sockets(path);
-	}
-
 	spin_lock(&tcp_transport->paths_lock);
-	set_bit(DTT_CONNECTING, &tcp_transport->flags);
-
-	err = -EDESTADDRREQ;
-	if (list_empty(&transport->paths)) {
+	drbd_path = list_first_or_null_rcu(&transport->paths, struct drbd_path, list);
+	if (!drbd_path) {
 		spin_unlock(&tcp_transport->paths_lock);
+		err = -EDESTADDRREQ;
 		goto out;
 	}
 
-	list_for_each_entry(drbd_path, &transport->paths, list) {
-		if (!drbd_path->listener) {
-			kref_get(&drbd_path->kref);
-			spin_unlock(&tcp_transport->paths_lock);
-			err = drbd_get_listener(drbd_path);
-			kref_put(&drbd_path->kref, drbd_destroy_path);
-			if (err)
-				goto out;
-			spin_lock(&tcp_transport->paths_lock);
-			drbd_path = list_first_entry_or_null(&transport->paths, struct drbd_path, list);
-			if (drbd_path)
-				continue;
-			else
-				break;
-		}
-	}
-
-	drbd_path = list_first_entry(&transport->paths, struct drbd_path, list);
 	connect_to_path = container_of(drbd_path, struct dtt_path, path);
 	spin_unlock(&tcp_transport->paths_lock);
 
@@ -1027,7 +1034,6 @@ randomize:
 	TR_ASSERT(transport, first_path == connect_to_path);
 	set_bit(TR_ESTABLISHED, &connect_to_path->path.flags);
 	drbd_path_event(transport, &connect_to_path->path, false);
-	dtt_put_listeners(transport);
 
 	dsocket->sk->sk_reuse = SK_CAN_REUSE; /* SO_REUSEADDR */
 	csocket->sk->sk_reuse = SK_CAN_REUSE; /* SO_REUSEADDR */
@@ -1079,16 +1085,8 @@ out_eagain:
 	err = -EAGAIN;
 
 out:
-	dtt_put_listeners(transport);
-
-	if (dsocket) {
-		kernel_sock_shutdown(dsocket, SHUT_RDWR);
-		sock_release(dsocket);
-	}
-	if (csocket) {
-		kernel_sock_shutdown(csocket, SHUT_RDWR);
-		sock_release(csocket);
-	}
+	dtt_socket_free(&dsocket);
+	dtt_socket_free(&csocket);
 
 	return err;
 }
@@ -1304,53 +1302,24 @@ static int dtt_add_path(struct drbd_path *drbd_path)
 	struct drbd_tcp_transport *tcp_transport =
 		container_of(transport, struct drbd_tcp_transport, transport);
 	struct dtt_path *path = container_of(drbd_path, struct dtt_path, path);
-	bool active;
 
 	clear_bit(TR_ESTABLISHED, &drbd_path->flags);
 	INIT_LIST_HEAD(&path->sockets);
-retry:
-	active = test_bit(DTT_CONNECTING, &tcp_transport->flags);
-	if (!active && drbd_path->listener)
-		drbd_put_listener(drbd_path);
 
-	if (active && !drbd_path->listener) {
-		int err = drbd_get_listener(drbd_path);
-		if (err)
-			return err;
-	}
+	if (!test_bit(DTT_CONNECTING, &tcp_transport->flags))
+		return 0;
 
-	spin_lock(&tcp_transport->paths_lock);
-	if (active != test_bit(DTT_CONNECTING, &tcp_transport->flags)) {
-		spin_unlock(&tcp_transport->paths_lock);
-		goto retry;
-	}
-	list_add_tail_rcu(&drbd_path->list, &transport->paths);
-	spin_unlock(&tcp_transport->paths_lock);
-
-	return 0;
+	return drbd_get_listener(drbd_path);
 }
 
-static int dtt_remove_path(struct drbd_path *drbd_path)
+static bool dtt_may_remove_path(struct drbd_path *drbd_path)
 {
-	struct drbd_transport *transport = drbd_path->transport;
-	struct drbd_tcp_transport *tcp_transport =
-		container_of(transport, struct drbd_tcp_transport, transport);
-	struct dtt_path *path = container_of(drbd_path, struct dtt_path, path);
+	return !test_bit(TR_ESTABLISHED, &drbd_path->flags);
+}
 
-	if (test_bit(TR_ESTABLISHED, &drbd_path->flags))
-		return -EBUSY;
-
-	set_bit(TR_UNREGISTERED, &drbd_path->flags);
-	/* Ensure flag visible before list manipulation. */
-	smp_wmb();
-
-	spin_lock(&tcp_transport->paths_lock);
-	list_del_rcu(&drbd_path->list);
-	spin_unlock(&tcp_transport->paths_lock);
-	synchronize_rcu();
-	drbd_put_listener(&path->path);
-
-	return 0;
+static void dtt_remove_path(struct drbd_path *drbd_path)
+{
+	drbd_put_listener(drbd_path);
 }
 
 static int __init dtt_initialize(void)