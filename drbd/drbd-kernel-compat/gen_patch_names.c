LINBIT/drbd

commit 6fd35546c2d2afd10afe04ab9cdf4ac9ca0b3e94

compat: sock: Remove ->sendpage*() in favour of sendmsg(MSG_SPLICE_PAGES)
See also upstream Linux kernel commits
dc97391e6610 ("sock: Remove ->sendpage*() in favour of sendmsg(MSG_SPLICE_PAGES)")
eeac7405c735 ("drbd: Use sendmsg(MSG_SPLICE_PAGES) rather than sendpage()")
4b9c2edaf728 ("drbd: swap bvec_set_page len and offset")

Original message:

Remove ->sendpage() and ->sendpage_locked().  sendmsg() with
MSG_SPLICE_PAGES should be used instead.  This allows multiple pages and
multipage folios to be passed through.

drbd/drbd-kernel-compat/gen_patch_names.c
@@ -127,6 +127,9 @@ int main(int argc, char **argv)
 	patch(1, "bvec_kmap_local", true, false,
 	      COMPAT_HAVE_BVEC_KMAP_LOCAL, "present");
 
+	patch(1, "msg_splice_pages", true, false,
+	      COMPAT_HAVE_MSG_SPLICE_PAGES, "present");
+
 	patch(1, "struct_bvec_iter", true, false,
 	      COMPAT_HAVE_STRUCT_BVEC_ITER, "present");
 

drbd/drbd-kernel-compat/cocci/msg_splice_pages__no_present.cocci
@@ -1,0 +1,16 @@
+@@
+expression flags;
+@@
+  flags
+- | MSG_SPLICE_PAGES
+
+@@
+identifier bvec, page, len, offset, msg, socket;
+@@
+- struct bio_vec bvec;
+  ...
+  int sent;
+- bvec_set_page(&bvec, page, len, offset);
+- iov_iter_bvec(&msg.msg_iter, ITER_SOURCE, &bvec, 1, len);
+- sent = sock_sendmsg(socket, &msg);
++ sent = socket->ops->sendpage(socket, page, offset, len, msg.msg_flags);

drbd/drbd-kernel-compat/tests/have_msg_splice_pages.c
@@ -1,0 +1,7 @@
+/* { "version": "v6.5-rc1", "commit": "dc97391e661009eab46783030d2404c9b6e6f2e7", "comment": "sock: Remove ->sendpage*() in favour of sendmsg(MSG_SPLICE_PAGES)", "author": "David Howells <dhowells@redhat.com>", "date": "Fri Jun 23 23:55:12 2023 +0100" } */
+#include <linux/net.h>
+
+int foo(void)
+{
+	return MSG_SPLICE_PAGES;
+}

drbd/drbd_transport_tcp.c
@@ -1193,18 +1193,22 @@ static int dtt_send_page(struct drbd_transport *transport, enum drbd_stream stre
 	struct drbd_tcp_transport *tcp_transport =
 		container_of(transport, struct drbd_tcp_transport, transport);
 	struct socket *socket = tcp_transport->stream[stream];
+	struct msghdr msg = { .msg_flags = msg_flags | MSG_NOSIGNAL | MSG_SPLICE_PAGES };
+	struct bio_vec bvec;
 	int len = size;
 	int err = -EIO;
 
 	if (!socket)
 		return -ENOTCONN;
 
-	msg_flags |= MSG_NOSIGNAL;
 	dtt_update_congested(tcp_transport);
 	do {
 		int sent;
 
-		sent = socket->ops->sendpage(socket, page, offset, len, msg_flags);
+		bvec_set_page(&bvec, page, len, offset);
+		iov_iter_bvec(&msg.msg_iter, ITER_SOURCE, &bvec, 1, len);
+
+		sent = sock_sendmsg(socket, &msg);
 		if (sent <= 0) {
 			if (sent == -EAGAIN) {
 				if (drbd_stream_send_timed_out(transport, stream))

commit 77a871dafb57bad23b9e368f11d6a3ae9aa8a909

compat: detect removal of sendpage directly
Upstream removed the ->sendpage() ops on the socket in favour of using sendmsg.
We used the newly introduced MSG_SPLICE_PAGES flag to detect this.

Turns out that the flag was backported to some LTS kernels (notably Debian 12).
The flag was backported without any of the supporting infrastructure, so some
implementations for sendmsg() reject the call based on this unknown flag.

The fix is to directly check the presence of the sendpage ops, as that is what
we are actually trying to patch.

Fixes: 6fd35546c2d2 ("compat: sock: Remove ->sendpage*() in favour of sendmsg(MSG_SPLICE_PAGES)")

drbd/drbd-kernel-compat/gen_patch_names.c
@@ -127,8 +127,8 @@ int main(int argc, char **argv)
 	patch(1, "bvec_kmap_local", true, false,
 	      COMPAT_HAVE_BVEC_KMAP_LOCAL, "present");
 
-	patch(1, "msg_splice_pages", true, false,
-	      COMPAT_HAVE_MSG_SPLICE_PAGES, "present");
+	patch(1, "sendpage", false, true,
+	      COMPAT_HAVE_SENDPAGE, "present");
 
 	patch(1, "struct_bvec_iter", true, false,
 	      COMPAT_HAVE_STRUCT_BVEC_ITER, "present");

drbd/drbd-kernel-compat/tests/have_sendpage.c
@@ -1,7 +1,7 @@
 /* { "version": "v6.5-rc1", "commit": "dc97391e661009eab46783030d2404c9b6e6f2e7", "comment": "sock: Remove ->sendpage*() in favour of sendmsg(MSG_SPLICE_PAGES)", "author": "David Howells <dhowells@redhat.com>", "date": "Fri Jun 23 23:55:12 2023 +0100" } */
 #include <linux/net.h>
 
-int foo(void)
+int foo(struct socket *socket, struct page *page)
 {
-	return MSG_SPLICE_PAGES;
+	return socket->ops->sendpage(socket, page, 0, 0, 0);
 }