c2b3f003dab47552f935aea5f5c6392a31a29308
Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>
2024-04-11 10:48:23 +0200

    build: fix builds when no compat patches are necessary
    Recent changes to the build system meant that when the list of patches to
    apply is empty, the build would fail because the rendered patch file did
    not exist.
    
    So we first check of the rendered patch file actually exists. If not, we
    simply put an empty file in it place.
    
    Also, use tabs for the gen_compat_patch.sh script consistently for the
    sanity of anyone touching it.

--- drbd/drbd-kernel-compat/gen_compat_patch.sh
+++ drbd/drbd-kernel-compat/gen_compat_patch.sh
@@ -19,8 +19,8 @@ function spatch_is_recent() {
 }
 
 function die_no_spatch() {
-   echo "ERROR: no suitable spatch found in \$PATH. Install package 'coccinelle'!"
-   exit 1
+	echo "ERROR: no suitable spatch found in \$PATH. Install package 'coccinelle'!"
+	exit 1
 }
 
 # generate compat patches by using the cache,
@@ -32,79 +32,90 @@ function die_no_spatch() {
 set -e
 
 if test -e .compat_patches_applied; then
-    echo "Removing compat patches"
-    patch -R -p0 --batch --reject-file=- < .compat_patches_applied
-    rm -f .compat_patches_applied
+	echo "Removing compat patches"
+	patch -R -p0 --batch --reject-file=- < .compat_patches_applied
+	rm -f .compat_patches_applied
 fi
 
 if ! spatch_is_recent; then
-    echo "INFO: spatch not recent enough, need spatch version >= $MIN_SPATCH_VERSION"
+	echo "INFO: spatch not recent enough, need spatch version >= $MIN_SPATCH_VERSION"
 fi
 
 if hash spatch && spatch_is_recent; then
-    K=$(cat $incdir/kernelrelease.txt || echo unknown kernel release)
-    echo "  GENPATCHNAMES   "$K
-    gcc -I $incdir -o $incdir/gen_patch_names -std=c99 drbd-kernel-compat/gen_patch_names.c
-    $incdir/gen_patch_names > $incdir/applied_cocci_files.txt
-    rm $incdir/gen_patch_names
-    # truncat them all
-    : > $incdir/.compat.cocci
-    : > $incdir/.compat.cocci.tmp
-    : > $incdir/.compat.patch
-    : > $incdir/.compat.patch.tmp
-    rm -f $incdir/.spatch.tty.out
-    for F in $(cat $incdir/applied_cocci_files.txt); do
-	F_cocci=drbd-kernel-compat/cocci/$F.cocci
-	if [ -e $F_cocci ] ; then
-	    (
-	    # so you can match spatch warnings to cocci source files
-	    dashes=${F_cocci//?/-}
-	    printf "\n// -%s-\n//  %s\n// -%s-\n" "$dashes" "$F_cocci" "$dashes"
-	    cat $F_cocci
-	    ) >> $incdir/.compat.cocci.tmp
+	K=$(cat $incdir/kernelrelease.txt || echo unknown kernel release)
+	echo "  GENPATCHNAMES   "$K
+	gcc -I $incdir -o $incdir/gen_patch_names -std=c99 drbd-kernel-compat/gen_patch_names.c
+	$incdir/gen_patch_names > $incdir/applied_cocci_files.txt
+	rm $incdir/gen_patch_names
+	# truncat them all
+	: > $incdir/.compat.cocci
+	: > $incdir/.compat.cocci.tmp
+	: > $incdir/.compat.patch
+	: > $incdir/.compat.patch.tmp
+	rm -f $incdir/.spatch.tty.out
+
+	for F in $(cat $incdir/applied_cocci_files.txt); do
+		F_cocci=drbd-kernel-compat/cocci/$F.cocci
+		if [ -e $F_cocci ] ; then
+			(
+			# so you can match spatch warnings to cocci source files
+			dashes=${F_cocci//?/-}
+			printf "\n// -%s-\n//  %s\n// -%s-\n" "$dashes" "$F_cocci" "$dashes"
+			cat $F_cocci
+			) >> $incdir/.compat.cocci.tmp
+		else
+			F_patch=drbd-kernel-compat/patches/$F.patch
+			cat $F_patch >> $incdir/.compat.patch.tmp
+		fi
+		sed -e "s:@COMPAT_PATCH_NAME@:$F:g" \
+			< drbd-kernel-compat/cocci/debugfs_compat_template.cocci.in \
+			>> $incdir/.compat.cocci.tmp
+	done
+
+	mv $incdir/.compat.cocci.tmp $incdir/.compat.cocci
+	mv $incdir/.compat.patch.tmp $incdir/.compat.patch
+
+	if [ -s $incdir/.compat.cocci ]; then
+		# sources=( ... ) passed in via environment
+		echo "	SPATCH	 $chksum  "$K
+		set +e
+		spatch --sp-file "$incdir/.compat.cocci" "${sources[@]}" \
+			--macro-file drbd-kernel-compat/cocci_macros.h \
+			--very-quiet \
+			--all-includes \
+			${SPATCH_DEBUG:+ --debug} \
+			> "$compat_patch.tmp" \
+			2> "$incdir/.spatch.stderr"
+		ex=$?
+		# if [[ $ex != 0 ]] || [[ ${V-0} != 0 ]] ; then
+		# I want to see the spatch warnings, even without V=...
+		if test -s $incdir/.spatch.stderr ; then
+			echo "	  $incdir/.compat.cocci" >&2
+			sed -e "s/^/	: /" < "$incdir/.spatch.stderr" >&2
+			# spatch warnings fatal? not yet.
+			# exit 1
+		fi
+		[[ $ex != 0 ]] && exit $ex
+		set -e
 	else
-	    F_patch=drbd-kernel-compat/patches/$F.patch
-	    cat $F_patch >> $incdir/.compat.patch.tmp
+		echo "	SPATCH	 $chksum  "$K" - nothing to do"
 	fi
-	sed -e "s:@COMPAT_PATCH_NAME@:$F:g" \
-		< drbd-kernel-compat/cocci/debugfs_compat_template.cocci.in \
-		>> $incdir/.compat.cocci.tmp
-    done
-    mv $incdir/.compat.cocci.tmp $incdir/.compat.cocci
-    mv $incdir/.compat.patch.tmp $incdir/.compat.patch
-    if [ -s $incdir/.compat.cocci ]; then
-	# sources=( ... ) passed in via environment
-	echo "	SPATCH	 $chksum  "$K
-	set +e
-	spatch --sp-file "$incdir/.compat.cocci" "${sources[@]}" \
-		--macro-file drbd-kernel-compat/cocci_macros.h \
-		--very-quiet \
-		--all-includes \
-		${SPATCH_DEBUG:+ --debug} \
-		> "$compat_patch.tmp" \
-		2> "$incdir/.spatch.stderr"
-	ex=$?
-	# if [[ $ex != 0 ]] || [[ ${V-0} != 0 ]] ; then
-	# I want to see the spatch warnings, even without V=...
-	if test -s $incdir/.spatch.stderr ; then
-		echo "	  $incdir/.compat.cocci" >&2
-		sed -e "s/^/	: /" < "$incdir/.spatch.stderr" >&2
-		# spatch warnings fatal? not yet.
-		# exit 1
+
+	if [ -s $incdir/.compat.patch ]; then
+		cat $incdir/.compat.patch >> $compat_patch.tmp
 	fi
-	[[ $ex != 0 ]] && exit $ex
-	set -e
-    else
-	echo "	SPATCH	 $chksum  "$K" - nothing to do"
-    fi
-    if [ -s $incdir/.compat.patch ]; then
-	cat $incdir/.compat.patch >> $compat_patch.tmp
-    fi
-    mv $compat_patch.tmp $compat_patch
-    # keep it around
-    # to better be able to match the "stderr" warnings to their source files
-    # rm -f $incdir/.compat.cocci
-    rm -f $incdir/.compat.patch
+
+	if [ -s $compat_patch.tmp ]; then
+		mv $compat_patch.tmp $compat_patch
+	else
+		# hooray, there are no compat patches necessary
+		touch $compat_patch
+	fi
+
+	# keep it around
+	# to better be able to match the "stderr" warnings to their source files
+	# rm -f $incdir/.compat.cocci
+	rm -f $incdir/.compat.patch
 else
 	if test -e ../.git; then
 		echo "  INFO: not trying spatch-as-a-service because you are trying"
@@ -122,36 +133,36 @@ else
 		die_no_spatch
 	fi
 
-    echo "  INFO: no suitable spatch found; trying spatch-as-a-service;"
-    echo "  be patient, may take up to 10 minutes"
-    echo "  if it is in the server side cache it might only take a second"
-    echo "  SPAAS    $chksum"
+	echo "  INFO: no suitable spatch found; trying spatch-as-a-service;"
+	echo "  be patient, may take up to 10 minutes"
+	echo "  if it is in the server side cache it might only take a second"
+	echo "  SPAAS    $chksum"
 
-    # check if SPAAS is even reachable
-    SPAAS_URL=${SPAAS_URL:-https://spaas.drbd.io}
-    if ! curl -fsS "${SPAAS_URL}/api/v1/hello"; then
-        echo "  ERROR: SPAAS is not reachable! Please check if your network"
-        echo "  configuration or some firewall prohibits access to "
-        echo "  '${SPAAS_URL}'."
-        exit 1
-    fi
+	# check if SPAAS is even reachable
+	SPAAS_URL=${SPAAS_URL:-https://spaas.drbd.io}
+	if ! curl -fsS "${SPAAS_URL}/api/v1/hello"; then
+		echo "  ERROR: SPAAS is not reachable! Please check if your network"
+		echo "  configuration or some firewall prohibits access to "
+		echo "  '${SPAAS_URL}'."
+		exit 1
+	fi
 
-    REL_VERSION=$(sed -ne '/^\#define REL_VERSION/{s/^[^"]*"\([^ "]*\).*/\1/;p;q;}' linux/drbd_config.h)
-    rm -f $compat_patch.tmp.header $compat_patch.tmp
-    if ! base64 $incdir/compat.h |
-	curl -T - -X POST -o $compat_patch.tmp -D $compat_patch.tmp.header -f \
-	    "${SPAAS_URL}/api/v1/spatch/${REL_VERSION}"
-    then
-	ex=${PIPESTATUS[*]}
-	(
-	echo "=== pipestatus: $ex"
-	cat $compat_patch.tmp.header $compat_patch.tmp
-	printf "\n===\n\n"
-	) >&2
-	exit ${ex##* }
-    else
-	mv $compat_patch.tmp $compat_patch
-    fi
-    echo "  You can create a new .tgz including this pre-computed compat patch"
-    echo "  by calling \"echo drbd/$compat_patch >>.filelist ; make tgz\""
+	REL_VERSION=$(sed -ne '/^\#define REL_VERSION/{s/^[^"]*"\([^ "]*\).*/\1/;p;q;}' linux/drbd_config.h)
+	rm -f $compat_patch.tmp.header $compat_patch.tmp
+	if ! base64 $incdir/compat.h |
+		curl -T - -X POST -o $compat_patch.tmp -D $compat_patch.tmp.header -f \
+		"${SPAAS_URL}/api/v1/spatch/${REL_VERSION}"
+	then
+		ex=${PIPESTATUS[*]}
+		(
+		echo "=== pipestatus: $ex"
+		cat $compat_patch.tmp.header $compat_patch.tmp
+		printf "\n===\n\n"
+		) >&2
+		exit ${ex##* }
+	else
+		mv $compat_patch.tmp $compat_patch
+	fi
+	echo "  You can create a new .tgz including this pre-computed compat patch"
+	echo "  by calling \"echo drbd/$compat_patch >>.filelist ; make tgz\""
 fi
