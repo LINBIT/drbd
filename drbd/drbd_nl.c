LINBIT/drbd

commit e0f412ccd5e214c11b09d18cefc914eb831e1434

drbd: Do not clog the own worker thread for an empty 2PC
The DRBD resource work queue is not made for long-running work
items. Also, a lot of effort was necessary to avoid deadlocks when
work items need random DRBD locks. The final nail in the coffin of
this approach is two-phase commits that run into a 30-second
timeout. It is not acceptable that other work items get delayed for 30
seconds.
Remove all that. Use a system work queue instead.

drbd/drbd_nl.c
@@ -964,7 +964,7 @@ static bool after_primary_lost_events_settled(struct drbd_resource *resource)
 	struct drbd_device *device;
 	int vnr;
 
-	if (test_bit(EMPTY_TWOPC_PENDING, &resource->flags))
+	if (test_bit(TRY_BECOME_UP_TO_DATE_PENDING, &resource->flags))
 		return false;
 
 	rcu_read_lock();
@@ -6869,7 +6869,6 @@ static int adm_del_resource(struct drbd_resource *resource)
 
 	timer_shutdown_sync(&resource->twopc_timer);
 	timer_shutdown_sync(&resource->peer_ack_timer);
-	timer_shutdown_sync(&resource->repost_up_to_date_timer);
 	call_rcu(&resource->rcu, drbd_reclaim_resource);
 
 	mutex_lock(&notification_mutex);

drbd/drbd_int.h
@@ -785,25 +785,12 @@ enum resource_flag {
 	TWOPC_EXECUTED,         /* Commited or aborted */
 	TWOPC_STATE_CHANGE_PENDING, /* set between sending commit and changing local state */
 
-	/*
-	 * The side effects of an empty state change two-phase commit are:
-	 *
-	 * * A local consistent disk can upgrade to up-to-date when no primary is reachable
-	 *   (or become outdated if the prepare packets reach a primary).
-	 *
-	 * * resource->members are updates
-	 *
-	 * * Faraway nodes might outdate themselves if they learn about the existence of a primary
-	 *   (with access to data) node.
-	 */
-	EMPTY_TWOPC_PENDING,
+	TRY_BECOME_UP_TO_DATE_PENDING,
 
 	DEVICE_WORK_PENDING,	/* tell worker that some device has pending work */
 	PEER_DEVICE_WORK_PENDING,/* tell worker that some peer_device has pending work */
-	RESOURCE_WORK_PENDING,  /* tell worker that some peer_device has pending work */
 
         /* to be used in drbd_post_work() */
-	EMPTY_TWOPC,  /* empty two-phase commit for its side effects */
 	R_UNREGISTERED,
 	DOWN_IN_PROGRESS,
 	CHECKING_PEERS,
@@ -991,7 +978,6 @@ struct drbd_resource {
 	spinlock_t listeners_lock;
 
 	struct timer_list peer_ack_timer; /* send a P_PEER_ACK after last completion */
-	struct timer_list repost_up_to_date_timer;
 
 	unsigned int w_cb_nr; /* keeps counting up */
 	struct drbd_thread_timing_details w_timing_details[DRBD_THREAD_DETAILS_HIST];
@@ -1020,6 +1006,19 @@ struct drbd_resource {
 	spinlock_t pp_lock;
 	int pp_vacant;
 	wait_queue_head_t pp_wait;
+
+	/*
+	 * The side effects of an empty state change two-phase commit are:
+	 *
+	 * * A local consistent disk can upgrade to up-to-date when no primary is reachable
+	 *   (or become outdated if the prepare packets reach a primary).
+	 *
+	 * * resource->members are updates
+	 *
+	 * * Faraway nodes might outdate themselves if they learn about the existence of a primary
+	 *   (with access to data) node.
+	 */
+	struct work_struct empty_twopc;
 };
 
 enum drbd_per_connection_ratelimit {
@@ -2031,7 +2030,6 @@ extern void drbd_check_peers(struct drbd_resource *resource);
 extern void drbd_check_peers_new_current_uuid(struct drbd_device *);
 extern void drbd_ping_peer(struct drbd_connection *connection);
 extern struct drbd_peer_device *peer_device_by_node_id(struct drbd_device *, int);
-extern void repost_up_to_date_fn(struct timer_list *t);
 extern void drbd_update_mdf_al_disabled(struct drbd_device *device, enum which_state which);
 
 static inline void ov_out_of_sync_print(struct drbd_peer_device *peer_device)
@@ -2371,16 +2369,6 @@ drbd_peer_device_post_work(struct drbd_peer_device *peer_device, int work_bit)
 		struct drbd_resource *resource = peer_device->device->resource;
 		struct drbd_work_queue *q = &resource->work;
 		if (!test_and_set_bit(PEER_DEVICE_WORK_PENDING, &resource->flags))
-			wake_up(&q->q_wait);
-	}
-}
-
-static inline void
-drbd_post_work(struct drbd_resource *resource, int work_bit)
-{
-	if (!test_and_set_bit(work_bit, &resource->flags)) {
-		struct drbd_work_queue *q = &resource->work;
-		if (!test_and_set_bit(RESOURCE_WORK_PENDING, &resource->flags))
 			wake_up(&q->q_wait);
 	}
 }

drbd/drbd_main.c
@@ -3539,7 +3539,6 @@ struct drbd_resource *drbd_create_resource(const char *name,
 	INIT_LIST_HEAD(&resource->peer_ack_work.list);
 	resource->peer_ack_work.cb = w_queue_peer_ack;
 	timer_setup(&resource->peer_ack_timer, peer_ack_timer_fn, 0);
-	timer_setup(&resource->repost_up_to_date_timer, repost_up_to_date_fn, 0);
 	sema_init(&resource->state_sem, 1);
 	resource->role[NOW] = R_SECONDARY;
 	resource->max_node_id = res_opts->node_id;
@@ -3562,6 +3561,7 @@ struct drbd_resource *drbd_create_resource(const char *name,
 	drbd_debugfs_resource_add(resource);
 	resource->cached_min_aggreed_protocol_version = drbd_protocol_version_min;
 	resource->members = NODE_MASK(res_opts->node_id);
+	INIT_WORK(&resource->empty_twopc, drbd_empty_twopc_work_fn);
 
 	ratelimit_state_init(&resource->ratelimit[D_RL_R_GENERIC], 5*HZ, 10);
 

drbd/drbd_sender.c
@@ -2357,41 +2357,6 @@ static int do_md_sync(struct drbd_device *device)
 	return 0;
 }
 
-void repost_up_to_date_fn(struct timer_list *t)
-{
-	struct drbd_resource *resource = from_timer(resource, t, repost_up_to_date_timer);
-	drbd_post_work(resource, EMPTY_TWOPC);
-}
-
-static int try_become_up_to_date(struct drbd_resource *resource)
-{
-	enum drbd_state_rv rv;
-
-	/* Doing a two_phase_commit from worker context is only possible
-	 * if twopc_work is not queued. Let it get executed first.
-	 *
-	 * Avoid deadlock on state_sem, in case someone holds it while
-	 * waiting for the completion of some after-state-change work.
-	 */
-	if (list_empty(&resource->twopc_work.list)) {
-		if (down_trylock(&resource->state_sem))
-			goto repost;
-		rv = twopc_after_lost_peer(resource, CS_ALREADY_SERIALIZED |
-			CS_VERBOSE | CS_SERIALIZE | CS_DONT_RETRY);
-		up(&resource->state_sem);
-		if (rv == SS_TIMEOUT || rv == SS_CONCURRENT_ST_CHG)
-			goto repost;
-		clear_bit(EMPTY_TWOPC_PENDING, &resource->flags);
-		wake_up_all(&resource->state_wait);
-		drbd_notify_peers_lost_primary(resource);
-	} else {
-	repost:
-		mod_timer(&resource->repost_up_to_date_timer, jiffies + HZ/10);
-	}
-
-	return 0;
-}
-
 /* only called from drbd_worker thread, no locking */
 void __update_timing_details(
 		struct drbd_thread_timing_details *tdp,
@@ -2506,9 +2471,6 @@ static void do_peer_device_work(struct drbd_peer_device *peer_device, const unsi
 		handle_congestion(peer_device);
 }
 
-#define DRBD_RESOURCE_WORK_MASK	\
-	(1UL << EMPTY_TWOPC)
-
 #define DRBD_DEVICE_WORK_MASK	\
 	((1UL << GO_DISKLESS)	\
 	|(1UL << MD_SYNC)	\
@@ -2581,14 +2543,6 @@ static void do_unqueued_device_work(struct drbd_resource *resource)
 		rcu_read_lock();
 	}
 	rcu_read_unlock();
-}
-
-static void do_unqueued_resource_work(struct drbd_resource *resource)
-{
-	unsigned long todo = get_work_bits(DRBD_RESOURCE_WORK_MASK, &resource->flags);
-
-	if (test_bit(EMPTY_TWOPC, &todo))
-		try_become_up_to_date(resource);
 }
 
 static bool dequeue_work_batch(struct drbd_work_queue *queue, struct list_head *work_list)
@@ -3070,15 +3024,14 @@ int drbd_worker(struct drbd_thread *thi)
 		drbd_thread_current_set_cpu(thi);
 
 		if (list_empty(&work_list)) {
-			bool w, r, d, p;
+			bool w, d, p;
 
 			update_worker_timing_details(resource, dequeue_work_batch);
 			wait_event_interruptible(resource->work.q_wait,
 				(w = dequeue_work_batch(&resource->work, &work_list),
-				 r = test_and_clear_bit(RESOURCE_WORK_PENDING, &resource->flags),
 				 d = test_and_clear_bit(DEVICE_WORK_PENDING, &resource->flags),
 				 p = test_and_clear_bit(PEER_DEVICE_WORK_PENDING, &resource->flags),
-				 w || r || d || p));
+				 w || d || p));
 
 			if (p) {
 				update_worker_timing_details(resource, do_unqueued_peer_device_work);
@@ -3088,10 +3041,6 @@ int drbd_worker(struct drbd_thread *thi)
 			if (d) {
 				update_worker_timing_details(resource, do_unqueued_device_work);
 				do_unqueued_device_work(resource);
-			}
-			if (r) {
-				update_worker_timing_details(resource, do_unqueued_resource_work);
-				do_unqueued_resource_work(resource);
 			}
 		}
 
@@ -3117,10 +3066,6 @@ int drbd_worker(struct drbd_thread *thi)
 	}
 
 	do {
-		if (test_and_clear_bit(RESOURCE_WORK_PENDING, &resource->flags)) {
-			update_worker_timing_details(resource, do_unqueued_resource_work);
-			do_unqueued_resource_work(resource);
-		}
 		if (test_and_clear_bit(DEVICE_WORK_PENDING, &resource->flags)) {
 			update_worker_timing_details(resource, do_unqueued_device_work);
 			do_unqueued_device_work(resource);

drbd/drbd_state.c
@@ -2443,11 +2443,8 @@ static void update_members(struct drbd_resource *resource)
 
 		/* Connection to peer lost. Check if we should remove it from the members */
 		if (drbd_need_twopc_after_lost_peer(connection) &&
-				!test_bit(EMPTY_TWOPC_PENDING, &resource->flags) &&
-				resource->members & peer_node_mask) {
-			set_bit(EMPTY_TWOPC_PENDING, &resource->flags);
-			drbd_post_work(resource, EMPTY_TWOPC);
-		}
+				resource->members & peer_node_mask)
+			schedule_work(&resource->empty_twopc);
 	}
 }
 
@@ -2970,10 +2967,8 @@ static void finish_state_change(struct drbd_resource *resource, const char *tag)
 		if (role[OLD] == R_PRIMARY && role[NEW] == R_SECONDARY)
 			clear_bit(NEW_CUR_UUID, &device->flags);
 
-		/* The NEW state version here is the same as the NOW version in
-		 * the context of w_after_state_change(). */
 		if (should_try_become_up_to_date(device, disk_state, NEW))
-			set_bit(EMPTY_TWOPC_PENDING, &resource->flags);
+			set_bit(TRY_BECOME_UP_TO_DATE_PENDING, &resource->flags);
 	}
 
 	for_each_connection(connection, resource) {
@@ -4242,7 +4237,7 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 	}
 
 	if (try_become_up_to_date || healed_primary)
-		drbd_post_work(resource, EMPTY_TWOPC);
+		schedule_work(&resource->empty_twopc);
 
 	drbd_notify_peers_lost_primary(resource);
 
@@ -5518,8 +5513,8 @@ static bool do_twopc_after_lost_peer(struct change_context *context, enum change
 	return phase != PH_PREPARE || reply->reachable_nodes != NODE_MASK(resource->res_opts.node_id);
 }
 
-enum drbd_state_rv twopc_after_lost_peer(struct drbd_resource *resource,
-					  enum chg_state_flags flags)
+static enum drbd_state_rv twopc_after_lost_peer(struct drbd_resource *resource,
+						enum chg_state_flags flags)
 {
 	struct change_context context = {
 		.resource = resource,
@@ -5535,6 +5530,17 @@ enum drbd_state_rv twopc_after_lost_peer(struct drbd_resource *resource,
 	   will agree to this change request. At commit time we know where to
 	   go from the D_CONSISTENT, since we got the primary mask. */
 	return change_cluster_wide_state(do_twopc_after_lost_peer, &context, "lost-peer");
+}
+
+void drbd_empty_twopc_work_fn(struct work_struct *work)
+{
+	struct drbd_resource *resource = container_of(work, struct drbd_resource, empty_twopc);
+
+	twopc_after_lost_peer(resource, CS_VERBOSE | CS_SERIALIZE);
+
+	clear_bit(TRY_BECOME_UP_TO_DATE_PENDING, &resource->flags);
+	wake_up_all(&resource->state_wait);
+	drbd_notify_peers_lost_primary(resource);
 }
 
 static bool do_change_disk_state(struct change_context *context, enum change_phase phase)

drbd/drbd_state.h
@@ -143,8 +143,6 @@ extern enum drbd_state_rv change_peer_disk_state(struct drbd_peer_device *peer_d
 					  enum chg_state_flags flags,
 					  const char *tag);
 
-enum drbd_state_rv twopc_after_lost_peer(struct drbd_resource *resource, enum chg_state_flags);
-
 extern void __change_resync_susp_user(struct drbd_peer_device *, bool);
 extern enum drbd_state_rv change_resync_susp_user(struct drbd_peer_device *, bool, enum chg_state_flags);
 extern void __change_resync_susp_peer(struct drbd_peer_device *, bool);
@@ -168,4 +166,6 @@ extern void drbd_notify_peers_lost_primary(struct drbd_resource *resource);
 extern bool drbd_data_accessible(struct drbd_device *, enum which_state);
 extern bool drbd_res_data_accessible(struct drbd_resource *resource);
 
+
+extern void drbd_empty_twopc_work_fn(struct work_struct *work);
 #endif

commit 16ee072ba391ab11e9a544e3d00f56bb41298118

drbd: clean up work item before destroying a resource
Cancel possible scheduled work item before destroying a resource.

Co-developed-by: Lars Ellenberg <lars.ellenberg@linbit.com>
Fixes: e0f412ccd ("drbd: Do not clog the own worker thread for an empty 2PC")

drbd/drbd_nl.c
@@ -6867,6 +6867,7 @@ static int adm_del_resource(struct drbd_resource *resource)
 	drbd_debugfs_resource_cleanup(resource);
 	mutex_unlock(&resources_mutex);
 
+	cancel_work_sync(&resource->empty_twopc);
 	timer_shutdown_sync(&resource->twopc_timer);
 	timer_shutdown_sync(&resource->peer_ack_timer);
 	call_rcu(&resource->rcu, drbd_reclaim_resource);