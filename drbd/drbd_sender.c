19209a72d7888e68a8e83a9e4e44875a5ea13687
Johannes Thoma <johannes@johannesthoma.com>
2024-07-03 14:54:36 +0000

    Some fixes of inter function flags.

--- drbd/drbd_sender.c
+++ drbd/drbd_sender.c
@@ -2798,7 +2798,7 @@ static void do_start_resync(struct drbd_peer_device *peer_device)
 static void handle_congestion(struct drbd_peer_device *peer_device)
 {
 	struct drbd_resource *resource = peer_device->device->resource;
-	unsigned long irq_flags;
+	KIRQL irq_flags;
 	struct net_conf *nc;
 	enum drbd_on_congestion on_congestion;
 
@@ -3315,7 +3315,7 @@ static void maybe_send_state_afer_ahead(struct drbd_connection *connection)
 static bool check_sender_todo(struct drbd_connection *connection, unsigned long *flags_p)
 {
 	rcu_read_lock();
-	tl_next_request_for_connection(connection);
+	tl_next_request_for_connection(connection, flags_p);
 
 	/* FIXME can we get rid of this additional lock? */
 	spin_lock_irq(&connection->sender_work.q_lock);
@@ -3338,7 +3338,7 @@ static void wait_for_sender_todo(struct drbd_connection *connection)
 	int uncork, cork;
 	bool got_something = 0;
 
-	got_something = check_sender_todo(connection);
+	got_something = check_sender_todo(connection, &spin_lock_irq_flags);
 	if (got_something)
 		return;
 
@@ -3359,7 +3359,7 @@ static void wait_for_sender_todo(struct drbd_connection *connection)
 		int send_barrier;
 		prepare_to_wait(&connection->sender_work.q_wait, &wait,
 				TASK_INTERRUPTIBLE);
-		if (check_sender_todo(connection) || signal_pending(current)) {
+		if (check_sender_todo(connection, &spin_lock_irq_flags) || signal_pending(current)) {
 			break;
 		}
 
@@ -3585,7 +3585,7 @@ static int process_one_request(struct drbd_connection *connection)
 	__req_mod(req, what, peer_device, &m);
 	read_unlock_irq(&connection->resource->state_rwlock);
 
-	check_sender_todo(connection);
+	check_sender_todo(connection, &spin_lock_irq_flags);
 
 	if (m.bio)
 		complete_master_bio(device, &m);
