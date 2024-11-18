8597f3d59aa71ea053f6d4f0c37324b113634959
Philipp Reisner <philipp.reisner@linbit.com>
2024-11-18 08:37:41 +0100

    compat: Workaround for Linux-6.12 bug in compat test have_strscpy.c
    To make it compatible with Linux-6.12:
    Workaround <linux/compiler.h> defines __must_be_array() and
    __must_be_cstr() and both expand to BUILD_BUG_ON_ZERO(), but
    <linux/build_bug.h> defines BUILD_BUG_ON_ZERO(). <linux/compiler.h>
    does not include <linux/build_bug.h> and Linus does not want that.
    
    See also
    https://lore.kernel.org/linux-kernel/20241114101402.156397-1-philipp.reisner@linbit.com/T/#t

--- drbd/drbd-kernel-compat/tests/have_strscpy.c
+++ drbd/drbd-kernel-compat/tests/have_strscpy.c
@@ -2,6 +2,19 @@
 
 #include <linux/string.h>
 
+/*
+ * For making it compatible with linux-6.12:
+ * Work around <linux/compiler.h> defines __must_be_array() and __must_be_cstr() and both expand to
+ * BUILD_BUG_ON_ZERO(), but <linux/build_bug.h> defines BUILD_BUG_ON_ZERO(). <linux/compiler.h> does
+ * not include <linux/build_bug.h> and Linus does not want that.
+ *
+ * See https://lore.kernel.org/linux-kernel/20241114101402.156397-1-philipp.reisner@linbit.com/T/#t
+ *
+ */
+#ifndef BUILD_BUG_ON_ZERO
+#define BUILD_BUG_ON_ZERO(e) ((int)(sizeof(struct { int:(-!!(e)); })))
+#endif
+
 ssize_t foo(char *dest, const char *src, size_t count)
 {
 	return strscpy(dest, src, count);
