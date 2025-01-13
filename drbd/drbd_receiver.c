LINBIT/drbd

commit c1bcb0c4c5a4fb7ef659fad1ed9b1c1b94e69a6f

drbd: do drbd_notify_peers_lost_primary() from conn_disconnect()
Instead of making the worker thread wait for the receiver of the lost
peer to terminate and then notifying the peers from the worker, just
notify them directly from the receiver before it terminates.

This fixes a complicated deadlock which can occur as follows:

* Peer S is Secondary, we are SyncTarget towards S.
* Peer P is Primary.
* We send a resync request to S.
* P sends a write to us which is held due to a conflict with the resync
  request.
* We lose connection to P, our receiver for P waits in conn_disconnect()
  for active_ee_cnt to reach zero. That is, for the write to complete.
* Our worker calls drbd_notify_peers_lost_primary() and waits for the
  receiver for P to terminate.
* S sends P_STATE, perhaps because it also lost connection to P. Our
  receiver for S waits in receive_state() for the worker to process a
  state change.
* S responds to the resync request, but we never receive the response
  because our receiver for S is stuck.

The deadlock dependency cycle in summary:
Write from P
-> Resync request to S
-> receiver for S
-> worker
-> receiver for P
-> (back to the top)

Break this cycle by removing the dependency from the worker to the
receiver.

drbd/drbd_receiver.c
@@ -9710,6 +9710,40 @@ static void cleanup_remote_state_change(struct drbd_connection *connection)
 		nested_twopc_abort(resource, &request);
 }
 
+static void drbd_notify_peers_lost_primary(struct drbd_connection *lost_peer)
+{
+	struct drbd_resource *resource = lost_peer->resource;
+	struct drbd_connection *connection;
+	u64 im;
+
+	for_each_connection_ref(connection, im, resource) {
+		struct drbd_peer_device *peer_device;
+		bool send_dagtag = false;
+		int vnr;
+
+		if (connection == lost_peer)
+			continue;
+		if (connection->cstate[NOW] != C_CONNECTED)
+			continue;
+
+		idr_for_each_entry(&connection->peer_devices, peer_device, vnr) {
+			struct drbd_device *device = peer_device->device;
+			u64 current_uuid = drbd_current_uuid(device);
+			u64 weak_nodes = drbd_weak_nodes_device(device);
+
+			if (device->disk_state[NOW] < D_INCONSISTENT ||
+			    peer_device->disk_state[NOW] < D_INCONSISTENT)
+				continue; /* Ignore if one side is diskless */
+
+			drbd_send_current_uuid(peer_device, current_uuid, weak_nodes);
+			send_dagtag = true;
+		}
+
+		if (send_dagtag)
+			drbd_send_peer_dagtag(connection, lost_peer);
+	}
+}
+
 static void conn_disconnect(struct drbd_connection *connection)
 {
 	struct drbd_resource *resource = connection->resource;
@@ -9865,6 +9899,9 @@ static void conn_disconnect(struct drbd_connection *connection)
 		drbd_thread_restart_nowait(&connection->receiver);
 	}
 	end_state_change(resource, &irq_flags, "disconnected");
+
+	if (test_and_clear_bit(NOTIFY_PEERS_LOST_PRIMARY, &connection->flags))
+		drbd_notify_peers_lost_primary(connection);
 
 	if (oc == C_DISCONNECTING)
 		change_cstate_tag(connection, C_STANDALONE, CS_VERBOSE | CS_HARD | CS_LOCAL_ONLY,

drbd/drbd_state.c
@@ -3017,6 +3017,17 @@ static void finish_state_change(struct drbd_resource *resource, const char *tag)
 		enum drbd_conn_state *cstate = connection->cstate;
 		enum drbd_role *peer_role = connection->peer_role;
 
+		/*
+		 * If we lose connection to a Primary node then we need to
+		 * inform our peers so that we can potentially do a
+		 * reconciliation resync. The function conn_disconnect()
+		 * informs the peers. So we must set the flag before stopping
+		 * the receiver.
+		 */
+		if (cstate[OLD] == C_CONNECTED && cstate[NEW] < C_CONNECTED &&
+				peer_role[OLD] == R_PRIMARY)
+			set_bit(NOTIFY_PEERS_LOST_PRIMARY, &connection->flags);
+
 		/* Receiver should clean up itself */
 		if (cstate[OLD] != C_DISCONNECTING && cstate[NEW] == C_DISCONNECTING)
 			drbd_thread_stop_nowait(&connection->receiver);
@@ -3441,58 +3452,6 @@ static void send_new_state_to_all_peer_devices(struct drbd_state_change *state_c
 
 		if (new_state.conn >= C_CONNECTED)
 			drbd_send_state(peer_device, new_state);
-	}
-}
-
-static bool receiver_exited_main_loop(struct drbd_connection *connection)
-{
-	enum drbd_conn_state cstate = connection->cstate[NOW];
-
-	return cstate == C_STANDALONE || cstate == C_UNCONNECTED ||
-		cstate == C_CONNECTING || cstate == C_CONNECTED;
-}
-
-void drbd_notify_peers_lost_primary(struct drbd_resource *resource)
-{
-	struct drbd_connection *connection, *lost_peer;
-	u64 im;
-
-	rcu_read_lock();
-	for_each_connection_rcu(lost_peer, resource) {
-		if (test_and_clear_bit(NOTIFY_PEERS_LOST_PRIMARY, &lost_peer->flags)) {
-			rcu_read_unlock();
-			goto found;
-		}
-	}
-	rcu_read_unlock();
-	return;
-found:
-
-	wait_event(resource->state_wait, receiver_exited_main_loop(lost_peer));
-	for_each_connection_ref(connection, im, resource) {
-		if (connection == lost_peer)
-			continue;
-		if (connection->cstate[NOW] == C_CONNECTED) {
-			struct drbd_peer_device *peer_device;
-			bool send_dagtag = false;
-			int vnr;
-
-			idr_for_each_entry(&connection->peer_devices, peer_device, vnr) {
-				struct drbd_device *device = peer_device->device;
-				u64 current_uuid = drbd_current_uuid(device);
-				u64 weak_nodes = drbd_weak_nodes_device(device);
-
-				if (device->disk_state[NOW] < D_INCONSISTENT ||
-				    peer_device->disk_state[NOW] < D_INCONSISTENT)
-					continue; /* Ignore if one side is diskless */
-
-				drbd_send_current_uuid(peer_device, current_uuid, weak_nodes);
-				send_dagtag = true;
-			}
-
-			if (send_dagtag)
-				drbd_send_peer_dagtag(connection, lost_peer);
-		}
 	}
 }
 
@@ -4243,7 +4202,6 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 		struct drbd_connection_state_change *connection_state_change = &state_change->connections[n_connection];
 		struct drbd_connection *connection = connection_state_change->connection;
 		enum drbd_conn_state *cstate = connection_state_change->cstate;
-		enum drbd_role *peer_role = connection_state_change->peer_role;
 		bool *susp_fen = connection_state_change->susp_fen;
 		enum drbd_fencing_policy fencing_policy;
 
@@ -4253,12 +4211,6 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 
 		if (susp_fen[NEW])
 			check_may_resume_io_after_fencing(state_change, n_connection);
-
-		if (peer_role[OLD] == R_PRIMARY &&
-		    cstate[OLD] == C_CONNECTED && cstate[NEW] < C_CONNECTED) {
-			/* A connection to a primary went down, notify other peers about that */
-			set_bit(NOTIFY_PEERS_LOST_PRIMARY, &connection->flags);
-		}
 
 		rcu_read_lock();
 		fencing_policy = connection->fencing_policy;
@@ -4286,8 +4238,6 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 
 	if (try_become_up_to_date || healed_primary)
 		drbd_schedule_empty_twopc(resource);
-
-	drbd_notify_peers_lost_primary(resource);
 
 	if (!still_connected)
 		mod_timer_pending(&resource->twopc_timer, jiffies);
@@ -5652,7 +5602,6 @@ void drbd_empty_twopc_work_fn(struct work_struct *work)
 
 	clear_bit(TRY_BECOME_UP_TO_DATE_PENDING, &resource->flags);
 	wake_up_all(&resource->state_wait);
-	drbd_notify_peers_lost_primary(resource);
 
 	kref_debug_put(&resource->kref_debug, 11);
 	kref_put(&resource->kref, drbd_destroy_resource);

drbd/drbd_state.h
@@ -163,7 +163,6 @@ extern enum determine_dev_size
 change_cluster_wide_device_size(struct drbd_device *, sector_t, uint64_t, enum dds_flags,
 				struct resize_parms *);
 
-extern void drbd_notify_peers_lost_primary(struct drbd_resource *resource);
 extern bool drbd_data_accessible(struct drbd_device *, enum which_state);
 extern bool drbd_res_data_accessible(struct drbd_resource *resource);
 

commit 75206bf1cbca2106ee803bc2f08e168433d91b18

drbd: fix wait_event() in receive_peer_dagtag()
We need to send P_PEER_DAGTAG before we start the resync. Otherwise,
the peer is surprised to get a bitmap, and it will complain about
being in the wrong state for receiving a bitmap.

So we must wait before starting the resync until the receiver thread
to the lost primary did that.

Fixes: c1bcb0c4c ("drbd: do drbd_notify_peers_lost_primary() from conn_disconnect()")

drbd/drbd_receiver.c
@@ -8867,10 +8867,13 @@ static int receive_peer_dagtag(struct drbd_connection *connection, struct packet
 		}
 	}
 
-	/* Need to wait until the other receiver thread has called the
-	   cleanup_unacked_peer_requests() function */
+	/* We must wait until the other receiver thread has called the
+	 * cleanup_unacked_peer_requests() and drbd_notify_peers_lost_primary() functions. If we
+	 * become a resync target, the peer would complain about being in the wrong state when he
+	 * gets the bitmap before the P_PEER_DAGTAG packet.
+	 */
 	wait_event(resource->state_wait,
-		   lost_peer->cstate[NOW] <= C_UNCONNECTED || lost_peer->cstate[NOW] == C_CONNECTING);
+		   !test_bit(NOTIFY_PEERS_LOST_PRIMARY, &lost_peer->flags));
 
 	dagtag_offset = atomic64_read(&lost_peer->last_dagtag_sector) - (s64)be64_to_cpu(p->dagtag);
 	if (strategy == SYNC_SOURCE_USE_BITMAP)  {
@@ -9900,8 +9903,10 @@ static void conn_disconnect(struct drbd_connection *connection)
 	}
 	end_state_change(resource, &irq_flags, "disconnected");
 
-	if (test_and_clear_bit(NOTIFY_PEERS_LOST_PRIMARY, &connection->flags))
+	if (test_bit(NOTIFY_PEERS_LOST_PRIMARY, &connection->flags)) {
 		drbd_notify_peers_lost_primary(connection);
+		clear_bit(NOTIFY_PEERS_LOST_PRIMARY, &connection->flags);
+	}
 
 	if (oc == C_DISCONNECTING)
 		change_cstate_tag(connection, C_STANDALONE, CS_VERBOSE | CS_HARD | CS_LOCAL_ONLY,