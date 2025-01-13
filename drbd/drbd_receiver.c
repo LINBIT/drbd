LINBIT/drbd

commit f2d5c3f7217fe4c33f4253bb10eb01e658b0869e

drbd: only do reconciliation resync if our disk is Consistent
This prevents a bug which causes us to get stuck in
WFBitMapT/Established. The bug occurred when we have no connection to a
Primary node, but a peer does. When that peer loses the connection to
the Primary, it sends us P_PEER_DAGTAG. Then we may try to start a
reconciliation resync. But we don't send P_PEER_DAGTAG, so the peer does
not start its side of the reconciliation resync. We end up stuck.

This situation only occurs when the peer is on the same current UUID as
us. That is, there were no writes on the Primary since it was promoted.
If there were writes then we enter a normal resync before P_PEER_DAGTAG
is received and the reconciliation resync is skipped due to the
replication state.

Fix this bug by ignoring P_PEER_DAGTAG if our disk is already Outdated.
In fact, we only expect to do a reconciliation resync when our disk is
Consistent, so test for precisely that case.

drbd/drbd_receiver.c
@@ -8173,6 +8173,8 @@ static int receive_peer_dagtag(struct drbd_connection *connection, struct packet
 
 		if (peer_device->repl_state[NOW] > L_ESTABLISHED)
 			goto out;
+		if (peer_device->device->disk_state[NOW] != D_CONSISTENT)
+			goto out;
 		if (!get_ldev(peer_device->device))
 			continue;
 		ps = drbd_uuid_compare(peer_device, &rule, &unused);

commit b3e7e91c8eec3e43cbe66e40e02b239096b97856

drbd: allow reconciliation resync if our disk is UpToDate
After a primary peer is lost, reconciliation resync considerations
(P_PEER_DAGTAG packet) and determining whether the primary is
reachable through another path happen concurrently.

This node might already be returned to UpToDate when it receives the
the P_PEER_DAGTAG packet.

Fixes: f2d5c3f7217fe4 ("drbd: only do reconciliation resync if our disk is Consistent")

drbd/drbd_receiver.c
@@ -8844,7 +8844,8 @@ static int receive_peer_dagtag(struct drbd_connection *connection, struct packet
 
 		if (peer_device->repl_state[NOW] > L_ESTABLISHED)
 			goto out;
-		if (peer_device->device->disk_state[NOW] != D_CONSISTENT)
+		if (peer_device->device->disk_state[NOW] != D_CONSISTENT &&
+		    peer_device->device->disk_state[NOW] != D_UP_TO_DATE)
 			goto out;
 		if (!get_ldev(peer_device->device))
 			continue;