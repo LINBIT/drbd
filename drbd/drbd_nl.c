LINBIT/drbd

commit 749ec5708d3862afde0e9a3aa4df61772e55345a

drbd: Optimize AL updates in a two-node cluster
In the two-node case, a Secondary may safely omit activity log updates
if the Primary peer can write its activity log. This can improve
latency unless storage transaction latency is negligible compared to
replication link latency.

In any configuration with more than two nodes with a backing disk, the
activity log is necessary, as it might be needed if we recover from a
common power failure and the former primary is missing when the power
comes back.

drbd/drbd_nl.c
@@ -2275,6 +2275,89 @@ out:
 	return err;
 }
 
+static struct drbd_connection *the_only_peer_with_disk(struct drbd_device *device,
+						       enum which_state which)
+{
+	const int my_node_id = device->resource->res_opts.node_id;
+	struct drbd_peer_md *peer_md = device->ldev->md.peers;
+	struct drbd_connection *connection = NULL;
+	struct drbd_peer_device *peer_device;
+	int node_id, peer_disks = 0;
+
+	for (node_id = 0; node_id < DRBD_NODE_ID_MAX; node_id++) {
+		if (node_id == my_node_id)
+			continue;
+
+		if (peer_md[node_id].flags & MDF_PEER_DEVICE_SEEN)
+			peer_disks++;
+
+		if (peer_disks > 1)
+			return NULL;
+
+		peer_device = peer_device_by_node_id(device, node_id);
+		if (peer_device) {
+			enum drbd_disk_state pdsk = peer_device->disk_state[which];
+
+			if (pdsk >= D_INCONSISTENT && pdsk != D_UNKNOWN)
+				connection = peer_device->connection;
+		}
+	}
+	return connection;
+}
+
+static void __update_mdf_al_disabled(struct drbd_device *device, bool al_updates,
+				     enum which_state which)
+{
+	struct drbd_md *md = &device->ldev->md;
+	struct drbd_connection *peer = NULL;
+	bool al_updates_old = !(md->flags & MDF_AL_DISABLED);
+	bool optimized = false;
+
+	if (al_updates)
+		peer = the_only_peer_with_disk(device, which);
+
+	if (al_updates && peer && peer->peer_role[which] == R_PRIMARY &&
+	    device->resource->role[which] == R_SECONDARY) {
+		al_updates = false;
+		optimized = true;
+	}
+
+	if (al_updates_old == al_updates)
+		return;
+
+	if (al_updates) {
+		drbd_info(device, "Enabling local AL-updates\n");
+		md->flags &= ~MDF_AL_DISABLED;
+	} else {
+		drbd_info(device, "Disabling local AL-updates %s\n",
+			  optimized ? "(optimization)" : "(config)");
+		md->flags |= MDF_AL_DISABLED;
+	}
+	drbd_md_mark_dirty(device);
+}
+
+/**
+ * update_mdf_al_disabled() - update the MDF_AL_DISABLED bit in md.flags
+ *
+ * This function also optimizes performance by turning off al-updates when:
+ * - the cluster has only two nodes with backing disk
+ * - the other node with a backing disk is the primary
+ */
+void drbd_update_mdf_al_disabled(struct drbd_device *device, enum which_state which)
+{
+	bool al_updates;
+
+	if (!get_ldev(device))
+		return;
+
+	rcu_read_lock();
+	al_updates = rcu_dereference(device->ldev->disk_conf)->al_updates;
+	rcu_read_unlock();
+	__update_mdf_al_disabled(device, al_updates, which);
+
+	put_ldev(device);
+}
+
 static int drbd_adm_disk_opts(struct sk_buff *skb, struct genl_info *info)
 {
 	struct drbd_config_context adm_ctx;
@@ -2347,10 +2430,7 @@ static int drbd_adm_disk_opts(struct sk_buff *skb, struct genl_info *info)
 
 	mutex_unlock(&resource->conf_update);
 
-	if (new_disk_conf->al_updates)
-		device->ldev->md.flags &= ~MDF_AL_DISABLED;
-	else
-		device->ldev->md.flags |= MDF_AL_DISABLED;
+	__update_mdf_al_disabled(device, new_disk_conf->al_updates, NOW);
 
 	if (new_disk_conf->md_flushes)
 		clear_bit(MD_NO_FUA, &device->flags);
@@ -3418,12 +3498,7 @@ static int drbd_adm_attach(struct sk_buff *skb, struct genl_info *info)
 
 	drbd_try_suspend_al(device); /* IO is still suspended here... */
 
-	rcu_read_lock();
-	if (rcu_dereference(device->ldev->disk_conf)->al_updates)
-		device->ldev->md.flags &= ~MDF_AL_DISABLED;
-	else
-		device->ldev->md.flags |= MDF_AL_DISABLED;
-	rcu_read_unlock();
+	drbd_update_mdf_al_disabled(device, NOW);
 
 	/* change_disk_state uses disk_state_from_md(device); in case D_NEGOTIATING not
 	   necessary, and falls back to a local state change */

drbd/drbd_actlog.c
@@ -429,10 +429,7 @@ static int __al_write_transaction(struct drbd_device *device, struct al_transact
 	if (drbd_bm_write_hinted(device))
 		err = -EIO;
 	else {
-		bool write_al_updates;
-		rcu_read_lock();
-		write_al_updates = rcu_dereference(device->ldev->disk_conf)->al_updates;
-		rcu_read_unlock();
+		bool write_al_updates = !(device->ldev->md.flags & MDF_AL_DISABLED);
 		if (write_al_updates) {
 			ktime_aggregate_delta(device, start_kt, al_mid_kt);
 			if (drbd_md_sync_page_io(device, device->ldev, sector, REQ_OP_WRITE)) {
@@ -530,11 +527,7 @@ void drbd_al_begin_io_commit(struct drbd_device *device)
 		/* Double check: it may have been committed by someone else
 		 * while we were waiting for the lock. */
 		if (device->act_log->pending_changes) {
-			bool write_al_updates;
-
-			rcu_read_lock();
-			write_al_updates = rcu_dereference(device->ldev->disk_conf)->al_updates;
-			rcu_read_unlock();
+			bool write_al_updates = !(device->ldev->md.flags & MDF_AL_DISABLED);
 
 			if (write_al_updates)
 				al_write_transaction(device);

drbd/drbd_int.h
@@ -2013,6 +2013,7 @@ extern void drbd_check_peers_new_current_uuid(struct drbd_device *);
 extern void drbd_ping_peer(struct drbd_connection *connection);
 extern struct drbd_peer_device *peer_device_by_node_id(struct drbd_device *, int);
 extern void repost_up_to_date_fn(struct timer_list *t);
+extern void drbd_update_mdf_al_disabled(struct drbd_device *device, enum which_state which);
 
 static inline void ov_out_of_sync_print(struct drbd_peer_device *peer_device)
 {

drbd/drbd_state.c
@@ -2819,6 +2819,10 @@ static void finish_state_change(struct drbd_resource *resource, const char *tag)
 
 			if (repl_state[OLD] > L_ESTABLISHED && repl_state[NEW] <= L_ESTABLISHED)
 				clear_bit(SYNC_SRC_CRASHED_PRI, &peer_device->flags);
+
+			if (peer_role[OLD] != peer_role[NEW] || role[OLD] != role[NEW] ||
+			    peer_disk_state[OLD] != peer_disk_state[NEW])
+				drbd_update_mdf_al_disabled(device, NEW);
 		}
 
 		for_each_connection(connection, resource) {

commit 53fe7073c53951849a997e7bf1b5f39eea1fb28f

drbd: make "day0" bitmap slots reliable again
We use our "activiy log" to make the bitmap "crash safe".

In the "two node" case, we optimize away the activity log transactions on a
Secondary, if the only diskful peer is Primary, because the activity log
that is kept on that Primary is sufficient to be "crash safe".

This is no longer true if we have more than one bitmap slot, and connect a
"crashed" secondary (or a snapshot of a secondary, which basically looks the
same as "crashed" at the point in time of the snapshot) to a new node,
and expect an unassigned bitmap slot to cover all to-be synced blocks
(aka "day0" bitmap).

We must be more strict when enabling this optimization:
optimize away the activity log transactions on a Secondary
only if there is exactly one bitmap slot
(and there is only one diskful peer, and that is primary).

Fixes: 749ec5708 (drbd: Optimize AL updates in a two-node cluster, 2024-01-24)

drbd/drbd_nl.c
@@ -2357,7 +2357,8 @@ static void __update_mdf_al_disabled(struct drbd_device *device, bool al_updates
 	if (al_updates)
 		peer = the_only_peer_with_disk(device, which);
 
-	if (al_updates && peer && peer->peer_role[which] == R_PRIMARY &&
+	if (al_updates && device->bitmap->bm_max_peers == 1 &&
+	    peer && peer->peer_role[which] == R_PRIMARY &&
 	    device->resource->role[which] == R_SECONDARY) {
 		al_updates = false;
 		optimized = true;