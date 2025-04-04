LINBIT/drbd

commit 6535593f3f2cdf4e764dcedb0ceb2546cef5fefa

build: test for working coccinelle instead of version
Second attempt, supersedes
288704a41 (build: test for working coccinelle instead of version, 2025-03-14)

Attempting to --parse-coccie each .cocci rule file individually won't work,
because some of our .cocci rule files depend on earlier ones in their series.

Also it may be overly strict, if the "problematic" rules are not even needed
for _this_ build.

Instead, first generate the aggregated rules file,
then syntax-check that result.

If our spatch is happy, try it.
If not, or if the actual spatch run fails,
still try to fall back to spatch-as-a-service (if enabled).

drbd/drbd-kernel-compat/gen_compat_patch.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-MIN_SPATCH_VERSION=1.2
+# actually, the 1.1 in fedora 40 seems to have the relevant patches backported.
+SUGGESTED_SPATCH_VERSION=1.2
 [[ ${V:-0} != [02] ]] && set -x
 
 # to be passed in via environment
 : ${sources[@]?}
 : ${compat_patch?}
 : ${chksum?}
-
-# test if the version $1 is greater (more recent) than $2.
-function version_gt() {
-	test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"
-}
-
-function spatch_is_recent() {
-	ver=$(spatch --version | sed -rn '1 s/^spatch version ([[:digit:]]+\.[^[:space:]]).*/\1/p')
-	! version_gt $MIN_SPATCH_VERSION "${ver:-0.bad.spatch.version.or.not.installed}"
-}
-
-function die_no_spatch() {
-	echo "ERROR: no suitable spatch found in \$PATH. Install package 'coccinelle'!"
-	exit 1
-}
 
 # generate compat patches by using the cache,
 # or using spatch,
@@ -31,17 +17,17 @@ function die_no_spatch() {
 
 set -e
 
+# compat with older checkouts
 if test -e .compat_patches_applied; then
 	echo "Removing compat patches"
 	patch -R -p0 --batch --reject-file=- < .compat_patches_applied
 	rm -f .compat_patches_applied
 fi
 
-if ! spatch_is_recent; then
-	echo "INFO: spatch not recent enough, need spatch version >= $MIN_SPATCH_VERSION"
-fi
+try_spatch()
+{
+	tried_spatch=false
 
-if hash spatch && spatch_is_recent; then
 	K=$(cat $incdir/kernelrelease.txt || echo unknown kernel release)
 	echo "  GENPATCHNAMES   "$K
 	gcc -I $incdir -o $incdir/gen_patch_names -std=c99 drbd-kernel-compat/gen_patch_names.c
@@ -77,7 +63,15 @@ if hash spatch && spatch_is_recent; then
 
 	if [ -s $incdir/.compat.cocci ]; then
 		# sources=( ... ) passed in via environment
-		echo "	SPATCH	 $chksum  "$K
+
+		hash spatch || return 1
+		echo "  COCCISYN  $chksum  "$K
+		if ! spatch --very-quiet --parse-cocci "$incdir/.compat.cocci" >/dev/null 2>&1 ; then
+			return 1
+		fi
+
+		tried_spatch=true
+		echo "  SPATCH  $chksum  "$K
 		set +e
 		spatch --sp-file "$incdir/.compat.cocci" "${sources[@]}" \
 			--macro-file drbd-kernel-compat/cocci_macros.h \
@@ -95,8 +89,8 @@ if hash spatch && spatch_is_recent; then
 			# spatch warnings fatal? not yet.
 			# exit 1
 		fi
-		[[ $ex != 0 ]] && exit $ex
 		set -e
+		[[ $ex != 0 ]] && return $ex
 	else
 		echo "	SPATCH	 $chksum  "$K" - nothing to do"
 	fi
@@ -116,26 +110,38 @@ if hash spatch && spatch_is_recent; then
 	# to better be able to match the "stderr" warnings to their source files
 	# rm -f $incdir/.compat.cocci
 	rm -f $incdir/.compat.patch
+	return 0
+}
+
+if try_spatch ; then
+	: local spatch run successful or not necessary.
 else
+	if $tried_spatch; then
+		echo "  local spatch run failed; see above."
+	else
+		echo "  ERROR: no (suitable) spatch found in \$PATH."
+	fi
+	# but still try spatch-as-a-service, maybe?
+		
 	if test -e ../.git; then
 		echo "  INFO: not trying spatch-as-a-service because you are trying"
 		echo "  to build DRBD from a git checkout. Please install a suitable"
-		echo "  version of coccinelle (>$MIN_SPATCH_VERSION) or try building from a"
+		echo "  version of coccinelle (>$SUGGESTED_SPATCH_VERSION) or try building from a"
 		echo "  release tarball."
-		die_no_spatch
+		exit 1
 	fi
 
 	if [[ $SPAAS != true ]]; then
 		echo "  INFO: spatch-as-a-service was disabled by your package"
 		echo "  maintainer (\$SPAAS = false). Install a suitable version"
-		echo "  of coccinelle (>$MIN_SPATCH_VERSION) or allow spatch-as-a-service by"
+		echo "  of coccinelle (>$SUGGESTED_SPATCH_VERSION) or allow spatch-as-a-service by"
 		echo "  setting \$SPAAS = true"
-		die_no_spatch
+		exit 1
 	fi
 
-	echo "  INFO: no suitable spatch found; trying spatch-as-a-service;"
-	echo "  be patient, may take up to 10 minutes"
-	echo "  if it is in the server side cache it might only take a second"
+	echo "  INFO: spatch failed, or no suitable spatch found; trying spatch-as-a-service;"
+	echo "  be patient, may take up to 10 minutes."
+	echo "  If it is in the server side cache it might only take a second."
 	echo "  SPAAS    $chksum"
 
 	# check if SPAAS is even reachable
@@ -163,6 +169,8 @@ else
 	else
 		mv $compat_patch.tmp $compat_patch
 	fi
-	echo "  You can create a new .tgz including this pre-computed compat patch"
-	echo "  by calling \"echo drbd/$compat_patch >>.filelist ; make tgz\""
 fi
+
+# still here?
+echo "  You can create a new .tgz including this pre-computed compat patch"
+echo "  by calling \"echo drbd/$compat_patch >>.filelist ; make tgz\""

commit 58856c64884bb45db5c1c52fbab320451d4ca785

build: gen_compat_patch.sh: fail if build environment is incomplete
If you have the relevant `gcc-some-version` installed to compile kernel code,
but are missing the generic `gcc`, or maybe only the standard c headers,
the generation of `compat.h` still works (using the kernel gcc-some-version),
but the compilation of `gen_patch_names.c` from within `gen_compat_patch.sh` may fail.

Previously, with `set -e` aka `errexit`, any failing command would cause
gen_compat_patch.sh to exit with an exit status != 0.

After I put the "try local spatch" sequence into a function try_spatch(),
and call it from within the condition of an `if try_spatch; then ...`,
that is no longer the case.  Oh the subtleties of "set -e" :-/

Add status variables, convert short-circuit returns to "return 0",
and call "try_spatch" outside of the condition,
to allow `errexit` to do its thing.

Fixes: 6535593f3 (build: test for working coccinelle instead of version, 2025-03-20)

drbd/drbd-kernel-compat/gen_compat_patch.sh
@@ -24,13 +24,22 @@ if test -e .compat_patches_applied; then
 	rm -f .compat_patches_applied
 fi
 
+# Because we are running under "set -e" aka "errexit",
+# this must not be called as a condition command inside an "if" or similar,
+# short-circuit returns are "return 0",
+# and the state propagation happens via variables:
+gcc_success=false
+need_spatch=false
+tried_spatch=false
+spatch_success=false
+compat_patch_generated=false
 try_spatch()
 {
-	tried_spatch=false
 
 	K=$(cat $incdir/kernelrelease.txt || echo unknown kernel release)
 	echo "  GENPATCHNAMES   "$K
 	gcc -I $incdir -o $incdir/gen_patch_names -std=c99 drbd-kernel-compat/gen_patch_names.c
+	gcc_success=true
 	$incdir/gen_patch_names > $incdir/applied_cocci_files.txt
 	rm $incdir/gen_patch_names
 	# truncat them all
@@ -62,17 +71,20 @@ try_spatch()
 	mv $incdir/.compat.patch.tmp $incdir/.compat.patch
 
 	if [ -s $incdir/.compat.cocci ]; then
-		# sources=( ... ) passed in via environment
-
-		hash spatch || return 1
+		need_spatch=true
+		if ! hash spatch >&/dev/null ; then
+			echo "    No local spatch found."
+			return 0
+		fi
 		echo "  COCCISYN  $chksum  "$K
 		if ! spatch --very-quiet --parse-cocci "$incdir/.compat.cocci" >/dev/null 2>&1 ; then
-			return 1
+			echo "    Local spatch found, but cannot parse our .cocci rules."
+			return 0
 		fi
-
 		tried_spatch=true
 		echo "  SPATCH  $chksum  "$K
 		set +e
+		# sources=( ... ) passed in via environment
 		spatch --sp-file "$incdir/.compat.cocci" "${sources[@]}" \
 			--macro-file drbd-kernel-compat/cocci_macros.h \
 			--very-quiet \
@@ -90,7 +102,8 @@ try_spatch()
 			# exit 1
 		fi
 		set -e
-		[[ $ex != 0 ]] && return $ex
+		[[ $ex != 0 ]] && return 0
+		spatch_success=true
 	else
 		echo "	SPATCH	 $chksum  "$K" - nothing to do"
 	fi
@@ -110,16 +123,27 @@ try_spatch()
 	# to better be able to match the "stderr" warnings to their source files
 	# rm -f $incdir/.compat.cocci
 	rm -f $incdir/.compat.patch
+	compat_patch_generated=true
 	return 0
 }
 
-if try_spatch ; then
+try_spatch
+if $compat_patch_generated ; then
 	: local spatch run successful or not necessary.
 else
-	if $tried_spatch; then
-		echo "  local spatch run failed; see above."
+	if $spatch_success; then
+		echo "  Local spatch run was successful, but we still had problems generating the final compat patch; see above."
+	elif $need_spatch; then
+		if $tried_spatch; then
+			echo "  Local spatch run failed; see above."
+		else
+			echo "  No (suitable) spatch found in \$PATH."
+		fi
+	elif $gcc_success; then
+		echo "  Problem generating the compat patch locally."
 	else
-		echo "  ERROR: no (suitable) spatch found in \$PATH."
+		echo "  Problem translating compat.h to the list of necessary patches."
+		echo "  We expect a standard build environment, including gcc and glibc-headers|libc-dev."
 	fi
 	# but still try spatch-as-a-service, maybe?
 		