LINBIT/drbd

commit f37f016f256a09e6279caa2535f862bb59340469

docker: add rhel10 image

Makefile
@@ -31,7 +31,7 @@ ARCH ?= amd64
 ifneq ($(strip $(ARCH)),)
 DOCKERREGISTRY := $(DOCKERREGISTRY)/$(ARCH)
 endif
-DOCKERIMAGES = rhel7 rhel8 rhel9 focal jammy noble flatcar amzn2 sles15
+DOCKERIMAGES = rhel7 rhel8 rhel9 rhel10 focal jammy noble flatcar amzn2 sles15
 DOCKERIMAGESTARGETS = $(addprefix dockerimage.,$(DOCKERIMAGES))
 
 # Use the SPAAS (spatch as a service) online service

docker/Dockerfile.rhel10
@@ -1,0 +1,53 @@
+ARG EXTRAPKGS=almalinux:10
+FROM $EXTRAPKGS as extrapkgs
+# by checking for /pkgs we can cache that step
+# and prepare images that already contain the packages.
+RUN mkdir /pkgs
+RUN dnf install -y 'dnf-command(download)' && cd /pkgs && dnf download elfutils-libelf-devel && rm -f *.i686.rpm # !lbbuild
+
+FROM registry.access.redhat.com/ubi10/ubi
+MAINTAINER Roland Kammerer <roland.kammerer@linbit.com>
+
+ENV DRBD_VERSION 9.2.13
+
+ARG release=1
+LABEL name="DRBD Kernel module load container" \
+      vendor="LINBIT" \
+		version="$DRBD_VERSION" \
+		release="$release" \
+		summary="DRBD's kernel component" \
+		description="DRBD's kernel component"
+RUN mkdir /licenses && cp /usr/share/licenses/shadow-utils/gpl-2.0.txt /licenses/
+
+RUN dnf -y update-minimal --security --sec-severity=Important --sec-severity=Critical && \
+	dnf install -y \
+		cpio \
+		diffutils \
+		gcc \
+		kmod \
+		make \
+		patch \
+		perl \
+		python3 \
+		python3-pip \
+		xz \
+	&& \
+	dnf clean all -y && \
+	pip-3 install https://github.com/LINBIT/python-lbdist/archive/master.tar.gz
+
+COPY --from=extrapkgs /pkgs /pkgs
+RUN dnf install -y /pkgs/*.rpm # !lbbuild
+# or
+# =lbbuild RUN curl -fsSL https://nexus.at.linbit.com/repository/lbbuild/from_rhel_repos.sh | bash -s -- elfutils-libelf-devel
+
+RUN rm -rf /pkgs
+
+# one can not comment copy
+RUN curl -fsSL https://pkg.linbit.com/downloads/drbd/9/drbd-${DRBD_VERSION}.tar.gz -o /drbd.tar.gz # !lbbuild
+# =lbbuild COPY /drbd.tar.gz /
+
+# these are now the kmod pkgs:
+COPY /pkgs /pkgs
+
+COPY /entry.sh /
+ENTRYPOINT /entry.sh

commit 673e701a5a18cdec7c3116eeff0dc44c70a35cc5

ci,release: also check RHEL 10 files
Fixes: f37f016f256a ("docker: add rhel10 image")

.gitlab/drbd-prepare-release.sh
@@ -21,7 +21,7 @@ dummy-release.sh drbd "$version" "$release" drbd-kernel.spec --source-online ${D
 sed -re "s/(#define REL_VERSION) .*/\1 \"$rel_version\"/g" drbd/linux/drbd_config.h > drbd/linux/drbd_config.h.tmp
 mv drbd/linux/drbd_config.h{.tmp,}
 
-for i in 7 8 9; do
+for i in 7 8 9 10; do
 	sed -re "s/(ENV DRBD_VERSION) .*/\1 $rel_version/g" docker/Dockerfile.rhel${i} > docker/Dockerfile.rhel${i}.tmp
 	mv docker/Dockerfile.rhel${i}{.tmp,}
 done

Makefile
@@ -167,7 +167,7 @@ check check_changelogs_up2date:
 	then									\
 	   printf "\nChangeLog:3:\tneeds update\n"; 				\
 	   up2date=false; fi ; 							\
-	for df in 7 8 9 ; do							\
+	for df in 7 8 9 10; do							\
 	if ! grep "^ENV DRBD_VERSION $$dver" docker/Dockerfile.rhel$$df ;	\
 	then 									\
 		printf "\nDockerfile.rhel$$df: needs update\n"; 		\