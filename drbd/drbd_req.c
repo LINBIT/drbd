LINBIT/drbd

commit d82ddc03f8c767a2556f5ac9545d3d5dcc58cfa8

drbd: postpone writes conflicting with postponed requests
DRBD needs to properly postpone writes conflicting with postponed
requests so that it releases references to the local backing device
while the request is waiting.

drbd/drbd_req.c
@@ -2097,7 +2097,7 @@ out:
 static void drbd_conflict_submit_write(struct drbd_request *req)
 {
 	struct drbd_device *device = req->device;
-	bool conflict = false;
+	struct drbd_interval *conflict;
 
 	spin_lock_irq(&device->interval_lock);
 	clear_bit(INTERVAL_SUBMIT_CONFLICT_QUEUED, &req->i.flags);
@@ -2108,10 +2108,27 @@ static void drbd_conflict_submit_write(struct drbd_request *req)
 		set_bit(INTERVAL_SUBMITTED, &req->i.flags);
 	spin_unlock_irq(&device->interval_lock);
 
-	/* If there is a conflict, the request will be submitted once the
-	 * conflict has cleared. */
-	if (!conflict)
+	if (conflict) {
+		if (drbd_interval_is_local(conflict)) {
+			struct drbd_request *conflicting_req =
+				container_of(conflict, struct drbd_request, i);
+
+			if (conflicting_req->local_rq_state & RQ_POSTPONED) {
+				req->local_rq_state |= RQ_POSTPONED;
+				if (req->private_bio) {
+					bio_put(req->private_bio);
+					req->private_bio = NULL;
+					put_ldev(device);
+				}
+				drbd_req_put_completion_ref(req, NULL, 1);
+			}
+		}
+		/* If there is a conflict, the request will be submitted once the
+		 * conflict has cleared.
+		 */
+	} else {
 		drbd_send_and_submit(req);
+	}
 }
 
 static bool inc_ap_bio_cond(struct drbd_device *device, int rw)

drbd/drbd_interval.h
@@ -127,6 +127,11 @@ static inline bool drbd_interval_is_verify(struct drbd_interval *i)
 	return i->type == INTERVAL_OV_READ_SOURCE || i->type == INTERVAL_OV_READ_TARGET;
 }
 
+static inline bool drbd_interval_is_local(struct drbd_interval *i)
+{
+	return i->type == INTERVAL_LOCAL_READ || i->type == INTERVAL_LOCAL_WRITE;
+}
+
 static inline void drbd_clear_interval(struct drbd_interval *i)
 {
 	RB_CLEAR_NODE(&i->rb);

commit fefd63efc71f934b6be2e9e18bed29ec02e08f58

drbd: prevent postponed requests also being retried on conflict release
Without this change, a request was left in the interval tree in a form
that allowed retrying when conflicts were resolved. At the same time,
the request was queued for retrying by the retry worker. When the
conflict was resolved first, the retry worker later found a request in
an unexpected state, or even a freed request, which led to memory
corruption.

Fixes: d82ddc03f8c7 ("drbd: postpone writes conflicting with postponed requests")

drbd/drbd_req.c
@@ -2107,30 +2107,37 @@ static void drbd_conflict_submit_write(struct drbd_request *req)
 	conflict = drbd_find_conflict(device, &req->i, 0);
 	if (drbd_interval_empty(&req->i))
 		drbd_insert_interval(&device->requests, &req->i);
-	if (!conflict)
+	if (!conflict) {
 		set_bit(INTERVAL_SUBMITTED, &req->i.flags);
+	} else if (drbd_interval_is_local(conflict)) {
+		struct drbd_request *conflicting_req =
+			container_of(conflict, struct drbd_request, i);
+
+		if (conflicting_req->local_rq_state & RQ_POSTPONED) {
+			req->local_rq_state |= RQ_POSTPONED;
+
+			/*
+			 * Remove interval from tree to prevent req from being
+			 * queued when conflicts are released.
+			 */
+			drbd_remove_interval(&device->requests, &req->i);
+		}
+	}
 	spin_unlock_irq(&device->interval_lock);
 
-	if (conflict) {
-		if (drbd_interval_is_local(conflict)) {
-			struct drbd_request *conflicting_req =
-				container_of(conflict, struct drbd_request, i);
-
-			if (conflicting_req->local_rq_state & RQ_POSTPONED) {
-				req->local_rq_state |= RQ_POSTPONED;
-				if (req->private_bio) {
-					bio_put(req->private_bio);
-					req->private_bio = NULL;
-					put_ldev(device);
-				}
-				drbd_req_put_completion_ref(req, NULL, 1);
-			}
-		}
-		/* If there is a conflict, the request will be submitted once the
-		 * conflict has cleared.
-		 */
-	} else {
+	/*
+	 * If there is a conflict, the request will be submitted once the
+	 * conflict has cleared.
+	 */
+	if (!conflict) {
 		drbd_send_and_submit(req);
+	} else if (req->local_rq_state & RQ_POSTPONED) {
+		if (req->private_bio) {
+			bio_put(req->private_bio);
+			req->private_bio = NULL;
+			put_ldev(device);
+		}
+		drbd_req_put_completion_ref(req, NULL, 1);
 	}
 }
 