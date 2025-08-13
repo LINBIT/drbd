LINBIT/drbd

commit ae1b0bdfa2e4ea59d15199b55a6f0c571844f576

rdma: Fix kernel crash in dtr_create_rx_desc()
Have the crash info as follow:
BUG: unable to handle kernel NULL pointer dereference at 0000000000000000
PGD 0
Oops: 0000 [#1] SMP NOPTI
CPU: 51 PID: 748 Comm: kworker/51:1 Kdump: loaded Tainted: G           OE    --------- -  - 4.18.0-372.19.1.es8_10.x86_64 #1
Hardware name: SuperCloud R5215 G13/R5215 G13, BIOS EG6.17.12 12/20/2024
Workqueue: events dtr_refill_rx_descs_work_fn [drbd_transport_rdma]
RIP: 0010:dtr_create_rx_desc+0xe1/0x310 [drbd_transport_rdma]
Code: 48 85 db 0f 84 85 01 00 00 48 89 5d 20 48 8b 0d e5 dd 4f c7 4c 89 7d 00 4c 8b 05 ea dd 4f c7 c7 45 18 00 00 00 00 48 8b 43 28 <8b> 00 89 45 34 48 8b 53 08 4c 89 f8 48 29 c8 48 8b 12 48 c1 f8 06
RSP: 0018:ff8baaf70e6a3e28 EFLAGS: 00010286
RAX: 0000000000000000 RBX: ff4b9894e7f1f800 RCX: ffdcf32780000000
RDX: 0000000000000002 RSI: ff8baaf70e6a3dc0 RDI: ff4b98b3edac2fa8
RBP: ff4b989488700280 R08: ff4b989340000000 R09: ff4b989488700280
R10: 0000000000001000 R11: 0000000000000009 R12: ff4b989e12a2b648
R13: ff4b989c5fbaca60 R14: 0000000000001000 R15: ffdcf328136d5100
FS:  0000000000000000(0000) GS:ff4b98b33fac0000(0000) knlGS:0000000000000000
CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
CR2: 0000000000000000 CR3: 00000015d3410004 CR4: 0000000000773ee0
DR0: 0000000000000000 DR1: 0000000000000000 DR2: 0000000000000000
DR3: 0000000000000000 DR6: 00000000fffe07f0 DR7: 0000000000000400
PKRU: 55555554
Call Trace:
 __dtr_refill_rx_desc+0x5d/0xb0 [drbd_transport_rdma]
 process_one_work+0x1a7/0x360
 ? create_worker+0x1a0/0x1a0
 worker_thread+0x30/0x390
 ? create_worker+0x1a0/0x1a0
 kthread+0x10a/0x120
 ? set_kthread_struct+0x40/0x40
 ret_from_fork+0x1f/0x40

(gdb) l *dtr_create_rx_desc+0xe1
0x1e01 is in dtr_create_rx_desc (/.../drbd_transport_rdma.c:2093).
2088                    goto out;
2089            }
2090            rx_desc->cm = cm;
2091            rx_desc->page = page;
2092            rx_desc->size = 0;
2093            rx_desc->sge.lkey = dtr_cm_to_lkey(cm);

drbd/drbd_transport_rdma.c
@@ -355,7 +355,7 @@ static int dtr_init_flow(struct dtr_path *path, enum drbd_stream stream);
 static int dtr_cm_alloc_rdma_res(struct dtr_cm *cm);
 static void __dtr_refill_rx_desc(struct dtr_path *path, enum drbd_stream stream);
 static int dtr_send_flow_control_msg(struct dtr_path *path, gfp_t gfp_mask);
-static struct dtr_cm *dtr_path_get_cm(struct dtr_path *path);
+static struct dtr_cm *dtr_path_get_cm_connected(struct dtr_path *path);
 static void dtr_destroy_cm(struct kref *kref);
 static void dtr_destroy_cm_keep_id(struct kref *kref);
 static int dtr_activate_path(struct dtr_path *path);
@@ -548,7 +548,7 @@ static int dtr_send(struct dtr_path *path, void *buf, size_t size, gfp_t gfp_mas
 
 	// pr_info("%s: dtr_send() size = %d data[0]:%lx\n", rdma_stream->name, (int)size, *(unsigned long*)buf);
 
-	cm = dtr_path_get_cm(path);
+	cm = dtr_path_get_cm_connected(path);
 	if (!cm)
 		goto out;
 
@@ -845,6 +845,18 @@ static struct dtr_cm *dtr_path_get_cm(struct dtr_path *path)
 	rcu_read_lock();
 	cm = __dtr_path_get_cm(path);
 	rcu_read_unlock();
+	return cm;
+}
+
+static struct dtr_cm *dtr_path_get_cm_connected(struct dtr_path *path)
+{
+	struct dtr_cm *cm;
+
+	cm = dtr_path_get_cm(path);
+	if (cm && cm->state != DSM_CONNECTED) {
+		kref_put(&cm->kref, dtr_destroy_cm);
+		cm = NULL;
+	}
 	return cm;
 }
 
@@ -2074,7 +2086,7 @@ static int dtr_create_rx_desc(struct dtr_flow *flow, gfp_t gfp_mask)
 	}
 	BUG_ON(PageHighMem(page));
 
-	cm = dtr_path_get_cm(path);
+	cm = dtr_path_get_cm_connected(path);
 	if (!cm) {
 		err = -ECONNRESET;
 		goto out;

commit 9398e073bf08a095ffd905663edc310d8c62a0bb

rdma: Post one rx-desc before connecting
Some RDMA low-level implementations require at least one posted
rx-desc to establish a connection. The Linux kernel rdma_rxe software
implementation is one of those.

Fix this by posting one rx-desc before connecting.

Fixes: ae1b0bdfa ("rdma: Fix kernel crash in dtr_create_rx_desc()")

drbd/drbd_transport_rdma.c
@@ -2062,7 +2062,7 @@ static void dtr_free_rx_desc(struct dtr_rx_desc *rx_desc)
 	kfree(rx_desc);
 }
 
-static int dtr_create_rx_desc(struct dtr_flow *flow, gfp_t gfp_mask)
+static int dtr_create_rx_desc(struct dtr_flow *flow, gfp_t gfp_mask, bool connected_only)
 {
 	struct dtr_path *path = flow->path;
 	struct drbd_transport *transport = path->path.transport;
@@ -2089,11 +2089,13 @@ static int dtr_create_rx_desc(struct dtr_flow *flow, gfp_t gfp_mask)
 	}
 	BUG_ON(PageHighMem(page));
 
-	cm = dtr_path_get_cm_connected(path);
-	if (!cm) {
-		err = -ECONNRESET;
+	err = -ECONNRESET;
+	cm = dtr_path_get_cm(path);
+	if (!cm)
 		goto out;
-	}
+	if (connected_only && cm->state != DSM_CONNECTED)
+		goto out_put;
+
 	rx_desc->cm = cm;
 	rx_desc->page = page;
 	rx_desc->size = 0;
@@ -2136,6 +2138,7 @@ static void dtr_refill_rx_descs_work_fn(struct work_struct *work)
 
 static void __dtr_refill_rx_desc(struct dtr_path *path, enum drbd_stream stream)
 {
+	struct drbd_transport *transport = path->path.transport;
 	struct dtr_flow *flow = &path->flow[stream];
 	int descs_want_posted, descs_max;
 
@@ -2144,7 +2147,9 @@ static void __dtr_refill_rx_desc(struct dtr_path *path, enum drbd_stream stream)
 
 	while (atomic_read(&flow->rx_descs_posted) < descs_want_posted &&
 	       atomic_read(&flow->rx_descs_allocated) < descs_max) {
-		int err = dtr_create_rx_desc(flow, (GFP_NOIO & ~__GFP_RECLAIM) | __GFP_NOWARN);
+		int err;
+
+		err = dtr_create_rx_desc(flow, (GFP_NOIO & ~__GFP_RECLAIM) | __GFP_NOWARN, true);
 		/*
 		 * drbd_alloc_pages() goes over the configured max_buffers, but throttles the
 		 * caller with sleeping 100ms for each of those excess pages.  By calling
@@ -2153,8 +2158,7 @@ static void __dtr_refill_rx_desc(struct dtr_path *path, enum drbd_stream stream)
 		 */
 		if (err == -ENOMEM) {
 			break;
-		} else if (err > 0) {
-			struct drbd_transport *transport = path->path.transport;
+		} else if (err) {
 			tr_err(transport, "dtr_create_rx_desc() = %d\n", err);
 			break;
 		}
@@ -2582,8 +2586,9 @@ static int _dtr_cm_alloc_rdma_res(struct dtr_cm *cm,
 		goto createqp_failed;
 	}
 
+	/* some RDMA transports need at least one rx desc for establishing a connection */
 	for (i = DATA_STREAM; i <= CONTROL_STREAM ; i++)
-		dtr_create_rx_desc(&path->flow[i], GFP_NOIO);
+		dtr_create_rx_desc(&path->flow[i], GFP_NOIO, false);
 
 	return 0;
 