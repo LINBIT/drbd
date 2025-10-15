LINBIT/drbd

commit c4c0e3bb538e90bc6bc2e62f19d549fd170abc1f

docker: find matching RPM based on metadata
For RHEL-like systems, our RPMS contain the expected symbol versions
in the package metadata. We can use this information to find the RPM
matching the currently running kernel: We invoke the scripts used
to generate these requirements on the currently running kernel and
and match them to the available RPMs.

This was triggered by a breakage in compatibility of a RHEL9 kernel,
where the matching OL9 kernel stayed compatible with the older version.
In this case the lbdisttool.py failed to generate the right module
to load on OL9. With this change, the correct modules for RHEL9 and OL9
are loaded.

docker/entry.sh
@@ -177,6 +177,32 @@ kos::deb::fromrepo() {
 	kos::deb::extract "$pkgdir"
 }
 
+kos::rpm::bestbyrpmprovides() {
+	local pkgdir="$1"
+	shift 1
+
+	sort > "$pkgdir/kernel.provides" \
+		<(find "/lib/modules/$(uname -r)" -name "symvers*" | /lib/rpm/kabi.sh) \
+		<(find "/lib/modules/$(uname -r)/kernel" -type f | /lib/rpm/redhat/find-provides.ksyms)
+
+	if [ ! -s "$pkgdir/kernel.provides" ]; then
+		debug "Failed to generate kernel provides"
+		return 1
+	fi
+
+	for RPM in "$@"; do
+		if [ -z "$(comm -13 "$pkgdir/kernel.provides" <(rpm -q -R "$RPM" | grep ^kernel))" ]; then
+			debug "Kernel module matching kernel provides: \"$RPM\""
+			echo "$RPM"
+			return 0
+		fi
+	done
+
+	debug "No kernel module matches kernel provides"
+
+	return 1
+}
+
 kos::rpm::fromshipped() {
 	local pkgdir="$1"
 	local family
@@ -184,7 +210,7 @@ kos::rpm::fromshipped() {
 
 	family="$(lbdisttool.py --family)"
 
-	best="$(lbdisttool.py --force-name "$family" -k "${PKGS}/${family}"*/*.rpm)"
+	best="$(kos::rpm::bestbyrpmprovides "$pkgdir" "${PKGS}/${family}"*/*.rpm || lbdisttool.py --force-name "$family" -k "${PKGS}/${family}"*/*.rpm)"
 	[ -n "$best" ] || die "Could not find matching rpm package for your kernel"
 	debug "Best kernel module package: \"$best\""
 	cp "$best" "$pkgdir"

docker/Dockerfile.rhel7
@@ -13,7 +13,7 @@ LABEL name="DRBD Kernel module load container" \
 RUN mkdir /licenses && cp /usr/share/licenses/shadow-utils-4.6/gpl-2.0.txt /licenses/
 
 RUN yum -y update-minimal --security --sec-severity=Important --sec-severity=Critical && \
-	yum install -y wget gcc make patch curl kmod yum-utils && yum clean all -y && \
+	yum install -y wget gcc make patch curl kmod yum-utils redhat-rpm-config && yum clean all -y && \
 	cd /tmp && wget https://github.com/LINBIT/python-lbdist/archive/master.tar.gz && \
 	tar xvf master.tar.gz && \
 	( cd python-lbdist-master && python setup.py install ) && \

docker/Dockerfile.rhel8
@@ -3,7 +3,7 @@ FROM $EXTRAPKGS as extrapkgs
 # by checking for /pkgs we can cache that step
 # and prepare images that already contain the packages.
 RUN mkdir /pkgs
-RUN dnf install -y 'dnf-command(download)' && cd /pkgs && dnf download elfutils-libelf-devel && rm -f *.i686.rpm # !lbbuild
+RUN dnf install -y 'dnf-command(download)' && cd /pkgs && dnf download elfutils-libelf-devel kernel-rpm-macros && rm -f *.i686.rpm # !lbbuild
 
 FROM registry.access.redhat.com/ubi8/ubi
 MAINTAINER Roland Kammerer <roland.kammerer@linbit.com>
@@ -27,7 +27,7 @@ RUN yum -y update-minimal --security --sec-severity=Important --sec-severity=Cri
 COPY --from=extrapkgs /pkgs /pkgs
 RUN yum install -y /pkgs/*.rpm # !lbbuild
 # or
-# =lbbuild RUN curl -fsSL https://nexus.at.linbit.com/repository/lbbuild/from_rhel_repos.sh | bash -s -- elfutils-libelf-devel
+# =lbbuild RUN curl -fsSL https://nexus.at.linbit.com/repository/lbbuild/from_rhel_repos.sh | bash -s -- elfutils-libelf-devel kernel-rpm-macros
 
 RUN rm -rf /pkgs
 

docker/Dockerfile.rhel9
@@ -3,7 +3,7 @@ FROM $EXTRAPKGS as extrapkgs
 # by checking for /pkgs we can cache that step
 # and prepare images that already contain the packages.
 RUN mkdir /pkgs
-RUN dnf install -y 'dnf-command(download)' && cd /pkgs && dnf download elfutils-libelf-devel && rm -f *.i686.rpm # !lbbuild
+RUN dnf install -y 'dnf-command(download)' && cd /pkgs && dnf download elfutils-libelf-devel kernel-rpm-macros && rm -f *.i686.rpm # !lbbuild
 
 FROM registry.access.redhat.com/ubi9/ubi
 MAINTAINER Roland Kammerer <roland.kammerer@linbit.com>
@@ -26,7 +26,7 @@ RUN dnf -y update-minimal --security --sec-severity=Important --sec-severity=Cri
 COPY --from=extrapkgs /pkgs /pkgs
 RUN dnf install -y /pkgs/*.rpm # !lbbuild
 # or
-# =lbbuild RUN curl -fsSL https://nexus.at.linbit.com/repository/lbbuild/from_rhel_repos.sh | bash -s -- elfutils-libelf-devel
+# =lbbuild RUN curl -fsSL https://nexus.at.linbit.com/repository/lbbuild/from_rhel_repos.sh | bash -s -- elfutils-libelf-devel kernel-rpm-macros
 
 RUN rm -rf /pkgs
 

commit d1e6ba858b6f9b40073b7e10af7696f6e5461e31

docker: improve RPM selection from requires
We compare the "Requires:" section of the DRBD rpms against the currently
running kernel. This works well enough, but sometimes RHEL still manages to
break even this, by making changes to for one reason or another do not show
up in the rpm Provides/Requires section.

This is exactly what happened with RHEL 9.4 kernel 5.14.0-427.88.1. This kernel
is not compatible with previous versions of the 9.4 release, leading to an
immediate kernel panic on loading such a module.

For the above case, we provide an updated kmod, but our logic selecting the RPM
to load failed, because it only looked at the RPM requires/provides. Now, after
filtering based on RPM metadata, sort again by using lbdisttool.py.

Fixes: c4c0e3bb538e ("docker: find matching RPM based on metadata")

docker/entry.sh
@@ -227,13 +227,8 @@ kos::rpm::bestbyrpmprovides() {
 		if [ -z "$(comm -13 "$kernel_provides_file" "$drbd_requires_file")" ]; then
 			debug "Kernel module matching kernel provides: \"$RPM\""
 			echo "$RPM"
-			return 0
 		fi
 	done
-
-	debug "No kernel module matches kernel provides"
-
-	return 1
 }
 
 kos::rpm::fromshipped() {
@@ -243,7 +238,14 @@ kos::rpm::fromshipped() {
 
 	family="$(lbdisttool.py --family)"
 
-	best="$(kos::rpm::bestbyrpmprovides "$pkgdir" "${PKGS}/${family}"*/*.rpm || lbdisttool.py --force-name "$family" -k "${PKGS}/${family}"*/*.rpm)"
+	# First try to filter best RPMs by package metadata
+	best="$(kos::rpm::bestbyrpmprovides "$pkgdir" "${PKGS}/${family}"*/*.rpm)"
+	if [ -z "$best" ]; then
+		# Either no RPM matches or not enough metadata, so just check all again
+		best="$(lbdisttool.py --force-name "$family" -k "${PKGS}/${family}"*/*.rpm)"
+	else
+		best="$(lbdisttool.py --force-name "$family" -k $best)"
+	fi
 	[ -n "$best" ] || die "Could not find matching rpm package for your kernel"
 	debug "Best kernel module package: \"$best\""
 	cp "$best" "$pkgdir"