LINBIT/drbd

commit 48143ea2dc6ece3dd424bc3c2be1dff08a0149ce

drbd: new DRBD_FF_BM_BLOCK_SHIFT protocol feature flag
In case our bitmap is finer granular than our peer's,
we have to re-align our resync requests to
"full peer bitmap blocks".
Otherwise we may clear our bits, but not theirs.

We have to communicate our bitmap granularity.

To avoid yet an other special packet for a four bit value,
squeeze it into the remaining _pad bit in struct o_qlim
as bm_block_shift_minus_12 [0: 4k .. 8: 1M],
then stored in peer_device->bm_block_shift.

Scale resync requests accordingly.

Add warnings during handshake if our bitmap granularity is not 4k,
if the peer is unaware.

drbd/drbd_sender.c
@@ -1252,7 +1252,9 @@ static int make_resync_request(struct drbd_peer_device *peer_device, int cancel)
 	bool request_ok = true;
 	unsigned long bit;
 	sector_t sector, prev_sector = 0;
-	unsigned int bm_block_shift = bm->bm_block_shift;
+	unsigned int bm_block_shift;
+	unsigned int peer_bm_block_shift;
+	unsigned int bits_per_peer_bit;
 
 	if (unlikely(cancel))
 		return 0;
@@ -1276,6 +1278,12 @@ static int make_resync_request(struct drbd_peer_device *peer_device, int cancel)
 		drbd_err(device, "Disk broke down during resync!\n");
 		return 0;
 	}
+	bm_block_shift = bm->bm_block_shift;
+	peer_bm_block_shift = peer_device->bm_block_shift;
+	if (peer_bm_block_shift > bm_block_shift)
+		bits_per_peer_bit = 1<<(peer_bm_block_shift - bm_block_shift);
+	else
+		bits_per_peer_bit = 1; // or maybe even only a partial bit ;-)
 
 	if (send_buffer_half_full(peer_device)) {
 		/* We still want to reschedule ourselves, so do not return. */
@@ -1314,9 +1322,13 @@ static int make_resync_request(struct drbd_peer_device *peer_device, int cancel)
 	/* don't let rs_sectors_came_in() re-schedule us "early"
 	 * just because the first reply came "fast", ... */
 	peer_device->rs_in_flight += bm_bit_to_sect(device->bitmap, number);
+	if (peer_device->bm_block_shift > bm_block_shift)
+		bits_per_peer_bit = 1<<(peer_device->bm_block_shift - bm_block_shift);
+	else
+		bits_per_peer_bit = 1; // or maybe even only a partial bit ;-)
 
 	clear_bit(RS_REQUEST_UNSUCCESSFUL, &peer_device->flags);
-	for (; i < number; i++) {
+	for (; i < number; i += bits_per_peer_bit) {
 		int err;
 
 		/* If we are aborting the requests or the peer is canceling
@@ -1336,6 +1348,7 @@ static int make_resync_request(struct drbd_peer_device *peer_device, int cancel)
 			goto request_done;
 		}
 
+		bit = ALIGN_DOWN(bit, bits_per_peer_bit);
 		sector = bm_bit_to_sect(bm, bit);
 
 		if (drbd_rs_c_min_rate_throttle(peer_device)) {
@@ -1351,7 +1364,7 @@ static int make_resync_request(struct drbd_peer_device *peer_device, int cancel)
 		}
 
 		prev_sector = sector;
-		size = bm_block_size(bm);
+		size = bm_block_size(bm) * bits_per_peer_bit;
 		optimal_bits_alignment = optimal_bits_for_alignment(bit, bm_block_shift);
 		optimal_bits_rate = round_to_powerof_2(number - i);
 		optimal_bits = min(optimal_bits_alignment, optimal_bits_rate) - 1;
@@ -1362,15 +1375,17 @@ static int make_resync_request(struct drbd_peer_device *peer_device, int cancel)
 			if (discard_granularity && size == discard_granularity)
 				break;
 
-			if (drbd_bm_test_bit(peer_device, bit + 1) != 1)
+			if (drbd_bm_count_bits(device, peer_device->bitmap_index,
+					       bit + bits_per_peer_bit,
+					       bit + bits_per_peer_bit * 2 - 1) == 0)
 				break;
-			size += bm_block_size(bm);
-			bit++;
-			i++;
+			size += bm_block_size(bm) * bits_per_peer_bit;
+			bit += bits_per_peer_bit;
+			i += bits_per_peer_bit;
 		}
 
 		/* set the offset to start the next drbd_bm_find_next from */
-		peer_device->resync_next_bit = bit + 1;
+		peer_device->resync_next_bit = bit + bits_per_peer_bit;
 
 		/* adjust very last sectors, in case we are oddly sized */
 		if (sector + (size>>9) > capacity)

drbd/drbd_int.h
@@ -1479,6 +1479,8 @@ struct drbd_peer_device {
 		unsigned int	io_opt;
 		unsigned int	max_bio_size;
 	} q_limits;
+	/* communicated as part of o_qlim, if agreed on DRBD_FF_BM_BLOCK_SHIFT */
+	int bm_block_shift;
 };
 
 struct conflict_worker {

drbd/drbd_main.c
@@ -1595,6 +1595,9 @@ int drbd_send_sizes(struct drbd_peer_device *peer_device,
 		p->qlim->io_opt = cpu_to_be32(bdev_io_opt(bdev));
 		p->qlim->discard_enabled = !!bdev_max_discard_sectors(bdev);
 		p->qlim->write_same_capable = 0;
+		if (connection->agreed_features & DRBD_FF_BM_BLOCK_SHIFT)
+			p->qlim->bm_block_shift_minus_12 =
+				device->bitmap->bm_block_shift - BM_BLOCK_SHIFT_4k;
 		put_ldev(device);
 	} else {
 		struct request_queue *q = device->rq_queue;

drbd/drbd_receiver.c
@@ -5982,6 +5982,22 @@ static int receive_sizes(struct drbd_connection *connection, struct packet_info 
 		peer_device->q_limits.io_opt = be32_to_cpu(qlim->io_opt);
 	}
 
+	if (connection->agreed_features & DRBD_FF_BM_BLOCK_SHIFT)
+		peer_device->bm_block_shift =
+			p->qlim->bm_block_shift_minus_12 + BM_BLOCK_SHIFT_4k;
+	else {
+		int bbs = bm_block_size(device->bitmap);
+		/* May work as long as this one is SyncTarget. May result in
+		 * funny never ending / repeating resyncs if the other guy is
+		 * SyncTarget, but unaware of bitmap granularity issues.
+		 */
+		if (have_ldev && bbs != BM_BLOCK_SIZE_4k)
+			drbd_warn(peer_device,
+				"My bitmap granularity is %u. Upgrade this peer to make it aware.\n",
+				bbs);
+		peer_device->bm_block_shift = BM_BLOCK_SHIFT_4k;
+	}
+
 	/* Leave drbd_reconsider_queue_parameters() before drbd_determine_dev_size().
 	   In case we cleared the QUEUE_FLAG_DISCARD from our queue in
 	   drbd_reconsider_queue_parameters(), we can be sure that after

drbd/linux/drbd_config.h
@@ -17,7 +17,8 @@ const char *drbd_buildtag(void);
 #define PRO_FEATURES (DRBD_FF_TRIM | DRBD_FF_THIN_RESYNC | DRBD_FF_WSAME | DRBD_FF_WZEROES | \
 		      DRBD_FF_RESYNC_DAGTAG | \
 		      DRBD_FF_2PC_V2 | DRBD_FF_RS_SKIP_UUID | \
-		      DRBD_FF_RESYNC_WITHOUT_REPLICATION)
+		      DRBD_FF_RESYNC_WITHOUT_REPLICATION | \
+		      DRBD_FF_BM_BLOCK_SHIFT)
 
 #define PRO_VERSION_8_MIN 86
 #define PRO_VERSION_8_MAX 101

commit 0b7a1ccee2f8ec7d607e9fcdcaf25d30aafbf957

drbd: Fix resync between peers with different bitmap granularity
When resyncing from a peer with larger bitmap granularity, the
resync-var-bm-bsize test revealed an unexpected connection abort and
these log lines:

res/0 drbd1 f42-1: /home/phil/src/drbd93/drbd/drbd_receiver.c:3754: sector: 101888s, size: 2097152
res f42-1: error receiving P_RS_DAGTAG_REQ, e: -22 l: 0!
res f42-1: conn( Connected -> ProtocolError ) peer( Secondary -> Unknown )

Fix this by considering the peers bitmap block size when calculating
the optimal_bits_alignement value.
While at it, remove the double evaluation of bits_per_peer_bit.

Fixes: 48143ea2dc6e ("drbd: new DRBD_FF_BM_BLOCK_SHIFT protocol feature flag")

drbd/drbd_sender.c
@@ -1257,14 +1257,13 @@ static int make_resync_request(struct drbd_peer_device *peer_device, int cancel)
 	int number = 0, rollback_i, size = 0, i = 0, optimal_bits;
 	struct drbd_connection *connection = peer_device->connection;
 	struct drbd_device *device = peer_device->device;
-	struct drbd_bitmap *bm = device->bitmap;
+	struct drbd_bitmap *bm = device->bitmap; /* deref bm members only after get_ldev() */
 	const sector_t capacity = get_capacity(device->vdisk);
 	bool request_ok = true;
 	unsigned long bit;
 	sector_t sector, prev_sector = 0;
-	unsigned int bm_block_shift;
-	unsigned int peer_bm_block_shift;
-	unsigned int bits_per_peer_bit;
+	unsigned int peer_bm_block_shift = peer_device->bm_block_shift;
+	unsigned int bm_block_shift, bits_per_peer_bit;
 
 	if (unlikely(cancel))
 		return 0;
@@ -1288,12 +1287,6 @@ static int make_resync_request(struct drbd_peer_device *peer_device, int cancel)
 		drbd_err(device, "Disk broke down during resync!\n");
 		return 0;
 	}
-	bm_block_shift = bm->bm_block_shift;
-	peer_bm_block_shift = peer_device->bm_block_shift;
-	if (peer_bm_block_shift > bm_block_shift)
-		bits_per_peer_bit = 1<<(peer_bm_block_shift - bm_block_shift);
-	else
-		bits_per_peer_bit = 1; // or maybe even only a partial bit ;-)
 
 	if (send_buffer_half_full(peer_device)) {
 		/* We still want to reschedule ourselves, so do not return. */
@@ -1306,6 +1299,7 @@ static int make_resync_request(struct drbd_peer_device *peer_device, int cancel)
 		rcu_read_unlock();
 	}
 
+	bm_block_shift = bm->bm_block_shift;
 	number = drbd_rs_number_requests(peer_device);
 	if (number < discard_granularity >> bm_block_shift)
 		number = discard_granularity >> bm_block_shift;
@@ -1332,10 +1326,9 @@ static int make_resync_request(struct drbd_peer_device *peer_device, int cancel)
 	/* don't let rs_sectors_came_in() re-schedule us "early"
 	 * just because the first reply came "fast", ... */
 	peer_device->rs_in_flight += bm_bit_to_sect(device->bitmap, number);
-	if (peer_device->bm_block_shift > bm_block_shift)
-		bits_per_peer_bit = 1<<(peer_device->bm_block_shift - bm_block_shift);
-	else
-		bits_per_peer_bit = 1; // or maybe even only a partial bit ;-)
+
+	bits_per_peer_bit = peer_bm_block_shift > bm_block_shift ?
+		1 << (peer_bm_block_shift - bm_block_shift) : 1;
 
 	clear_bit(RS_REQUEST_UNSUCCESSFUL, &peer_device->flags);
 	for (; i < number; i += bits_per_peer_bit) {
@@ -1375,7 +1368,8 @@ static int make_resync_request(struct drbd_peer_device *peer_device, int cancel)
 
 		prev_sector = sector;
 		size = bm_block_size(bm) * bits_per_peer_bit;
-		optimal_bits_alignment = optimal_bits_for_alignment(bit, bm_block_shift);
+		optimal_bits_alignment = optimal_bits_for_alignment(bit,
+						max(peer_bm_block_shift, bm_block_shift));
 		optimal_bits_rate = round_to_powerof_2(number - i);
 		optimal_bits = min(optimal_bits_alignment, optimal_bits_rate) - 1;
 