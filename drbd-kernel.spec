LINBIT/drbd

commit 510bec079683f018d19175b01d6a30043f431a52

build: make COMPAT_84 flag controllable in packaging
Add a build option for debian and a --with flag for rpmbuild to toggle
the newly introduced CONFIG_DRBD_COMPAT_84 flag.

drbd-kernel.spec
@@ -32,6 +32,9 @@ BuildRequires: %kernel_module_package_buildreqs
 # rpmbuild --with dkms to build drbd-dkms package
 %bcond_with dkms
 
+# rpmbuild --with compat_84 to include in-kernel compat code for drbd 8.4
+%bcond_with compat_84
+
 # rpmbuild --define "ofed_kernel_dir /usr/src/ofa_kernel/x86_64/4.18.0-147.5.1..."
 # to build against an some mlnx-ofa_kernel-devel
 %if %{defined ofed_kernel_dir}
@@ -146,7 +149,8 @@ for flavor in %flavors_to_build; do
 	%{?_ofed_version:BUILD_OFED=1} \
 	%{?ofed_kernel_dir:OFED_KERNEL_DIR=%{ofed_kernel_dir}} \
 	%{?_ofed_version:OFED_VERSION=%{_ofed_version}} \
-	%{?with_gcov:GCOV_PROFILE=y}
+	%{?with_gcov:GCOV_PROFILE=y} \
+	%{?with_compat_84:CONFIG_DRBD_COMPAT_84=y}
 done
 
 %install

debian/rules
@@ -51,6 +51,10 @@ ifneq (,$(filter gcov,$(DEB_BUILD_OPTIONS)))
 extra_make_flags = GCOV_PROFILE=y
 endif
 
+ifneq (,$(filter compat_84,$(DEB_BUILD_OPTIONS)))
+extra_make_flags += CONFIG_DRBD_COMPAT_84=y
+endif
+
 deb_build_parallel := $(patsubst parallel=%,%,$(filter parallel=%,$(DEB_BUILD_OPTIONS)))
 ifdef deb_build_parallel
 -j := -j$(deb_build_parallel)

commit 4b24a303216bfae58cb2a407672ef46a3dabefb1

build: Fix building RPMs with compat_84 enabled
The "COMPAT_84=y" was already on the build command line, but not on the
make install command line. So the make install sees that invocations
won't match, builds without the compat define, installs those, and rpm
then collects the binaries from the second build.

Fixes: 510bec079683 ("build: make COMPAT_84 flag controllable in packaging")

drbd-kernel.spec
@@ -176,6 +176,7 @@ for flavor in %flavors_to_build ; do
 	%{?ofed_kernel_dir:OFED_KERNEL_DIR=%{ofed_kernel_dir}} \
 	%{?_ofed_version:OFED_VERSION=%{_ofed_version}} \
 	%{?with_gcov:GCOV_PROFILE=y} \
+	%{?with_compat_84:CONFIG_DRBD_COMPAT_84=y} \
 	cmd_depmod=:
     kernelrelease=$(cat %{kernel_source $flavor}/include/config/kernel.release || make -s -C %{kernel_source $flavor} kernelrelease)
     mv drbd/build-current/.kernel.config.gz drbd/k-config-$kernelrelease.gz