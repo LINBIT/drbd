LINBIT/drbd

commit cec99e5ac8061c1cc9fc24d5bd269dc9934a9883

drbd: Add reading and writing of legacy drbd-8.4 metadata
For merging DRBD-9 to upstream Linux, we need to present a smooth
migration path. Enable the DRBD driver to read and write DRBD-8.4
metadata when a resource is in legacy compatibility mode.

Co-developed-by: Lars Ellenberg <lars.ellenberg@linbit.com>

drbd/drbd_legacy_84.c
@@ -1,0 +1,274 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include "drbd_legacy_84.h"
+
+/*
+ *   drbd-8.4                      drbd-9 md.flags                   drbd-9 peer-md.flags
+ * MDF_CONSISTENT      1 << 0  MDF_CONSISTENT =        1 << 0,   MDF_PEER_CONNECTED =    1 << 0,
+ * MDF_PRIMARY_IND     1 << 1  MDF_PRIMARY_IND =       1 << 1,   MDF_PEER_OUTDATED =     1 << 1,
+ * MDF_CONNECTED_IND   1 << 2                                    MDF_PEER_FENCING =      1 << 2,
+ * MDF_FULL_SYNC       1 << 3                                    MDF_PEER_FULL_SYNC =    1 << 3,
+ * MDF_WAS_UP_TO_DATE  1 << 4  MDF_WAS_UP_TO_DATE =    1 << 4,   MDF_PEER_DEVICE_SEEN =  1 << 4,
+ * MDF_PEER_OUT_DATED  1 << 5
+ * MDF_CRASHED_PRIMARY 1 << 6  MDF_CRASHED_PRIMARY =   1 << 6,
+ * MDF_AL_CLEAN        1 << 7  MDF_AL_CLEAN =          1 << 7,
+ * MDF_AL_DISABLED     1 << 8  MDF_AL_DISABLED =       1 << 8,
+ *                             MDF_PRIMARY_LOST_QUORUM = 1 << 9,
+ *                             MDF_HAVE_QUORUM =       1 << 10,
+ *                                                                MDF_NODE_EXISTS =      1 << 16,
+ */
+
+#define MDF_84_MASK (MDF_CONSISTENT | MDF_PRIMARY_IND | MDF_WAS_UP_TO_DATE | \
+		     MDF_CRASHED_PRIMARY | MDF_AL_CLEAN | MDF_AL_DISABLED)
+#define MDF_84_PEER_MASK (MDF_PEER_FULL_SYNC)
+#define MDF_84_CONNECTED_IND (1<<2)
+#define MDF_84_PEER_OUTDATED (1<<5)
+
+struct meta_data_on_disk_84 {
+	u64 la_size_sect;      /* last agreed size. */
+	u64 uuid[UI_SIZE];   /* UUIDs. */
+	u64 device_uuid;
+	u64 reserved_u64_1;
+	u32 flags;             /* MDF */
+	u32 magic;
+	u32 md_size_sect;
+	u32 al_offset;         /* offset to this block */
+	u32 al_nr_extents;     /* important for restoring the AL (userspace) */
+	      /* `-- act_log->nr_elements <-- ldev->dc.al_extents */
+	u32 bm_offset;         /* offset to the bitmap, from here */
+	u32 bm_bytes_per_bit;  /* BM_BLOCK_SIZE */
+	u32 la_peer_max_bio_size;   /* last peer max_bio_size */
+
+	/* see al_tr_number_to_on_disk_sector() */
+	u32 al_stripes;
+	u32 al_stripe_size_4k;
+
+	u8 reserved_u8[4096 - (7*8 + 10*4)];
+} __packed;
+
+
+static const char * const drbd_conn_s_names[] = {
+	[C_STANDALONE]       = "StandAlone",
+	[C_DISCONNECTING]    = "Disconnecting",
+	[C_UNCONNECTED]      = "Unconnected",
+	[C_TIMEOUT]          = "Timeout",
+	[C_BROKEN_PIPE]      = "BrokenPipe",
+	[C_NETWORK_FAILURE]  = "NetworkFailure",
+	[C_PROTOCOL_ERROR]   = "ProtocolError",
+	[C_CONNECTING]       = "WFConnection",
+	/* [C_WF_REPORT_PARAMS] = "WFReportParams", */
+	[C_TEAR_DOWN]        = "TearDown",
+	[C_CONNECTED]        = "Connected",
+	[L_STARTING_SYNC_S]  = "StartingSyncS",
+	[L_STARTING_SYNC_T]  = "StartingSyncT",
+	[L_WF_BITMAP_S]      = "WFBitMapS",
+	[L_WF_BITMAP_T]      = "WFBitMapT",
+	[L_WF_SYNC_UUID]     = "WFSyncUUID",
+	[L_SYNC_SOURCE]      = "SyncSource",
+	[L_SYNC_TARGET]      = "SyncTarget",
+	[L_PAUSED_SYNC_S]    = "PausedSyncS",
+	[L_PAUSED_SYNC_T]    = "PausedSyncT",
+	[L_VERIFY_S]         = "VerifyS",
+	[L_VERIFY_T]         = "VerifyT",
+	[L_AHEAD]            = "Ahead",
+	[L_BEHIND]           = "Behind",
+};
+
+static const char write_ordering_chars[] = {
+	[WO_NONE] = 'n',
+	[WO_DRAIN_IO] = 'd',
+	[WO_BDEV_FLUSH] = 'f',
+	[WO_BIO_BARRIER] = 'b',
+};
+
+
+static int seq_print_device_proc_drbd(struct seq_file *m, struct drbd_device *device);
+
+int nr_drbd8_devices;
+
+void drbd_md_decode_84(struct meta_data_on_disk_84 *on_disk, struct drbd_md *md,
+		       int *max_peers, int *bytes_per_bit)
+{
+	struct drbd_peer_md *peer_md;
+	const int peer_node_id = 0; /* setup_node_ids_84() moves it later */
+	u32 on_disk_flags;
+	int i;
+
+	md->effective_size = be64_to_cpu(on_disk->la_size_sect);
+	md->current_uuid = be64_to_cpu(on_disk->uuid[UI_CURRENT]);
+	md->prev_members = 0;
+	md->device_uuid = be64_to_cpu(on_disk->device_uuid);
+	md->md_size_sect = be32_to_cpu(on_disk->md_size_sect);
+	md->al_offset = be32_to_cpu(on_disk->al_offset);
+
+	md->bm_offset = be32_to_cpu(on_disk->bm_offset);
+
+	on_disk_flags = be32_to_cpu(on_disk->flags);
+	md->flags = on_disk_flags & MDF_84_MASK;
+
+	*max_peers = 1;
+	*bytes_per_bit = be32_to_cpu(on_disk->bm_bytes_per_bit);
+	md->node_id = -1; /* no node_id in the drbd-8.4 meta-data */
+	md->al_stripes = be32_to_cpu(on_disk->al_stripes);
+	md->al_stripe_size_4k = be32_to_cpu(on_disk->al_stripe_size_4k);
+
+
+	for (i = 0; i < DRBD_NODE_ID_MAX; i++) {
+		peer_md = &md->peers[i];
+
+		peer_md->bitmap_uuid = 0;
+		peer_md->bitmap_dagtag = 0;
+		peer_md->flags = 0;
+		peer_md->bitmap_index = -1;
+	}
+	peer_md = &md->peers[peer_node_id];
+	peer_md->bitmap_uuid = be64_to_cpu(on_disk->uuid[UI_BITMAP]);
+	peer_md->bitmap_index = 0;
+	peer_md->flags = on_disk_flags & MDF_84_PEER_MASK;
+	peer_md->flags |= MDF_HAVE_BITMAP;
+	peer_md->flags |= on_disk_flags & MDF_84_PEER_OUTDATED ? MDF_PEER_OUTDATED : 0;
+	peer_md->flags |= on_disk_flags & MDF_84_CONNECTED_IND ? MDF_PEER_CONNECTED : 0;
+
+
+	for (i = UI_HISTORY_START; i < UI_HISTORY_END; i++)
+		md->history_uuids[i - UI_HISTORY_START] = be64_to_cpu(on_disk->uuid[i]);
+}
+
+void drbd_md_encode_84(struct drbd_device *device, struct meta_data_on_disk_84 *buffer)
+{
+	struct drbd_md *md = &device->ldev->md;
+	int peer_node_id = !md->node_id;
+	struct drbd_peer_md *peer_md = &md->peers[peer_node_id];
+	u32 flags = (md->flags & MDF_84_MASK) | (peer_md->flags & MDF_84_PEER_MASK);
+	int i;
+
+	flags |= peer_md->flags & MDF_PEER_OUTDATED ? MDF_84_PEER_OUTDATED : 0;
+	flags |= peer_md->flags & MDF_PEER_CONNECTED ? MDF_84_CONNECTED_IND : 0;
+	buffer->la_size_sect = cpu_to_be64(md->effective_size);
+	buffer->device_uuid = cpu_to_be64(md->device_uuid);
+	buffer->uuid[UI_CURRENT] = cpu_to_be64(md->current_uuid);
+	buffer->uuid[UI_BITMAP] = cpu_to_be64(peer_md->bitmap_uuid);
+	for (i = UI_HISTORY_START; i < UI_HISTORY_END; i++)
+		buffer->uuid[i] = cpu_to_be64(md->history_uuids[i - UI_HISTORY_START]);
+	buffer->reserved_u64_1 = 0;
+	buffer->flags = cpu_to_be32(flags);
+	buffer->magic = cpu_to_be32(DRBD_MD_MAGIC_84_UNCLEAN);
+	buffer->md_size_sect = cpu_to_be32(md->md_size_sect);
+	buffer->al_offset = cpu_to_be32(md->al_offset);
+	buffer->al_nr_extents = cpu_to_be32(device->act_log->nr_elements);
+	buffer->bm_offset = cpu_to_be32(md->bm_offset);
+	buffer->bm_bytes_per_bit = cpu_to_be32(BM_BLOCK_SIZE);
+	buffer->la_peer_max_bio_size = cpu_to_be32(device->device_conf.max_bio_size);
+
+	buffer->al_stripes = cpu_to_be32(md->al_stripes);
+	buffer->al_stripe_size_4k = cpu_to_be32(md->al_stripe_size_4k);
+}
+
+
+static int compare_sockaddr(struct sockaddr_storage *a_sockaddr,
+			    struct sockaddr_storage *b_sockaddr)
+{
+	if (a_sockaddr->ss_family < b_sockaddr->ss_family)
+		return -1;
+	if (a_sockaddr->ss_family > b_sockaddr->ss_family)
+		return 1;
+	if (a_sockaddr->ss_family == AF_INET) {
+		struct sockaddr_in *a4_sockaddr = (struct sockaddr_in *)a_sockaddr;
+		struct sockaddr_in *b4_sockaddr = (struct sockaddr_in *)b_sockaddr;
+		int cmp = memcmp(&a4_sockaddr->sin_addr, &b4_sockaddr->sin_addr,
+									sizeof(struct in_addr));
+		if (cmp)
+			return cmp;
+		if (a4_sockaddr->sin_port < b4_sockaddr->sin_port)
+			return -1;
+		if (a4_sockaddr->sin_port > b4_sockaddr->sin_port)
+			return 1;
+		return 0;
+	} else if (a_sockaddr->ss_family == AF_INET6) {
+		struct sockaddr_in6 *a6_sockaddr = (struct sockaddr_in6 *)a_sockaddr;
+		struct sockaddr_in6 *b6_sockaddr = (struct sockaddr_in6 *)b_sockaddr;
+		int cmp = memcmp(&a6_sockaddr->sin6_addr, &b6_sockaddr->sin6_addr,
+									sizeof(struct in6_addr));
+		if (!cmp)
+			return cmp;
+		if (a6_sockaddr->sin6_port < b6_sockaddr->sin6_port)
+			return -1;
+		if (a6_sockaddr->sin6_port > b6_sockaddr->sin6_port)
+			return 1;
+		return 0;
+	}
+	pr_err("drbd: %s: Invalid sockaddr family %ul\n", __func__, a_sockaddr->ss_family);
+	return 1;
+}
+
+/*
+ * This is drbd 8 userspace compat mode, so we do not have a node_id yet. Since
+ * we only ever have two peers, we can arbitrate our node ids by comparing IP
+ * addresses. The lower address gets node id 0, the other one gets 1.
+ */
+void drbd_setup_node_ids_84(struct drbd_connection *connection, struct drbd_path *path)
+{
+	int vnr, cmp, peer_node_id, my_node_id = -1, nr_legacy = 0, nr_v9 = 0;
+	struct drbd_resource *resource = connection->resource;
+	struct drbd_peer_device *peer_device;
+	struct drbd_device *device;
+
+	idr_for_each_entry(&resource->devices, device, vnr) {
+		if (test_bit(LEGACY_84_MD, &device->flags)) {
+			nr_legacy++;
+		} else {
+			nr_v9++;
+			if (get_ldev(device)) {
+				if (my_node_id == -1)
+					my_node_id = device->ldev->md.node_id;
+				else if (my_node_id != device->ldev->md.node_id)
+					drbd_err(connection, "inconsistent node_ids\n");
+				put_ldev(device);
+			}
+		}
+	}
+
+	if (nr_legacy && nr_v9)
+		drbd_err(connection, "legacy-84 and drbd-9 metadata in one resource\n");
+
+	if (my_node_id == -1) {
+		cmp = compare_sockaddr(&path->my_addr, &path->peer_addr);
+		D_ASSERT(connection, cmp != 0); /* addresses cannot be equal */
+		my_node_id = cmp == 1;
+		drbd_info(connection, "drbd8 userspace compat mode: setting my node id to %d\n",
+			  my_node_id);
+	}
+	peer_node_id = !my_node_id;
+
+	/* setting up all node_ids*/
+	resource->res_opts.node_id = my_node_id;
+	connection->peer_node_id = peer_node_id;
+	idr_for_each_entry(&resource->devices, device, vnr) {
+		peer_device = list_first_entry_or_null(&device->peer_devices,
+						       struct drbd_peer_device, peer_devices);
+		peer_device->node_id = peer_node_id;
+		peer_device->bitmap_index = 0;
+
+		if (get_ldev(device)) {
+			const struct drbd_peer_md clear = { .bitmap_index = -1 };
+			struct drbd_md *md = &device->ldev->md;
+			struct drbd_peer_md *to = &md->peers[peer_node_id];
+			int i;
+
+			md->node_id = my_node_id;
+
+			for (i = 0; i < DRBD_NODE_ID_MAX; i++) {
+				struct drbd_peer_md *from = &md->peers[i];
+
+				if (from->bitmap_index != -1) {
+					if (from != to) {
+						*to = *from;
+						*from = clear;
+					}
+					break;
+				}
+			}
+			put_ldev(device);
+		}
+	}
+}

drbd/Kbuild.drbd
@@ -87,6 +87,7 @@ drbd-y += drbd_interval.o
 drbd-y += drbd_state.o
 drbd-y += drbd_nla.o
 drbd-y += drbd_transport.o
+drbd-y += drbd_legacy_84.o
 drbd-y += $(compat_objs)
 
 drbd-$(CONFIG_DRBD_DAX)   += drbd_dax_pmem.o

drbd/Kbuild.drbd-module-sources
@@ -75,6 +75,8 @@ drbd_sender.c
 drbd_state.c
 drbd_state.h
 drbd_state_change.h
+drbd_legacy_84.c
+drbd_legacy_84.h
 drbd_transport.c
 drbd_transport_lb-tcp.c
 drbd_transport_rdma.c

drbd/Makefile.cocci-sources
@@ -25,6 +25,8 @@ sources-ignored += drbd_state.h
 sources-ignored += drbd_state_change.h
 sources-ignored += drbd_vli.h
 sources-ignored += kref_debug.h
+sources-ignored += drbd_legacy_84.h
+sources-ignored += drbd_legacy_84.c
 
 sources += drbd_actlog.c
 sources += drbd_bitmap.c

drbd/drbd_int.h
@@ -591,6 +591,7 @@ enum device_flag {
 	OUTDATE_ON_2PC_COMMIT,
 	RESTORE_QUORUM,		/* Restore quorum when we have the same members as before */
 	RESTORING_QUORUM,	/* sanitize_state() -> finish_state_change() */
+	LEGACY_84_MD,
 };
 
 /* flag bits per peer device */

drbd/drbd_legacy_84.h
@@ -1,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#ifndef __DRBD_LEGACY_84_H
+#define __DRBD_LEGACY_84_H
+
+#include "drbd_int.h"
+
+struct meta_data_on_disk_84;
+
+void drbd_md_decode_84(struct meta_data_on_disk_84 *on_disk, struct drbd_md *md,
+		       int *max_peers, int *bytes_per_bit);
+void drbd_md_encode_84(struct drbd_device *device, struct meta_data_on_disk_84 *buffer);
+void drbd_setup_node_ids_84(struct drbd_connection *connection, struct drbd_path *path);
+
+#endif  /* __DRBD_LEGACY_84_H */

drbd/drbd_main.c
@@ -47,6 +47,7 @@
 #include "drbd_vli.h"
 #include "drbd_debugfs.h"
 #include "drbd_meta_data.h"
+#include "drbd_legacy_84.h"
 #include "drbd_dax_pmem.h"
 
 static int drbd_open(struct gendisk *gd, blk_mode_t mode);
@@ -4545,7 +4546,7 @@ fail:
 /* meta data management */
 
 static
-void drbd_md_encode(struct drbd_device *device, struct meta_data_on_disk_9 *buffer)
+void drbd_md_encode_9(struct drbd_device *device, struct meta_data_on_disk_9 *buffer)
 {
 	int i;
 
@@ -4579,6 +4580,14 @@ void drbd_md_encode(struct drbd_device *device, struct meta_data_on_disk_9 *buff
 
 	buffer->al_stripes = cpu_to_be32(device->ldev->md.al_stripes);
 	buffer->al_stripe_size_4k = cpu_to_be32(device->ldev->md.al_stripe_size_4k);
+}
+
+static void drbd_md_encode(struct drbd_device *device, void *buffer)
+{
+	if (test_bit(LEGACY_84_MD, &device->flags))
+		drbd_md_encode_84(device, buffer);
+	else
+		drbd_md_encode_9(device, buffer);
 }
 
 int drbd_md_write(struct drbd_device *device, struct meta_data_on_disk_9 *buffer)

drbd/drbd_nl.c
@@ -34,6 +34,7 @@
 #include <net/sock.h>
 
 #include "drbd_meta_data.h"
+#include "drbd_legacy_84.h"
 
 /* .doit */
 static int drbd_adm_new_minor(struct sk_buff *skb, struct genl_info *info);
@@ -123,8 +124,8 @@ static int drbd_msg_put_info(struct sk_buff *skb, const char *info)
 	if (err) {
 		nla_nest_cancel(skb, nla);
 		return err;
-	} else
-		nla_nest_end(skb, nla);
+	}
+	nla_nest_end(skb, nla);
 	return 0;
 }
 
@@ -2828,12 +2829,10 @@ static int open_backing_devices(struct drbd_device *device,
 	return NO_ERROR;
 }
 
-static int check_activity_log_stripe_size(struct drbd_device *device,
-		struct meta_data_on_disk_9 *on_disk,
-		struct drbd_md *in_core)
+static int check_activity_log_stripe_size(struct drbd_device *device, struct drbd_md *md)
 {
-	u32 al_stripes = be32_to_cpu(on_disk->al_stripes);
-	u32 al_stripe_size_4k = be32_to_cpu(on_disk->al_stripe_size_4k);
+	u32 al_stripes = md->al_stripes;
+	u32 al_stripe_size_4k = md->al_stripe_size_4k;
 	u64 al_size_4k;
 
 	/* both not set: default to old fixed size activity log */
@@ -2862,9 +2861,7 @@ static int check_activity_log_stripe_size(struct drbd_device *device,
 	if (al_size_4k < (32768 >> 9)/8)
 		goto err;
 
-	in_core->al_stripe_size_4k = al_stripe_size_4k;
-	in_core->al_stripes = al_stripes;
-	in_core->al_size_4k = al_size_4k;
+	md->al_size_4k = al_size_4k;
 
 	return 0;
 err:
@@ -2873,13 +2870,11 @@ err:
 	return -EINVAL;
 }
 
-static int check_offsets_and_sizes(struct drbd_device *device,
-		struct meta_data_on_disk_9 *on_disk,
-		struct drbd_backing_dev *bdev)
+static int check_offsets_and_sizes(struct drbd_device *device, struct drbd_backing_dev *bdev,
+				   int max_peers)
 {
 	sector_t capacity = drbd_get_capacity(bdev->md_bdev);
-	struct drbd_md *in_core = &bdev->md;
-	u32 max_peers = be32_to_cpu(on_disk->bm_max_peers);
+	struct drbd_md *md = &bdev->md;
 	s32 on_disk_al_sect;
 	s32 on_disk_bm_sect;
 
@@ -2889,10 +2884,6 @@ static int check_offsets_and_sizes(struct drbd_device *device,
 	}
 	device->bitmap->bm_max_peers = max_peers;
 
-	in_core->al_offset = be32_to_cpu(on_disk->al_offset);
-	in_core->bm_offset = be32_to_cpu(on_disk->bm_offset);
-	in_core->md_size_sect = be32_to_cpu(on_disk->md_size_sect);
-
 	/* The on-disk size of the activity log, calculated from offsets, and
 	 * the size of the activity log calculated from the stripe settings,
 	 * should match.
@@ -2901,34 +2892,34 @@ static int check_offsets_and_sizes(struct drbd_device *device,
 	 * Right now, that would break how resize is implemented.
 	 * TODO: make drbd_determine_dev_size() (and the drbdmeta tool) aware
 	 * of possible unused padding space in the on disk layout. */
-	if (in_core->al_offset < 0) {
-		if (in_core->bm_offset > in_core->al_offset)
+	if (md->al_offset < 0) {
+		if (md->bm_offset > md->al_offset)
 			goto err;
-		on_disk_al_sect = -in_core->al_offset;
-		on_disk_bm_sect = in_core->al_offset - in_core->bm_offset;
+		on_disk_al_sect = -md->al_offset;
+		on_disk_bm_sect = md->al_offset - md->bm_offset;
 	} else {
-		if (in_core->al_offset != (4096 >> 9))
+		if (md->al_offset != (4096 >> 9))
 			goto err;
-		if (in_core->bm_offset < in_core->al_offset + in_core->al_size_4k * (4096 >> 9))
+		if (md->bm_offset < md->al_offset + md->al_size_4k * (4096 >> 9))
 			goto err;
 
-		on_disk_al_sect = in_core->bm_offset - (4096 >> 9);
-		on_disk_bm_sect = in_core->md_size_sect - in_core->bm_offset;
+		on_disk_al_sect = md->bm_offset - (4096 >> 9);
+		on_disk_bm_sect = md->md_size_sect - md->bm_offset;
 	}
 
 	/* old fixed size meta data is exactly that: fixed. */
-	if (in_core->meta_dev_idx >= 0) {
-		if (in_core->md_size_sect != (128 << 20 >> 9)
-		||  in_core->al_offset != (4096 >> 9)
-		||  in_core->bm_offset != (4096 >> 9) + (32768 >> 9)
-		||  in_core->al_stripes != 1
-		||  in_core->al_stripe_size_4k != (32768 >> 12))
+	if (md->meta_dev_idx >= 0) {
+		if (md->md_size_sect != (128 << 20 >> 9)
+		||  md->al_offset != (4096 >> 9)
+		||  md->bm_offset != (4096 >> 9) + (32768 >> 9)
+		||  md->al_stripes != 1
+		||  md->al_stripe_size_4k != (32768 >> 12))
 			goto err;
 	}
 
-	if (capacity < in_core->md_size_sect)
+	if (capacity < md->md_size_sect)
 		goto err;
-	if (capacity - in_core->md_size_sect < drbd_md_first_sector(bdev))
+	if (capacity - md->md_size_sect < drbd_md_first_sector(bdev))
 		goto err;
 
 	/* should be aligned, and at least 32k */
@@ -2937,18 +2928,18 @@ static int check_offsets_and_sizes(struct drbd_device *device,
 
 	/* should fit (for now: exactly) into the available on-disk space;
 	 * overflow prevention is in check_activity_log_stripe_size() above. */
-	if (on_disk_al_sect != in_core->al_size_4k * (4096 >> 9))
+	if (on_disk_al_sect != md->al_size_4k * (4096 >> 9))
 		goto err;
 
 	/* again, should be aligned */
-	if (in_core->bm_offset & 7)
+	if (md->bm_offset & 7)
 		goto err;
 
 	/* FIXME check for device grow with flex external meta data? */
 
 	/* can the available bitmap space cover the last agreed device size? */
 	if (on_disk_bm_sect < drbd_capacity_to_on_disk_bm_sect(
-				in_core->effective_size, max_peers))
+				md->effective_size, max_peers))
 		goto err;
 
 	return 0;
@@ -2957,10 +2948,10 @@ err:
 	drbd_err(device, "meta data offsets don't make sense: idx=%d "
 			"al_s=%u, al_sz4k=%u, al_offset=%d, bm_offset=%d, "
 			"md_size_sect=%u, la_size=%llu, md_capacity=%llu\n",
-			in_core->meta_dev_idx,
-			in_core->al_stripes, in_core->al_stripe_size_4k,
-			in_core->al_offset, in_core->bm_offset, in_core->md_size_sect,
-			(unsigned long long)in_core->effective_size,
+			md->meta_dev_idx,
+			md->al_stripes, md->al_stripe_size_4k,
+			md->al_offset, md->bm_offset, md->md_size_sect,
+			(unsigned long long)md->effective_size,
 			(unsigned long long)capacity);
 
 	return -EINVAL;
@@ -2986,56 +2977,111 @@ static void drbd_err_and_skb_info(struct drbd_config_context *adm_ctx, const cha
 	kfree(text);
 }
 
+static void decode_md_9(struct meta_data_on_disk_9 *on_disk, struct drbd_md *md,
+			int *max_peers, int *bytes_per_bit)
+{
+	int i;
+
+	md->effective_size = be64_to_cpu(on_disk->effective_size);
+	md->current_uuid = be64_to_cpu(on_disk->current_uuid);
+	md->prev_members = be64_to_cpu(on_disk->members);
+	md->device_uuid = be64_to_cpu(on_disk->device_uuid);
+	md->md_size_sect = be32_to_cpu(on_disk->md_size_sect);
+	md->al_offset = be32_to_cpu(on_disk->al_offset);
+
+	md->bm_offset = be32_to_cpu(on_disk->bm_offset);
+
+	md->flags = be32_to_cpu(on_disk->flags);
+
+	*max_peers = be32_to_cpu(on_disk->bm_max_peers);
+	*bytes_per_bit = be32_to_cpu(on_disk->bm_bytes_per_bit);
+	md->node_id = be32_to_cpu(on_disk->node_id);
+	md->al_stripes = be32_to_cpu(on_disk->al_stripes);
+	md->al_stripe_size_4k = be32_to_cpu(on_disk->al_stripe_size_4k);
+
+
+	for (i = 0; i < DRBD_NODE_ID_MAX; i++) {
+		struct drbd_peer_md *peer_md = &md->peers[i];
+
+		peer_md->bitmap_uuid = be64_to_cpu(on_disk->peers[i].bitmap_uuid);
+		peer_md->bitmap_dagtag = be64_to_cpu(on_disk->peers[i].bitmap_dagtag);
+		peer_md->flags = be32_to_cpu(on_disk->peers[i].flags);
+		peer_md->bitmap_index = be32_to_cpu(on_disk->peers[i].bitmap_index);
+
+		if (peer_md->bitmap_index == -1)
+			continue;
+		peer_md->flags |= MDF_HAVE_BITMAP;
+	}
+	for (i = 0; i < ARRAY_SIZE(on_disk->history_uuids); i++)
+		md->history_uuids[i] = be64_to_cpu(on_disk->history_uuids[i]);
+
+	BUILD_BUG_ON(ARRAY_SIZE(md->history_uuids) != ARRAY_SIZE(on_disk->history_uuids));
+}
+
+
+static void decode_magic(struct meta_data_on_disk_9 *on_disk, u32 *magic, u32 *flags)
+{
+	/* magic and flags are in at the same offsets in 8.4 and 9 */
+	*magic = be32_to_cpu(on_disk->magic);
+	*flags = be32_to_cpu(on_disk->flags);
+}
+
 static
 int drbd_md_decode(struct drbd_config_context *adm_ctx,
 		   struct drbd_backing_dev *bdev,
-		   struct meta_data_on_disk_9 *buffer)
+		   void *buffer)
 {
 	struct drbd_device *device = adm_ctx->device;
 	u32 magic, flags;
-	int i, rv = NO_ERROR;
+	int i, bytes_per_bit, rv = NO_ERROR;
 	int my_node_id = device->resource->res_opts.node_id;
 	u32 max_peers;
 
-	magic = be32_to_cpu(buffer->magic);
-	flags = be32_to_cpu(buffer->flags);
-	if (magic == DRBD_MD_MAGIC_09 && !(flags & MDF_AL_CLEAN)) {
-			/* btw: that's Activity Log clean, not "all" clean. */
-		drbd_err_and_skb_info(adm_ctx, "Found unclean meta data. Did you \"drbdadm apply-al\"?\n");
+	decode_magic(buffer, &magic, &flags);
+	if ((magic == DRBD_MD_MAGIC_09 && !(flags & MDF_AL_CLEAN)) ||
+	    magic == DRBD_MD_MAGIC_84_UNCLEAN ||
+	    (magic == DRBD_MD_MAGIC_08 && !(flags & MDF_AL_CLEAN))) {
+		/* btw: that's Activity Log clean, not "all" clean. */
+		drbd_err_and_skb_info(adm_ctx,
+				"Found unclean meta data. Did you \"drbdadm apply-al\"?\n");
 		rv = ERR_MD_UNCLEAN;
 		goto err;
 	}
 	rv = ERR_MD_INVALID;
-	if (magic != DRBD_MD_MAGIC_09) {
-		if (magic == DRBD_MD_MAGIC_07 ||
-		    magic == DRBD_MD_MAGIC_08 ||
-		    magic == DRBD_MD_MAGIC_84_UNCLEAN)
-			drbd_err_and_skb_info(adm_ctx, "Found old meta data magic. Did you \"drbdadm create-md\"?\n");
+	if (magic != DRBD_MD_MAGIC_09 && magic !=
+	    DRBD_MD_MAGIC_84_UNCLEAN && magic !=  DRBD_MD_MAGIC_08) {
+		if (magic == DRBD_MD_MAGIC_07)
+			drbd_err_and_skb_info(adm_ctx,
+				"Found old meta data magic. Did you \"drbdadm create-md\"?\n");
 		else
-			drbd_err_and_skb_info(adm_ctx, "Meta data magic not found. Did you \"drbdadm create-md\"?\n");
+			drbd_err_and_skb_info(adm_ctx,
+				"Meta data magic not found. Did you \"drbdadm create-md\"?\n");
 		goto err;
 	}
 
-	if (be32_to_cpu(buffer->bm_bytes_per_bit) != BM_BLOCK_SIZE) {
+	if (magic == DRBD_MD_MAGIC_09) {
+		clear_bit(LEGACY_84_MD, &device->flags);
+		decode_md_9(buffer, &bdev->md, &max_peers, &bytes_per_bit);
+	} else {
+		if (!device->resource->res_opts.drbd8_compat_mode) {
+			drbd_err_and_skb_info(adm_ctx,
+				"Found old meta data magic. Did you \"drbdadm create-md\"?\n");
+			goto err;
+		}
+		set_bit(LEGACY_84_MD, &device->flags);
+		drbd_md_decode_84(buffer, &bdev->md, &max_peers, &bytes_per_bit);
+	}
+
+	if (bytes_per_bit != BM_BLOCK_SIZE) {
 		drbd_err_and_skb_info(adm_ctx, "unexpected bm_bytes_per_bit: %u (expected %u)\n",
-		    be32_to_cpu(buffer->bm_bytes_per_bit), BM_BLOCK_SIZE);
+		    bytes_per_bit, BM_BLOCK_SIZE);
 		goto err;
 	}
 
-	if (check_activity_log_stripe_size(device, buffer, &bdev->md))
+	if (check_activity_log_stripe_size(device, &bdev->md))
 		goto err;
-	if (check_offsets_and_sizes(device, buffer, bdev))
+	if (check_offsets_and_sizes(device, bdev, max_peers))
 		goto err;
-
-	bdev->md.effective_size = be64_to_cpu(buffer->effective_size);
-	bdev->md.current_uuid = be64_to_cpu(buffer->current_uuid);
-	bdev->md.flags = be32_to_cpu(buffer->flags);
-	bdev->md.prev_members = be64_to_cpu(buffer->members);
-
-	bdev->md.device_uuid = be64_to_cpu(buffer->device_uuid);
-	bdev->md.node_id = be32_to_cpu(buffer->node_id);
-
-	bdev->md.node_id = be32_to_cpu(buffer->node_id);
 
 	if (bdev->md.node_id != -1 && bdev->md.node_id != my_node_id) {
 		drbd_err_and_skb_info(adm_ctx, "ambiguous node id: meta-data: %d, config: %d\n",
@@ -3043,18 +3089,11 @@ int drbd_md_decode(struct drbd_config_context *adm_ctx,
 		goto err;
 	}
 
-	max_peers = be32_to_cpu(buffer->bm_max_peers);
 	for (i = 0; i < DRBD_NODE_ID_MAX; i++) {
 		struct drbd_peer_md *peer_md = &bdev->md.peers[i];
 
-		peer_md->bitmap_uuid = be64_to_cpu(buffer->peers[i].bitmap_uuid);
-		peer_md->bitmap_dagtag = be64_to_cpu(buffer->peers[i].bitmap_dagtag);
-		peer_md->flags = be32_to_cpu(buffer->peers[i].flags);
-		peer_md->bitmap_index = be32_to_cpu(buffer->peers[i].bitmap_index);
-
 		if (peer_md->bitmap_index == -1)
 			continue;
-		peer_md->flags |= MDF_HAVE_BITMAP;
 		if (i == my_node_id) {
 			drbd_err_and_skb_info(adm_ctx, "my own node id (%d) should not have a bitmap index (%d)\n",
 				my_node_id, peer_md->bitmap_index);
@@ -3068,9 +3107,6 @@ int drbd_md_decode(struct drbd_config_context *adm_ctx,
 		/* maybe: for each bitmap_index != -1, create a connection object
 		 * with peer_node_id = i, unless already present. */
 	}
-	BUILD_BUG_ON(ARRAY_SIZE(bdev->md.history_uuids) != ARRAY_SIZE(buffer->history_uuids));
-	for (i = 0; i < ARRAY_SIZE(buffer->history_uuids); i++)
-		bdev->md.history_uuids[i] = be64_to_cpu(buffer->history_uuids[i]);
 
 	rv = NO_ERROR;
 
@@ -3092,7 +3128,7 @@ err:
 static int drbd_md_read(struct drbd_config_context *adm_ctx, struct drbd_backing_dev *bdev)
 {
 	struct drbd_device *device = adm_ctx->device;
-	struct meta_data_on_disk_9 *buffer;
+	void *buffer;
 	int rv;
 
 	if (device->disk_state[NOW] != D_DISKLESS)
@@ -4557,40 +4593,6 @@ check_path_usable(const struct drbd_config_context *adm_ctx,
 	return NO_ERROR;
 }
 
-static int compare_sockaddr(struct sockaddr_storage *a_sockaddr,
-			    struct sockaddr_storage *b_sockaddr)
-{
-	if (a_sockaddr->ss_family < b_sockaddr->ss_family)
-		return -1;
-	if (a_sockaddr->ss_family > b_sockaddr->ss_family)
-		return 1;
-	if (a_sockaddr->ss_family == AF_INET) {
-		struct sockaddr_in *a4_sockaddr = (struct sockaddr_in *)a_sockaddr;
-		struct sockaddr_in *b4_sockaddr = (struct sockaddr_in *)b_sockaddr;
-		int cmp = memcmp(&a4_sockaddr->sin_addr, &b4_sockaddr->sin_addr, sizeof(struct in_addr));
-		if (cmp)
-			return cmp;
-		if (a4_sockaddr->sin_port < b4_sockaddr->sin_port)
-			return -1;
-		if (a4_sockaddr->sin_port > b4_sockaddr->sin_port)
-			return 1;
-		return 0;
-	} else if (a_sockaddr->ss_family == AF_INET6) {
-		struct sockaddr_in6 *a6_sockaddr = (struct sockaddr_in6 *)a_sockaddr;
-		struct sockaddr_in6 *b6_sockaddr = (struct sockaddr_in6 *)b_sockaddr;
-		int cmp = memcmp(&a6_sockaddr->sin6_addr, &b6_sockaddr->sin6_addr, sizeof(struct in6_addr));
-		if (!cmp)
-			return cmp;
-		if (a6_sockaddr->sin6_port < b6_sockaddr->sin6_port)
-			return -1;
-		if (a6_sockaddr->sin6_port > b6_sockaddr->sin6_port)
-			return 1;
-		return 0;
-	} else {
-		pr_err("drbd: %s: Invalid sockaddr family %ul\n", __func__, a_sockaddr->ss_family);
-	}
-	return 1;
-}
 
 static enum drbd_ret_code
 adm_add_path(struct drbd_config_context *adm_ctx,  struct genl_info *info)
@@ -4644,18 +4646,9 @@ adm_add_path(struct drbd_config_context *adm_ctx,  struct genl_info *info)
 
 	kref_init(&path->kref);
 
-	if (connection->resource->res_opts.drbd8_compat_mode) {
-		/*
-		 * This is drbd 8 userspace compat mode, so we do not have a
-		 * node_id yet. Since we only ever have two peers, we can
-		 * arbitrate our node ids by comparing IP addresses. The lower
-		 * address gets node id 0, the other one gets 1.
-		 */
-		int cmp = compare_sockaddr(&path->my_addr, &path->peer_addr);
-		D_ASSERT(connection, cmp != 0); /* addresses cannot be equal */
-		connection->resource->res_opts.node_id = cmp == -1 ? 0 : 1;
-		drbd_info(connection, "drbd8 userspace compat mode: setting my node id to %d\n", connection->resource->res_opts.node_id);
-	}
+	if (connection->resource->res_opts.drbd8_compat_mode)
+		drbd_setup_node_ids_84(connection, path);
+
 
 	/* Exclusive with transport op "prepare_connect()" */
 	mutex_lock(&resource->conf_update);
@@ -6928,6 +6921,7 @@ static int drbd_adm_new_resource(struct sk_buff *skb, struct genl_info *info)
 		 * will be set to an actual value when the resource is
 		 * connected later.
 		 */
+		res_opts.auto_promote = false;
 	} else if (res_opts.node_id >= DRBD_NODE_ID_MAX) {
 		pr_err("drbd: invalid node id (%d)\n", res_opts.node_id);
 		retcode = ERR_INVALID_REQUEST;

commit 0f8c441759ccf498521f2d244b50f70462e6e81a

drbd: Fix /proc/drbd output for connected resources
This is only relevant when CONFIG_DRBD_COMPAT_84=y is
enabled. /proc/drbd displayed "(null)" instead of "Connected" for
resources with their replication state established.

Fixes: cec99e5ac8061 ("drbd: Add reading and writing of legacy drbd-8.4 metadata")

drbd/drbd_legacy_84.c
@@ -57,9 +57,10 @@ static const char * const drbd_conn_s_names[] = {
 	[C_NETWORK_FAILURE]  = "NetworkFailure",
 	[C_PROTOCOL_ERROR]   = "ProtocolError",
 	[C_CONNECTING]       = "WFConnection",
-	/* [C_WF_REPORT_PARAMS] = "WFReportParams", */
+	/* [C_WF_REPORT_PARAMS] = "WFReportParams", does no longer exist in drbd-9.x */
 	[C_TEAR_DOWN]        = "TearDown",
-	[C_CONNECTED]        = "Connected",
+	[C_CONNECTED]        = "WFReportParams", /* drbd-8.4 for "Negotiating" or "Off" */
+	[L_ESTABLISHED]      = "Connected",
 	[L_STARTING_SYNC_S]  = "StartingSyncS",
 	[L_STARTING_SYNC_T]  = "StartingSyncT",
 	[L_WF_BITMAP_S]      = "WFBitMapS",