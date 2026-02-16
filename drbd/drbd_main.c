LINBIT/drbd

commit 464c5c7d3f4c1f8deed25ae6df2d63d13a7c3d83

drbd: drbd_alloc_bitmap() only after drbd_read_md
Preparation commit to enable configurable bitmap granularity.
We can allocate the bitmap only after we know the bitmap granularity and max_peers.
We need to drbd_read_md() first.

I also do not see why we would still require a "device->bitmap"
just because we have a device.

Drop the drbd_bm_alloc() from drbd_create_device() and check_net_options().
Call it only in drbd_adm_attach, after drbd_read_md().

Change a few helper funtions to not dereference device->bitmap
unless we know we have a bitmap.
During early attach, we can get to the "bm_max_peers" via ldev->md instead.

drbd/drbd_main.c
@@ -4178,9 +4178,6 @@ enum drbd_ret_code drbd_create_device(struct drbd_config_context *adm_ctx, unsig
 	if (!device->md_io.page)
 		goto out_no_io_page;
 
-	device->bitmap = drbd_bm_alloc();
-	if (!device->bitmap)
-		goto out_no_bitmap;
 	spin_lock_init(&device->interval_lock);
 	device->read_requests = RB_ROOT;
 	device->requests = RB_ROOT;
@@ -4307,8 +4304,6 @@ out_no_peer_device:
 		kfree(peer_device);
 	}
 
-	drbd_bm_free(device);
-out_no_bitmap:
 	__free_page(device->md_io.page);
 out_no_io_page:
 	put_disk(disk);
@@ -4566,7 +4561,7 @@ void drbd_md_encode_9(struct drbd_device *device, struct meta_data_on_disk_9 *bu
 
 	buffer->bm_offset = cpu_to_be32(device->ldev->md.bm_offset);
 	buffer->la_peer_max_bio_size = cpu_to_be32(device->device_conf.max_bio_size);
-	buffer->bm_max_peers = cpu_to_be32(device->bitmap->bm_max_peers);
+	buffer->bm_max_peers = cpu_to_be32(device->ldev->md.max_peers);
 	buffer->node_id = cpu_to_be32(device->ldev->md.node_id);
 	for (i = 0; i < DRBD_NODE_ID_MAX; i++) {
 		struct drbd_peer_md *peer_md = &device->ldev->md.peers[i];
@@ -5937,7 +5932,7 @@ static sector_t bm_sect_to_max_capacity(unsigned int bm_max_peers, sector_t bm_s
 sector_t drbd_get_max_capacity(
 		struct drbd_device *device, struct drbd_backing_dev *bdev, bool warn)
 {
-	unsigned int bm_max_peers = device->bitmap->bm_max_peers;
+	unsigned int bm_max_peers = bdev->md.max_peers;
 	sector_t backing_bdev_capacity = drbd_get_capacity(bdev->backing_bdev);
 	sector_t bm_sect;
 	sector_t backing_capacity_remaining;

drbd/drbd_sender.c
@@ -3005,6 +3005,7 @@ static void go_diskless(struct drbd_device *device)
 	}
 
 	drbd_md_sync_if_dirty(device);
+	drbd_bm_free(device);
 	change_disk_state(device, D_DISKLESS, CS_HARD, "go-diskless", NULL);
 }
 

drbd/drbd_actlog.c
@@ -823,8 +823,6 @@ int drbd_set_sync(struct drbd_device *device, sector_t sector, int size,
 	unsigned long irq_flags;
 	int count = 0;
 
-	mask &= (1 << device->bitmap->bm_max_peers) - 1;
-
 	if (size <= 0 || !IS_ALIGNED(size, 512)) {
 		drbd_err(device, "%s sector: %llus, size: %d\n",
 			 __func__, (unsigned long long)sector, size);
@@ -833,6 +831,8 @@ int drbd_set_sync(struct drbd_device *device, sector_t sector, int size,
 
 	if (!get_ldev(device))
 		return false; /* no disk, no metadata, no bitmap to set bits in */
+
+	mask &= (1 << device->bitmap->bm_max_peers) - 1;
 
 	nr_sectors = get_capacity(device->vdisk);
 	esector = sector + (size >> 9) - 1;

drbd/drbd_bitmap.c
@@ -191,7 +191,7 @@ void drbd_bm_unlock(struct drbd_device *device)
 		return;
 	}
 
-	if (!(device->bitmap->bm_flags & BM_LOCK_ALL))
+	if (!(b->bm_flags & BM_LOCK_ALL))
 		drbd_err(device, "FIXME bitmap not locked in bm_unlock\n");
 
 	b->bm_flags &= ~BM_LOCK_ALL;
@@ -262,7 +262,7 @@ static void bm_page_unlock_io(struct drbd_device *device, int page_nr)
 	struct drbd_bitmap *b = device->bitmap;
 	void *addr = &page_private(b->bm_pages[page_nr]);
 	clear_bit_unlock(BM_PAGE_IO_LOCK, addr);
-	wake_up(&device->bitmap->bm_io_wait);
+	wake_up(&b->bm_io_wait);
 }
 
 /* set _before_ submit_io, so it may be reset due to being changed
@@ -395,7 +395,7 @@ static struct page **bm_realloc_pages(struct drbd_bitmap *b, unsigned long want)
 	return new_pages;
 }
 
-struct drbd_bitmap *drbd_bm_alloc(void)
+struct drbd_bitmap *drbd_bm_alloc(unsigned int max_peers, unsigned int bm_block_shift)
 {
 	struct drbd_bitmap *b;
 
@@ -408,7 +408,8 @@ struct drbd_bitmap *drbd_bm_alloc(void)
 	mutex_init(&b->bm_change);
 	init_waitqueue_head(&b->bm_io_wait);
 
-	b->bm_max_peers = 1;
+	b->bm_max_peers = max_peers;
+	b->bm_block_shift = bm_block_shift;
 
 	return b;
 }
@@ -862,7 +863,7 @@ static u64 drbd_md_on_disk_bits(struct drbd_device *device)
 	/* for interoperability between 32bit and 64bit architectures,
 	 * we round on 64bit words.  FIXME do we still need this? */
 	word64_on_disk = bitmap_sectors << (9 - 3); /* x * (512/8) */
-	do_div(word64_on_disk, device->bitmap->bm_max_peers);
+	do_div(word64_on_disk, device->ldev->md.max_peers);
 	return word64_on_disk << 6; /* x * 64 */;
 }
 

drbd/drbd_int.h
@@ -688,6 +688,7 @@ struct drbd_bitmap {
 
 	enum bm_flag bm_flags;
 	unsigned int bm_max_peers;
+	unsigned int bm_block_shift; /* ln2 of bytes per bit for this bitmap */
 
 	/* exclusively to be used by __al_write_transaction(),
 	 * and drbd_bm_write_hinted() -> bm_rw() called from there.
@@ -735,6 +736,10 @@ struct drbd_md {
 
 	s32 al_offset;	/* signed relative sector offset to activity log */
 	s32 bm_offset;	/* signed relative sector offset to bitmap */
+
+	u32 max_peers;
+	u32 bm_block_size;
+	u32 bm_block_shift; /* ilog2(bm_block_size) */
 
 	struct drbd_peer_md peers[DRBD_NODE_ID_MAX];
 	u64 history_uuids[HISTORY_UUIDS];
@@ -1809,8 +1814,7 @@ void drbd_cleanup_device(struct drbd_device *device);
 void drbd_print_uuids(struct drbd_peer_device *peer_device, const char *text);
 void drbd_queue_unplug(struct drbd_device *device);
 
-u64 drbd_capacity_to_on_disk_bm_sect(u64 capacity_sect,
-				     unsigned int max_peers);
+u64 drbd_capacity_to_on_disk_bm_sect(u64 capacity_sect, const struct drbd_md *md);
 void drbd_md_set_sector_offsets(struct drbd_device *device,
 				struct drbd_backing_dev *bdev);
 int drbd_md_write(struct drbd_device *device,
@@ -1949,6 +1953,15 @@ sector_t drbd_partition_data_capacity(struct drbd_device *device);
 #define BM_BIT_TO_SECT(x)   ((sector_t)(x)<<(BM_BLOCK_SHIFT-9))
 #define BM_SECT_PER_BIT     BM_BIT_TO_SECT(1)
 
+static inline unsigned long sect_to_bit(sector_t s, unsigned int bm_block_shift)
+{
+	return s >> (bm_block_shift - 9);
+}
+static inline sector_t sect_per_bit(unsigned int bm_block_shift)
+{
+	return (sector_t)1 << (bm_block_shift - 9);
+}
+
 /* Send P_PEERS_IN_SYNC in steps defined by this shift. Set to the activity log
  * extent shift since the P_PEERS_IN_SYNC intervals are broken up based on
  * activity log extents anyway. */
@@ -2000,7 +2013,7 @@ static inline int interval_to_al_extents(struct drbd_interval *i)
 	return 1 + last - first; /* worst case: all touched extends are cold. */
 }
 
-struct drbd_bitmap *drbd_bm_alloc(void);
+struct drbd_bitmap *drbd_bm_alloc(unsigned int max_peers, unsigned int bm_block_shift);
 int  drbd_bm_resize(struct drbd_device *device, sector_t capacity,
 		    bool set_new_bits);
 void drbd_bm_free(struct drbd_device *device);

drbd/drbd_legacy_84.c
@@ -86,8 +86,7 @@ static int seq_print_device_proc_drbd(struct seq_file *m, struct drbd_device *de
 
 int nr_drbd8_devices;
 
-void drbd_md_decode_84(struct meta_data_on_disk_84 *on_disk, struct drbd_md *md,
-		       int *max_peers, int *bytes_per_bit)
+void drbd_md_decode_84(struct meta_data_on_disk_84 *on_disk, struct drbd_md *md)
 {
 	struct drbd_peer_md *peer_md;
 	const int peer_node_id = 0; /* setup_node_ids_84() moves it later */
@@ -106,8 +105,8 @@ void drbd_md_decode_84(struct meta_data_on_disk_84 *on_disk, struct drbd_md *md,
 	on_disk_flags = be32_to_cpu(on_disk->flags);
 	md->flags = on_disk_flags & MDF_84_MASK;
 
-	*max_peers = 1;
-	*bytes_per_bit = be32_to_cpu(on_disk->bm_bytes_per_bit);
+	md->max_peers = 1;
+	md->bm_block_size = be32_to_cpu(on_disk->bm_bytes_per_bit);
 	md->node_id = -1; /* no node_id in the drbd-8.4 meta-data */
 	md->al_stripes = be32_to_cpu(on_disk->al_stripes);
 	md->al_stripe_size_4k = be32_to_cpu(on_disk->al_stripe_size_4k);

drbd/drbd_legacy_84.h
@@ -10,14 +10,13 @@ struct meta_data_on_disk_84;
 #ifdef CONFIG_DRBD_COMPAT_84
 extern int nr_drbd8_devices;
 
-void drbd_md_decode_84(struct meta_data_on_disk_84 *on_disk, struct drbd_md *md,
-		       int *max_peers, int *bytes_per_bit);
+void drbd_md_decode_84(struct meta_data_on_disk_84 *on_disk, struct drbd_md *md);
 void drbd_md_encode_84(struct drbd_device *device, struct meta_data_on_disk_84 *buffer);
 int drbd_setup_node_ids_84(struct drbd_connection *connection, struct drbd_path *path,
 			   unsigned int peer_node_id);
 bool drbd_show_legacy_device(struct seq_file *seq, void *v);
 #else
-#define drbd_md_decode_84(O, M, MP, B) { *MP = 1; *B = BM_BLOCK_SIZE; }
+#define drbd_md_decode_84(O, M) do {} while (0)
 #define drbd_md_encode_84(D, M) do {} while (0)
 #define drbd_setup_node_ids_84(C, P, N) (0)
 #define drbd_show_legacy_device(S, V) (false)

drbd/drbd_nl.c
@@ -1417,7 +1417,7 @@ out:
 	return 0;
 }
 
-u64 drbd_capacity_to_on_disk_bm_sect(u64 capacity_sect, unsigned int max_peers)
+u64 drbd_capacity_to_on_disk_bm_sect(u64 capacity_sect, const struct drbd_md *md)
 {
 	u64 bits, bytes;
 
@@ -1425,13 +1425,15 @@ u64 drbd_capacity_to_on_disk_bm_sect(u64 capacity_sect, unsigned int max_peers)
 	 * convert to number of bits needed, and round that up to 64bit words
 	 * to ease interoperability between 32bit and 64bit architectures.
 	 */
-	bits = ALIGN(BM_SECT_TO_BIT(ALIGN(capacity_sect, BM_SECT_PER_BIT)), 64);
+	bits = ALIGN(sect_to_bit(
+			ALIGN(capacity_sect, sect_per_bit(md->bm_block_shift)),
+			md->bm_block_shift), 64);
 
 	/* convert to bytes, multiply by number of peers,
 	 * and, because we do all our meta data IO in 4k blocks,
 	 * round up to full 4k
 	 */
-	bytes = ALIGN(bits / 8 * max_peers, 4096);
+	bytes = ALIGN(bits / 8 * md->max_peers, 4096);
 
 	/* convert to number of sectors */
 	return bytes >> 9;
@@ -1463,12 +1465,6 @@ void drbd_md_set_sector_offsets(struct drbd_device *device,
 {
 	sector_t md_size_sect = 0;
 	unsigned int al_size_sect = bdev->md.al_size_4k * 8;
-	int max_peers;
-
-	if (device->bitmap)
-		max_peers = device->bitmap->bm_max_peers;
-	else
-		max_peers = 1;
 
 	bdev->md.md_offset = drbd_md_ss(bdev);
 
@@ -1495,7 +1491,7 @@ void drbd_md_set_sector_offsets(struct drbd_device *device,
 		 * and the activity log; */
 		md_size_sect = drbd_capacity_to_on_disk_bm_sect(
 				drbd_get_capacity(bdev->backing_bdev),
-				max_peers)
+				&bdev->md)
 			+ (4096 >> 9) + al_size_sect;
 
 		bdev->md.md_size_sect = md_size_sect;
@@ -2590,12 +2586,11 @@ static int
 allocate_bitmap_index(struct drbd_peer_device *peer_device,
 		      struct drbd_backing_dev *nbc)
 {
-	struct drbd_device *device = peer_device->device;
 	const int peer_node_id = peer_device->connection->peer_node_id;
 	struct drbd_peer_md *peer_md = &nbc->md.peers[peer_node_id];
 	int bitmap_index;
 
-	bitmap_index = drbd_unallocated_index(nbc, device->bitmap->bm_max_peers);
+	bitmap_index = drbd_unallocated_index(nbc, nbc->md.max_peers);
 	if (bitmap_index == -1) {
 		drbd_err(peer_device, "Not enough free bitmap slots\n");
 		return -ENOSPC;
@@ -2870,19 +2865,17 @@ err:
 	return -EINVAL;
 }
 
-static int check_offsets_and_sizes(struct drbd_device *device, struct drbd_backing_dev *bdev,
-				   int max_peers)
+static int check_offsets_and_sizes(struct drbd_device *device, struct drbd_backing_dev *bdev)
 {
 	sector_t capacity = drbd_get_capacity(bdev->md_bdev);
 	struct drbd_md *md = &bdev->md;
 	s32 on_disk_al_sect;
 	s32 on_disk_bm_sect;
 
-	if (max_peers > DRBD_PEERS_MAX) {
+	if (bdev->md.max_peers > DRBD_PEERS_MAX) {
 		drbd_err(device, "bm_max_peers too high\n");
 		goto err;
 	}
-	device->bitmap->bm_max_peers = max_peers;
 
 	/* The on-disk size of the activity log, calculated from offsets, and
 	 * the size of the activity log calculated from the stripe settings,
@@ -2939,7 +2932,7 @@ static int check_offsets_and_sizes(struct drbd_device *device, struct drbd_backi
 
 	/* can the available bitmap space cover the last agreed device size? */
 	if (on_disk_bm_sect < drbd_capacity_to_on_disk_bm_sect(
-				md->effective_size, max_peers))
+				md->effective_size, md))
 		goto err;
 
 	return 0;
@@ -2977,8 +2970,7 @@ static void drbd_err_and_skb_info(struct drbd_config_context *adm_ctx, const cha
 	kfree(text);
 }
 
-static void decode_md_9(struct meta_data_on_disk_9 *on_disk, struct drbd_md *md,
-			int *max_peers, int *bytes_per_bit)
+static void decode_md_9(struct meta_data_on_disk_9 *on_disk, struct drbd_md *md)
 {
 	int i;
 
@@ -2993,8 +2985,8 @@ static void decode_md_9(struct meta_data_on_disk_9 *on_disk, struct drbd_md *md,
 
 	md->flags = be32_to_cpu(on_disk->flags);
 
-	*max_peers = be32_to_cpu(on_disk->bm_max_peers);
-	*bytes_per_bit = be32_to_cpu(on_disk->bm_bytes_per_bit);
+	md->max_peers = be32_to_cpu(on_disk->bm_max_peers);
+	md->bm_block_size = be32_to_cpu(on_disk->bm_bytes_per_bit);
 	md->node_id = be32_to_cpu(on_disk->node_id);
 	md->al_stripes = be32_to_cpu(on_disk->al_stripes);
 	md->al_stripe_size_4k = be32_to_cpu(on_disk->al_stripe_size_4k);
@@ -3033,9 +3025,8 @@ int drbd_md_decode(struct drbd_config_context *adm_ctx,
 {
 	struct drbd_device *device = adm_ctx->device;
 	u32 magic, flags;
-	int i, bytes_per_bit, rv = NO_ERROR;
+	int i, rv = NO_ERROR;
 	int my_node_id = device->resource->res_opts.node_id;
-	u32 max_peers;
 
 	decode_magic(buffer, &magic, &flags);
 	if ((magic == DRBD_MD_MAGIC_09 && !(flags & MDF_AL_CLEAN)) ||
@@ -3061,7 +3052,7 @@ int drbd_md_decode(struct drbd_config_context *adm_ctx,
 
 	if (magic == DRBD_MD_MAGIC_09) {
 		clear_bit(LEGACY_84_MD, &device->flags);
-		decode_md_9(buffer, &bdev->md, &max_peers, &bytes_per_bit);
+		decode_md_9(buffer, &bdev->md);
 	} else {
 		if (!device->resource->res_opts.drbd8_compat_mode) {
 			drbd_err_and_skb_info(adm_ctx,
@@ -3069,18 +3060,19 @@ int drbd_md_decode(struct drbd_config_context *adm_ctx,
 			goto err;
 		}
 		set_bit(LEGACY_84_MD, &device->flags);
-		drbd_md_decode_84(buffer, &bdev->md, &max_peers, &bytes_per_bit);
+		drbd_md_decode_84(buffer, &bdev->md);
 	}
 
-	if (bytes_per_bit != BM_BLOCK_SIZE) {
+	if (bdev->md.bm_block_size != BM_BLOCK_SIZE) {
 		drbd_err_and_skb_info(adm_ctx, "unexpected bm_bytes_per_bit: %u (expected %u)\n",
-		    bytes_per_bit, BM_BLOCK_SIZE);
+		    bdev->md.bm_block_size, BM_BLOCK_SIZE);
 		goto err;
 	}
+	bdev->md.bm_block_shift = ilog2(BM_BLOCK_SIZE);
 
 	if (check_activity_log_stripe_size(device, &bdev->md))
 		goto err;
-	if (check_offsets_and_sizes(device, bdev, max_peers))
+	if (check_offsets_and_sizes(device, bdev))
 		goto err;
 
 	if (bdev->md.node_id != -1 && bdev->md.node_id != my_node_id) {
@@ -3099,9 +3091,9 @@ int drbd_md_decode(struct drbd_config_context *adm_ctx,
 				my_node_id, peer_md->bitmap_index);
 			goto err;
 		}
-		if (peer_md->bitmap_index < -1 || peer_md->bitmap_index >= max_peers) {
+		if (peer_md->bitmap_index < -1 || peer_md->bitmap_index >= bdev->md.max_peers) {
 			drbd_err_and_skb_info(adm_ctx, "peer node id %d: bitmap index (%d) exceeds allocated bitmap slots (%d)\n",
-				i, peer_md->bitmap_index, max_peers);
+				i, peer_md->bitmap_index, bdev->md.max_peers);
 			goto err;
 		}
 		/* maybe: for each bitmap_index != -1, create a connection object
@@ -3274,19 +3266,23 @@ static int drbd_adm_attach(struct sk_buff *skb, struct genl_info *info)
 		peer_device->rs_failed = 0;
 	}
 
-	if (!device->bitmap) {
-		device->bitmap = drbd_bm_alloc();
-		if (!device->bitmap) {
-			retcode = ERR_NOMEM;
-			goto fail;
-		}
-	}
-
 	/* Read our meta data super block early.
 	 * This also sets other on-disk offsets. */
 	retcode = drbd_md_read(&adm_ctx, nbc);
 	if (retcode != NO_ERROR)
 		goto fail;
+
+	if (device->bitmap) {
+		drbd_err_and_skb_info(&adm_ctx, "already has a bitmap, this should not happen\n");
+		retcode = ERR_INVALID_REQUEST;
+		goto fail;
+	}
+
+	device->bitmap = drbd_bm_alloc(nbc->md.max_peers, nbc->md.bm_block_shift);
+	if (!device->bitmap) {
+		retcode = ERR_NOMEM;
+		goto fail;
+	}
 
 	sanitize_disk_conf(device, new_disk_conf, nbc);
 
@@ -3812,22 +3808,10 @@ static enum drbd_ret_code
 check_net_options(struct drbd_connection *connection, struct net_conf *new_net_conf)
 {
 	enum drbd_ret_code rv;
-	struct drbd_peer_device *peer_device;
-	int i;
 
 	rcu_read_lock();
 	rv = _check_net_options(connection, rcu_dereference(connection->transport.net_conf), new_net_conf);
 	rcu_read_unlock();
-
-	/* connection->peer_devices protected by resource->conf_update here */
-	idr_for_each_entry(&connection->peer_devices, peer_device, i) {
-		struct drbd_device *device = peer_device->device;
-		if (!device->bitmap) {
-			device->bitmap = drbd_bm_alloc();
-			if (!device->bitmap)
-				return ERR_NOMEM;
-		}
-	}
 
 	return rv;
 }
@@ -6246,6 +6230,8 @@ static void peer_device_to_statistics(struct peer_device_statistics *s,
 				      struct drbd_peer_device *pd)
 {
 	struct drbd_device *device = pd->device;
+	struct drbd_md *md;
+	struct drbd_peer_md *peer_md;
 	unsigned long now = jiffies;
 	unsigned long rs_left = 0;
 	int i;
@@ -6259,6 +6245,14 @@ static void peer_device_to_statistics(struct peer_device_statistics *s,
 	s->peer_dev_pending = atomic_read(&pd->ap_pending_cnt) +
 			      atomic_read(&pd->rs_pending_cnt);
 	s->peer_dev_unacked = atomic_read(&pd->unacked_cnt);
+	s->peer_dev_uuid_flags = pd->uuid_flags;
+
+	/* Below are resync / verify / bitmap / meta data stats.
+	 * Without disk, we don't have those.
+	 */
+	if (!get_ldev(device))
+		return;
+
 	s->peer_dev_out_of_sync = BM_BIT_TO_SECT(drbd_bm_total_weight(pd));
 
 	if (is_verify_state(pd, NOW)) {
@@ -6299,18 +6293,15 @@ static void peer_device_to_statistics(struct peer_device_statistics *s,
 		 */
 	}
 
-	if (get_ldev(device)) {
-		struct drbd_md *md = &device->ldev->md;
-		struct drbd_peer_md *peer_md = &md->peers[pd->node_id];
+	md = &device->ldev->md;
+	peer_md = &md->peers[pd->node_id];
 
-		spin_lock_irq(&md->uuid_lock);
-		s->peer_dev_bitmap_uuid = peer_md->bitmap_uuid;
-		spin_unlock_irq(&md->uuid_lock);
-		s->peer_dev_flags = peer_md->flags;
-		put_ldev(device);
-	}
+	spin_lock_irq(&md->uuid_lock);
+	s->peer_dev_bitmap_uuid = peer_md->bitmap_uuid;
+	spin_unlock_irq(&md->uuid_lock);
+	s->peer_dev_flags = peer_md->flags;
 
-	s->peer_dev_uuid_flags = pd->uuid_flags;
+	put_ldev(device);
 }
 
 static int drbd_adm_dump_peer_devices_done(struct netlink_callback *cb)

commit 9723ef43b7d5d3ee7e32ae8aa2204f3c93b8e962

drbd: move drbd_bm_free() from go_diskless() to drbd_ldev_destroy()
Commit 464c5c7d introduced freeing the bitmap of an existing device.
Only a device that has a backing disk can have a bitmap. Phrased
differently, a diskless node can never have a bitmap. A consequence of
this is that the get_ldev()/put_ldev() protection is sufficient to
protect accesses to device->bitmap. That extents the previously
existing convention that every access to device->ldev needs to be
protected by get_ldev()/put_ldev().

The get_ldev()/put_ldev() delays freeing of a backing device, and from
now on also a bitmap, until any other context stoped using the bitmap
or ldevl objects.

Here is the stacktrace from a LINSTOR tests that triggered the above insight:

 BUG: kernel NULL pointer dereference, address: 000000000000016c
 #PF: supervisor read access in kernel mode
 #PF: error_code(0x0000) - not-present page
[...]
 Call Trace:
  <TASK>
  ? show_regs+0x6d/0x80
  ? __die+0x24/0x80
  ? page_fault_oops+0x99/0x1b0
  ? do_user_addr_fault+0x2ee/0x6b0
  ? exc_page_fault+0x83/0x1b0
  ? asm_exc_page_fault+0x27/0x30
  ? drbd_set_sync+0x2e/0x410 [drbd]
  ? dtt_recv+0x157/0x270 [drbd_transport_tcp]
  ? srso_alias_return_thunk+0x5/0xfbef5
  drbd_set_all_out_of_sync+0x1c/0x30 [drbd]
  receive_rs_deallocated+0xd1/0x270 [drbd]
  ? __pfx_receive_rs_deallocated+0x10/0x10 [drbd]
  drbd_receiver+0x5a0/0xae0

Move drbd_bm_free() to drbd_ldev_destroy(), which runs as deferred
work only after the disk state is D_DISKLESS and local_cnt has reached
zero. This guarantees that no get_ldev() holders remain when the bitmap is
freed.

Fixes: 464c5c7d ("drbd: drbd_alloc_bitmap() only after drbd_read_md")

drbd/drbd_main.c
@@ -4070,6 +4070,7 @@ static void drbd_ldev_destroy(struct work_struct *ws)
 {
 	struct drbd_device *device = container_of(ws, struct drbd_device, ldev_destroy_work);
 
+	drbd_bm_free(device);
 	lc_destroy(device->act_log);
 	device->act_log = NULL;
 	__acquire(local);

drbd/drbd_sender.c
@@ -3016,7 +3016,6 @@ static void go_diskless(struct drbd_device *device)
 	}
 
 	drbd_md_sync_if_dirty(device);
-	drbd_bm_free(device);
 	change_disk_state(device, D_DISKLESS, CS_HARD, "go-diskless", NULL);
 }
 