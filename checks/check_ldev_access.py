LINBIT/drbd

commit da3cdff802ab521ca7108d05cca5ec53c8112da7

drbd: avoid potential NULL deref of device->bitmap
If we don't even have a bitmap, pretend it was at 4k granularity.

drbd/drbd_receiver.c
@@ -5986,12 +5986,12 @@ static int receive_sizes(struct drbd_connection *connection, struct packet_info 
 		peer_device->bm_block_shift =
 			p->qlim->bm_block_shift_minus_12 + BM_BLOCK_SHIFT_4k;
 	else {
-		int bbs = bm_block_size(device->bitmap);
+		int bbs = device->bitmap ? bm_block_size(device->bitmap) : BM_BLOCK_SIZE_4k;
 		/* May work as long as this one is SyncTarget. May result in
 		 * funny never ending / repeating resyncs if the other guy is
 		 * SyncTarget, but unaware of bitmap granularity issues.
 		 */
-		if (have_ldev && bbs != BM_BLOCK_SIZE_4k)
+		if (bbs != BM_BLOCK_SIZE_4k)
 			drbd_warn(peer_device,
 				"My bitmap granularity is %u. Upgrade this peer to make it aware.\n",
 				bbs);

commit f77db64d5be2b6f2af0a87dcf979347a88c9454b

drbd: protect bitmap access in receive_sizes()
Testing device->bitmap might return a positive value for an already
diskless device, and the worker thread in parallel frees the bitmap
object.
Fix that by testing if the receiver thread holds a reference to ldev
instead. This guarantees that the bitmap will not go away until this
thread gives up the reference by calling put_ldev().

Fixes: da3cdff802ab5 ("drbd: avoid potential NULL deref of device->bitmap")

checks/check_ldev_access.py
@@ -507,6 +507,12 @@ def _analyze_block(block, regions):
                 i += 1
                 continue
 
+        # Pattern D: ldev_var ? consequence : alternative
+        # Mark the consequence of ternary expressions guarded by an
+        # ldev variable as protected.
+        if ldev_vars:
+            _find_ternary_ldev_guards(stmt, ldev_vars, regions)
+
         _recurse_children(stmt, regions)
         i += 1
 
@@ -599,6 +605,20 @@ def _is_ldev_var_guard(stmt, ldev_vars):
     if consequence.type == "compound_statement":
         return consequence
     return None
+
+
+def _find_ternary_ldev_guards(node, ldev_vars, regions):
+    """Find ``ldev_var ? consequence : alternative`` and mark *consequence*
+    as a protected region."""
+    for n in walk_body(node):
+        if n.type != "conditional_expression":
+            continue
+        cond = n.child_by_field_name("condition")
+        consequence = n.child_by_field_name("consequence")
+        if cond is None or consequence is None:
+            continue
+        if cond.type == "identifier" and text(cond) in ldev_vars:
+            regions.append((consequence.start_byte, consequence.end_byte))
 
 
 def _is_negated_get_ldev_if(stmt):

drbd/drbd_receiver.c
@@ -6061,11 +6061,11 @@ static int receive_sizes(struct drbd_connection *connection, struct packet_info 
 		peer_device->q_limits.io_opt = be32_to_cpu(qlim->io_opt);
 	}
 
-	if (connection->agreed_features & DRBD_FF_BM_BLOCK_SHIFT)
+	if (connection->agreed_features & DRBD_FF_BM_BLOCK_SHIFT) {
 		peer_device->bm_block_shift =
 			p->qlim->bm_block_shift_minus_12 + BM_BLOCK_SHIFT_4k;
-	else {
-		int bbs = device->bitmap ? bm_block_size(device->bitmap) : BM_BLOCK_SIZE_4k;
+	} else {
+		int bbs = have_ldev ? bm_block_size(device->bitmap) : BM_BLOCK_SIZE_4k;
 		/* May work as long as this one is SyncTarget. May result in
 		 * funny never ending / repeating resyncs if the other guy is
 		 * SyncTarget, but unaware of bitmap granularity issues.