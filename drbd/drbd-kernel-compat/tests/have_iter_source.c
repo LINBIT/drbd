LINBIT/drbd

commit eed6170cc450de6c9ea7d1641613811e8b482c1c

drbd: Rename send_zc_bio() to send_bio(, MSG_SPLICE_PAGES)
This follows in spirit the removal of sendpage() in favour of
sendmsg(MSG_SPLICE_PAGES). Apart from following upstream ideas,
it also removes more lines than it adds.

drbd/drbd-kernel-compat/cocci/sendpage__yes_present.cocci
@@ -1,8 +1,54 @@
+@ find_msg_splice_pages @
 @@
-expression flags;
+ MSG_SPLICE_PAGES
+
+@ dtt_send_bio_rule @
+struct drbd_transport *transport;
+int err;
+expression d_stream, page, offset, len, msg_flags_ex;
+identifier msg_flags;
 @@
-  flags
-- | MSG_SPLICE_PAGES
+dtt_send_bio(..., unsigned int msg_flags)
+{
+<...
++ if (msg_flags & MSG_SPLICE_PAGES) {
+  err = dtt_send_page(transport, d_stream, page, offset, len,
+- msg_flags_ex
++ (msg_flags_ex) & ~MSG_SPLICE_PAGES
+  );
++ } else {
++	err = _dtt_send(container_of(transport, struct drbd_tcp_transport, transport),
++	      		container_of(transport, struct drbd_tcp_transport, transport)->stream[d_stream],
++			page_address(page) + offset, len, msg_flags_ex);
++	if (err > 0)
++		err = 0;
++ }
+...>
+}
+
+@@
+struct dtl_transport *dtl_transport;
+int err;
+expression flow, page, offset, len, msg_flags_ex;
+identifier msg_flags;
+@@
+dtl_send_bio_pages(..., unsigned int msg_flags)
+{
+<...
++ if (msg_flags & MSG_SPLICE_PAGES) {
+  err = _dtl_send_page(dtl_transport, flow, page, offset, len,
+- msg_flags_ex
++ (msg_flags_ex) & ~MSG_SPLICE_PAGES
+  );
++ } else {
++	err = _dtl_send(dtl_transport, flow,
++	      		page_address(page) + offset, len, msg_flags_ex);
++	if (err > 0)
++		err = 0;
++ }
+...>
+}
+
 
 @@
 identifier bvec, page, len, offset, msg, socket;
@@ -15,3 +61,9 @@ identifier sent = sent;
 - iov_iter_bvec(&msg.msg_iter, ITER_SOURCE, &bvec, 1, len);
 - sent = sock_sendmsg(socket, &msg);
 + sent = socket->ops->sendpage(socket, page, offset, len, msg.msg_flags);
+
+
+@ define_msg_splice_pages depends on find_msg_splice_pages || dtt_send_bio_rule @
+@@
+ #include <...>
++ #define MSG_SPLICE_PAGES 0x8000000

drbd/drbd_transport_tcp.c
@@ -121,8 +121,8 @@ static int dtt_net_conf_change(struct drbd_transport *transport, struct net_conf
 static void dtt_set_rcvtimeo(struct drbd_transport *transport, enum drbd_stream stream, long timeout);
 static long dtt_get_rcvtimeo(struct drbd_transport *transport, enum drbd_stream stream);
 static int dtt_send_page(struct drbd_transport *transport, enum drbd_stream, struct page *page,
-		int offset, size_t size, unsigned msg_flags);
-static int dtt_send_zc_bio(struct drbd_transport *, struct bio *bio);
+		int offset, size_t size, unsigned int msg_flags);
+static int dtt_send_bio(struct drbd_transport *, struct bio *bio, unsigned int msg_flags);
 static bool dtt_stream_ok(struct drbd_transport *transport, enum drbd_stream stream);
 static bool dtt_hint(struct drbd_transport *transport, enum drbd_stream stream, enum drbd_tr_hints hint);
 static void dtt_debugfs_show(struct drbd_transport *transport, struct seq_file *m);
@@ -152,7 +152,7 @@ static struct drbd_transport_class tcp_transport_class = {
 		.set_rcvtimeo = dtt_set_rcvtimeo,
 		.get_rcvtimeo = dtt_get_rcvtimeo,
 		.send_page = dtt_send_page,
-		.send_zc_bio = dtt_send_zc_bio,
+		.send_bio = dtt_send_bio,
 		.stream_ok = dtt_stream_ok,
 		.hint = dtt_hint,
 		.debugfs_show = dtt_debugfs_show,
@@ -1492,7 +1492,7 @@ static int dtt_send_page(struct drbd_transport *transport, enum drbd_stream stre
 	struct drbd_tcp_transport *tcp_transport =
 		container_of(transport, struct drbd_tcp_transport, transport);
 	struct socket *socket = tcp_transport->stream[stream];
-	struct msghdr msg = { .msg_flags = msg_flags | MSG_NOSIGNAL | MSG_SPLICE_PAGES };
+	struct msghdr msg = { .msg_flags = msg_flags | MSG_NOSIGNAL };
 	struct bio_vec bvec;
 	int len = size;
 	int err = -EIO;
@@ -1537,7 +1537,7 @@ static int dtt_send_page(struct drbd_transport *transport, enum drbd_stream stre
 	return err;
 }
 
-static int dtt_send_zc_bio(struct drbd_transport *transport, struct bio *bio)
+static int dtt_send_bio(struct drbd_transport *transport, struct bio *bio, unsigned int msg_flags)
 {
 	struct bio_vec bvec;
 	struct bvec_iter iter;
@@ -1547,7 +1547,7 @@ static int dtt_send_zc_bio(struct drbd_transport *transport, struct bio *bio)
 
 		err = dtt_send_page(transport, DATA_STREAM, bvec.bv_page,
 				      bvec.bv_offset, bvec.bv_len,
-				      bio_iter_last(bvec, iter) ? 0 : MSG_MORE);
+				      msg_flags | (bio_iter_last(bvec, iter) ? 0 : MSG_MORE));
 		if (err)
 			return err;
 	}

drbd/drbd_main.c
@@ -2320,136 +2320,53 @@ void *drbd_prepare_drequest_csum(struct drbd_peer_request *peer_req, enum drbd_p
 			dagtag_node_id, dagtag);
 }
 
-/* The idea of sendpage seems to be to put some kind of reference
- * to the page into the skb, and to hand it over to the NIC. In
- * this process get_page() gets called.
- *
- * As soon as the page was really sent over the network put_page()
- * gets called by some part of the network layer. [ NIC driver? ]
- *
- * [ get_page() / put_page() increment/decrement the count. If count
- *   reaches 0 the page will be freed. ]
- *
- * This works nicely with pages from FSs.
- * But this means that in protocol A we might signal IO completion too early!
- *
- * In order not to corrupt data during a resync we must make sure
- * that we do not reuse our own buffer pages (EEs) to early, therefore
- * we have the net_ee list.
- *
- * XFS seems to have problems, still, it submits pages with page_count == 0!
- * As a workaround, we disable sendpage on pages
- * with page_count == 0 or PageSlab.
+/* sendmsg(MSG_SPLICE_PAGES) (former (sendpage()) increases the page ref_count
+ * and hands it to the network stack. After the NIC DMA sends the data, it
+ * decreases that page's ref_count.
+ * We may not do this for protocol A, where we could complete a write operation
+ * before the network stack sends the data.
  */
-static int _drbd_send_page(struct drbd_peer_device *peer_device, struct page *page,
-			    int offset, size_t size, unsigned msg_flags)
+static int
+drbd_send_bio(struct drbd_peer_device *peer_device, struct bio *bio, unsigned int msg_flags)
 {
 	struct drbd_connection *connection = peer_device->connection;
 	struct drbd_transport *transport = &connection->transport;
 	struct drbd_transport_ops *tr_ops = &transport->class->ops;
 	int err;
 
-	err = tr_ops->send_page(transport, DATA_STREAM, page, offset, size, msg_flags);
-	if (!err)
-		peer_device->send_cnt += size >> 9;
+	if (drbd_disable_sendpage)
+		msg_flags &= ~MSG_SPLICE_PAGES;
 
-	return err;
-}
+	/* e.g. XFS meta- & log-data is in slab pages have !sendpage_ok(page) */
+	if (msg_flags & MSG_SPLICE_PAGES) {
+		struct bvec_iter iter;
+		struct bio_vec bvec;
 
-static int _drbd_no_send_page(struct drbd_peer_device *peer_device, struct page *page,
-			      int offset, size_t size, unsigned msg_flags)
-{
-	struct drbd_connection *connection = peer_device->connection;
-	struct drbd_send_buffer *sbuf = &connection->send_buffer[DATA_STREAM];
-	char *from_base;
-	void *buffer2;
-	int err;
-
-	buffer2 = alloc_send_buffer(connection, size, DATA_STREAM);
-	from_base = kmap_atomic(page);
-	memcpy(buffer2, from_base + offset, size);
-	kunmap_atomic(from_base);
-
-	if (msg_flags & MSG_MORE) {
-		sbuf->pos += sbuf->allocated_size;
-		sbuf->allocated_size = 0;
-		err = 0;
-	} else {
-		err = flush_send_buffer(connection, DATA_STREAM);
-	}
-
-	return err;
-}
-
-static int _drbd_send_bio(struct drbd_peer_device *peer_device, struct bio *bio)
-{
-	struct drbd_connection *connection = peer_device->connection;
-	struct bio_vec bvec;
-	struct bvec_iter iter;
-
-	/* Flush send buffer and make sure PAGE_SIZE is available... */
-	alloc_send_buffer(connection, PAGE_SIZE, DATA_STREAM);
-	connection->send_buffer[DATA_STREAM].allocated_size = 0;
-
-	/* hint all but last page with MSG_MORE */
-	bio_for_each_segment(bvec, bio, iter) {
-		int err;
-
-		err = _drbd_no_send_page(peer_device, bvec.bv_page,
-					 bvec.bv_offset, bvec.bv_len,
-					 bio_iter_last(bvec, iter) ? 0 : MSG_MORE);
-		if (err)
-			return err;
-
-		peer_device->send_cnt += bvec.bv_len >> 9;
-	}
-	return 0;
-}
-
-static int _drbd_send_zc_bio(struct drbd_peer_device *peer_device, struct bio *bio)
-{
-	struct bio_vec bvec;
-	struct bvec_iter iter;
-	bool no_zc = drbd_disable_sendpage;
-
-	/* e.g. XFS meta- & log-data is in slab pages, which have a
-	 * page_count of 0 and/or have PageSlab() set.
-	 * we cannot use send_page for those, as that does get_page();
-	 * put_page(); and would cause either a VM_BUG directly, or
-	 * __page_cache_release a page that would actually still be referenced
-	 * by someone, leading to some obscure delayed Oops somewhere else. */
-	if (!no_zc)
 		bio_for_each_segment(bvec, bio, iter) {
 			struct page *page = bvec.bv_page;
 
 			if (!sendpage_ok(page)) {
-				no_zc = true;
+				msg_flags &= ~MSG_SPLICE_PAGES;
 				break;
 			}
 		}
-
-	if (no_zc) {
-		return _drbd_send_bio(peer_device, bio);
-	} else {
-		struct drbd_connection *connection = peer_device->connection;
-		struct drbd_transport *transport = &connection->transport;
-		struct drbd_transport_ops *tr_ops = &transport->class->ops;
-		int err;
-
-		flush_send_buffer(connection, DATA_STREAM);
-
-		err = tr_ops->send_zc_bio(transport, bio);
-		if (!err)
-			peer_device->send_cnt += bio->bi_iter.bi_size >> 9;
-
-		return err;
 	}
+
+	flush_send_buffer(connection, DATA_STREAM);
+
+	err = tr_ops->send_bio(transport, bio, msg_flags);
+	if (!err)
+		peer_device->send_cnt += bio->bi_iter.bi_size >> 9;
+
+	return err;
 }
 
 static int _drbd_send_zc_ee(struct drbd_peer_device *peer_device,
 			    struct drbd_peer_request *peer_req)
 {
-	bool use_sendpage = !(peer_req->flags & EE_RELEASE_TO_MEMPOOL);
+	struct drbd_transport *transport = &peer_device->connection->transport;
+	struct drbd_transport_ops *tr_ops = &transport->class->ops;
+	unsigned int msg_flags = peer_req->flags & EE_RELEASE_TO_MEMPOOL ? 0 : MSG_SPLICE_PAGES;
 	struct page *page = peer_req->page_chain.head;
 	unsigned len = peer_req->i.size;
 	int err;
@@ -2464,12 +2381,8 @@ static int _drbd_send_zc_ee(struct drbd_peer_device *peer_device,
 				page, page_chain_offset(page), page_chain_size(page));
 		}
 
-		if (likely(use_sendpage))
-			err = _drbd_send_page(peer_device, page, 0, l,
-					      page_chain_next(page) ? MSG_MORE : 0);
-		else
-			err = _drbd_no_send_page(peer_device, page, 0, l,
-						 page_chain_next(page) ? MSG_MORE : 0);
+		err = tr_ops->send_page(transport, DATA_STREAM, page, 0, l,
+					msg_flags | (page_chain_next(page) ? MSG_MORE : 0));
 		if (err)
 			return err;
 		len -= l;
@@ -2568,10 +2481,10 @@ int drbd_send_dblock(struct drbd_peer_device *peer_device, struct drbd_request *
 		 * out ok after sending on this side, but does not fit on the
 		 * receiving side, we sure have detected corruption elsewhere.
 		 */
-		if (!(s & (RQ_EXP_RECEIVE_ACK | RQ_EXP_WRITE_ACK)) || digest_size)
-			err = _drbd_send_bio(peer_device, req->master_bio);
-		else
-			err = _drbd_send_zc_bio(peer_device, req->master_bio);
+		bool proto_b_or_c = (s & (RQ_EXP_RECEIVE_ACK | RQ_EXP_WRITE_ACK));
+		int msg_flags =  proto_b_or_c && !digest_size ? MSG_SPLICE_PAGES : 0;
+
+		err = drbd_send_bio(peer_device, req->master_bio, msg_flags);
 
 		/* double check digest, sometimes buffers have been modified in flight. */
 		if (digest_size > 0) {

drbd/drbd_transport_lb-tcp.c
@@ -139,7 +139,7 @@ static void dtl_set_rcvtimeo(struct drbd_transport *transport, enum drbd_stream 
 static long dtl_get_rcvtimeo(struct drbd_transport *transport, enum drbd_stream stream);
 static int dtl_send_page(struct drbd_transport *transport, enum drbd_stream, struct page *page,
 		int offset, size_t size, unsigned int msg_flags);
-static int dtl_send_zc_bio(struct drbd_transport *, struct bio *bio);
+static int dtl_send_bio(struct drbd_transport *, struct bio *bio, unsigned int msg_flags);
 static bool dtl_stream_ok(struct drbd_transport *transport, enum drbd_stream stream);
 static bool dtl_hint(struct drbd_transport *transport, enum drbd_stream stream,
 		     enum drbd_tr_hints hint);
@@ -179,7 +179,7 @@ static struct drbd_transport_class dtl_transport_class = {
 		.set_rcvtimeo = dtl_set_rcvtimeo,
 		.get_rcvtimeo = dtl_get_rcvtimeo,
 		.send_page = dtl_send_page,
-		.send_zc_bio = dtl_send_zc_bio,
+		.send_bio = dtl_send_bio,
 		.stream_ok = dtl_stream_ok,
 		.hint = dtl_hint,
 		.debugfs_show = dtl_debugfs_show,
@@ -1631,7 +1631,7 @@ static int dtl_select_send_flow(struct dtl_transport *dtl_transport,
 static int _dtl_send_page(struct dtl_transport *dtl_transport, struct dtl_flow *flow,
 			  struct page *page, int offset, size_t size, unsigned int msg_flags)
 {
-	struct msghdr msg = { .msg_flags = msg_flags | MSG_NOSIGNAL | MSG_SPLICE_PAGES };
+	struct msghdr msg = { .msg_flags = msg_flags | MSG_NOSIGNAL };
 	struct drbd_transport *transport = &dtl_transport->transport;
 	struct socket *sock = flow->sock;
 	struct bio_vec bvec;
@@ -1716,7 +1716,7 @@ static int dtl_bio_chunk_size_available(struct bio *bio, int wmem_available,
 }
 
 static int dtl_send_bio_pages(struct dtl_transport *dtl_transport, struct dtl_flow *flow,
-		struct bio *bio, struct bvec_iter *iter, int chunk)
+			struct bio *bio, struct bvec_iter *iter, int chunk, unsigned int msg_flags)
 {
 	struct bio_vec bvec;
 
@@ -1726,7 +1726,7 @@ static int dtl_send_bio_pages(struct dtl_transport *dtl_transport, struct dtl_fl
 		bvec = bio_iter_iovec(bio, *iter);
 		err = _dtl_send_page(dtl_transport, flow, bvec.bv_page,
 				bvec.bv_offset, bvec.bv_len,
-				bio_iter_last(bvec, *iter) ? 0 : MSG_MORE);
+				msg_flags | (bio_iter_last(bvec, *iter) ? 0 : MSG_MORE));
 		if (err)
 			return err;
 		chunk -= bvec.bv_len;
@@ -1736,7 +1736,8 @@ static int dtl_send_bio_pages(struct dtl_transport *dtl_transport, struct dtl_fl
 	return 0;
 }
 
-static int dtl_send_zc_bio(struct drbd_transport *transport, struct bio *bio)
+static int dtl_send_bio(struct drbd_transport *transport, struct bio *bio,
+			   unsigned int msg_flags)
 {
 	struct dtl_transport *dtl_transport =
 		container_of(transport, struct dtl_transport, transport);
@@ -1777,7 +1778,7 @@ static int dtl_send_zc_bio(struct drbd_transport *transport, struct bio *bio)
 				goto out;
 		}
 
-		err = dtl_send_bio_pages(dtl_transport, flow, bio, &iter, chunk);
+		err = dtl_send_bio_pages(dtl_transport, flow, bio, &iter, chunk, msg_flags);
 		if (err)
 			goto out;
 	} while (iter.bi_size);

drbd/drbd_transport_rdma.c
@@ -322,7 +322,7 @@ static void dtr_set_rcvtimeo(struct drbd_transport *transport, enum drbd_stream 
 static long dtr_get_rcvtimeo(struct drbd_transport *transport, enum drbd_stream stream);
 static int dtr_send_page(struct drbd_transport *transport, enum drbd_stream stream, struct page *page,
 		int offset, size_t size, unsigned msg_flags);
-static int dtr_send_zc_bio(struct drbd_transport *, struct bio *bio);
+static int dtr_send_bio(struct drbd_transport *, struct bio *bio, unsigned int msg_flags);
 static int dtr_recv_pages(struct drbd_transport *transport, struct drbd_page_chain_head *chain, size_t size);
 static bool dtr_stream_ok(struct drbd_transport *transport, enum drbd_stream stream);
 static bool dtr_hint(struct drbd_transport *transport, enum drbd_stream stream, enum drbd_tr_hints hint);
@@ -392,7 +392,7 @@ static struct drbd_transport_class rdma_transport_class = {
 		.set_rcvtimeo = dtr_set_rcvtimeo,
 		.get_rcvtimeo = dtr_get_rcvtimeo,
 		.send_page = dtr_send_page,
-		.send_zc_bio = dtr_send_zc_bio,
+		.send_bio = dtr_send_bio,
 		.recv_pages = dtr_recv_pages,
 		.stream_ok = dtr_stream_ok,
 		.hint = dtr_hint,
@@ -3195,11 +3195,12 @@ static void dtr_update_congested(struct drbd_transport *transport)
 }
 
 static int dtr_send_page(struct drbd_transport *transport, enum drbd_stream stream,
-			 struct page *page, int offset, size_t size, unsigned msg_flags)
+			 struct page *caller_page, int offset, size_t size, unsigned int msg_flags)
 {
 	struct dtr_transport *rdma_transport =
 		container_of(transport, struct dtr_transport, transport);
 	struct dtr_tx_desc *tx_desc;
+	struct page *page;
 	int err;
 
 	// pr_info("%s: in send_page, size: %zu\n", rdma_stream->name, size);
@@ -3211,7 +3212,19 @@ static int dtr_send_page(struct drbd_transport *transport, enum drbd_stream stre
 	if (!tx_desc)
 		return -ENOMEM;
 
-	get_page(page); /* The put_page() is in dtr_tx_cq_event_handler() */
+	if (msg_flags & MSG_SPLICE_PAGES) {
+		page = caller_page;
+		get_page(page); /* The put_page() is in dtr_tx_cq_event_handler() */
+	} else {
+		void *from;
+
+		page = drbd_alloc_pages(transport, 1, GFP_NOIO);
+		from = kmap_local_page(caller_page);
+		memcpy(page_address(page), from + offset, size);
+		kunmap_local(from);
+		offset = 0;
+	}
+
 	tx_desc->type = SEND_PAGE;
 	tx_desc->page = page;
 	tx_desc->nr_sges = 1;
@@ -3318,7 +3331,7 @@ static int dtr_send_bio_part(struct dtr_transport *rdma_transport,
 }
 #endif
 
-static int dtr_send_zc_bio(struct drbd_transport *transport, struct bio *bio)
+static int dtr_send_bio(struct drbd_transport *transport, struct bio *bio, unsigned int msg_flags)
 {
 #if SENDER_COMPACTS_BVECS
 	struct dtr_transport *rdma_transport =
@@ -3336,6 +3349,7 @@ static int dtr_send_zc_bio(struct drbd_transport *transport, struct bio *bio)
 		return -ECONNRESET;
 
 #if SENDER_COMPACTS_BVECS
+	/* TODO obey !MSG_SPLICE_PAGES in msg_flags */
 	bio_for_each_segment(bvec, bio, iter) {
 		size_tx_desc += bvec.bv_len;
 		//tr_info(transport, " bvec len = %d\n", bvec.bv_len);
@@ -3365,8 +3379,7 @@ out:
 #else
 	bio_for_each_segment(bvec, bio, iter) {
 		err = dtr_send_page(transport, DATA_STREAM,
-			bvec.bv_page, bvec.bv_offset, bvec.bv_len,
-			0 /* flags currently unused by dtr_send_page */);
+			bvec.bv_page, bvec.bv_offset, bvec.bv_len, msg_flags);
 		if (err)
 			break;
 	}

commit dd894df9a8bc82269331891ef16ebbb691cbd66b

drbd: tcp: copy instead of referencing pages on TLS connections
With "net { tls yes; }" the data socket runs the kTLS software send
path (tls_sw_sendmsg). DRBD hands it the application's bio pages, either
via MSG_SPLICE_PAGES or as a plain bvec iterator. In both cases tls_sw
takes references on those pages while building its own encrypted
records, and on this path the references are not reliably dropped again.
The leak accumulates on frequently reused pages (e.g. an XFS log buffer)
and only surfaces much later as a "Bad page state ... nonzero _refcount"
warning when the page's real owner frees it. It is easily reproduced by
running an XFS filesystem on top of a TLS-protected DRBD device: writing
and unmounting trips the warning during XFS log handling.

There is nothing to gain from zero-copy on a TLS connection anyway: kTLS
always copies the payload into freshly allocated records to encrypt it.

Fix this by sending bio data through a mapped kernel address (a kvec)
when the connection uses TLS. A kvec iterator forces tls_sw onto its
copy path, so it never references our pages. MSG_SPLICE_PAGES is dropped
for the same reason. Non-TLS connections keep using the zero-copy bvec
path unchanged. TLS cannot be switched while connected, so net_conf->tls
is stable and can be read directly in the send path.

Fixes: eed6170cc450 ("drbd: Rename send_zc_bio() to send_bio(, MSG_SPLICE_PAGES)")
Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>

drbd/drbd-kernel-compat/cocci/iter_source__no_present.cocci
@@ -1,0 +1,15 @@
+// The iov_iter direction initializers ITER_SOURCE / ITER_DEST were added in
+// v6.2 by commit de4eda9de2d9 ("use less confusing names for iov_iter
+// direction initializers"). Before that, the raw WRITE / READ values were
+// used. Provide them for files that still reference ITER_SOURCE (e.g. the TLS
+// copy path in dtt_send_page()).
+
+@@
+@@
+- ITER_SOURCE
++ WRITE
+
+@@
+@@
+- ITER_DEST
++ READ

drbd/drbd-kernel-compat/cocci/sendpage__yes_present.cocci
@@ -50,17 +50,27 @@ dtl_send_bio_pages(..., unsigned int msg_flags)
 }
 
 
+// Rewrite the bvec-based sock_sendmsg() into a sendpage() call. There may be
+// other (kvec-based) sock_sendmsg() calls in the same function, e.g. the TLS
+// copy path in dtt_send_page(), so match the bvec sequence on its own rather
+// than anchoring on surrounding statements. Match the iter direction as a
+// metavariable so this rule does not depend on whether the ITER_SOURCE ->
+// WRITE rewrite (iter_source__no_present.cocci) has already been applied.
 @@
-identifier bvec, page, len, offset, msg, socket;
-identifier sent = sent;
+expression page, len, offset, dir;
+identifier msg, socket, sent, bvec;
 @@
-- struct bio_vec bvec;
-  ...
-  int sent;
 - bvec_set_page(&bvec, page, len, offset);
-- iov_iter_bvec(&msg.msg_iter, ITER_SOURCE, &bvec, 1, len);
+- iov_iter_bvec(&msg.msg_iter, dir, &bvec, 1, len);
 - sent = sock_sendmsg(socket, &msg);
 + sent = socket->ops->sendpage(socket, page, offset, len, msg.msg_flags);
+
+// Drop the bio_vec declaration left unused by the rewrite above.
+@@
+identifier bvec;
+@@
+- struct bio_vec bvec;
+  ... when != bvec
 
 
 @ define_msg_splice_pages depends on find_msg_splice_pages || dtt_send_bio_rule @

drbd/drbd-kernel-compat/gen_patch_names.c
@@ -130,6 +130,9 @@ int main(int argc, char **argv)
 	patch(1, "sendpage", false, true,
 	      COMPAT_HAVE_SENDPAGE, "present");
 
+	patch(1, "iter_source", true, false,
+	      COMPAT_HAVE_ITER_SOURCE, "present");
+
 	patch(1, "ib_device", true, false,
 	      COMPAT_IB_DEVICE_HAS_OPS, "has_ops");
 

drbd/drbd_transport_tcp.c
@@ -1516,22 +1516,50 @@ static int dtt_send_page(struct drbd_transport *transport, enum drbd_stream stre
 	struct drbd_tcp_transport *tcp_transport =
 		container_of(transport, struct drbd_tcp_transport, transport);
 	struct socket *socket = tcp_transport->stream[stream];
-	struct msghdr msg = { .msg_flags = msg_flags | MSG_NOSIGNAL };
+	struct net_conf *nc;
+	struct msghdr msg;
 	struct bio_vec bvec;
 	int len = size;
 	int err = -EIO;
+	bool tls;
 
 	if (!socket)
 		return -ENOTCONN;
+
+	rcu_read_lock();
+	nc = rcu_dereference(transport->net_conf);
+	tls = nc && nc->tls;
+	rcu_read_unlock();
+
+	/* With kTLS the payload is encrypted into freshly allocated TLS records,
+	 * so there is no zero-copy to preserve. Doing so would cause page ref bugs.
+	 */
+	if (tls)
+		msg_flags &= ~MSG_SPLICE_PAGES;
+
+	msg = (struct msghdr) { .msg_flags = msg_flags | MSG_NOSIGNAL };
 
 	dtt_update_congested(tcp_transport);
 	do {
 		int sent;
 
-		bvec_set_page(&bvec, page, len, offset);
-		iov_iter_bvec(&msg.msg_iter, ITER_SOURCE, &bvec, 1, len);
+		if (tls) {
+			/* Map and send at most one page at a time. */
+			struct page *p = page + (offset >> PAGE_SHIFT);
+			unsigned int poff = offset & ~PAGE_MASK;
+			unsigned int chunk = min_t(int, len, PAGE_SIZE - poff);
+			void *addr = kmap_local_page(p);
+			struct kvec iov = { .iov_base = addr + poff, .iov_len = chunk };
 
-		sent = sock_sendmsg(socket, &msg);
+			iov_iter_kvec(&msg.msg_iter, ITER_SOURCE, &iov, 1, chunk);
+			sent = sock_sendmsg(socket, &msg);
+			kunmap_local(addr);
+		} else {
+			bvec_set_page(&bvec, page, len, offset);
+			iov_iter_bvec(&msg.msg_iter, ITER_SOURCE, &bvec, 1, len);
+
+			sent = sock_sendmsg(socket, &msg);
+		}
 		if (sent <= 0) {
 			if (sent == -EAGAIN) {
 				if (drbd_stream_send_timed_out(transport, stream))

drbd/drbd-kernel-compat/tests/have_iter_source.c
@@ -1,0 +1,8 @@
+/* { "version": "v6.2-rc1", "commit": "de4eda9de2d957ef2d6a8365a01e26a435e958cb", "comment": "iov_iter direction initializers ITER_SOURCE/ITER_DEST were added", "author": "Al Viro <viro@zeniv.linux.org.uk>", "date": "Thu Sep 15 20:25:47 2022 -0400" } */
+
+#include <linux/uio.h>
+
+int foo(void)
+{
+	return ITER_SOURCE;
+}