2346732efc3c6b092076ae50f9c911ff40e142c0
Johannes Khoshnazar-Thoma <johannes@johannesthoma.com>
2026-06-26 13:50:02 +0200

    Call FINALIZE_WORK function whenever a work_struct is (implicitly) freed.
    We cannot kref the work_struct because they are embedded inside
    another struct (drbd_resource, drbd_connection, drbd_device).
    On the other hand we need to free the work_struct_internals,
    which are kref'ed. Therefore we need to know when the work_struct's
    are freed. The FINALIZE_WORK macro just makes an extra kref_put
    on the associated work_struct_internal. Also we want to make sure
    that the INIT_WORK is called only once for each work_struct.

--- drbd/drbd_transport_tcp.c
+++ drbd/drbd_transport_tcp.c
@@ -158,6 +158,8 @@ static struct drbd_transport_class tcp_transport_class = {
 	.list = LIST_HEAD_INIT(tcp_transport_class.list),
 };
 
+static void dtt_control_data_ready_work(struct work_struct *item);
+
 static int dtt_init(struct drbd_transport *transport)
 {
 	struct drbd_tcp_transport *tcp_transport =
@@ -175,6 +177,12 @@ static int dtt_init(struct drbd_transport *transport)
 		tcp_transport->rbuf[i].pos = buffer;
 	}
 	timer_setup(&tcp_transport->control_timer, dtt_control_timer_fn, 0);
+
+	/* Do that here. INIT_WORK will allocate memory and should not
+	 * be called multiple times. Also see dtt_free() for the corresponding
+	 * FINIALIZE_WORK.
+	 */
+	INIT_WORK(&tcp_transport->control_data_ready_work, dtt_control_data_ready_work);
 
 	return 0;
 fail:
@@ -225,6 +233,7 @@ static void dtt_free(struct drbd_transport *transport, enum drbd_tr_free_op free
 			free_page((unsigned long)tcp_transport->rbuf[i].base);
 			tcp_transport->rbuf[i].base = NULL;
 		}
+		FINALIZE_WORK(&tcp_transport->control_data_ready_work);
 	}
 }
 
@@ -1417,7 +1426,6 @@ randomize:
 		INIT_WORK(&tcp_transport->control_data_ready_work, dtt_control_data_ready_work);
 #endif
 	}
-	INIT_WORK(&tcp_transport->control_data_ready_work, dtt_control_data_ready_work);
 
 	TR_ASSERT(transport, first_path == connect_to_path);
 	set_bit(TR_ESTABLISHED, &connect_to_path->path.flags);
