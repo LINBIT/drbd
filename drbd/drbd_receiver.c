LINBIT/drbd

commit a9c85cf583fcc81237756caf4abf293718cd4fa9

drbd: Improve setting up request queue limits on diskless nodes
So far, we have failed to do anything useful with the queue limits on
diskless nodes. Moving forward, we will have physical and logical
block sizes right.

Well, as long as we connect to the diskfull node with the largest
physical sector size before promoting to primary.

So far, we called blk_set_stacking_limits() followed by a
blk_stack_limits() while establishing a new connection. That opened a
short window in which the block subsystem might create too large BIOs.

drbd/drbd_receiver.c
@@ -5196,7 +5196,6 @@ static int receive_sizes(struct drbd_connection *connection, struct packet_info 
 	struct drbd_peer_device *peer_device, *peer_device_it = NULL;
 	struct drbd_device *device;
 	struct p_sizes *p = pi->data;
-	struct o_qlim *o = (connection->agreed_features & DRBD_FF_WSAME) ? p->qlim : NULL;
 	uint64_t p_size, p_usize, p_csize;
 	uint64_t my_usize, my_max_size, cur_size;
 	enum determine_dev_size dd = DS_UNCHANGED;
@@ -5337,13 +5336,23 @@ static int receive_sizes(struct drbd_connection *connection, struct packet_info 
 		}
 	}
 
+	if (connection->agreed_features & DRBD_FF_WSAME) {
+		struct o_qlim *qlim = p->qlim;
+
+		peer_device->q_limits.physical_block_size = be32_to_cpu(qlim->physical_block_size);
+		peer_device->q_limits.logical_block_size = be32_to_cpu(qlim->logical_block_size);
+		peer_device->q_limits.alignment_offset = be32_to_cpu(qlim->alignment_offset);
+		peer_device->q_limits.io_min = be32_to_cpu(qlim->io_min);
+		peer_device->q_limits.io_opt = be32_to_cpu(qlim->io_opt);
+	}
+
 	/* Leave drbd_reconsider_queue_parameters() before drbd_determine_dev_size().
 	   In case we cleared the QUEUE_FLAG_DISCARD from our queue in
 	   drbd_reconsider_queue_parameters(), we can be sure that after
 	   drbd_determine_dev_size() no REQ_OP_DISCARDs are in the queue. */
 	if (have_ldev) {
 		enum dds_flags local_ddsf = ddsf;
-		drbd_reconsider_queue_parameters(device, device->ldev, o);
+		drbd_reconsider_queue_parameters(device, device->ldev);
 
 		/* To support thinly provisioned nodes (partial resync) joining later,
 		   clear all bitmap slots, including the unused ones. */
@@ -5363,7 +5372,7 @@ static int receive_sizes(struct drbd_connection *connection, struct packet_info 
 	} else {
 		uint64_t new_size = 0;
 
-		drbd_reconsider_queue_parameters(device, NULL, o);
+		drbd_reconsider_queue_parameters(device, NULL);
 		/* In case I am diskless, need to accept the peer's *current* size.
 		 *
 		 * At this point, the peer knows more about my disk, or at

drbd/drbd-kernel-compat/cocci/blk_queue_update_readahead__no_present.cocci
@@ -1,7 +1,8 @@
 @@
+expression e;
 struct request_queue *q, b;
 @@
-blk_stack_limits(&q->limits, &b->limits, 0);
+blk_stack_limits(e, &b->limits, 0);
 - blk_queue_update_readahead(q);
 + if (q->backing_dev_info->ra_pages !=
 +     b->backing_dev_info->ra_pages) {

drbd/drbd_int.h
@@ -1289,6 +1289,13 @@ struct drbd_peer_device {
 		bool was_ahead;
 	} todo;
 	union drbd_state connect_state;
+	struct {
+		unsigned int	physical_block_size;
+		unsigned int	logical_block_size;
+		unsigned int	alignment_offset;
+		unsigned int	io_min;
+		unsigned int	io_opt;
+	} q_limits;
 };
 
 struct submit_worker {
@@ -1914,7 +1921,7 @@ drbd_determine_dev_size(struct drbd_device *, sector_t peer_current_size,
 			enum dds_flags, struct resize_parms *) __must_hold(local);
 extern void resync_after_online_grow(struct drbd_peer_device *);
 extern void drbd_reconsider_queue_parameters(struct drbd_device *device,
-			struct drbd_backing_dev *bdev, struct o_qlim *o);
+			struct drbd_backing_dev *bdev);
 extern bool barrier_pending(struct drbd_resource *resource);
 extern enum drbd_state_rv drbd_set_role(struct drbd_resource *, enum drbd_role, bool, struct sk_buff *);
 extern bool conn_try_outdate_peer(struct drbd_connection *connection);

drbd/drbd_main.c
@@ -3691,6 +3691,12 @@ struct drbd_peer_device *create_peer_device(struct drbd_device *device, struct d
 	peer_device->resync_wenr = LC_FREE;
 	peer_device->resync_finished_pdsk = D_UNKNOWN;
 
+	peer_device->q_limits.physical_block_size = SECTOR_SIZE;
+	peer_device->q_limits.logical_block_size = SECTOR_SIZE;
+	peer_device->q_limits.alignment_offset = 0;
+	peer_device->q_limits.io_min = SECTOR_SIZE;
+	peer_device->q_limits.io_opt = PAGE_SIZE;
+
 	return peer_device;
 }
 

drbd/drbd_nl.c
@@ -1997,16 +1997,35 @@ static void fixup_discard_support(struct drbd_device *device, struct request_que
 }
 
 static void drbd_setup_queue_param(struct drbd_device *device, struct drbd_backing_dev *bdev,
-				   unsigned int max_bio_size, struct o_qlim *o)
+				   unsigned int max_bio_size)
 {
 	struct request_queue * const q = device->rq_queue;
 	unsigned int max_hw_sectors = max_bio_size >> 9;
+	struct queue_limits common_limits = { 0 }; /* sizeof(struct queue_limits) ~ 110 bytes */
+	struct queue_limits peer_limits = { 0 };
+	struct drbd_peer_device *peer_device;
 	struct request_queue *b = NULL;
+
+	blk_set_stacking_limits(&common_limits);
+
+	rcu_read_lock();
+	for_each_peer_device_rcu(peer_device, device) {
+		if (!test_bit(HAVE_SIZES, &peer_device->flags) &&
+		    peer_device->repl_state[NOW] < L_ESTABLISHED)
+			continue;
+		blk_set_stacking_limits(&peer_limits);
+		peer_limits.logical_block_size = peer_device->q_limits.logical_block_size;
+		peer_limits.physical_block_size = peer_device->q_limits.physical_block_size;
+		peer_limits.alignment_offset = peer_device->q_limits.alignment_offset;
+		peer_limits.io_min = peer_device->q_limits.io_min;
+		peer_limits.io_opt = peer_device->q_limits.io_opt;
+		blk_stack_limits(&common_limits, &peer_limits, 0);
+	}
+	rcu_read_unlock();
 
 	if (bdev) {
 		b = bdev->backing_bdev->bd_disk->queue;
 		max_hw_sectors = min(queue_max_hw_sectors(b), max_bio_size >> 9);
-		blk_set_stacking_limits(&q->limits);
 	}
 
 	blk_queue_max_hw_sectors(q, max_hw_sectors);
@@ -2015,14 +2034,15 @@ static void drbd_setup_queue_param(struct drbd_device *device, struct drbd_backi
 	decide_on_discard_support(device, bdev);
 
 	if (b) {
-		blk_stack_limits(&q->limits, &b->limits, 0);
+		blk_stack_limits(&common_limits, &b->limits, 0);
 		disk_update_readahead(device->vdisk);
 	}
+	q->limits = common_limits;
 	fixup_write_zeroes(device, q);
 	fixup_discard_support(device, q);
 }
 
-void drbd_reconsider_queue_parameters(struct drbd_device *device, struct drbd_backing_dev *bdev, struct o_qlim *o)
+void drbd_reconsider_queue_parameters(struct drbd_device *device, struct drbd_backing_dev *bdev)
 {
 	unsigned int max_bio_size = device->device_conf.max_bio_size;
 	struct drbd_peer_device *peer_device;
@@ -2039,7 +2059,7 @@ void drbd_reconsider_queue_parameters(struct drbd_device *device, struct drbd_ba
 	}
 	read_unlock_irq(&device->resource->state_rwlock);
 
-	drbd_setup_queue_param(device, bdev, max_bio_size, o);
+	drbd_setup_queue_param(device, bdev, max_bio_size);
 }
 
 /* Make sure IO is suspended before calling this function(). */
@@ -2287,7 +2307,7 @@ int drbd_adm_disk_opts(struct sk_buff *skb, struct genl_info *info)
 
 	if (old_disk_conf->discard_zeroes_if_aligned !=
 	    new_disk_conf->discard_zeroes_if_aligned)
-		drbd_reconsider_queue_parameters(device, device->ldev, NULL);
+		drbd_reconsider_queue_parameters(device, device->ldev);
 
 	drbd_md_sync_if_dirty(device);
 
@@ -3230,7 +3250,7 @@ int drbd_adm_attach(struct sk_buff *skb, struct genl_info *info)
 	device->read_cnt = 0;
 	device->writ_cnt = 0;
 
-	drbd_reconsider_queue_parameters(device, device->ldev, NULL);
+	drbd_reconsider_queue_parameters(device, device->ldev);
 
 	/* If I am currently not R_PRIMARY,
 	 * but meta data primary indicator is set,

commit 2333e8404ffd09068991fa486995830773eb13e0

drbd: reject a peer's bogus block sizes
A peer that announces a physical_block_size below one sector crashes the
receiver:

  divide error: 0000 [#1] PREEMPT SMP PTI
  RIP: 0010:blk_stack_limits+0x1c8/0x550
   get_common_queue_limits+0xff/0x140 [drbd]
   receive_sizes+0x4d5/0xd50 [drbd]

get_common_queue_limits() stacks the P_SIZES values onto the device's own
limits. blk_stack_limits() divides by max(physical_block_size, io_min) >>
SECTOR_SHIFT and trusts its caller to pass valid block sizes.

Fix this by holding the announced block sizes to the block layer's own
rule when P_SIZES arrives: disconnect unless each is a power of two of at
least one sector. io_min, io_opt and alignment_offset stay unchecked: none
can make the divisor zero once physical_block_size is valid, and a peer
may announce zero for them legitimately. Clamping instead of rejecting
would hand the device limits no peer ever announced.

Fixes: a9c85cf583fc ("drbd: Improve setting up request queue limits on diskless nodes")
Signed-off-by: Lars Ellenberg <lars.ellenberg@linbit.com>

drbd/drbd_receiver.c
@@ -6186,6 +6186,33 @@ static unsigned int conn_max_bio_size(struct drbd_connection *connection)
 		return DRBD_MAX_SIZE_H80_PACKET;
 }
 
+static bool is_valid_block_size(unsigned int size)
+{
+	return size >= SECTOR_SIZE && size <= DRBD_MAX_BIO_SIZE && is_power_of_2(size);
+}
+
+static bool peer_block_sizes_are_sane(struct drbd_peer_device *peer_device, struct p_sizes *p)
+{
+	unsigned int logical_block_size, physical_block_size;
+
+	/* Older peers do not send queue limits at all. */
+	if (!(peer_device->connection->agreed_features & DRBD_FF_WSAME))
+		return true;
+
+	logical_block_size = be32_to_cpu(p->qlim->logical_block_size);
+	physical_block_size = be32_to_cpu(p->qlim->physical_block_size);
+
+	if (!is_valid_block_size(logical_block_size) ||
+	    !is_valid_block_size(physical_block_size)) {
+		drbd_err(peer_device,
+			 "Peer sent bogus block sizes (logical:%u physical:%u), disconnecting\n",
+			 logical_block_size, physical_block_size);
+		return false;
+	}
+
+	return true;
+}
+
 static struct drbd_peer_device *get_neighbor_device(struct drbd_device *device,
 		enum drbd_neighbor neighbor)
 {
@@ -6268,6 +6295,9 @@ static int receive_sizes(struct drbd_connection *connection, struct packet_info 
 		goto out;
 	}
 	have_mutex = true;
+
+	if (!peer_block_sizes_are_sane(peer_device, p))
+		goto disconnect;
 
 	/* just store the peer's disk size for now.
 	 * we still need to figure out whether we accept that. */