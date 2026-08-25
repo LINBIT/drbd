LINBIT/drbd

commit 2289b548ed5d37869e458bd617902b8548b689e5

drbd: introduce RCU on the transfer log

drbd/drbd_debugfs.c
@@ -359,25 +359,29 @@ static void seq_print_resource_transfer_log_summary(struct seq_file *m,
 
 	seq_puts(m, "n\tdevice\tvnr\t" RQ_HDR);
 	spin_lock_irq(&resource->req_lock);
-	list_for_each_entry(req, &resource->transfer_log, tl_requests) {
+	rcu_read_lock();
+	list_for_each_entry_rcu(req, &resource->transfer_log, tl_requests) {
 		struct drbd_device *device = req->device;
 		struct drbd_peer_device *peer_device;
 		unsigned int tmp = 0;
 		unsigned int s;
 		++count;
 
-		/* don't disable irq "forever" */
+		/* don't disable preemption "forever" */
 		if (!(count & 0x1ff)) {
-			struct drbd_request *req_next;
+			struct list_head *next_hdr;
 			kref_get(&req->kref);
-			spin_unlock_irq(&resource->req_lock);
+			rcu_read_unlock();
 			cond_resched();
-			spin_lock_irq(&resource->req_lock);
-			req_next = list_next_entry(req, tl_requests);
-			if (kref_put(&req->kref, drbd_req_destroy))
-				req = req_next;
-			if (&req->tl_requests == &resource->transfer_log)
-				break;
+			rcu_read_lock();
+			next_hdr = list_next_rcu(&req->tl_requests);
+			if (kref_put(&req->kref, drbd_req_destroy)) {
+				if (next_hdr == &resource->transfer_log)
+					break;
+				req = list_entry_rcu(next_hdr,
+						     struct drbd_request,
+						     tl_requests);
+			}
 		}
 
 		s = req->local_rq_state;
@@ -392,7 +396,7 @@ static void seq_print_resource_transfer_log_summary(struct seq_file *m,
 			tmp |= 2;
 
 		for_each_peer_device(peer_device, device) {
-			s = req->net_rq_state[peer_device->node_id];
+			s = READ_ONCE(req->net_rq_state[peer_device->node_id]);
 			if (s & RQ_NET_MASK) {
 				if (!(s & RQ_NET_SENT))
 					tmp |= 4;
@@ -410,6 +414,7 @@ static void seq_print_resource_transfer_log_summary(struct seq_file *m,
 		if (show_state == 0x1f)
 			break;
 	}
+	rcu_read_unlock();
 	spin_unlock_irq(&resource->req_lock);
 }
 

drbd/drbd_int.h
@@ -382,6 +382,7 @@ struct drbd_request {
 	spinlock_t rq_lock;
 	unsigned int local_rq_state;
 	u16 net_rq_state[DRBD_NODE_ID_MAX];
+	struct rcu_head rcu;
 };
 
 struct drbd_epoch {
@@ -867,6 +868,7 @@ struct drbd_resource {
 					 * &drbd_request */
 	unsigned long flags;
 
+	spinlock_t tl_update_lock; /* this lock for updating, RCU ...*/
 	struct list_head transfer_log;	/* all requests not yet fully processed */
 	struct drbd_request *tl_previous_write;
 

drbd/drbd_main.c
@@ -315,9 +315,7 @@ struct drbd_peer_device *__drbd_next_peer_device_ref(u64 *visited,
 	     req = __tl_next_req_ref(&next, req, tl))
 
 #define tl_for_each_req_ref(req, next, tl)				\
-	for (req = __tl_first_req_ref_continue(&next,			\
-	list_first_entry_or_null(tl, struct drbd_request, tl_requests), \
-				      tl);				\
+	for (req = __tl_first_req_ref(&next, tl);			\
 	     req;							\
 	     req = __tl_next_req_ref(&next, req, tl))
 
@@ -326,11 +324,29 @@ static struct drbd_request *__tl_first_req_ref_continue(struct drbd_request **pn
 							struct list_head *transfer_log)
 {
 	if (req) {
-		struct drbd_request *next = list_next_entry(req, tl_requests);
-		if (&next->tl_requests != transfer_log)
+		struct list_head *next_head;
+		struct drbd_request *next;
+
+		rcu_read_lock();
+		next_head = list_next_rcu(&req->tl_requests);
+		next = list_entry_rcu(next_head, struct drbd_request, tl_requests);
+		if (next_head != transfer_log)
 			kref_get(&next->kref);
+		rcu_read_unlock();
 		*pnext = next;
 	}
+	return req;
+}
+
+static struct drbd_request *__tl_first_req_ref(struct drbd_request **pnext,
+					       struct list_head *transfer_log)
+{
+	struct drbd_request *req;
+	rcu_read_lock();
+	req = __tl_first_req_ref_continue(pnext,
+		list_first_or_null_rcu(transfer_log, struct drbd_request, tl_requests),
+		transfer_log);
+	rcu_read_unlock();
 	return req;
 }
 
@@ -338,18 +354,24 @@ static struct drbd_request *__tl_next_req_ref(struct drbd_request **pnext,
 					      struct drbd_request *req,
 					      struct list_head *transfer_log)
 {
+	struct list_head *next_head;
 	struct drbd_request *next = *pnext;
 	bool next_is_head = (&next->tl_requests == transfer_log);
 
+	rcu_read_lock();
 	do {
-		if (next_is_head)
+		if (next_is_head) {
+			rcu_read_unlock();
 			return NULL;
+		}
 		req = next;
-		next = list_next_entry(req, tl_requests);
-		next_is_head = (&next->tl_requests == transfer_log);
+		next_head = list_next_rcu(&req->tl_requests);
+		next_is_head = (next_head == transfer_log);
+		next = list_entry_rcu(next_head, struct drbd_request, tl_requests);
 		if (!next_is_head)
 			kref_get(&next->kref);
 	} while (kref_put(&req->kref, drbd_req_destroy));
+	rcu_read_unlock();
 	*pnext = next;
 	return req;
 }
@@ -397,13 +419,12 @@ void tl_release(struct drbd_connection *connection,
 	struct drbd_request *req_y = NULL;
 	int expect_epoch = 0;
 	int expect_size = 0;
-	const int idx = connection->peer_node_id;
 
-	spin_lock_irq(&resource->req_lock);
-
+	spin_lock_irq(&connection->resource->req_lock);
+	rcu_read_lock();
 	/* find oldest not yet barrier-acked write request,
 	 * count writes in its epoch. */
-	list_for_each_entry(r, &resource->transfer_log, tl_requests) {
+	list_for_each_entry_rcu(r, &resource->transfer_log, tl_requests) {
 		struct drbd_peer_device *peer_device =
 			conn_peer_device(connection, r->device->vnr);
 		const int idx = peer_device->node_id;
@@ -513,7 +534,7 @@ void tl_release(struct drbd_connection *connection,
 	/* this extra list walk restart is paranoia,
 	 * to catch requests being barrier-acked "unexpectedly".
 	 * It usually should find the same req again, or some READ preceding it. */
-	list_for_each_entry(req, &resource->transfer_log, tl_requests)
+	list_for_each_entry_rcu(req, &resource->transfer_log, tl_requests)
 		if (req->epoch == expect_epoch)
 			break;
 	tl_for_each_req_ref_continue(req, r, &resource->transfer_log) {
@@ -529,7 +550,8 @@ void tl_release(struct drbd_connection *connection,
 			break;
 		}
 	}
-	spin_unlock_irq(&resource->req_lock);
+	rcu_read_unlock();
+	spin_unlock_irq(&connection->resource->req_lock);
 
 	/* urgently flush out peer acks for P_CONFIRM_STABLE */
 	if (req_y) {
@@ -542,7 +564,8 @@ void tl_release(struct drbd_connection *connection,
 	return;
 
 bail:
-	spin_unlock_irq(&resource->req_lock);
+	rcu_read_unlock();
+	spin_unlock_irq(&connection->resource->req_lock);
 	change_cstate(connection, C_PROTOCOL_ERROR, CS_HARD);
 }
 
@@ -3419,6 +3442,7 @@ struct drbd_resource *drbd_create_resource(const char *name,
 	kref_debug_init(&resource->kref_debug, &resource->kref, &kref_class_resource);
 	idr_init(&resource->devices);
 	INIT_LIST_HEAD(&resource->connections);
+	spin_lock_init(&resource->tl_update_lock);
 	INIT_LIST_HEAD(&resource->transfer_log);
 	INIT_LIST_HEAD(&resource->peer_ack_list);
 	timer_setup(&resource->peer_ack_timer, peer_ack_timer_fn, 0);

drbd/drbd_receiver.c
@@ -8846,7 +8846,7 @@ static void destroy_peer_ack_req(struct kref *kref)
 		container_of(kref, struct drbd_request, kref);
 
 	list_del(&req->tl_requests);
-	mempool_free(req, &drbd_request_mempool);
+	call_rcu(&req->rcu, drbd_reclaim_req);
 }
 
 static void cleanup_peer_ack_list(struct drbd_connection *connection)

drbd/drbd_req.c
@@ -30,7 +30,6 @@
 #include "drbd_int.h"
 #include "drbd_req.h"
 
-
 static bool drbd_may_do_local_read(struct drbd_device *device, sector_t sector, int size);
 
 #ifndef __disk_stat_inc
@@ -109,10 +108,16 @@ static struct drbd_request *drbd_req_new(struct drbd_device *device, struct bio 
 	return req;
 }
 
+void drbd_reclaim_req(struct rcu_head *rp)
+{
+	struct drbd_request *req = container_of(rp, struct drbd_request, rcu);
+	mempool_free(req, &drbd_request_mempool);
+}
+
 static void req_destroy_no_send_peer_ack(struct kref *kref)
 {
 	struct drbd_request *req = container_of(kref, struct drbd_request, kref);
-	mempool_free(req, &drbd_request_mempool);
+	call_rcu(&req->rcu, drbd_reclaim_req);
 }
 
 void drbd_queue_peer_ack(struct drbd_resource *resource, struct drbd_request *req)
@@ -193,9 +198,8 @@ void drbd_req_destroy(struct kref *kref)
 
 #ifdef CONFIG_DRBD_TIMING_STATS
 	if (s & RQ_WRITE) {
-		unsigned long flags;
 
-		spin_lock_irqsave(&device->timing_lock, flags);
+		spin_lock(&device->timing_lock); /* local irq already disabled */
 		device->reqs++;
 		ktime_aggregate(device, req, in_actlog_kt);
 		ktime_aggregate(device, req, pre_submit_kt);
@@ -208,7 +212,7 @@ void drbd_req_destroy(struct kref *kref)
 			ktime_aggregate_pd(peer_device, node_id, req, acked_kt);
 			ktime_aggregate_pd(peer_device, node_id, req, net_done_kt);
 		}
-		spin_unlock_irqrestore(&device->timing_lock, flags);
+		spin_unlock(&device->timing_lock);
 	}
 #endif
 
@@ -234,10 +238,12 @@ void drbd_req_destroy(struct kref *kref)
 		return;
 	}
 
+	spin_lock(&resource->tl_update_lock); /* local irq already disabled */
 	destroy_next = req->destroy_next;
-	list_del_init(&req->tl_requests);
+	list_del_rcu(&req->tl_requests);
 	if (resource->tl_previous_write == req)
 		resource->tl_previous_write = NULL;
+	spin_unlock(&resource->tl_update_lock);
 
 	/* finally remove the request from the conflict detection
 	 * respective block_id verification interval tree. */
@@ -320,7 +326,7 @@ void drbd_req_destroy(struct kref *kref)
 				drbd_queue_peer_ack(resource, peer_ack_req);
 				peer_ack_req = NULL;
 			} else
-				mempool_free(peer_ack_req, &drbd_request_mempool);
+				call_rcu(&peer_ack_req->rcu, drbd_reclaim_req);
 		}
 		req->device = NULL;
 		resource->peer_ack_req = req;
@@ -330,7 +336,7 @@ void drbd_req_destroy(struct kref *kref)
 		if (!peer_ack_req)
 			resource->last_peer_acked_dagtag = req->dagtag_sector;
 	} else
-		mempool_free(req, &drbd_request_mempool);
+		call_rcu(&req->rcu, drbd_reclaim_req);
 
 	/* In both branches of the if above, the reference to device gets released */
 	kref_debug_put(&device->kref_debug, 6);
@@ -546,11 +552,13 @@ static void advance_conn_req_next(struct drbd_connection *connection, struct drb
 {
 	if (connection->todo.req_next != req)
 		return;
-	list_for_each_entry_continue(req, &connection->resource->transfer_log, tl_requests) {
+	rcu_read_lock();
+	list_for_each_entry_continue_rcu(req, &connection->resource->transfer_log, tl_requests) {
 		const unsigned s = req->net_rq_state[connection->peer_node_id];
 		if (s & RQ_NET_QUEUED)
 			break;
 	}
+	rcu_read_unlock();
 	if (&req->tl_requests == &connection->resource->transfer_log)
 		req = NULL;
 	connection->todo.req_next = req;
@@ -583,7 +591,7 @@ static void advance_cache_ptr(struct drbd_connection *connection,
 		rcu_read_unlock();
 		return;
 	}
-	list_for_each_entry_continue(req, &connection->resource->transfer_log, tl_requests) {
+	list_for_each_entry_continue_rcu(req, &connection->resource->transfer_log, tl_requests) {
 		const unsigned s = READ_ONCE(req->net_rq_state[connection->peer_node_id]);
 		if ((s & is_set) && !(s & is_clear))
 			break;
@@ -1766,6 +1774,7 @@ static void drbd_send_and_submit(struct drbd_device *device, struct drbd_request
 	/* no point in adding empty flushes to the transfer log,
 	 * they are mapped to drbd barriers already. */
 	if (likely(req->i.size != 0)) {
+		spin_lock(&resource->tl_update_lock); /* local irq already disabled */
 		if (rw == WRITE) {
 			struct drbd_request *prev_write;
 
@@ -1779,7 +1788,8 @@ static void drbd_send_and_submit(struct drbd_device *device, struct drbd_request
 				prev_write->destroy_next = req;
 			}
 		}
-		list_add_tail(&req->tl_requests, &resource->transfer_log);
+		list_add_tail_rcu(&req->tl_requests, &resource->transfer_log);
+		spin_unlock(&resource->tl_update_lock);
 	}
 
 	if (rw == WRITE) {

drbd/drbd_req.h
@@ -315,6 +315,7 @@ extern void __tl_walk(struct drbd_resource *const resource,
 		const enum drbd_req_event what);
 extern void drbd_queue_peer_ack(struct drbd_resource *resource, struct drbd_request *req);
 extern bool drbd_should_do_remote(struct drbd_peer_device *, enum which_state);
+extern void drbd_reclaim_req(struct rcu_head *rp);
 
 /* this is in drbd_main.c */
 extern void drbd_restart_request(struct drbd_request *req);

drbd/drbd_sender.c
@@ -2472,7 +2472,7 @@ static struct drbd_request *__next_request_for_connection(
 {
 	struct drbd_request *req;
 
-	list_for_each_entry(req, &connection->resource->transfer_log, tl_requests) {
+	list_for_each_entry_rcu(req, &connection->resource->transfer_log, tl_requests) {
 		unsigned s = req->net_rq_state[connection->peer_node_id];
 		if (!(s & RQ_NET_QUEUED))
 			continue;
@@ -2502,7 +2502,7 @@ static struct drbd_request *tl_mark_for_resend_by_connection(struct drbd_connect
 	 */
 restart:
 	req = list_prepare_entry(tmp, &connection->resource->transfer_log, tl_requests);
-	list_for_each_entry_continue(req, &connection->resource->transfer_log, tl_requests) {
+	list_for_each_entry_continue_rcu(req, &connection->resource->transfer_log, tl_requests) {
 		/* potentially needed in complete_master_bio below */
 		device = req->device;
 		peer_device = conn_peer_device(connection, device->vnr);
@@ -2544,11 +2544,13 @@ restart:
 		 * RESEND actually caused this request to be finished off, we
 		 * complete the master bio, outside of the lock. */
 		if (m.bio || need_resched()) {
+			rcu_read_unlock();
 			spin_unlock_irq(&connection->resource->req_lock);
 			if (m.bio)
 				complete_master_bio(device, &m);
 			cond_resched();
 			spin_lock_irq(&connection->resource->req_lock);
+			rcu_read_lock();
 			goto restart;
 		}
 		if (!req_oldest)
@@ -2597,6 +2599,7 @@ static void maybe_send_state_afer_ahead(struct drbd_connection *connection)
  */
 static bool check_sender_todo(struct drbd_connection *connection)
 {
+	rcu_read_lock();
 	tl_next_request_for_connection(connection);
 
 	/* we did lock_irq above already. */
@@ -2604,6 +2607,7 @@ static bool check_sender_todo(struct drbd_connection *connection)
 	spin_lock(&connection->sender_work.q_lock);
 	list_splice_tail_init(&connection->sender_work.q, &connection->todo.work_list);
 	spin_unlock(&connection->sender_work.q_lock);
+	rcu_read_unlock();
 
 	return connection->todo.req
 		|| need_unplug(connection)

commit fff2e89e4a1e705e4d46cf6ea30011e880e11b35

drbd: hold state_rwlock in debugfs transfer log walk
seq_print_resource_transfer_log_summary() calls drbd_put_ref_tl_walk()
from process context with interrupts enabled and without the
state_rwlock read lock. Take that lock as indicated by the lockdep
assertion in drbd_put_ref_tl_walk().

Fixes: 2289b548ed5d ("drbd: introduce RCU on the transfer log")
Signed-off-by: Joel Colledge <joel.colledge@linbit.com>

drbd/drbd_debugfs.c
@@ -473,7 +473,9 @@ static void seq_print_resource_transfer_log_summary(struct seq_file *m,
 			cond_resched();
 			rcu_read_lock();
 			next_hdr = rcu_dereference(list_next_rcu(&req->tl_requests));
+			read_lock_irq(&resource->state_rwlock);
 			drbd_put_ref_tl_walk(req, 0, 1);
+			read_unlock_irq(&resource->state_rwlock);
 			if (!refcount_read(&req->done_ref)) {
 				if (next_hdr == &resource->transfer_log)
 					break;