LINBIT/drbd

commit b7edd722461cd94d3e5b2dc133352db309dc9551

drbd: Also put modifications of the bitmap UUIDs under the new uuid_sem
while doing so it became clear that the call of __drbd_uuid_set_bitmap()
from uuid_fixup_resync_start2() should be _drbd_uuid_set_bitmap(), because
it needs that locking.

drbd/drbd_int.h
@@ -1539,7 +1539,6 @@ extern void drbd_uuid_new_current(struct drbd_device *device, bool forced);
 extern void drbd_uuid_new_current_by_user(struct drbd_device *device);
 extern void _drbd_uuid_push_history(struct drbd_device *device, u64 val) __must_hold(local);
 extern u64 _drbd_uuid_pull_history(struct drbd_peer_device *peer_device) __must_hold(local);
-extern void __drbd_uuid_set_bitmap(struct drbd_peer_device *peer_device, u64 val) __must_hold(local);
 extern void drbd_uuid_resync_starting(struct drbd_peer_device *peer_device); __must_hold(local);
 extern u64 drbd_uuid_resync_finished(struct drbd_peer_device *peer_device) __must_hold(local);
 extern void drbd_uuid_detect_finished_resyncs(struct drbd_peer_device *peer_device) __must_hold(local);

drbd/drbd_main.c
@@ -4559,7 +4559,7 @@ static void __drbd_uuid_set_current(struct drbd_device *device, u64 val)
 	drbd_set_exposed_data_uuid(device, val);
 }
 
-void __drbd_uuid_set_bitmap(struct drbd_peer_device *peer_device, u64 val)
+static void __drbd_uuid_set_bitmap(struct drbd_peer_device *peer_device, u64 val)
 {
 	struct drbd_device *device = peer_device->device;
 	struct drbd_peer_md *peer_md = &device->ldev->md.peers[peer_device->node_id];
@@ -4583,9 +4583,11 @@ void _drbd_uuid_set_bitmap(struct drbd_peer_device *peer_device, u64 val) __must
 	struct drbd_device *device = peer_device->device;
 	unsigned long flags;
 
+	down_write(&device->uuid_sem);
 	spin_lock_irqsave(&device->ldev->md.uuid_lock, flags);
 	__drbd_uuid_set_bitmap(peer_device, val);
 	spin_unlock_irqrestore(&device->ldev->md.uuid_lock, flags);
+	up_write(&device->uuid_sem);
 }
 
 void drbd_uuid_set_bitmap(struct drbd_peer_device *peer_device, u64 uuid) __must_hold(local)
@@ -4594,12 +4596,14 @@ void drbd_uuid_set_bitmap(struct drbd_peer_device *peer_device, u64 uuid) __must
 	unsigned long flags;
 	u64 previous_uuid;
 
+	down_write(&device->uuid_sem);
 	spin_lock_irqsave(&device->ldev->md.uuid_lock, flags);
 	previous_uuid = drbd_bitmap_uuid(peer_device);
 	if (previous_uuid)
 		_drbd_uuid_push_history(device, previous_uuid);
 	__drbd_uuid_set_bitmap(peer_device, uuid);
 	spin_unlock_irqrestore(&device->ldev->md.uuid_lock, flags);
+	up_write(&device->uuid_sem);
 }
 
 static u64 rotate_current_into_bitmap(struct drbd_device *device, u64 weak_nodes, u64 dagtag) __must_hold(local)
@@ -4866,6 +4870,11 @@ void drbd_uuid_new_current(struct drbd_device *device, bool forced)
 
 void drbd_uuid_new_current_by_user(struct drbd_device *device)
 {
+	struct drbd_peer_device *peer_device;
+
+	for_each_peer_device(peer_device, device)
+		drbd_uuid_set_bitmap(peer_device, 0); /* Rotate UI_BITMAP to History 1, etc... */
+
 	if (get_ldev(device)) {
 		__drbd_uuid_new_current(device, false, false);
 		put_ldev(device);

drbd/drbd_receiver.c
@@ -3785,7 +3785,7 @@ static enum sync_strategy uuid_fixup_resync_start2(struct drbd_peer_device *peer
 				return REQUIRES_PROTO_91;
 
 			bitmap_uuid = _drbd_uuid_pull_history(peer_device);
-			__drbd_uuid_set_bitmap(peer_device, bitmap_uuid);
+			_drbd_uuid_set_bitmap(peer_device, bitmap_uuid);
 
 			drbd_info(device, "Last syncUUID did not get through, corrected:\n");
 			drbd_uuid_dump_self(peer_device,

drbd/drbd_nl.c
@@ -5658,8 +5658,6 @@ int drbd_adm_new_c_uuid(struct sk_buff *skb, struct genl_info *info)
 		}
 	}
 
-	for_each_peer_device(peer_device, device)
-		drbd_uuid_set_bitmap(peer_device, 0); /* Rotate UI_BITMAP to History 1, etc... */
 	drbd_uuid_new_current_by_user(device); /* New current, previous to UI_BITMAP */
 
 	if (args.force_resync) {

commit 6facf3bb6adc5907316e1eee3d7e0f18149e829f

drbd: fix self-deadlock in uuid_fixup_resync_start2()
drbd_handshake() calls drbd_uuid_compare() while holding the uuid_lock
spinlock. With agreed_pro_version < 110, drbd_uuid_compare() can reach
uuid_fixup_resync_start2(), which called _drbd_uuid_set_bitmap(). That
helper does down_write(&device->uuid_sem) -- sleeping under a spinlock
-- followed by spin_lock_irqsave() on the very uuid_lock the caller
already holds: an instant self-deadlock, dormant only because it needs
a DRBD 8.4 compat peer together with the "last P_SYNC_UUID did not get
through" condition.

Commit b7edd722461c ("drbd: Also put modifications of the bitmap
UUIDs under the new uuid_sem") switched this call site from
__drbd_uuid_set_bitmap() to _drbd_uuid_set_bitmap(), missing that the
caller already holds uuid_lock.
The uuid_sem is not needed here: with agreed_pro_version < 110 we
deliberately do not hold uuid_sem during the initial UUID exchange
because there are no other peers which could interfere (see
connect_work()).

Switch the call site back to __drbd_uuid_set_bitmap() and export it
again. Document that drbd_uuid_compare() requires uuid_lock, and take
it around the drbd_uuid_compare() call in drbd_try_to_get_resynced(),
which read the UUIDs without it.

Also make check_lock_order.py executable, as it no longer finds issues,
and from now on the CI should run this check.

Found by checks/check_lock_order.py.

Fixes: b7edd722461c ("drbd: Also put modifications of the bitmap UUIDs under the new uuid_sem")
Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>

drbd/drbd_int.h
@@ -1930,6 +1930,7 @@ int drbd_md_sync_if_dirty(struct drbd_device *device);
 void drbd_uuid_received_new_current(struct drbd_peer_device *from_pd, u64 val,
 				    u64 weak_nodes);
 void drbd_uuid_set_bitmap(struct drbd_peer_device *peer_device, u64 uuid);
+void __drbd_uuid_set_bitmap(struct drbd_peer_device *peer_device, u64 val);
 void _drbd_uuid_set_bitmap(struct drbd_peer_device *peer_device, u64 val);
 void _drbd_uuid_set_current(struct drbd_device *device, u64 val);
 void drbd_uuid_new_current(struct drbd_device *device, bool forced);

drbd/drbd_main.c
@@ -4838,7 +4838,7 @@ void drbd_set_peer_bitmap_uuid(struct drbd_peer_md *peer_md, u64 bitmap_uuid, u6
 		clear_bit(__MDF_PEER_DIVERGENCE_BITMAP, &peer_md->flags);
 }
 
-static void __drbd_uuid_set_bitmap(struct drbd_peer_device *peer_device, u64 val)
+void __drbd_uuid_set_bitmap(struct drbd_peer_device *peer_device, u64 val)
 {
 	struct drbd_device *device = peer_device->device;
 	struct drbd_peer_md *peer_md = &device->ldev->md.peers[peer_device->node_id];

drbd/drbd_receiver.c
@@ -4830,7 +4830,7 @@ uuid_fixup_resync_start2(struct drbd_peer_device *peer_device, enum sync_rule *r
 				return REQUIRES_PROTO_91;
 
 			bitmap_uuid = _drbd_uuid_pull_history(peer_device);
-			_drbd_uuid_set_bitmap(peer_device, bitmap_uuid);
+			__drbd_uuid_set_bitmap(peer_device, bitmap_uuid);
 
 			drbd_info(device, "Last syncUUID did not get through, corrected:\n");
 			drbd_uuid_dump_self(peer_device,
@@ -4899,6 +4899,8 @@ static enum sync_strategy drbd_uuid_compare(struct drbd_peer_device *peer_device
 	u64 local_uuid_flags = 0;
 	u64 self, peer;
 	int i, j;
+
+	lockdep_assert_held(&device->ldev->md.uuid_lock);
 
 	resolved_uuid = drbd_resolved_uuid(peer_device, &local_uuid_flags) & ~UUID_PRIMARY;
 	bitmap_uuid = drbd_bitmap_uuid(peer_device);
@@ -8388,7 +8390,9 @@ static void drbd_try_to_get_resynced(struct drbd_device *device)
 		if (peer_device->disk_state[NOW] != D_UP_TO_DATE)
 			continue;
 
+		spin_lock_irq(&device->ldev->md.uuid_lock);
 		strategy = drbd_uuid_compare(peer_device, &rule, &peer_node_id);
+		spin_unlock_irq(&device->ldev->md.uuid_lock);
 		disk_states_to_strategy(peer_device, peer_device->disk_state[NOW], &strategy, &rule,
 					&peer_node_id);
 		drbd_info(peer_device, "strategy = %s\n", strategy_descriptor(strategy).name);