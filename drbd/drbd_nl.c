LINBIT/drbd

commit 785c7adcf14bcdd7378a6a8d227350ef48bbd083

drbd: fix pointless warning in drbd_bitmap.c
bm_rw_range() outputs a warning when it gets called without one of the
BM_*_LOCK bits set in bm_flags.

WARNING: CPU: 1 PID: 3217 at /home/phil/src/drbd09/drbd/drbd_bitmap.c:1278 bm_rw_range.constprop.0+0x47a/0x520 [drbd]

Followed by a complete register dump and stack trace. Some users
confuse it with an OOPS.

The recent commit f5f748d7b28c introduced this. This highlights that
currently, our CI pipeline does not fail its tests when DRBD prints a
warning to the logfile.

I am fixing this by using BM_LOCK_BULK here.

drbd/drbd_nl.c
@@ -2531,10 +2531,15 @@ static int clear_peer_slot(struct drbd_device *device, int peer_node_id, u32 md_
 	peer_md->bitmap_index = -1;
 
 	if (free_bitmap_slot) {
+		drbd_bm_lock(device, __func__, BM_LOCK_BULK);
 		/*
-		 * No drbd_bm_lock() here, as bitmap OPs might happen in parallel,
-		 * and this is no issue as new dirty bits will already go to
-		 * the slot we are copying to.
+		 * Regular bitmap OPs (calling into bm_op()) can run in parallel to
+		 * drbd_bm_copy_slot() and interleave with it as drbd_bm_copy_slot()
+		 * gives up its locks when it moves on to the next source page.
+		 * The bitmap->bm_all_slots_lock ensures that drbd_set_sync()
+		 * (which iterates over multiple slots) does not interleave with
+		 * drbd_bm_copy_slot() while it copies data from one slot to another
+		 * one.
 		 */
 		if (from_index != -1)
 			drbd_bm_copy_slot(device, from_index, freed_index);
@@ -2542,6 +2547,7 @@ static int clear_peer_slot(struct drbd_device *device, int peer_node_id, u32 md_
 			_drbd_bm_set_many_bits(device, freed_index, 0, -1UL);
 
 		drbd_bm_write(device, NULL);
+		drbd_bm_unlock(device);
 	}
 
 	/*

drbd/drbd_bitmap.c
@@ -139,7 +139,7 @@ bm_print_lock_info(struct drbd_device *device, unsigned int bitmap_index, enum b
  */
 static void
 _drbd_bm_lock(struct drbd_device *device, struct drbd_peer_device *peer_device,
-	      char *why, enum bm_flag flags)
+	      const char *why, enum bm_flag flags)
 {
 	struct drbd_bitmap *b = device->bitmap;
 	int trylock_failed;
@@ -173,7 +173,7 @@ _drbd_bm_lock(struct drbd_device *device, struct drbd_peer_device *peer_device,
 	b->bm_locked_peer = peer_device;
 }
 
-void drbd_bm_lock(struct drbd_device *device, char *why, enum bm_flag flags)
+void drbd_bm_lock(struct drbd_device *device, const char *why, enum bm_flag flags)
 {
 	_drbd_bm_lock(device, NULL, why, flags);
 }

drbd/drbd_int.h
@@ -653,7 +653,7 @@ struct drbd_bitmap {
 	unsigned int al_bitmap_hints[2*AL_UPDATES_PER_TRANSACTION];
 
 	/* debugging aid, in case we are still racy somewhere */
-	char          *bm_why;
+	const char    *bm_why;
 	char          bm_task_comm[TASK_COMM_LEN];
 	pid_t         bm_task_pid;
 	struct drbd_peer_device *bm_locked_peer;
@@ -1868,7 +1868,7 @@ extern void drbd_bm_merge_lel(struct drbd_peer_device *peer_device, size_t offse
 extern void drbd_bm_get_lel(struct drbd_peer_device *peer_device, size_t offset,
 		size_t number, unsigned long *buffer);
 
-extern void drbd_bm_lock(struct drbd_device *device, char *why, enum bm_flag flags);
+extern void drbd_bm_lock(struct drbd_device *device, const char *why, enum bm_flag flags);
 extern void drbd_bm_unlock(struct drbd_device *device);
 extern void drbd_bm_slot_lock(struct drbd_peer_device *peer_device, char *why, enum bm_flag flags);
 extern void drbd_bm_slot_unlock(struct drbd_peer_device *peer_device);

commit 0e7d618e712f98cdb97c9bdfc2d3297b7fa60144

drbd: fix ABBA deadlock between clear_peer_slot() and whole-bitmap IO
clear_peer_slot() acquired the meta-data IO buffer (md_buffer) first
and the bitmap lock second. Whole-bitmap operations such as
drbd_bmio_set_n_write() run with the bitmap lock already held (taken
by drbd_bitmap_io() or w_bitmap_io()) and acquire md_buffer inside via
drbd_md_sync(). Racing the two wedges both threads forever: the
clear_peer_slot() caller sleeps uninterruptibly on the bitmap lock
while holding md_buffer, and the bitmap io_fn loops in
drbd_md_get_buffer():

  drbdsetup[3862169] going to 'clear_peer_slot' but bitmap already
    locked for 'set_n_write sync_handshake' by drbd_w_volume-d[3066456]
  Waited 11s on md_buffer for __drbd_md_sync; in use by
    clear_peer_slot; still trying...

In the field this was hit on a node whose diskless primary peer had
its bitmap= peer-device option toggled repeatedly by LINSTOR while a
reconnect ran the "set_n_write sync_handshake" bitmap modification.
Since the stuck bitmap operation ran on the resource worker, state
change completion stalled as well, escalating into cluster-wide
two-phase-commit timeouts.

Fix this by taking the bitmap lock before md_buffer in
clear_peer_slot(), matching the lock order of the whole-bitmap
operations.

Fixes: 785c7adcf14b ("drbd: fix pointless warning in drbd_bitmap.c")
Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>

drbd/drbd_nl.c
@@ -2716,6 +2716,9 @@ static int clear_peer_slot(struct drbd_device *device, int peer_node_id, u32 md_
 
 		from_index = drbd_unallocated_index(device->ldev, device->bitmap->bm_max_peers);
 		freed_index = peer_md->bitmap_index;
+
+		/* Take the bitmap lock before md_buffer. Correct order. */
+		drbd_bm_lock(device, __func__, BM_LOCK_BULK);
 	}
 	buffer = drbd_md_get_buffer(device, __func__); /* lock meta-data IO to superblock */
 	if (buffer == NULL)
@@ -2728,7 +2731,6 @@ static int clear_peer_slot(struct drbd_device *device, int peer_node_id, u32 md_
 	peer_md->bitmap_index = -1;
 
 	if (free_bitmap_slot) {
-		drbd_bm_lock(device, __func__, BM_LOCK_BULK);
 		/*
 		 * Regular bitmap OPs (calling into bm_op()) can run in parallel to
 		 * drbd_bm_copy_slot() and interleave with it as drbd_bm_copy_slot()
@@ -2744,7 +2746,6 @@ static int clear_peer_slot(struct drbd_device *device, int peer_node_id, u32 md_
 			_drbd_bm_set_many_bits(device, freed_index, 0, -1UL);
 
 		drbd_bm_write(device, NULL);
-		drbd_bm_unlock(device);
 	}
 
 	/*
@@ -2768,8 +2769,10 @@ static int clear_peer_slot(struct drbd_device *device, int peer_node_id, u32 md_
 	drbd_md_put_buffer(device);
 
  out_no_buffer:
-	if (free_bitmap_slot)
+	if (free_bitmap_slot) {
+		drbd_bm_unlock(device);
 		drbd_resume_io(device);
+	}
 
 	put_ldev(device);
 