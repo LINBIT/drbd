LINBIT/drbd

commit 1f59029d752149fa9be3b11d97ca8ddac1a8d57d

drbd: Send config details, size, data UUIDs, and initial state before first 2pc
When establishing a connection DRBD changed connection state before
changing the states of the peer_devices. With that also the UUID comparison
took place after the first two phase commit for establishing the conneciton
(= changing connection state).
A consequence of that is that we are not able to detect a split-brain
situation when both nodes are primary. The state-engine denies the conneciton
we never come to the point where we even receive or look at the data UUIDs.

In comparison to drbd-8.4 this is a step back. 8.4 compared UUIDs first,
thus detected a split-brain situation before it even looked at the
current roles of the nodes. Users are complaining about that.

This patch changes the order of actions during establishing a connection
(only if both node are recent enough to support it).

That require to move the try_to_get_resynced() logic to the generic state
engine. To sanitize_state(), where it should be.

drbd/drbd_int.h
@@ -538,6 +538,7 @@ enum device_flag {
 	PRIMARY_LOST_QUORUM,
 	TIEBREAKER_QUORUM,	/* Tiebreaker keeps quorum; used to avoid too verbose logging */
 	DESTROYING_DEV,
+	TRY_TO_GET_RESYNC,
 };
 
 /* flag bits per peer device */
@@ -1202,6 +1203,7 @@ struct drbd_peer_device {
 	struct {/* sender todo per peer_device */
 		bool was_ahead;
 	} todo;
+	union drbd_state connect_state;
 };
 
 struct submit_worker {
@@ -2027,6 +2029,7 @@ extern bool drbd_have_local_disk(struct drbd_resource *resource);
 extern enum drbd_state_rv drbd_support_2pc_resize(struct drbd_resource *resource);
 extern enum determine_dev_size
 drbd_commit_size_change(struct drbd_device *device, struct resize_parms *rs, u64 nodes_to_reach);
+extern void drbd_try_to_get_resynced(struct drbd_device *device);
 
 static inline sector_t drbd_get_capacity(struct block_device *bdev)
 {

drbd/drbd_receiver.c
@@ -690,24 +690,10 @@ int drbd_connected(struct drbd_peer_device *peer_device)
 	return err;
 }
 
-void connect_timer_fn(struct timer_list *t)
-{
-	struct drbd_connection *connection = from_timer(connection, t, connect_timer);
-	struct drbd_resource *resource = connection->resource;
-	unsigned long irq_flags;
-
-	spin_lock_irqsave(&resource->req_lock, irq_flags);
-	drbd_queue_work(&connection->sender_work, &connection->connect_timer_work);
-	spin_unlock_irqrestore(&resource->req_lock, irq_flags);
-}
-
 static void conn_connect2(struct drbd_connection *connection)
 {
 	struct drbd_peer_device *peer_device;
 	int vnr;
-
-	atomic_set(&connection->ap_in_flight, 0);
-	atomic_set(&connection->rs_in_flight, 0);
 
 	rcu_read_lock();
 	idr_for_each_entry(&connection->peer_devices, peer_device, vnr) {
@@ -726,6 +712,36 @@ static void conn_connect2(struct drbd_connection *connection)
 		kref_put(&device->kref, drbd_destroy_device);
 	}
 	rcu_read_unlock();
+
+}
+
+static bool initial_states_received(struct drbd_connection *connection)
+{
+	struct drbd_peer_device *peer_device;
+	int vnr;
+	bool rv = true;
+
+	rcu_read_lock();
+	idr_for_each_entry(&connection->peer_devices, peer_device, vnr) {
+		if (!test_bit(INITIAL_STATE_RECEIVED, &peer_device->flags)) {
+			rv = false;
+			break;
+		}
+	}
+	rcu_read_unlock();
+
+	return rv;
+}
+
+void connect_timer_fn(struct timer_list *t)
+{
+	struct drbd_connection *connection = from_timer(connection, t, connect_timer);
+	struct drbd_resource *resource = connection->resource;
+	unsigned long irq_flags;
+
+	spin_lock_irqsave(&resource->req_lock, irq_flags);
+	drbd_queue_work(&connection->sender_work, &connection->connect_timer_work);
+	spin_unlock_irqrestore(&resource->req_lock, irq_flags);
 }
 
 static int connect_work(struct drbd_work *work, int cancel)
@@ -739,6 +755,19 @@ static int connect_work(struct drbd_work *work, int cancel)
 	if (connection->cstate[NOW] != C_CONNECTING)
 		goto out_put;
 
+	if (connection->agreed_pro_version >= 117) {
+		struct net_conf *nc;
+
+		rcu_read_lock();
+		nc = rcu_dereference(connection->transport.net_conf);
+		t = nc->ping_timeo * 4 * HZ/10;
+		rcu_read_unlock();
+		wait_event_interruptible_timeout(connection->ee_wait,
+						 initial_states_received(connection),
+						 t);
+	}
+
+	t = resource->res_opts.auto_promote_timeout * HZ / 10;
 	do {
 		rv = change_cstate(connection, C_CONNECTED, CS_SERIALIZE | CS_VERBOSE | CS_DONT_RETRY);
 		if (rv != SS_PRIMARY_READER)
@@ -755,7 +784,8 @@ static int connect_work(struct drbd_work *work, int cancel)
 	} while (t > 0);
 
 	if (rv >= SS_SUCCESS) {
-		conn_connect2(connection);
+		if (connection->agreed_pro_version < 117)
+			conn_connect2(connection);
 	} else if (rv == SS_TIMEOUT || rv == SS_CONCURRENT_ST_CHG) {
 		if (connection->cstate[NOW] != C_CONNECTING)
 			goto out_put;
@@ -897,7 +927,13 @@ start:
 		goto retry;
 	}
 
+	atomic_set(&connection->ap_in_flight, 0);
+	atomic_set(&connection->rs_in_flight, 0);
+
 	if (connection->agreed_pro_version >= 110) {
+		if (connection->agreed_pro_version >= 117)
+			conn_connect2(connection);
+
 		if (resource->res_opts.node_id < connection->peer_node_id) {
 			kref_get(&connection->kref);
 			kref_debug_get(&connection->kref_debug, 11);
@@ -5695,6 +5731,9 @@ retry:
 			__outdate_myself(resource);
 	}
 
+	if (mask.conn && val.conn == C_CONNECTED &&
+	    connection->agreed_pro_version >= 117)
+		apply_connect(connection, flags & CS_PREPARED);
 	rv = end_state_change(resource, &irq_flags);
 out:
 
@@ -6692,17 +6731,17 @@ static int process_twopc(struct drbd_connection *connection,
 		if (peer_device && rv >= SS_SUCCESS && !(flags & CS_ABORT))
 			drbd_md_sync_if_dirty(peer_device->device);
 
-		if (rv >= SS_SUCCESS && !(flags & CS_ABORT)) {
-			if (affected_connection &&
-			    mask.conn == conn_MASK && val.conn == C_CONNECTED)
-				conn_connect2(connection);
-		}
+		if (connection->agreed_pro_version < 117 &&
+		    rv >= SS_SUCCESS && !(flags & CS_ABORT) &&
+		    affected_connection &&
+		    mask.conn == conn_MASK && val.conn == C_CONNECTED)
+			conn_connect2(connection);
 	}
 
 	return 0;
 }
 
-static void try_to_get_resynced(struct drbd_device *device)
+void drbd_try_to_get_resynced(struct drbd_device *device)
 {
 	int best_resync_peer_preference = 0;
 	struct drbd_peer_device *best_peer_device = NULL;
@@ -6744,7 +6783,7 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 	union drbd_state old_peer_state, peer_state;
 	enum drbd_disk_state peer_disk_state, new_disk_state = D_MASK;
 	enum drbd_repl_state new_repl_state;
-	bool peer_was_resync_target, try_to_get_resync = false;
+	bool peer_was_resync_target;
 	enum chg_state_flags begin_state_chg_flags = CS_VERBOSE;
 	int rv;
 
@@ -6944,41 +6983,7 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 			begin_state_chg_flags |= CS_FORCE_RECALC;
 			peer_device->negotiation_result = new_repl_state;
 		}
-	} else if (peer_state.role == R_PRIMARY &&
-		   peer_device->disk_state[NOW] == D_UNKNOWN && peer_state.disk == D_DISKLESS &&
-		   device->disk_state[NOW] >= D_NEGOTIATING) {
-		/* I got connected to a diskless primary */
-		if ((peer_device->current_uuid & ~UUID_PRIMARY) ==
-		    (drbd_current_uuid(device) & ~UUID_PRIMARY)) {
-			if (device->disk_state[NOW] < D_UP_TO_DATE) {
-				drbd_info(peer_device, "Upgrading local disk to D_UP_TO_DATE since current UUID matches.\n");
-				new_disk_state = D_UP_TO_DATE;
-			}
-		} else {
-			/* Try to get a resync from some other node that is D_UP_TO_DATE. */
-			try_to_get_resync = true;
-
-			if (device->disk_state[NOW] == D_UP_TO_DATE) {
-				drbd_info(peer_device, "Downgrading local disk to D_CONSISTENT since current UUID differs.\n");
-				new_disk_state = D_CONSISTENT;
-				/* This is a "safety net"; it can only happen if fencing and quorum
-				   are both disabled. This alone would be racy, look for
-				   "Do not trust this guy!" (see also may_return_to_up_to_date()) */
-			}
-		}
-	} else if (resource->role[NOW] == R_PRIMARY && device->disk_state[NOW] == D_DISKLESS &&
-		   peer_disk_state == D_UP_TO_DATE &&
-		   (peer_device->current_uuid & ~UUID_PRIMARY) !=
-		   (device->exposed_data_uuid & ~UUID_PRIMARY)) {
-		/* Do not trust this guy!
-		   He pretends to be D_UP_TO_DATE, but has a different current UUID. Do not
-		   accept him as D_UP_TO_DATE but downgrade that to D_CONSISTENT here. He will
-		   do the same. We need to do it here to avoid that the peer is visible as
-		   D_UP_TO_DATE at all. Otherwise we could ship read requests to it!
-		 */
-		peer_disk_state = D_CONSISTENT;
 	}
-
 	/* This is after the point where we did UUID comparison and joined with the
 	   diskless case again. Releasing uuid_sem here */
 	if (test_and_clear_bit(HOLDING_UUID_READ_LOCK, &peer_device->flags)) {
@@ -7006,6 +7011,18 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 	}
 
 	set_bit(INITIAL_STATE_RECEIVED, &peer_device->flags);
+	if (connection->cstate[NOW] == C_CONNECTING) {
+		/* Since protocol 117 state comes before change on the cstate */
+		peer_device->connect_state = (union drbd_state)
+			{ { .disk = new_disk_state,
+			    .conn = new_repl_state,
+			    .peer = peer_state.role,
+			    .pdsk = peer_disk_state,
+			    .peer_isp = peer_state.aftr_isp | peer_state.user_isp } };
+
+		wake_up(&connection->ee_wait);
+		return 0;
+	}
 
 	spin_lock_irq(&resource->req_lock);
 	begin_state_change_locked(resource, begin_state_chg_flags);
@@ -7030,7 +7047,7 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 
 	rv = end_state_change_locked(resource);
 	new_repl_state = peer_device->repl_state[NOW];
-	set_bit(INITIAL_STATE_PROCESSED, &peer_device->flags);
+	set_bit(INITIAL_STATE_PROCESSED, &peer_device->flags); /* Only relevant for agreed_pro_version < 117 */
 	spin_unlock_irq(&resource->req_lock);
 
 	if (rv < SS_SUCCESS)
@@ -7047,10 +7064,7 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 		}
 	}
 
-	clear_bit(DISCARD_MY_DATA, &peer_device->flags);
-
-	if (try_to_get_resync)
-		try_to_get_resynced(device);
+	clear_bit(DISCARD_MY_DATA, &peer_device->flags); /* Only relevant for agreed_pro_version < 117 */
 
 	drbd_md_sync(device); /* update connected indicator, effective_size, ... */
 

drbd/drbd_state.c
@@ -1951,6 +1951,8 @@ static void sanitize_state(struct drbd_resource *resource)
 			enum drbd_conn_state *cstate = connection->cstate;
 			enum drbd_disk_state min_disk_state, max_disk_state;
 			enum drbd_disk_state min_peer_disk_state, max_peer_disk_state;
+			enum drbd_role *peer_role = connection->peer_role;
+			bool uuids_match;
 
 			if (repl_state[NEW] < L_ESTABLISHED) {
 				peer_device->resync_susp_peer[NEW] = false;
@@ -2130,6 +2132,40 @@ static void sanitize_state(struct drbd_resource *resource)
 				disk_state[NEW] = D_UP_TO_DATE;
 
 			peer_device->uuid_flags &= ~UUID_FLAG_GOT_STABLE;
+
+			uuids_match =
+				(peer_device->current_uuid & ~UUID_PRIMARY) ==
+				(drbd_current_uuid(device) & ~UUID_PRIMARY);
+
+			if (peer_role[OLD] == R_UNKNOWN && peer_role[NEW] == R_PRIMARY &&
+			    peer_disk_state[NEW] == D_DISKLESS && disk_state[NEW] >= D_NEGOTIATING) {
+				/* Got connected to a diskless primary */
+				if (uuids_match) {
+					if (device->disk_state[NOW] < D_UP_TO_DATE) {
+						drbd_info(peer_device, "Upgrading local disk to D_UP_TO_DATE since current UUID matches.\n");
+						disk_state[NEW] = D_UP_TO_DATE;
+					}
+				} else {
+					set_bit(TRY_TO_GET_RESYNC, &device->flags);
+					if (disk_state[NEW] == D_UP_TO_DATE) {
+						drbd_info(peer_device, "Downgrading local disk to D_CONSISTENT since current UUID differs.\n");
+						disk_state[NEW] = D_CONSISTENT;
+						/* This is a "safety net"; it can only happen if fencing and quorum
+						   are both disabled. This alone would be racy, look for
+						   "Do not trust this guy!" (see also may_return_to_up_to_date()) */
+					}
+				}
+			}
+			if (peer_disk_state[OLD] == D_UNKNOWN && peer_disk_state[NEW] == D_UP_TO_DATE &&
+			    role[NEW] == R_PRIMARY && disk_state[NEW] == D_DISKLESS && !uuids_match) {
+				/* Do not trust this guy!
+				   He pretends to be D_UP_TO_DATE, but has a different current UUID. Do not
+				   accept him as D_UP_TO_DATE but downgrade that to D_CONSISTENT here. He will
+				   do the same. We need to do it here to avoid that the peer is visible as
+				   D_UP_TO_DATE at all. Otherwise we could ship read requests to it!
+				*/
+				peer_disk_state[NEW] = D_CONSISTENT;
+			}
 		}
 
 		if (resource->res_opts.quorum != QOU_OFF)
@@ -3642,6 +3678,12 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 		    may_return_to_up_to_date(device, NOW))
 			try_become_up_to_date = true;
 
+		if (test_bit(TRY_TO_GET_RESYNC, &device->flags)) {
+			/* Got connected to a diskless primary */
+			clear_bit(TRY_TO_GET_RESYNC, &device->flags);
+			drbd_try_to_get_resynced(device);
+		}
+
 		drbd_md_sync_if_dirty(device);
 
 		if (role[NEW] == R_PRIMARY && have_quorum[OLD] && !have_quorum[NEW])
@@ -4950,6 +4992,29 @@ static void __change_cstate_and_outdate(struct drbd_connection *connection,
 	}
 }
 
+void apply_connect(struct drbd_connection *connection, bool commit)
+{
+	struct drbd_peer_device *peer_device;
+	int vnr;
+
+	idr_for_each_entry(&connection->peer_devices, peer_device, vnr) {
+		struct drbd_device *device = peer_device->device;
+		union drbd_state s = peer_device->connect_state;
+
+		if (s.disk != D_MASK)
+			__change_disk_state(device, s.disk);
+		if (device->disk_state[NOW] != D_NEGOTIATING)
+			__change_repl_state(peer_device, s.conn);
+		__change_peer_disk_state(peer_device, s.pdsk);
+		__change_resync_susp_peer(peer_device, s.peer_isp);
+
+		if (commit) {
+			set_bit(INITIAL_STATE_PROCESSED, &peer_device->flags);
+			clear_bit(DISCARD_MY_DATA, &peer_device->flags);
+		}
+	}
+}
+
 struct change_cstate_context {
 	struct change_context context;
 	struct drbd_connection *connection;
@@ -4960,12 +5025,13 @@ static bool do_change_cstate(struct change_context *context, enum change_phase p
 {
 	struct change_cstate_context *cstate_context =
 		container_of(context, struct change_cstate_context, context);
+	struct drbd_connection *connection = cstate_context->connection;
 
 	if (phase == PH_PREPARE) {
 		cstate_context->outdate_what = OUTDATE_NOTHING;
 		if (context->val.conn == C_DISCONNECTING && !(context->flags & CS_HARD)) {
 			cstate_context->outdate_what =
-				outdate_on_disconnect(cstate_context->connection);
+				outdate_on_disconnect(connection);
 			switch(cstate_context->outdate_what) {
 			case OUTDATE_DISKS:
 				context->mask.disk = disk_MASK;
@@ -4980,9 +5046,13 @@ static bool do_change_cstate(struct change_context *context, enum change_phase p
 			}
 		}
 	}
-	__change_cstate_and_outdate(cstate_context->connection,
+	__change_cstate_and_outdate(connection,
 				    context->val.conn,
 				    cstate_context->outdate_what);
+
+	if (context->val.conn == C_CONNECTED &&
+	    connection->agreed_pro_version >= 117)
+		apply_connect(connection, phase == PH_COMMIT);
 
 	if (phase == PH_COMMIT) {
 		struct drbd_resource *resource = context->resource;
@@ -4997,7 +5067,7 @@ static bool do_change_cstate(struct change_context *context, enum change_phase p
 	return phase != PH_PREPARE ||
 	       context->val.conn == C_CONNECTED ||
 	       (context->val.conn == C_DISCONNECTING &&
-		connection_has_connected_peer_devices(cstate_context->connection));
+		connection_has_connected_peer_devices(connection));
 }
 
 /**

drbd/drbd_state.h
@@ -125,6 +125,7 @@ extern void __change_resync_susp_user(struct drbd_peer_device *, bool);
 extern enum drbd_state_rv change_resync_susp_user(struct drbd_peer_device *, bool, enum chg_state_flags);
 extern void __change_resync_susp_peer(struct drbd_peer_device *, bool);
 extern void __change_resync_susp_dependency(struct drbd_peer_device *, bool);
+extern void apply_connect(struct drbd_connection *, bool);
 
 struct drbd_work;
 extern int abort_nested_twopc_work(struct drbd_work *, int);

drbd/linux/drbd_config.h
@@ -22,7 +22,7 @@ extern const char *drbd_buildtag(void);
 
 #define REL_VERSION "9.0.23-1"
 #define PRO_VERSION_MIN 86
-#define PRO_VERSION_MAX 116
+#define PRO_VERSION_MAX 117
 
 #ifndef __CHECKER__   /* for a sparse run, we need all STATICs */
 #define DBG_ALL_SYMBOLS /* no static functs, improves quality of OOPS traces */

commit e6dcd7a68daa593a2bb9628e0c9d376c4bb9e588

drbd: run drbd_try_to_get_resynced() outside of the worker thread
w_after_state_change() called drbd_try_to_get_resynced() directly on
the resource worker. When the resulting handshake decides to set and
write a bitmap slot, bitmap_mod_after_handshake() calls
drbd_bitmap_io(), which suspends application IO and performs blocking
whole-bitmap IO. That may not run on the worker, because the worker is
needed to make progress on queued bitmap IO and to complete state
changes; drbd_bitmap_io() asserts exactly that:

  ASSERTION current != device->resource->worker.task FAILED in
    drbd_bitmap_io

In the field, the worker blocked in this call path (on md_buffer, held
by a concurrent clear_peer_slot()), which stalled state change
completion on the node and escalated into two-phase-commit timeouts on
all nodes of the cluster.

Fix this by deferring drbd_try_to_get_resynced() to a per-device work
item on the system workqueue. The work item holds a reference on the
device; connection or disk state changes that happen before it runs
are handled gracefully by the existing state checks in
drbd_try_to_get_resynced().

Fixes: 1f59029d7521 ("drbd: Send config details, size, data UUIDs, and initial state before first 2pc")
Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>

drbd/drbd_int.h
@@ -1625,6 +1625,9 @@ struct drbd_device {
 	/* Used to close backing devices and destroy related structures. */
 	struct work_struct ldev_destroy_work;
 
+	/* Runs drbd_try_to_get_resynced() outside of the worker thread. */
+	struct work_struct try_get_resynced_work;
+
 	struct request_queue *rq_queue;
 	struct gendisk	    *vdisk;
 
@@ -2502,7 +2505,7 @@ enum drbd_state_rv drbd_support_2pc_resize(struct drbd_resource *resource);
 enum determine_dev_size
 drbd_commit_size_change(struct drbd_device *device, struct resize_parms *rs,
 			u64 nodes_to_reach);
-void drbd_try_to_get_resynced(struct drbd_device *device);
+void drbd_try_get_resynced_work_fn(struct work_struct *ws);
 bool diskless_primary_can_replay_to(struct drbd_peer_device *peer_device);
 u64 diskless_primary_present_current_uuid(struct drbd_peer_device *peer_device);
 void drbd_process_rs_discards(struct drbd_peer_device *peer_device,

drbd/drbd_main.c
@@ -4235,6 +4235,7 @@ enum drbd_ret_code drbd_create_device(struct drbd_config_context *adm_ctx, unsig
 	}
 
 	INIT_WORK(&device->ldev_destroy_work, drbd_ldev_destroy);
+	INIT_WORK(&device->try_get_resynced_work, drbd_try_get_resynced_work_fn);
 
 	device->vdisk = disk;
 	device->rq_queue = disk->queue;

drbd/drbd_receiver.c
@@ -8370,7 +8370,7 @@ retry:
 	}
 }
 
-void drbd_try_to_get_resynced(struct drbd_device *device)
+static void drbd_try_to_get_resynced(struct drbd_device *device)
 {
 	struct drbd_peer_device *peer_device, *best_peer_device = NULL;
 	enum sync_strategy best_strategy = UNDETERMINED;
@@ -8410,6 +8410,22 @@ void drbd_try_to_get_resynced(struct drbd_device *device)
 		drbd_send_uuids(peer_device, UUID_FLAG_RESYNC | UUID_FLAG_DISKLESS_PRIMARY, 0);
 	}
 	put_ldev(device);
+}
+
+/*
+ * drbd_try_to_get_resynced() must not run on the worker thread: the bitmap
+ * modification after the handshake performs blocking whole-bitmap IO, which
+ * the worker itself may need to make progress on (drbd_bitmap_io() asserts
+ * that it is not called from the worker). Triggered from
+ * w_after_state_change() via this work function instead.
+ */
+void drbd_try_get_resynced_work_fn(struct work_struct *ws)
+{
+	struct drbd_device *device =
+		container_of(ws, struct drbd_device, try_get_resynced_work);
+
+	drbd_try_to_get_resynced(device);
+	kref_put(&device->kref, drbd_destroy_device);
 }
 
 static void finish_nested_twopc(struct drbd_connection *connection)

drbd/drbd_state.c
@@ -4636,10 +4636,11 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 		if (should_try_become_up_to_date(device, disk_state, NOW))
 			try_become_up_to_date = true;
 
-		if (test_bit(TRY_TO_GET_RESYNC, &device->flags)) {
+		if (test_and_clear_bit(TRY_TO_GET_RESYNC, &device->flags)) {
 			/* Got connected to a diskless primary */
-			clear_bit(TRY_TO_GET_RESYNC, &device->flags);
-			drbd_try_to_get_resynced(device);
+			kref_get(&device->kref);
+			if (!schedule_work(&device->try_get_resynced_work))
+				kref_put(&device->kref, drbd_destroy_device);
 		}
 
 		drbd_md_sync_if_dirty(device);