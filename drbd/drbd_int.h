LINBIT/drbd

commit 9b79cbafdc71f865a434e3d4d22d6455d36f79a3

drbd: Introduce the disk state D_DETACHING
* With that eliminate the "invisible" WAS_IO_ERROR bit.
* The transition to D_FAILED is always a local transaction. Previously
  it was a cluster wide transaction in case it was used for a detach.
* Logfiles are now clear without ambiguity it was an IO error or an
  administrative detach.

drbd/drbd_int.h
@@ -605,8 +605,7 @@ enum {
 	MD_NO_BARRIER,		/* meta data device does not support barriers,
 				   so don't even try */
 	GO_DISKLESS,		/* Disk is being detached, on io-error or admin request. */
-	WAS_IO_ERROR,		/* Local disk failed, returned IO error */
-	WAS_READ_ERROR,		/* Local disk READ failed (set additionally to the above) */
+	WAS_READ_ERROR,		/* Local disk READ failed, returned IO error */
 	FORCE_DETACH,		/* Force-detach from local disk, aborting any pending local IO */
 	NEW_CUR_UUID,		/* Create new current UUID when thawing IO */
 	AL_SUSPENDED,		/* Activity logging is currently suspended. */
@@ -2015,7 +2014,6 @@ static inline void __drbd_chk_io_error_(struct drbd_device *device,
 		 * we read meta data only once during attach,
 		 * which will fail in case of errors.
 		 */
-		set_bit(WAS_IO_ERROR, &device->flags);
 		if (df == DRBD_READ_ERROR)
 			set_bit(WAS_READ_ERROR, &device->flags);
 		if (df == DRBD_FORCE_DETACH)
@@ -2293,7 +2291,7 @@ static inline void put_ldev(struct drbd_device *device)
 		if (device->disk_state[NOW] == D_DISKLESS)
 			/* even internal references gone, safe to destroy */
 			drbd_ldev_destroy(device);
-		if (device->disk_state[NOW] == D_FAILED)
+		if (device->disk_state[NOW] == D_FAILED || device->disk_state[NOW] == D_DETACHING)
 			/* all application IO references gone. */
 			if (!test_and_set_bit(GO_DISKLESS, &device->flags))
 				drbd_queue_work(&device->resource->work, &device->go_diskless);
@@ -2371,6 +2369,7 @@ static inline bool drbd_state_is_stable(struct drbd_device *device)
 	case D_CONSISTENT:
 	case D_UP_TO_DATE:
 	case D_FAILED:
+	case D_DETACHING:
 		/* disk state is stable as well. */
 		break;
 

drbd/drbd_main.c
@@ -1146,8 +1146,15 @@ int drbd_send_state(struct drbd_peer_device *peer_device, union drbd_state state
 	if (!p)
 		return -EIO;
 
-	if (peer_device->connection->agreed_pro_version < 110)
+	if (peer_device->connection->agreed_pro_version < 110) {
 		state.weak = 0;
+
+		/* D_DETACHING was introduced with drbd-9.0 */
+		if (state.disk > D_DETACHING)
+			state.disk--;
+		if (state.pdsk > D_DETACHING)
+			state.pdsk--;
+	}
 
 	p->state = cpu_to_be32(state.i); /* Within the send mutex */
 	return drbd_send_command(peer_device, sock, P_STATE, sizeof(*p), NULL, 0);
@@ -3654,7 +3661,7 @@ void drbd_md_sync(struct drbd_device *device)
 
 	/* We use here D_FAILED and not D_ATTACHING because we try to write
 	 * metadata even if we detach due to a disk failure! */
-	if (!get_ldev_if_state(device, D_FAILED))
+	if (!get_ldev_if_state(device, D_DETACHING))
 		return;
 
 	buffer = drbd_md_get_buffer(device);
@@ -4291,7 +4298,8 @@ static int w_go_diskless(struct drbd_work *w, int unused)
 	struct drbd_device *device =
 		container_of(w, struct drbd_device, go_diskless);
 
-	D_ASSERT(device, device->disk_state[NOW] == D_FAILED);
+	D_ASSERT(device, device->disk_state[NOW] == D_FAILED ||
+		         device->disk_state[NOW] == D_DETACHING);
 	/* we cannot assert local_cnt == 0 here, as get_ldev_if_state will
 	 * inc/dec it frequently. Once we are D_DISKLESS, no one will touch
 	 * the protected members anymore, though, so once put_ldev reaches zero

drbd/drbd_receiver.c
@@ -5200,6 +5200,14 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 
 	peer_state.i = be32_to_cpu(p->state);
 
+	if (peer_device->connection->agreed_pro_version < 110) {
+		/* Before drbd-9.0 there was no D_DETACHING it was D_FAILED... */
+		if (peer_state.disk >= D_DETACHING)
+			peer_state.disk++;
+		if (peer_state.pdsk >= D_DETACHING)
+			peer_state.pdsk++;
+	}
+
 	peer_disk_state = peer_state.disk;
 	if (peer_state.disk == D_NEGOTIATING) {
 		peer_disk_state = peer_device->uuid_flags & UUID_FLAG_INCONSISTENT ?
@@ -5311,7 +5319,7 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 		if (new_repl_state == -1) {
 			new_repl_state = L_ESTABLISHED;
 			if (device->disk_state[NOW] == D_NEGOTIATING) {
-				change_disk_state(device, D_FAILED, CS_HARD);
+				change_disk_state(device, D_DETACHING, CS_HARD);
 			} else if (peer_state.disk == D_NEGOTIATING) {
 				drbd_err(device, "Disk attach process on the peer node was aborted.\n");
 				peer_state.disk = D_DISKLESS;
@@ -6856,7 +6864,7 @@ static int got_NegRSDReply(struct drbd_connection *connection, struct packet_inf
 
 	dec_rs_pending(peer_device);
 
-	if (get_ldev_if_state(device, D_FAILED)) {
+	if (get_ldev_if_state(device, D_DETACHING)) {
 		drbd_rs_complete_io(peer_device, sector);
 		switch (pi->cmd) {
 		case P_NEG_RS_DREPLY:

drbd/drbd_actlog.c
@@ -1577,7 +1577,7 @@ void drbd_rs_cancel_all(struct drbd_peer_device *peer_device)
 	struct drbd_device *device = peer_device->device;
 	spin_lock_irq(&device->al_lock);
 
-	if (get_ldev_if_state(device, D_FAILED)) { /* Makes sure ->resync is there. */
+	if (get_ldev_if_state(device, D_DETACHING)) { /* Makes sure ->resync is there. */
 		lc_reset(peer_device->resync_lru);
 		put_ldev(device);
 	}
@@ -1602,7 +1602,7 @@ int drbd_rs_del_all(struct drbd_peer_device *peer_device)
 
 	spin_lock_irq(&device->al_lock);
 
-	if (get_ldev_if_state(device, D_FAILED)) {
+	if (get_ldev_if_state(device, D_DETACHING)) {
 		/* ok, ->resync is there. */
 		for (i = 0; i < peer_device->resync_lru->nr_elements; i++) {
 			e = lc_element_by_index(peer_device->resync_lru, i);

drbd/drbd_nl.c
@@ -1875,7 +1875,6 @@ int drbd_adm_attach(struct sk_buff *skb, struct genl_info *info)
 
 	/* make sure there is no leftover from previous force-detach attempts */
 	clear_bit(FORCE_DETACH, &device->flags);
-	clear_bit(WAS_IO_ERROR, &device->flags);
 	clear_bit(WAS_READ_ERROR, &device->flags);
 
 	/* and no leftover from previously aborted resync or verify, either */
@@ -2278,18 +2277,18 @@ static int adm_detach(struct drbd_device *device, int force)
 
 	if (force) {
 		set_bit(FORCE_DETACH, &device->flags);
-		change_disk_state(device, D_FAILED, CS_HARD);
+		change_disk_state(device, D_DETACHING, CS_HARD);
 		retcode = SS_SUCCESS;
 		goto out;
 	}
 
 	drbd_suspend_io(device); /* so no-one is stuck in drbd_al_begin_io */
 	retcode = stable_state_change(device->resource,
-		change_disk_state(device, D_FAILED,
+		change_disk_state(device, D_DETACHING,
 			CS_VERBOSE | CS_WAIT_COMPLETE | CS_SERIALIZE));
-	/* D_FAILED will transition to DISKLESS. */
+	/* D_DETACHING will transition to DISKLESS. */
 	ret = wait_event_interruptible(device->misc_wait,
-			get_disk_state(device) != D_FAILED);
+			get_disk_state(device) != D_DETACHING);
 	if (retcode >= SS_SUCCESS)
 		drbd_cleanup_device(device);
 	drbd_resume_io(device);
@@ -3500,7 +3499,8 @@ int drbd_adm_resume_io(struct sk_buff *skb, struct genl_info *info)
 			if (peer_device->repl_state[NOW] < L_ESTABLISHED)
 				tl_clear(connection);
 			if (device->disk_state[NOW] == D_DISKLESS ||
-			    device->disk_state[NOW] == D_FAILED)
+			    device->disk_state[NOW] == D_FAILED ||
+			    device->disk_state[NOW] == D_DETACHING)
 				tl_restart(connection, FAIL_FROZEN_DISK_IO);
 		}
 	}

drbd/drbd_req.c
@@ -239,7 +239,7 @@ tail_recursion:
 		 * In that case we do not want to touch the bitmap at all.
 		 */
 		if ((s & (RQ_POSTPONED|RQ_LOCAL_MASK|RQ_NET_MASK)) != RQ_POSTPONED &&
-		    req->i.size && get_ldev_if_state(device, D_FAILED)) {
+		    req->i.size && get_ldev_if_state(device, D_DETACHING)) {
 			char *id_to_bit = device->ldev->id_to_bit;
 			unsigned long bits = -1, mask = -1;
 			int node_id, max_node_id = device->resource->max_node_id;
@@ -272,7 +272,7 @@ tail_recursion:
 		 * we would forget to resync the corresponding extent.
 		 */
 		if (s & RQ_IN_ACT_LOG) {
-			if (get_ldev_if_state(device, D_FAILED)) {
+			if (get_ldev_if_state(device, D_DETACHING)) {
 				drbd_al_complete_io(device, &req->i);
 				put_ldev(device);
 			} else if (drbd_ratelimit()) {

drbd/drbd_sender.c
@@ -1097,7 +1097,7 @@ int w_e_end_rsdata_req(struct drbd_work *w, int cancel)
 		return 0;
 	}
 
-	if (get_ldev_if_state(device, D_FAILED)) {
+	if (get_ldev_if_state(device, D_DETACHING)) {
 		drbd_rs_complete_io(peer_device, peer_req->i.sector);
 		put_ldev(device);
 	}

drbd/drbd_state.c
@@ -1122,7 +1122,8 @@ static enum drbd_state_rv is_valid_transition(struct drbd_resource *resource)
 
 	idr_for_each_entry(&resource->devices, device, vnr) {
 		/* we cannot fail (again) if we already detached */
-		if (device->disk_state[NEW] == D_FAILED && device->disk_state[OLD] == D_DISKLESS) {
+		if ((device->disk_state[NEW] == D_FAILED || device->disk_state[NEW] == D_DETACHING) &&
+		    device->disk_state[OLD] == D_DISKLESS) {
 			return SS_IS_DISKLESS;
 		}
 	}
@@ -1401,7 +1402,8 @@ static void sanitize_state(struct drbd_resource *resource)
 			/* A detach is a cluster wide transaction. The peer_disk_state updates
 			   are coming in while we have it prepared. When the cluster wide
 			   state change gets committed prevent D_DISKLESS -> D_FAILED */
-			if (peer_disk_state[OLD] == D_DISKLESS && peer_disk_state[NEW] == D_FAILED)
+			if (peer_disk_state[OLD] == D_DISKLESS &&
+			    (peer_disk_state[NEW] == D_FAILED || peer_disk_state[NEW] == D_DETACHING))
 				peer_disk_state[NEW] = D_DISKLESS;
 		}
 		if (disk_state[OLD] == D_UP_TO_DATE)
@@ -1567,6 +1569,7 @@ static void finish_state_change(struct drbd_resource *resource, struct completio
 		 * drbd_ldev_destroy() won't happen before our corresponding
 		 * w_after_state_change works run, where we put_ldev again. */
 		if ((disk_state[OLD] != D_FAILED && disk_state[NEW] == D_FAILED) ||
+		    (disk_state[OLD] != D_DETACHING && disk_state[NEW] == D_DETACHING) ||
 		    (disk_state[OLD] != D_DISKLESS && disk_state[NEW] == D_DISKLESS))
 			atomic_inc(&device->local_cnt);
 
@@ -1721,7 +1724,7 @@ static void finish_state_change(struct drbd_resource *resource, struct completio
 					mdf |= MDF_CONSISTENT;
 				if (disk_state[NEW] > D_OUTDATED)
 					mdf |= MDF_WAS_UP_TO_DATE;
-			} else if (disk_state[NEW] == D_FAILED &&
+			} else if ((disk_state[NEW] == D_FAILED || disk_state[NEW] == D_DETACHING) &&
 				   mdf & MDF_WAS_UP_TO_DATE &&
 				   primary_and_data_present(device)) {
 				/* There are cases when we still can update meta-data event disk
@@ -2372,7 +2375,8 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 
 		/* first half of local IO error, failure to attach,
 		 * or administrative detach */
-		if (disk_state[OLD] != D_FAILED && disk_state[NEW] == D_FAILED) {
+		if ((disk_state[OLD] != D_FAILED && disk_state[NEW] == D_FAILED) ||
+		    (disk_state[OLD] != D_DETACHING && disk_state[NEW] == D_DETACHING)) {
 			enum drbd_io_error_p eh = EP_PASS_ON;
 			int was_io_error = 0;
 
@@ -2388,7 +2392,7 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 				eh = rcu_dereference(device->ldev->disk_conf)->on_io_error;
 				rcu_read_unlock();
 
-				was_io_error = test_and_clear_bit(WAS_IO_ERROR, &device->flags);
+				was_io_error = disk_state[NEW] == D_FAILED;
 
 				if (was_io_error && eh == EP_CALL_HELPER)
 					drbd_khelper(device, NULL, "local-io-error");
@@ -2408,14 +2412,6 @@ static int w_after_state_change(struct drbd_work *w, int unused)
 				 */
 				if (test_and_clear_bit(FORCE_DETACH, &device->flags))
 					tl_abort_disk_io(device);
-
-				/* current state still has to be D_FAILED,
-				 * there is only one way out: to D_DISKLESS,
-				 * and that may only happen after our put_ldev below. */
-				if (device->disk_state[NOW] != D_FAILED)
-					drbd_err(device,
-						 "ASSERT FAILED: disk is %s during detach\n",
-						 drbd_disk_str(device->disk_state[NOW]));
 
 				send_new_state_to_all_peer_devices(state_change, n_device);
 
@@ -3450,8 +3446,8 @@ static bool do_change_disk_state(struct change_context *context, bool prepare)
 	}
 	__change_disk_state(device, context->val.disk);
 	return !prepare || negotiation_cluster_wide ||
-	       (device->disk_state[NOW] != D_FAILED &&
-		context->val.disk == D_FAILED &&
+	       (device->disk_state[NOW] != D_DETACHING &&
+		context->val.disk == D_DETACHING &&
 		device_has_connected_peer_devices(device));
 }
 

commit 4f58ff1a57e52b852b1e8218d65e1c991b345476

drbd: convert disk states in state change requests to an 8.4 peer
DRBD 9 inserted D_DETACHING into enum drbd_disk_state, so every disk
state from D_FAILED up is one higher than in DRBD 8.4. send_state() and
receive_state() compensate for a peer below protocol version 110;
conn_send_state_req() and the P_STATE_CHG_REQ receive path do not, so a
state change request crosses the 8.4 boundary with the wrong numbers.

A DRBD 9 Secondary disconnecting gracefully from an 8.4 Primary proposes
to outdate itself. D_OUTDATED arrives as D_UNKNOWN, the 8.4 node refuses
with SS_PRIMARY_NOP, and the admin sees "(-10) State change was refused
by peer node"; drbdadm down is refused the same way, since it disconnects
without force first. In the other direction the 8.4 node asks for
D_OUTDATED and D_INCONSISTENT is applied, so the DRBD 9 node reports its
data as not a consistent image when it is one.

Fix this by converting in both missing places, and move the rule into
drbd_disk_states_to_84() / _from_84() so the four sites that cross the
boundary cannot drift apart again. Only val is converted: mask.disk and
mask.pdsk are bit masks.

Fixes: 9b79cbafdc71 ("drbd: Introduce the disk state D_DETACHING")
Signed-off-by: Lars Ellenberg <lars.ellenberg@linbit.com>

drbd/drbd_int.h
@@ -2920,6 +2920,25 @@ int conn_send_twopc_request(struct drbd_connection *connection,
 int drbd_send_peer_ack(struct drbd_connection *connection, u64 mask,
 		       u64 dagtag_sector);
 
+/* DRBD 9 inserted D_DETACHING into enum drbd_disk_state; every state from
+ * D_FAILED up is one higher than in DRBD 8.4, which used D_FAILED for both.
+ */
+static inline void drbd_disk_states_to_84(union drbd_state *s)
+{
+	if (s->disk > D_DETACHING)
+		s->disk--;
+	if (s->pdsk > D_DETACHING)
+		s->pdsk--;
+}
+
+static inline void drbd_disk_states_from_84(union drbd_state *s)
+{
+	if (s->disk >= D_DETACHING)
+		s->disk++;
+	if (s->pdsk >= D_DETACHING)
+		s->pdsk++;
+}
+
 static inline void drbd_thread_stop(struct drbd_thread *thi)
 {
 	_drbd_thread_stop(thi, false, true);

drbd/drbd_main.c
@@ -1718,13 +1718,8 @@ static int send_state(struct drbd_connection *connection, int vnr, union drbd_st
 	if (!p)
 		return -EIO;
 
-	if (connection->agreed_pro_version < 110) {
-		/* D_DETACHING was introduced with drbd-9.0 */
-		if (state.disk > D_DETACHING)
-			state.disk--;
-		if (state.pdsk > D_DETACHING)
-			state.pdsk--;
-	}
+	if (connection->agreed_pro_version < 110)
+		drbd_disk_states_to_84(&state);
 
 	p->state = cpu_to_be32(state.i); /* Within the send mutex */
 	return send_command(connection, vnr, P_STATE, DATA_STREAM);
@@ -1760,6 +1755,11 @@ int conn_send_state_req(struct drbd_connection *connection, int vnr, enum drbd_p
 	p = conn_prepare_command(connection, sizeof(*p), DATA_STREAM);
 	if (!p)
 		return -EIO;
+
+	/* Only val holds disk states; mask.disk and mask.pdsk are bit masks. */
+	if (connection->agreed_pro_version < 110)
+		drbd_disk_states_to_84(&val);
+
 	p->mask = cpu_to_be32(mask.i);
 	p->val = cpu_to_be32(val.i);
 

drbd/drbd_receiver.c
@@ -8338,6 +8338,9 @@ static int receive_req_state(struct drbd_connection *connection, struct packet_i
 	state_change->mask.i = be32_to_cpu(p->mask);
 	state_change->val.i = be32_to_cpu(p->val);
 
+	/* Only val holds disk states; mask.disk and mask.pdsk are bit masks. */
+	drbd_disk_states_from_84(&state_change->val);
+
 	/* P_STATE_CHG_REQ packets must have a valid vnr.  P_CONN_ST_CHG_REQ
 	 * packets have an undefined vnr. */
 	if (pi->cmd == P_STATE_CHG_REQ) {
@@ -9563,13 +9566,8 @@ static int receive_state(struct drbd_connection *connection, struct packet_info 
 
 	peer_state.i = be32_to_cpu(p->state);
 
-	if (connection->agreed_pro_version < 110) {
-		/* Before drbd-9.0 there was no D_DETACHING it was D_FAILED... */
-		if (peer_state.disk >= D_DETACHING)
-			peer_state.disk++;
-		if (peer_state.pdsk >= D_DETACHING)
-			peer_state.pdsk++;
-	}
+	if (connection->agreed_pro_version < 110)
+		drbd_disk_states_from_84(&peer_state);
 
 	if (pi->vnr == -1) {
 		if (peer_state.role == R_SECONDARY) {