LINBIT/drbd

commit 3ba5b50a9fb671a7280a047a0c90f5b2bd332bdb

lb-tcp: Fix dtl_recv_pages()
The assumption that we always get complete pages over one flow (path)
was wrong. We might need to switch to another flow (path) while
receiving a page.

drbd/drbd_transport_lb-tcp.c
@@ -395,6 +395,7 @@ static int dtl_wait_data_cond(struct dtl_transport *dtl_transport,
 		}
 		flow->recv_sequence = be32_to_cpu(header.sequence);
 		flow->recv_bytes = be32_to_cpu(header.bytes);
+		TR_ASSERT(transport, flow->recv_bytes > 0);
 
 		if (flow->recv_sequence == stream->recv_sequence + 1)
 			goto found;
@@ -484,11 +485,36 @@ dtl_recv(struct drbd_transport *transport, enum drbd_stream st, void **buf, size
 }
 
 static int
+_dtl_recv_page(struct dtl_transport *dtl_transport, struct page *page, int size)
+{
+	void *data = kmap_local_page(page);
+	void *pos = data;
+	struct dtl_flow *flow;
+	int err;
+
+	while (size) {
+		err = dtl_select_recv_flow(dtl_transport, DATA_STREAM, &flow);
+		if (err)
+			goto out;
+
+		err = dtl_recv_short(flow->socket, data, min(size, flow->recv_bytes), 0);
+		if (err < 0)
+			goto out;
+		size -= err;
+		pos += err;
+		dtl_received(dtl_transport, flow, err);
+	}
+	err = pos - data;
+out:
+	kunmap_local(data);
+	return err;
+}
+
+static int
 dtl_recv_pages(struct drbd_transport *transport, struct drbd_page_chain_head *chain, size_t size)
 {
 	struct dtl_transport *dtl_transport =
 		container_of(transport, struct dtl_transport, transport);
-	struct dtl_flow *flow;
 	struct page *page;
 	int err;
 
@@ -499,18 +525,12 @@ dtl_recv_pages(struct drbd_transport *transport, struct drbd_page_chain_head *ch
 
 	page_chain_for_each(page) {
 		size_t len = min_t(int, size, PAGE_SIZE);
-		void *data = kmap(page);
 
-		err = dtl_select_recv_flow(dtl_transport, DATA_STREAM, &flow);
-		if (err)
-			goto fail;
-		err = dtl_recv_short(flow->socket, data, len, 0);
-		kunmap(page);
-		set_page_chain_offset(page, 0);
-		set_page_chain_size(page, len);
+		err = _dtl_recv_page(dtl_transport, page, len);
 		if (err < 0)
 			goto fail;
-		dtl_received(dtl_transport, flow, err);
+		set_page_chain_offset(page, 0);
+		set_page_chain_size(page, len);
 		size -= err;
 	}
 	if (unlikely(size)) {

commit b544275e2368ff79792f0f573c1f7348094776bd

drbd: lb-tcp: end the page receive loop when the peer closes
When the peer closes the data connection in the middle of a payload,
dtl_recv_short() returns 0. _dtl_recv_page() treats only a negative
return as an error, so "size" never decreases and the loop repeats at
full CPU speed while holding a kmap_local mapping. This happens with
and without load balancing: flow->recv_bytes is decremented by 0 in the
first case and is INT_MAX in the second, so the loop never runs out of
credit either way.

Fix this by leaving the loop on a 0 return too. dtl_recv_pages() then
finds bytes missing, warns and returns -ENODATA, closing the connection
as any other receive error does.

Master fixes this as part of commit 50157a9ae7cd ("drbd: handle recv
returning 0 (EOF) in recv_bio"); the remainder of that commit repairs
the recv_bio() conversion, which 9.2 does not have.

Fixes: 3ba5b50a9fb6 ("lb-tcp: Fix dtl_recv_pages()")
Signed-off-by: Lars Ellenberg <lars.ellenberg@linbit.com>

drbd/drbd_transport_lb-tcp.c
@@ -471,7 +471,7 @@ _dtl_recv_page(struct dtl_transport *dtl_transport, struct page *page, int size)
 			goto out;
 
 		err = dtl_recv_short(flow->sock, pos, min(size, flow->recv_bytes), 0);
-		if (err < 0)
+		if (err <= 0)
 			goto out;
 		size -= err;
 		pos += err;